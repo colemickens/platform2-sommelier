c48f560ed04eeefb575707826890f4c1ce8a6ecc
Chris Masone <cmasone@google.com>
2009-12-08 18:15:02 -0800

    session doesnt need to daemonize and wait to close pam session
    Review URL: http://codereview.chromium.org/471006

--- login_manager/chromeos_login_manager/session.cc
+++ login_manager/chromeos_login_manager/session.cc
@@ -7,32 +7,7 @@
 #include "base/logging.h"
 #include "pam_client.h"
 #include <errno.h>
-#include <signal.h>
 #include <string.h>
-
-void SIGTERMHandler(int signal) {
-  // Having this handler, even though it does nothing, allows this process
-  // to exit pause() upon receiving SIGTERM without dying.
-}
-
-int DaemonizeAndWait(chromeos::PamClient& pam) {
-  LOG(INFO) << "daemonizing in " << getpid();
-  if (daemon(0, 1) == -1) {
-    LOG(ERROR) << "Couldn't daemonize: " << strerror(errno) << "\n";
-    return 1;
-  }
-  LOG(INFO) << "daemonized, and I'm now " << getpid();
-  
-  struct sigaction action;
-  memset(&action, 0, sizeof(action));
-  action.sa_handler = SIGTERMHandler;
-  sigaction(SIGTERM, &action, NULL);
-  LOG(INFO) << "Pausing " << getpid();
-  pause();
-  pam.CloseSession();  // best-effort.
-  LOG(INFO) << "returning in " << getpid();
-  return 0;
-}
 
 int do_pam_stuff(chromeos::PamClient& pam,
 		 const char* username,
@@ -52,16 +27,6 @@ int do_pam_stuff(chromeos::PamClient& pam,
     LOG(ERROR) << "Couldn't start session: " << pam.GetLastPamResult() << "\n";
     return 1;
   }
-
-  pid_t pid = fork();
-  LOG(INFO) << "forked in " << getpid();
-  if (pid == -1) {
-    LOG(ERROR) << "Couldn't fork: " << strerror(errno) << "\n";
-    return 1;
-  } else if (pid == 0) {
-    // In the child.
-    return DaemonizeAndWait(pam);
-  }
   return 0;
 }
 
@@ -80,11 +45,6 @@ int main(int argc, char* argv[]) {
   chromeos::PamClient pam;
   pam.Init("slim");
   int return_value = do_pam_stuff(pam, argv[1], password);
-  if (return_value) {
-    // This is best-effort.  If we failed before the session opened,
-    // this will fail, but we don't care.
-    pam.CloseSession();
-  }
 
   // TODO SecureMemset; maybe get that function into our base library?
   memset(password, 0, password_len);
