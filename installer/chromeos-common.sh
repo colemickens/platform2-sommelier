d6b71b0679500ee44326ec2092d9e83be5105658
Bill Richardson <wfrichar@chromium.org>
2010-04-14 12:46:14 -0700

    Add new GPT partitions, hopefully for the last time.
    This change puts these GPT partitions on the image:
    
    1 - stateful partiton
    2 - kernel A
    3 - rootfs A
    4 - kernel B
    5 - rootfs B
    6 - kernel C (512-byte partition only, nothing in it)
    7 - rootfs C (512-byte partition only, nothing in it)
    8 - OEM customization (16M)
    9 - future use
    10 - future use
    11 - future use
    12 - EFI System Partition (only used by EFI BIOS systems)
    
    Whether creating a USB key or a full install, all partitions are created and
    numbered the same way. For the USB key, the B kernel and B rootfs is stubbed
    out, so the USB key can't be upgraded via memento (that's nothing new).
    /usr/sbin/chromeos-install will duplicate the A image to populate the B
    image on the hard drive.
    
    This change will also align active partitions to a 2M boundary to speed up
    various development and upgrade functions.
    
    Review URL: http://codereview.chromium.org/1539036

--- installer/chromeos-common.sh
+++ installer/chromeos-common.sh
@@ -8,10 +8,11 @@
 # image.
 
 # Here are the GUIDs we'll be using to identify various partitions.
-STATEFUL_GUID='ebd0a0a2-b9e5-4433-87c0-68b6b72699c7'
+DATA_GUID='ebd0a0a2-b9e5-4433-87c0-68b6b72699c7'
 KERN_GUID='fe3a2a5d-4f32-41a7-b725-accc3285a309'
 ROOTFS_GUID='3cb8e202-3b7e-47dd-8a3c-7ff2a13cfcec'
 ESP_GUID='28732ac1-1ff8-d211-ba4b-00a0c93ec93b'
+FUTURE_GUID='2e0a753d-9e48-43b0-8337-b15192cb1b5e'
 
 
 # The GPT tables describe things in terms of 512-byte sectors, but some
@@ -45,29 +46,31 @@ numsectors() {
   fi
 }
 
-# Round a number of 512-byte sectors up to an integral number of 4096-byte
-# blocks.
+# Round a number of 512-byte sectors up to an integral number of 2Mb
+# blocks. Divisor is 2 * 1024 * 1024 / 512 == 4096.
 # Invoke as: subshell
 # Args: SECTORS
 # Return: Next largest multiple-of-8 sectors (ex: 4->8, 33->40, 32->32)
 roundup() {
   local num=$1
-  local rem=$(( $num % 8 ))
+  local div=${2:-4096}
+  local rem=$(( $num % $div ))
 
   if [ $rem -ne 0 ]; then
-    num=$(($num + 8 - $rem))
+    num=$(($num + $div - $rem))
   fi
   echo $num
 }
 
-# Truncate a number of 512-byte sectors down to an integral number of 4096-byte
-# blocks.
+# Truncate a number of 512-byte sectors down to an integral number of 2Mb
+# blocks. Divisor is 2 * 1024 * 1024 / 512 == 4096.
 # Invoke as: subshell
 # Args: SECTORS
 # Return: Next smallest multiple-of-8 sectors (ex: 4->0, 33->32, 32->32)
 rounddown() {
   local num=$1
-  local rem=$(( $num % 8 ))
+  local div=${2:-4096}
+  local rem=$(( $num % $div ))
 
   if [ $rem -ne 0 ]; then
     num=$(($num - $rem))
@@ -97,19 +100,21 @@ fi
 # Args: TARGET ROOTFS_IMG KERNEL_IMG STATEFUL_IMG PMBRCODE ESP_IMG FORCE_FULL
 # Return: nothing
 # Side effects: Sets these global variables describing the GPT partitions
-#   (all untis are 512-byte sectors):
+#   (all units are 512-byte sectors):
+#   NUM_ESP_SECTORS
 #   NUM_KERN_SECTORS
+#   NUM_OEM_SECTORS
+#   NUM_RESERVED_SECTORS
 #   NUM_ROOTFS_SECTORS
 #   NUM_STATEFUL_SECTORS
-#   NUM_RESERVED_SECTORS
-#   NUM_ESP_SECTORS
-#   START_KERN_A
-#   START_STATEFUL
-#   START_ROOTFS_A
-#   START_KERN_B
-#   START_ROOTFS_B
-#   START_RESERVED
 #   START_ESP
+#   START_KERN_A
+#   START_KERN_B
+#   START_OEM
+#   START_RESERVED
+#   START_ROOTFS_A
+#   START_ROOTFS_B
+#   START_STATEFUL
 install_gpt() {
   local outdev=$1
   local rootfs_img=$2
@@ -121,14 +126,23 @@ install_gpt() {
 
   # The gpt tool requires a fixed-size target to work on, so we may have to
   # create a file of the appropriate size. Let's figure out what that size is
-  # now. The partition layout is gonna look something like this:
+  # now. The full partition layout will look something like this (indented
+  # lines indicate reserved regions that do not have any useful content at the
+  # moment).
   #
   #   PMBR (512 bytes)
   #   Primary GPT Header (512 bytes)
   #   Primary GPT Table (16K)
+  #     Kernel C (placeholder for future use only)    partition 6
+  #     Rootfs C (placeholder for future use only)    partition 7
+  #     future use                                    partition 9
+  #     future use                                    partition 10
+  #     future use                                    partition 11
   #   Kernel A                                        partition 2
   #   Kernel B                                        partition 4
-  #   reserved space for additional partitions        partition 6,7,8,...
+  #   OEM Customization (16M)                         partition 8
+  #     reserved space (64M)
+  #   EFI System Partition (temporary)                partition 12
   #   Stateful partition (as large as possible)       partition 1
   #   Rootfs B                                        partition 5
   #   Rootfs A                                        partition 3
@@ -137,30 +151,42 @@ install_gpt() {
   #
   # Please refer to the official ChromeOS documentation for the details and
   # explanation behind the layout and partition numbering scheme. The short
-  # version is that 1) we want everything above partition 3 to be optional when
-  # we're creating a bootable USB key, 2) we want to be able to add new
-  # partitions later without breaking current scripts, and 3) we may need to
-  # increase the size of the rootfs during an upgrade, which means shrinking
-  # the size of the stateful partition.
+  # version is that 1) we want to avoid ever changing the purpose or number of
+  # an existing partition, 2) we want to be able to add new partitions later
+  # without breaking current scripts, and 3) we may someday need to increase
+  # the size of the rootfs during an upgrade, which means shrinking the size of
+  # the stateful partition on a live system.
   #
-  # One nonobvious contraint is that the ext2-based filesystems typically use
-  # 4096-byte blocks. We'll need a little padding at each end of the disk to
-  # align the useable space to that size boundary.
+  # The EFI GPT spec requires that all valid partitions be at least one sector
+  # in size, and non-overlapping.
 
   # Here are the size limits that we're currently requiring
   local max_kern_sectors=32768        # 16M
   local max_rootfs_sectors=2097152    # 1G
+  local max_oem_sectors=32768         # 16M
   local max_reserved_sectors=131072   # 64M
   local max_esp_sectors=32768         # 16M
   local min_stateful_sectors=262144   # 128M, expands to fill available space
 
   local num_pmbr_sectors=1
   local num_gpt_hdr_sectors=1
-  local num_gpt_table_sectors=32              # 16K
+  local num_gpt_table_sectors=32      # 16K
   local num_footer_sectors=$(($num_gpt_hdr_sectors + $num_gpt_table_sectors))
   local num_header_sectors=$(($num_pmbr_sectors + $num_footer_sectors))
 
-  local start_useful=$(roundup $num_header_sectors)
+  # In order to align to a 4096-byte boundary, there should be several empty
+  # sectors available following the header. We'll pack the single-sector-sized
+  # unused partitions in there.
+  local start_kern_c=$(($num_header_sectors))
+  local num_kern_c_sectors=1
+  local start_rootfs_c=$(($start_kern_c + 1))
+  local num_rootfs_c_sectors=1
+  local start_future_9=$(($start_rootfs_c + 1))
+  local num_future_sectors=1
+  local start_future_10=$(($start_future_9 + 1))
+  local start_future_11=$(($start_future_10 + 1))
+
+  local start_useful=$(roundup $(($start_future_11 + 1)))
 
   # What are we doing?
   if [ -b "$outdev" -o "$force_full" = "true" ]; then
@@ -174,55 +200,70 @@ install_gpt() {
     # Full install, use max sizes and create both A & B images.
     NUM_KERN_SECTORS=$max_kern_sectors
     NUM_ROOTFS_SECTORS=$max_rootfs_sectors
+    NUM_OEM_SECTORS=$max_oem_sectors
     NUM_ESP_SECTORS=$max_esp_sectors
     NUM_RESERVED_SECTORS=$max_reserved_sectors
 
     # Where do things go?
     START_KERN_A=$start_useful
+    local num_kern_a_sectors=$NUM_KERN_SECTORS
     START_KERN_B=$(($START_KERN_A + $NUM_KERN_SECTORS))
-    START_ESP=$(($START_KERN_B + $NUM_KERN_SECTORS))
-    START_RESERVED=$(($START_ESP + $NUM_ESP_SECTORS))
-    START_STATEFUL=$(($START_RESERVED + $NUM_RESERVED_SECTORS))
+    local num_kern_b_sectors=$NUM_KERN_SECTORS
+    START_OEM=$(($START_KERN_B + $NUM_KERN_SECTORS))
+    START_RESERVED=$(($START_OEM + $NUM_OEM_SECTORS))
+    START_ESP=$(($START_RESERVED + $NUM_RESERVED_SECTORS))
+    START_STATEFUL=$(($START_ESP + $NUM_ESP_SECTORS))
 
     local total_sectors=$(numsectors $outdev)
     local start_gpt_footer=$(($total_sectors - $num_footer_sectors))
     local end_useful=$(rounddown $start_gpt_footer)
 
     START_ROOTFS_A=$(($end_useful - $NUM_ROOTFS_SECTORS))
+    local num_rootfs_a_sectors=$NUM_ROOTFS_SECTORS
     START_ROOTFS_B=$(($START_ROOTFS_A - $NUM_ROOTFS_SECTORS))
+    local num_rootfs_b_sectors=$NUM_ROOTFS_SECTORS
 
     NUM_STATEFUL_SECTORS=$(($START_ROOTFS_B - $START_STATEFUL))
   else
     # Just a local file.
     local sudo=
 
-    # We'll only populate partitions 1, 2, 3. Image B isn't required for this,
-    # and the others are still theoretical.
-    NUM_KERN_SECTORS=$(roundup $(numsectors $kernel_img))
-    NUM_ROOTFS_SECTORS=$(roundup $(numsectors $rootfs_img))
+    # We're just going to fill partitions 1, 2, 3, 8, and 12. The others will
+    # be present but as small as possible. The disk layout isn't crucial here,
+    # because we won't be able to upgrade this image in-place as it's only for
+    # installation purposes.
     NUM_STATEFUL_SECTORS=$(roundup $(numsectors $stateful_img))
+    NUM_KERN_SECTORS=$(roundup $(numsectors $kernel_img))
+    local num_kern_a_sectors=$NUM_KERN_SECTORS
+    local num_kern_b_sectors=1
+    NUM_ROOTFS_SECTORS=$(roundup $(numsectors $rootfs_img))
+    local num_rootfs_a_sectors=$NUM_ROOTFS_SECTORS
+    local num_rootfs_b_sectors=1
+    NUM_OEM_SECTORS=$max_oem_sectors
     NUM_ESP_SECTORS=$(roundup $(numsectors $esp_img))
-    NUM_RESERVED_SECTORS=0
+    NUM_RESERVED_SECTORS=1
 
     START_KERN_A=$start_useful
-    START_ESP=$(($START_KERN_A + $NUM_KERN_SECTORS))
-    START_STATEFUL=$(($START_ESP + $NUM_ESP_SECTORS))
-    START_ROOTFS_A=$(($START_STATEFUL + $NUM_STATEFUL_SECTORS))
-    START_KERN_B=""
-    START_ROOTFS_B=""
-    START_RESERVED=""
+    START_ROOTFS_A=$(($START_KERN_A + $NUM_KERN_SECTORS))
+    START_STATEFUL=$(($START_ROOTFS_A + $NUM_ROOTFS_SECTORS))
+    START_OEM=$(($START_STATEFUL + $NUM_STATEFUL_SECTORS))
+    START_ESP=$(($START_OEM + $NUM_OEM_SECTORS))
+    START_KERN_B=$(($START_ESP + $NUM_ESP_SECTORS))
+    START_ROOTFS_B=$((START_KERN_B + $num_kern_b_sectors))
+    START_RESERVED=$(($START_ROOTFS_B + $num_rootfs_b_sectors))
 
     # For minimal install, we're not worried about the secondary GPT header
     # being at the end of the device because we're almost always writing to a
     # file. If that's not true, the secondary will just be invalid.
-    local start_gpt_footer=$(($START_ROOTFS_A + $NUM_ROOTFS_SECTORS))
+    local start_gpt_footer=$(($START_RESERVED + $NUM_RESERVED_SECTORS))
     local end_useful=$start_gpt_footer
 
     local total_sectors=$(($start_gpt_footer + $num_footer_sectors))
 
     # Create the image file if it doesn't exist.
     if [ ! -e ${outdev} ]; then
-      dd if=/dev/zero of=${outdev} bs=512 count=1 seek=$(($total_sectors - 1))
+      $sudo dd if=/dev/zero of=${outdev} bs=512 count=1 \
+        seek=$(($total_sectors - 1))
     fi
   fi
 
@@ -240,33 +281,52 @@ install_gpt() {
   $sudo $GPT create ${outdev}
 
   $sudo $GPT add -b ${START_STATEFUL} -s ${NUM_STATEFUL_SECTORS} \
-    -t ${STATEFUL_GUID} ${outdev}
+    -t ${DATA_GUID} ${outdev}
   $sudo $GPT label -i 1 -l "STATE" ${outdev}
 
-  $sudo $GPT add -b ${START_KERN_A} -s ${NUM_KERN_SECTORS} \
+  $sudo $GPT add -b ${START_KERN_A} -s ${num_kern_a_sectors} \
     -t ${KERN_GUID} ${outdev}
   $sudo $GPT label -i 2 -l "KERN-A" ${outdev}
 
-  $sudo $GPT add -b ${START_ROOTFS_A} -s ${NUM_ROOTFS_SECTORS} \
+  $sudo $GPT add -b ${START_ROOTFS_A} -s ${num_rootfs_a_sectors} \
     -t ${ROOTFS_GUID} ${outdev}
   $sudo $GPT label -i 3 -l "ROOT-A" ${outdev}
 
-  # add the rest of the partitions for a full install
-  if [ -n "$START_KERN_B" ]; then
-    $sudo $GPT add -b ${START_KERN_B} -s ${NUM_KERN_SECTORS} \
-      -t ${KERN_GUID} ${outdev}
-    $sudo $GPT label -i 4 -l "KERN-B" ${outdev}
+  $sudo $GPT add -b ${START_KERN_B} -s ${num_kern_b_sectors} \
+    -t ${KERN_GUID} ${outdev}
+  $sudo $GPT label -i 4 -l "KERN-B" ${outdev}
 
-    $sudo $GPT add -b ${START_ROOTFS_B} -s ${NUM_ROOTFS_SECTORS} \
-      -t ${ROOTFS_GUID} ${outdev}
-    $sudo $GPT label -i 5 -l "ROOT-B" ${outdev}
-  fi
+  $sudo $GPT add -b ${START_ROOTFS_B} -s ${num_rootfs_b_sectors} \
+    -t ${ROOTFS_GUID} ${outdev}
+  $sudo $GPT label -i 5 -l "ROOT-B" ${outdev}
 
-  # add the ESP partition
+  $sudo $GPT add -b ${start_kern_c} -s ${num_kern_c_sectors} \
+    -t ${KERN_GUID} ${outdev}
+  $sudo $GPT label -i 6 -l "KERN-C" ${outdev}
+
+  $sudo $GPT add -b ${start_rootfs_c} -s ${num_rootfs_c_sectors} \
+    -t ${ROOTFS_GUID} ${outdev}
+  $sudo $GPT label -i 7 -l "ROOT-C" ${outdev}
+
+  $sudo $GPT add -b ${START_OEM} -s ${NUM_OEM_SECTORS} \
+    -t ${FUTURE_GUID} ${outdev}
+  $sudo $GPT label -i 8 -l "OEM" ${outdev}
+
+  $sudo $GPT add -b ${start_future_9} -s ${num_future_sectors} \
+    -t ${FUTURE_GUID} ${outdev}
+  $sudo $GPT label -i 9 -l "reserved" ${outdev}
+
+  $sudo $GPT add -b ${start_future_10} -s ${num_future_sectors} \
+    -t ${FUTURE_GUID} ${outdev}
+  $sudo $GPT label -i 10 -l "reserved" ${outdev}
+
+  $sudo $GPT add -b ${start_future_11} -s ${num_future_sectors} \
+    -t ${FUTURE_GUID} ${outdev}
+  $sudo $GPT label -i 11 -l "reserved" ${outdev}
+
   $sudo $GPT add -b ${START_ESP} -s ${NUM_ESP_SECTORS} \
     -t ${ESP_GUID} ${outdev}
-  # FIXME: no label because I've lost count of the partition number
-  # I'll fix this when I've modified gpt to accept a label arg to 'add'.
+  $sudo $GPT label -i 12 -l "EFI-SYSTEM" ${outdev}
 
   # Create the PMBR and instruct it to boot ROOT-A
   $sudo $GPT boot -i 3 -b ${pmbrcode} ${outdev}
