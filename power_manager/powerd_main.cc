9c12d8c406f52fa233d6ca087f1cc8dff2483f98
David James <davidjames@google.com>
2010-06-01 18:14:21 -0700

    Update powerd to log to a named log file instead of powerd.out.
    TEST=Tested on device. Also ran test suite.
    BUG=none
    
    Review URL: http://codereview.chromium.org/2352003

--- power_manager/powerd_main.cc
+++ power_manager/powerd_main.cc
@@ -7,19 +7,57 @@
 
 #include <string>
 
+#include "base/command_line.h"
 #include "base/logging.h"
+#include "base/string_util.h"
 #include "cros/chromeos_cros_api.h"
 #include "power_manager/powerd.h"
 
 using std::string;
 
 DEFINE_string(prefs_dir, "",
-              "Directory to store logs and settings.");
+              "Directory to store settings.");
+DEFINE_string(log_dir, "",
+              "Directory to store logs.");
+
+// Returns true on success.
+static bool SetUpLogSymlink(const string& symlink_path,
+                            const string& log_basename) {
+  unlink(symlink_path.c_str());
+  if (symlink(log_basename.c_str(), symlink_path.c_str()) == -1) {
+    PLOG(ERROR) << "Unable to create symlink " << symlink_path
+                << " pointing at " << log_basename;
+    return false;
+  }
+  return true;
+}
+
+static string GetTimeAsString(time_t utime) {
+  struct tm tm;
+  CHECK(localtime_r(&utime, &tm) == &tm);
+  char str[16];
+  CHECK(strftime(str, sizeof(str), "%Y%m%d-%H%M%S", &tm) == 15);
+  return string(str);
+}
 
 int main(int argc, char* argv[]) {
   google::ParseCommandLineFlags(&argc, &argv, true);
   CHECK(!FLAGS_prefs_dir.empty()) << "--prefs_dir is required";
+  CHECK(!FLAGS_log_dir.empty()) << "--log_dir is required";
   CHECK(argc == 1) << "Unexpected arguments. Try --help";
+  CommandLine::Init(argc, argv);
+
+  const string log_latest =
+      StringPrintf("%s/powerd.LATEST", FLAGS_log_dir.c_str());
+  const string log_basename =
+      StringPrintf("powerd.%s", GetTimeAsString(::time(NULL)).c_str());
+  const string log_path = FLAGS_log_dir + "/" + log_basename;
+  CHECK(SetUpLogSymlink(log_latest, log_basename));
+  logging::InitLogging(log_path.c_str(),
+                       logging::LOG_ONLY_TO_FILE,
+                       logging::DONT_LOCK_LOG_FILE,
+                       logging::APPEND_TO_OLD_LOG_FILE);
+
   FilePath prefs_dir(FLAGS_prefs_dir);
   power_manager::PowerPrefs prefs(prefs_dir);
   string err;
