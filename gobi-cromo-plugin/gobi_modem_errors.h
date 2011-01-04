5cb51b37a12d54deb2f37e50ac5b659688c705e6
David Rochberg <rochberg@chromium.org>
2011-01-04 13:22:06 -0500

    Add gobi_modem_errors.h
    Change-Id: I3057ef69b595f8b53ec8fd5e53c9262a81dd3e12
    TEST=NONE
    BUG=chromium-os:4667
    
    Review URL: https://chromereview.prom.corp.google.com/2224013

---
+++ gobi-cromo-plugin/gobi_modem_errors.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Errors used for GobiModem and subclasses.  This file is included
+// multiple times: once in the .h, and once in the .cc, hence no
+// include guard
+
+DEFINE_ERROR(Activation)
+DEFINE_ERROR(Connect)
+DEFINE_ERROR(Disconnect)
+DEFINE_ERROR(FirmwareLoad)
+DEFINE_ERROR(Sdk)
+DEFINE_ERROR(Mode)
+DEFINE_ERROR(OperationInitiated)
+DEFINE_ERROR(InvalidArgument)
+DEFINE_MM_ERROR(NoNetwork)
+DEFINE_MM_ERROR(OperationNotAllowed)
