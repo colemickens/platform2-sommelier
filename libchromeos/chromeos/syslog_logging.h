5f24487e06f075376a8683aed30ff7f324bc4919
Ken Mixter <kmixter@google.com>
2011-03-02 11:50:12 -0800

    libchromeos: add process control library, syslog logging capability, and run unit tests
    Change-Id: I9d9a00eeb649666efc1f2fdac1914e4bf6f1149c
    
    BUG=n0ne
    TEST=unit tests
    
    Review URL: http://codereview.chromium.org/6509006

---
+++ libchromeos/chromeos/syslog_logging.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_SYSLOG_LOGGING_H_
+#define CHROMEOS_SYSLOG_LOGGING_H_
+
+#include <string>
+
+namespace chromeos {
+
+enum InitFlags {
+  kLogToSyslog = 1,
+  kLogToStderr = 2
+};
+
+// Initialize logging subsystem.  |flags| indicates
+void InitLog(int init_flags);
+// Convenience function for configuring syslog via openlog.  Users
+// could call openlog directly except for naming collisions between
+// base/logging.h and syslog.h.  Similarly users cannot pass the
+// normal parameters so we pick a representative set.  |log_pid|
+// causes pid to be logged with |ident|.
+void OpenLog(const char* ident, bool log_pid);
+// Start accumulating the logs to a string.  This is inefficient, so
+// do not set to true if large numbers of log messages are coming.
+// Accumulated logs are only ever cleared when the clear function ings
+// called.
+void LogToString(bool enabled);
+// Get the accumulated logs as a string.
+std::string GetLog();
+// Clear the accumulated logs.
+void ClearLog();
+// Returns true if the accumulated log contains the given string.  Useful
+// for testing.
+bool FindLog(const char* string);
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_SYSLOG_LOGGING_H_
--- libchromeos/testrunner.cc
+++ libchromeos/testrunner.cc
@@ -4,20 +4,12 @@
 
 // based on pam_google_testrunner.cc
 
+#include <chromeos/test_helpers.h>
 #include <gtest/gtest.h>
 #include <glib-object.h>
-#include <base/logging.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   ::g_type_init();
-
-#if 0
-  // Initializing with a name does not seem to be changing the log file name.
-  // And options for logging to stderr are being ignored. If we don't initialize
-  // logging goes to stderr by default.
-  google::InitGoogleLogging("power_unittest");
-#endif
-
-  ::testing::InitGoogleTest(&argc, argv);
+  SetUpTests(&argc, argv, true);
   return RUN_ALL_TESTS();
 }
