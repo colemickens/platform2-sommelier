b182e505a459e40cac5f64b5abb5197f56997051
Jason Glasgow <jglasgow@chromium.org>
2011-03-31 18:01:56 -0400

    Move modem command to the modem-utilities repository
    See also http://codereview.chromium.org/6780027/
    
    BUG=chrome-os-partner:2985
    TEST=build modem-utilities & gobi-cromo-plugin.  Verify modem command in crosh
    
    Review URL: http://codereview.chromium.org/6728027
    
    Change-Id: Ica0b000fbd259b2e7774843929e65a64db50e6ba

---
+++ modem-utilities/mm.sh
@@ -0,0 +1,58 @@
+#!/bin/dash
+# Copyright (c) 2009 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Library for talking to ModemManager from sh. Copied (ugh) from flimflam.
+
+MM=${MM:-/org/chromium/ModemManager}
+IDBUS=org.freedesktop.DBus
+IDBUS_PROPERTIES=$IDBUS.Properties
+IMM=org.freedesktop.ModemManager
+IMODEM=$IMM.Modem
+IMODEM_SIMPLE=$IMODEM.Simple
+IMODEM_CDMA=$IMODEM.Cdma
+IMODEM_GSM=$IMODEM.Gsm
+IMODEM_GSM_CARD=$IMODEM_GSM.Card
+IMODEM_GSM_NETWORK=$IMODEM_GSM.Network
+IMODEM_GOBI=org.chromium.ModemManager.Modem.Gobi
+IMODEMS="$IMODEM $IMODEM_SIMPLE $IMODEM_CDMA $IMODEM_GSM $IMODEM_GOBI"
+DEST=`echo $MM | sed -e 's!^/!!g' -e 's!/!.!g'`
+
+MTYPE_GSM=1
+MTYPE_CDMA=2
+
+dbus () {
+	local obj=$1
+	local method=$2
+	shift 2
+
+	dbus-send --system --print-reply --fixed \
+        --dest=$DEST "$obj" "$method" $*
+}
+
+modems () {
+	dbus $MM $IMM.EnumerateDevices | awk '{print $2}'
+}
+
+modemprops () {
+	local modem=$1
+	for i in $IMODEMS; do
+		dbus $modem $IDBUS_PROPERTIES.GetAll string:$i 2>/dev/null \
+			| awk '/[^[:space:]] [^[:space:]]/ {print $N}'
+	done
+}
+
+modemprop () {
+	local modem=$1
+	local prop=$2
+	modemprops $modem | grep /$prop | awk '{print $2}'
+}
+
+modem_activation_state () {
+        local modem=$1
+        if [ -n $modem ] ; then
+                dbus $modem $IMODEM_SIMPLE.GetStatus |\
+                      awk '/\/activation_state/ {print $2}'
+        fi
+}
