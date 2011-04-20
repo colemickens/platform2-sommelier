17c18c05fc79f8425c22ca7a1961f65133471fc3
Doug Anderson <dianders@chromium.org>
2011-04-20 15:06:13 -0700

    Handle different i2c bus enumerations for light sensor.
    Change-Id: I14b5c03b6febae3581b15de0668d6be41c8bfb14
    
    BUG=chromium-os:11098
    TEST=
    
    1. Rebooted and tested that on normal kernel (Cr-48), this works:
       cat /sys/class/iio/device0/lux
    2. Rebooted and tested that on a kernel-next, this works:
       cat /sys/bus/iio/devices/device0/illuminance0_input
    
    Review URL: http://codereview.chromium.org/6834037

--- power_manager/light-sensor-install.sh
+++ power_manager/light-sensor-install.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
 
-# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Install a light sensor driver on a given bus and address.
+# Here's the 'udevadm monitor --property' we're responding to...
+# ...at least on
+#KERNEL[1302714506.346339] add      /bus/pci/drivers/i801_smbus (drivers)
+#UDEV_LOG=6
+#ACTION=add
+#DEVPATH=/bus/pci/drivers/i801_smbus
+#SUBSYSTEM=drivers
+
+# We're always going to install a tsl2563 light sensor for now.  It should
+# always be on an i2c bus and have address 0x29.
 NAME=tsl2563
-BUS=2
 ADDRESS=0x29
 
+# Be generic--don't assume we end up on any given numbered i2c device.
+# On 2.6.32 on Cr-48, we get:
+#   /sys/bus/pci/drivers/i801_smbus/0000:00:1f.3/i2c-2
+# On 2.6.38 on Cr-48, we get:
+#   /sys/bus/pci/drivers/i801_smbus/0000:00:1f.3/i2c-14
+#
+# TODO(dianders): A better way to find the i2c bus?
+I2C_PATH=$(echo /sys/$DEVPATH/*/i2c-*)
+
 # Use the new_device interface to instantiate it.
-echo $NAME $ADDRESS > /sys/class/i2c-adapter/i2c-${BUS}/new_device
+echo $NAME $ADDRESS > "$I2C_PATH"/new_device
