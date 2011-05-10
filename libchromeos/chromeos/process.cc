91dcd1a247a7d2937ba936ac0cca2d3a0e4ae5ee
Gaurav Shah <gauravsh@chromium.org>
2011-05-09 18:19:08 -0700

    Change GID of the child process first before changing the UID.
    When dropping privileges, once the EUID of the process changes, it may no longer have the permissions to changes its EGID.
    
    BUG=none
    TEST=manual
    
    Change-Id: Iba1be351331a1579fce9f5ccad82a375b5d59e5e
    Reviewed-on: http://gerrit.chromium.org/gerrit/575
    Tested-by: Gaurav Shah <gauravsh@chromium.org>
    Reviewed-by: Ken Mixter <kmixter@chromium.org>

--- libchromeos/chromeos/process.cc
+++ libchromeos/chromeos/process.cc
@@ -163,14 +163,14 @@ bool ProcessImpl::Start() {
         HANDLE_EINTR(close(output_handle));
       }
     }
-    if (uid_ >= 0 && setresuid(uid_, uid_, uid_) < 0) {
-      int saved_errno = errno;
-      LOG(ERROR) << "Unable to set UID to " << uid_ << ": " << saved_errno;
-      _exit(kErrorExitStatus);
-    }
     if (gid_ >= 0 && setresgid(gid_, gid_, gid_) < 0) {
       int saved_errno = errno;
       LOG(ERROR) << "Unable to set GID to " << gid_ << ": " << saved_errno;
+      _exit(kErrorExitStatus);
+    }
+    if (uid_ >= 0 && setresuid(uid_, uid_, uid_) < 0) {
+      int saved_errno = errno;
+      LOG(ERROR) << "Unable to set UID to " << uid_ << ": " << saved_errno;
       _exit(kErrorExitStatus);
     }
     execv(argv[0], &argv[0]);
