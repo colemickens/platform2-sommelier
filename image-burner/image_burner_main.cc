adbab13043ef96fd8846d465f8d29b79c446fe57
Chris Masone <cmasone@chromium.org>
2011-05-12 15:43:32 -0700

    [image-burner] Roll forward to new libchrome
    BUG=chromium-os:14304
    TEST=build, unit tests
    
    Change-Id: I9b32600077aae9c0154cabfe3241443da73b882a
    Reviewed-on: http://gerrit.chromium.org/gerrit/818
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Tested-by: Chris Masone <cmasone@chromium.org>

--- image-burner/image_burner_main.cc
+++ image-burner/image_burner_main.cc
@@ -10,10 +10,11 @@
 int main(int argc, char* argv[]) {
   CommandLine::Init(argc, argv);
 
-  logging::InitLogging("/var/log/image_burner.log", 
-                        logging::LOG_TO_BOTH_FILE_AND_SYSTEM_DEBUG_LOG,
-                        logging::LOCK_LOG_FILE,
-                        logging::DELETE_OLD_LOG_FILE);
+  logging::InitLogging("/var/log/image_burner.log",
+                       logging::LOG_TO_BOTH_FILE_AND_SYSTEM_DEBUG_LOG,
+                       logging::LOCK_LOG_FILE,
+                       logging::DELETE_OLD_LOG_FILE,
+                       logging::DISABLE_DCHECK_FOR_NON_OFFICIAL_RELEASE_BUILDS);
   g_type_init();
 
   imageburn::ImageBurnService service;
