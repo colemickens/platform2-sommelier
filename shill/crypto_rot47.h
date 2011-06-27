e56b5c91fce8848f1c19f06b2539fb2119ea9fe1
Darin Petkov <petkov@chromium.org>
2011-06-27 13:37:22 -0700

    shill: Implement a rot47 crypto module.
    This module is required for parity with flimflam and will be used to scramble
    values in the perstistent store.
    
    BUG=chromium-os:16964
    TEST=unit test
    
    Change-Id: I20de52eebfe6f9f5ccd4ddcb4c7e3e94edbb7af7
    Reviewed-on: http://gerrit.chromium.org/gerrit/3258
    Tested-by: Darin Petkov <petkov@chromium.org>
    Reviewed-by: Darin Petkov <petkov@chromium.org>

---
+++ shill/crypto_rot47.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SHILL_CRYPTO_ROT47_
+#define SHILL_CRYPTO_ROT47_
+
+#include <string>
+
+#include "shill/crypto_interface.h"
+
+namespace shill {
+
+// ROT47 crypto module implementation.
+class CryptoROT47 : public CryptoInterface {
+ public:
+  static const char kID[];
+
+  // Inherited from CryptoInterface.
+  virtual std::string GetID();
+  virtual bool Encrypt(const std::string &plaintext, std::string *ciphertext);
+  virtual bool Decrypt(const std::string &ciphertext, std::string *plaintext);
+};
+
+}  // namespace shill
+
+#endif  // SHILL_CRYPTO_ROT47_
