ce9c4d3062e23e1b3b2ec24087313769331921a0
Simon Que <sque@chromium.org>
2011-08-09 13:41:53 -0700

    Add hysteresis to battery remaing time calculation
    Do not allow random swings in reported battery discharge time.  The
    calculated linear time can vary by a lot.  With this patch, there is a
    limit to how much variation is allowed.  Anything outside the variation
    range will be clipped.  Only when the time has been outside of the range
    for long enough will it be allowed to become the new "baseline" time.
    
    BUG=chromium-os:14506
    TEST=Run 'FEATURES=test emerge-x86-mario power_manager' and make sure
    the tests pass.
    
    Change-Id: Ib2a99e7d2804fc1d68a7fa9949be96a2e8a7478e
    Signed-off-by: Simon Que <sque@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/6565
    Reviewed-by: Sameer Nanda <snanda@chromium.org>

--- power_manager/power_supply.h
+++ power_manager/power_supply.h
@@ -5,10 +5,13 @@
 #ifndef POWER_MANAGER_POWER_SUPPLY_H_
 #define POWER_MANAGER_POWER_SUPPLY_H_
 
+#include <gtest/gtest_prod.h>  // for FRIEND_TEST
+
 #include <string>
 
 #include "base/basictypes.h"
 #include "base/file_path.h"
+#include "base/time.h"
 #include "power_manager/power_prefs.h"
 
 namespace chromeos {
@@ -38,8 +41,17 @@ class PowerSupply {
   const FilePath& battery_path() const { return battery_path_; }
 
  private:
+  friend class PowerSupplyTest;
+  FRIEND_TEST(PowerSupplyTest, TestDischargingWithHysteresis);
+
   // Find sysfs directories to read from.
   void GetPowerSupplyPaths();
+
+  // Computes time remaining based on energy drain rate.
+  double GetLinearTimeToEmpty();
+
+  // Determine remaining time when charging or discharging.
+  void CalculateRemainingTime(chromeos::PowerStatus *status);
 
   // Use the PowerPrefs functions to read info from sysfs.
   class PowerInfoReader : public PowerPrefs {
@@ -65,6 +77,7 @@ class PowerSupply {
   double current_now_;
   double cycle_count_;
   double voltage_now_;
+  double nominal_voltage_;
 
   // Indicates whether battery is present.
   bool battery_is_present_;
@@ -74,6 +87,20 @@ class PowerSupply {
   std::string serial_number_;
   std::string technology_;
   std::string type_;
+
+  // These are used for using hysteresis to avoid large swings in calculated
+  // remaining battery time.
+  double acceptable_variance_;
+  base::TimeDelta hysteresis_time_;
+  bool found_acceptable_time_range_;
+  double acceptable_time_;
+  base::TimeDelta time_outside_of_acceptable_range_;
+  base::Time last_acceptable_range_time_;
+  base::Time last_poll_time_;
+  base::Time discharge_start_time_;
+  // Use a function pointer to get the current time.  This way base::Time::Now()
+  // can be mocked out by inserting an alternate function.
+  base::Time (*time_now_func)();
 
   DISALLOW_COPY_AND_ASSIGN(PowerSupply);
 };
