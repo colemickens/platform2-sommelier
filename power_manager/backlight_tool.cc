168859b82214c23eddc4f7edd40dd692e2202c7b
Thiago Farina <tfarina@chromium.org>
2011-08-14 21:18:12 -0300

    Rename backlight-tool.cc to backlight_tool.cc
    BUG=None
    TEST=None
    
    Change-Id: I5e5e3e97a2c7c90954216487b888b9f753dacb01
    Signed-off-by: Thiago Farina <tfarina@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/5971
    Reviewed-by: Simon Que <sque@chromium.org>

--- power_manager/backlight_tool.cc
+++ power_manager/backlight_tool.cc
@@ -8,17 +8,14 @@
 #include <cstdio>
 
 #include "base/logging.h"
-#include "base/string_util.h"
 #include "power_manager/backlight.h"
 #include "power_manager/power_constants.h"
-
-// backlight-tool: A simple tool to get and set the brightness level of the
-//                 display backlight.
 
 DEFINE_bool(get_brightness, false, "Get current brightness level.");
 DEFINE_bool(get_max_brightness, false, "Get max brightness level.");
 DEFINE_int64(set_brightness, -1, "Set brightness level.");
 
+// A simple tool to get and set the brightness level of the display backlight.
 int main(int argc, char* argv[]) {
   google::ParseCommandLineFlags(&argc, &argv, true);
   CHECK(argc == 1) << "Unexpected arguments. Try --help";
@@ -35,8 +32,7 @@ int main(int argc, char* argv[]) {
     else if (FLAGS_get_max_brightness)
       printf("%" PRIi64 "\n", max);
   }
-  if (FLAGS_set_brightness >= 0) {
+  if (FLAGS_set_brightness >= 0)
     CHECK(backlight.SetBrightness(FLAGS_set_brightness));
-  }
   return 0;
 }
