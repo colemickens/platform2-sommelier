6f1ad565fe26f7b193d9b86e8716d2e98795f543
Terry Lambert <tlambert@chromium.org>
2011-08-19 15:19:10 -0700

    BUG: rescan serial device if .tp_disable present and switching drivers
    BUG=chromium-os:19433
    TEST=tested on device
    
    Change-Id: I4852e2ce550740e4da5df4815b688cf15e30dbcf
    Reviewed-on: http://gerrit.chromium.org/gerrit/6367
    Tested-by: Andrew de los Reyes <adlr@chromium.org>
    Reviewed-by: Andrew de los Reyes <adlr@chromium.org>

--- login_manager/xstart.sh
+++ login_manager/xstart.sh
@@ -7,4 +7,10 @@
 trap '' USR1 TTOU TTIN
 # The '-noreset' works around an apparent bug in the X server that
 # makes us fail to boot sometimes; see http://crosbug.com/7578.
+if [ -f /home/chronos/.tp_disable -a -f /tmp/synset.serio ] ; then
+  SERIAL_DEVICE=`cat /tmp/synset.serio`
+  SERIAL_NAME=`basename ${SERIAL_DEVICE}`
+  echo -n "rescan" > /sys/devices/platform/i8042/${SERIAL_NAME}/drvctl
+fi
 exec /usr/bin/X -noreset -nolisten tcp vt01 -auth $1 2> /dev/null
+cat /tmp/synset.serio
