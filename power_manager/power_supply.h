7213fd558cf5fb74f2c722241d664c2b8e681639
Kuan Tan <kuan@chromium.org>
2011-08-23 04:42:11 -0700

    Revert "Add hysteresis to battery remaing time calculation"
    This reverts commit 39495e5e1fa05d387a69f14aba135238e4214cee
    
    Change-Id: I2b5dbca4afd84e10484aa17157e83ef32e28b563
    Reviewed-on: http://gerrit.chromium.org/gerrit/6484
    Tested-by: Kuan Tan <kuan@chromium.org>
    Reviewed-by: Kuan Tan <kuan@chromium.org>

--- power_manager/power_supply.h
+++ power_manager/power_supply.h
@@ -5,13 +5,10 @@
 #ifndef POWER_MANAGER_POWER_SUPPLY_H_
 #define POWER_MANAGER_POWER_SUPPLY_H_
 
-#include <gtest/gtest_prod.h>  // for FRIEND_TEST
-
 #include <string>
 
 #include "base/basictypes.h"
 #include "base/file_path.h"
-#include "base/time.h"
 #include "power_manager/power_prefs.h"
 
 namespace chromeos {
@@ -41,17 +38,8 @@ class PowerSupply {
   const FilePath& battery_path() const { return battery_path_; }
 
  private:
-  friend class PowerSupplyTest;
-  FRIEND_TEST(PowerSupplyTest, TestDischargingWithHysteresis);
-
   // Find sysfs directories to read from.
   void GetPowerSupplyPaths();
-
-  // Computes time remaining based on energy drain rate.
-  double GetLinearTimeToEmpty();
-
-  // Determine remaining time when charging or discharging.
-  void CalculateRemainingTime(chromeos::PowerStatus *status);
 
   // Use the PowerPrefs functions to read info from sysfs.
   class PowerInfoReader : public PowerPrefs {
@@ -77,7 +65,6 @@ class PowerSupply {
   double current_now_;
   double cycle_count_;
   double voltage_now_;
-  double nominal_voltage_;
 
   // Indicates whether battery is present.
   bool battery_is_present_;
@@ -87,17 +74,6 @@ class PowerSupply {
   std::string serial_number_;
   std::string technology_;
   std::string type_;
-
-  // These are used for using hysteresis to avoid large swings in calculated
-  // remaining battery time.
-  double acceptable_variance_;
-  base::TimeDelta hysteresis_time_;
-  bool found_acceptable_time_range_;
-  double acceptable_time_;
-  base::TimeDelta time_outside_of_acceptable_range_;
-  base::Time last_acceptable_range_time_;
-  base::Time last_poll_time_;
-  base::Time discharge_start_time_;
 
   DISALLOW_COPY_AND_ASSIGN(PowerSupply);
 };
