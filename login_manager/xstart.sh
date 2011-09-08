4da10b178d74f4ddb63579ddc231bc7cb333dae5
Terry Lambert <tlambert@chromium.org>
2011-09-08 10:38:42 -0700

    Cleanup comments, dead test code, new marker file.
    This code is a workaround for a bug in our startup ordering between a
    writable data area being available to check for a marker file vs.
    starting up the udevd.  The bug manifests as the udevd winning the race
    vs. the mount and creating a serio_raw0 device and putting the mouse
    i8042 PS/2 interface into raw mode when the .tp_disable file is present.
    
    Resolving this bug correctly would require parameter NVRAM, which would
    potentially result in a security issue, or changing the startup order to
    ensure mounting before udevd runs, which would result in a startup mount
    regression.
    
    BUG=chromium-os:20190
    TEST=Run code on ZGB
    
    Change-Id: I0e786806bf2a5b67d8253924d4b0a639bcfcff66
    Reviewed-on: http://gerrit.chromium.org/gerrit/7413
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Terry Lambert <tlambert@chromium.org>

--- login_manager/xstart.sh
+++ login_manager/xstart.sh
@@ -5,12 +5,20 @@
 # found in the LICENSE file.
 
 trap '' USR1 TTOU TTIN
-# The '-noreset' works around an apparent bug in the X server that
-# makes us fail to boot sometimes; see http://crosbug.com/7578.
-if [ -f /home/chronos/.tp_disable -a -f /tmp/synset.serio ] ; then
-  SERIAL_DEVICE=`cat /tmp/synset.serio`
+# The following code checks for a marker file, and if present, identifies
+# the syntp_drv serial device and disables it for testing.  The code is
+# normally not run, since the file is not there, and so it does not
+# proceed to the second (expensive) half of the test.
+SERIAL_MARKER=/mnt/stateful_partition/var/lib/Synaptics/run/synset.serio
+if [ -f /home/chronos/.tp_disable -a \
+     -f $(SERIAL_MARKER) ] ; then
+  # If the syntp driver is running (synset.serio exists), but should be
+  # disabled (.tp_disable exists), reconfigure the i8042 driver to disable
+  # the serio_raw device currently being used by syntp (/dev/serio_rawN).
+  SERIAL_DEVICE=`cat $(SERIAL_MARKER)`
   SERIAL_NAME=`basename ${SERIAL_DEVICE}`
   echo -n "rescan" > /sys/devices/platform/i8042/${SERIAL_NAME}/drvctl
 fi
+# The '-noreset' works around an apparent bug in the X server that
+# makes us fail to boot sometimes; see http://crosbug.com/7578.
 exec /usr/bin/X -noreset -nolisten tcp vt01 -auth $1 2> /dev/null
-cat /tmp/synset.serio
