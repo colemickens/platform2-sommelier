f64370d0021aeed378264c043223c1cf64bd28fe
Terry Lambert <tlambert@chromium.org>
2011-10-11 19:54:24 -0700

    Use udevadm to determine the name when disabling serio_rawN
    Formerly, we relied on a Synaptics created file.  Since we've modified
    the Synaptics closed source driver to not rely on information files
    in order to close a race condition between udev discovering a device
    and running the udev rules and the mount of the stateful partition,
    we now need to modify xstart.sh to get the information in a similar
    fashion.
    
    BUG=chromium-os:20511
    TEST=Use crosh tpcontrol command on Alex, verify no change in behaviour
    
    Change-Id: If9d48add3a426fb0228fc13185cae06ac9d08827
    Reviewed-on: https://gerrit.chromium.org/gerrit/9921
    Commit-Ready: Terry Lambert <tlambert@chromium.org>
    Tested-by: Terry Lambert <tlambert@chromium.org>
    Reviewed-by: Terry Lambert <tlambert@chromium.org>

--- login_manager/xstart.sh
+++ login_manager/xstart.sh
@@ -9,15 +9,21 @@ trap '' USR1 TTOU TTIN
 # the syntp_drv serial device and disables it for testing.  The code is
 # normally not run, since the file is not there, and so it does not
 # proceed to the second (expensive) half of the test.
-SERIAL_MARKER=/mnt/stateful_partition/var/lib/Synaptics/run/synset.serio
-if [ -f /home/chronos/.tp_disable -a \
-     -f ${SERIAL_MARKER} ] ; then
-  # If the syntp driver is running (synset.serio exists), but should be
-  # disabled (.tp_disable exists), reconfigure the i8042 driver to disable
-  # the serio_raw device currently being used by syntp (/dev/serio_rawN).
-  SERIAL_DEVICE=`cat ${SERIAL_MARKER}`
-  SERIAL_NAME=`basename ${SERIAL_DEVICE}`
-  echo -n "rescan" > /sys/devices/platform/i8042/${SERIAL_NAME}/drvctl
+if [ -f /home/chronos/.tp_disable ] ; then
+  # serio_rawN devices come from the udev rules for the Synaptics binary
+  # driver throwing the PS/2 interface into raw mode.  If we find any
+  # that are TP devices, put them back into normal mode to use the default
+  # driver instead.
+  for P in /sys/class/misc/serio_raw*
+  do
+    # Note: It's OK if globbing fails, since the next line will drop us out
+    udevadm info -q env -p $P | grep -q ID_INPUT_TOUCHPAD=1 || continue
+    # Rescan everything; things that don't have another driver will just
+    # restart in serio_rawN mode again, since they won't be looking for
+    # the disable in their udev rules.
+    SERIAL_DEVICE=/sys$(dirname $(dirname $(udevadm info -q path -p $P)))
+    echo -n "rescan" > ${SERIAL_DEVICE}/drvctl
+  done
 fi
 # The '-noreset' works around an apparent bug in the X server that
 # makes us fail to boot sometimes; see http://crosbug.com/7578.
