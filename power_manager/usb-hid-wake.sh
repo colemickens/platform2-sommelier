0a35dbb347d5b92c7c25141b60c57ceb3e0e4d41
Benson Leung <bleung@chromium.org>
2011-10-18 18:17:04 -0700

    Configure usb-hid devices to wake.
    Keyboard devices are configured enabled by default,
    this will capture usb mice as well.
    
    BUG=chrome-os-partner:6286
    TEST=plug in a usb mouse into any of the 4
    ports on the back.
    powerd_suspend
    Click the mouse button to wake.
    
    Change-Id: I8ca55ff78b1861e78ed3b5f4c7b775c176d6b0fd
    Signed-off-by: Benson Leung <bleung@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/10285
    Reviewed-by: Simon Que <sque@chromium.org>

---
+++ power_manager/usb-hid-wake.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+#
+# Enable wakeup for usb-hid devices
+#
+
+listed_by_type() {
+    device=$1
+    device_base=`basename $device`
+    list=$2
+    for driver_type in $list; do
+        if grep -q DRIVER=$driver_type $device/uevent; then
+            return 0
+        fi
+        # Check child devices as well.  The actual driver type is
+        # listed in a child device, not the top-level device.
+        for subdevice in $device/$device_base*; do
+            if [ -f $subdevice/uevent ]; then
+                if grep -q DRIVER=$driver_type\
+                  $subdevice/uevent; then
+                    return 0
+                fi
+            fi
+        done
+    done
+    return 1
+}
+
+device=/sys/bus/usb/devices/$1
+if listed_by_type $device usbhid; then
+    echo "enabled" > $device/power/wakeup
+fi
