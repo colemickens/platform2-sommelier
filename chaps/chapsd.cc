abc437b3fafd3a1bb23fd66f4131315428360ad3
Darren Krahn <dkrahn@chromium.org>
2011-11-18 10:58:31 -0800

    Removed SIGTERM handler to avoid a crash during session tear-down.
    BUG=chromium-os:23133
    TEST=Attempt to reproduce crash as described in bug.
    
    Change-Id: I174729ca3f2e24315ef10337e09984a9d7abe140

--- chaps/chapsd.cc
+++ chaps/chapsd.cc
@@ -18,10 +18,6 @@
 
 scoped_ptr<DBus::BusDispatcher> g_dispatcher;
 
-static void Terminate(int sig) {
-  g_dispatcher->leave();
-}
-
 int main(int argc, char** argv) {
   CommandLine::Init(argc, argv);
   CommandLine::StringVector args = CommandLine::ForCurrentProcess()->args();
@@ -38,8 +34,6 @@ int main(int argc, char** argv) {
   CHECK(lib_target.get()) << "Out of memory.";
   if (!lib_target->Init())
     LOG(FATAL) << "Failed to initialize PKCS #11 library (" << args[0] << ").";
-  signal(SIGTERM, Terminate);
-  signal(SIGINT, Terminate);
   try {
     chaps::ChapsAdaptor adaptor(lib_target.get());
     g_dispatcher->enter();
