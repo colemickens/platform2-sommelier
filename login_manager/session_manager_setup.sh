e48629663d1753557ecea3c415ecda389c59764c
Mitsuru Oshima <oshima@chromium.org>
2012-02-28 07:20:27 -0800

    Revert "Revert "Temporarily disable plugins to troubleshoot timeout issue""
    This reverts commit 4f7594a5484062871cd469ae7701e81ab4e88c05
    
    Change-Id: Ia335ffba9e0ca27a3d0a453d142567e8ee26706c
    Reviewed-on: https://gerrit.chromium.org/gerrit/16959
    Commit-Ready: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Tested-by: Mitsuru Oshima <oshima@chromium.org>

--- login_manager/session_manager_setup.sh
+++ login_manager/session_manager_setup.sh
@@ -322,10 +322,8 @@ exec /sbin/session_manager --uid=${USER_ID} -- \
             --user-data-dir="$DATA_DIR" \
             --webui-login \
             --no-protector \
-            "$REGISTER_PLUGINS" \
             ${ACCELERATED_FLAGS} \
             ${AURA_FLAGS} \
-            ${FLASH_FLAGS} \
             ${SCREENSAVER_FLAG} \
             ${SHOW_VOLUME_STATUS_FLAG} \
             ${SKIP_OOBE} \
