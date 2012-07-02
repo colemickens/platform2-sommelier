76ca560cd464868583f4c3938f276993716b440b
Elly Jones <ellyjones@chromium.org>
2012-07-02 15:01:16 -0400

    [debugd] add setuid cras helper
    BUG=chromium-os:30740
    TEST=Adhoc
    GetLog cras works now
    
    Change-Id: I3995bf4664791a3ce02527907750184d46fc040a
    Signed-off-by: Elly Jones <ellyjones@chromium.org>

--- debugd/src/log_tool.cc
+++ debugd/src/log_tool.cc
@@ -53,7 +53,7 @@ static struct Log logs[] = {
   { "chrome_system_log", "/bin/cat /var/log/chrome/chrome" },
   { "cpu", "/usr/bin/uname -p" },
   { "cpuinfo", "/bin/cat /proc/cpuinfo" },
-  { "cras", "/usr/bin/cras_test_client --dump_server_info" },
+  { "cras", "/usr/libexec/debugd/helpers/cras_server_info" },
   { "dmesg", "/bin/dmesg" },
   { "ec_info", "/bin/cat /var/log/ec_info.txt" },
   { "font_info", "/usr/share/userfeedback/scripts/font_info" },
