c18dee39a364d36859907f67feb13f3c9086169a
Yuri Gorshenin <ygorshenin@chromium.org>
2012-08-16 23:03:46 +0400

    [debugd] Fixed SetLogging DBus call.
    Fixed call to ModemManager SetLogging method via DBus. Added runtime
    search of alive ModemManagers.
    
    BUG=chromium-os:31968
    TEST=manual testing on the Alex device
    
    Change-Id: I9a9f70701b156ac6663de1d5aad23469dbf540e1
    Reviewed-on: https://gerrit.chromium.org/gerrit/25615
    Reviewed-by: Yuri Gorshenin <ygorshenin@chromium.org>
    Reviewed-by: Elly Jones <ellyjones@chromium.org>
    Commit-Ready: Yuri Gorshenin <ygorshenin@chromium.org>
    Tested-by: Yuri Gorshenin <ygorshenin@chromium.org>

--- debugd/src/debug_mode_tool.cc
+++ debugd/src/debug_mode_tool.cc
@@ -9,8 +9,13 @@
 #include "proxies/org.chromium.flimflam.Manager.h"
 #include "proxies/org.freedesktop.DBus.Properties.h"
 #include "proxies/org.freedesktop.ModemManager.h"
+#include "proxies/org.freedesktop.ModemManager1.h"
 
 namespace debugd {
+
+const char* const kDBusPath = "/org/freedesktop/DBus";
+const char* const kDBusInterface = "org.freedesktop.DBus";
+const char* const kDBusListNames = "ListNames";
 
 const char* const kFlimflamPath = "/";
 const char* const kFlimflamService = "org.chromium.flimflam";
@@ -19,8 +24,16 @@ const char* const kSupplicantPath = "/fi/w1/wpa_supplicant1";
 const char* const kSupplicantService = "fi.w1.wpa_supplicant1";
 const char* const kSupplicantIface = "fi.w1.wpa_supplicant1";
 
+const char* const kModemManager = "ModemManager";
+
+const char* const kShillModemManagerPath = "/org/chromium/ModemManager";
+const char* const kShillModemManagerService = "org.chromium.ModemManager";
+
 const char* const kModemManagerPath = "/org/freedesktop/ModemManager";
 const char* const kModemManagerService = "org.freedesktop.ModemManager";
+
+const char* const kModemManager1Path = "/org/freedesktop/ModemManager1";
+const char* const kModemManager1Service = "org.freedesktop.ModemManager1";
 
 class ManagerProxy
     : public org::chromium::flimflam::Manager_proxy,
@@ -46,6 +59,18 @@ class ModemManagerProxy
     virtual void DeviceRemoved(const DBus::Path&) { }
 };
 
+class ModemManager1Proxy
+    : public org::freedesktop::ModemManager1_proxy,
+      public DBus::ObjectProxy {
+  public:
+    ModemManager1Proxy(DBus::Connection* connection, const char* path,
+                       const char* service) :
+        DBus::ObjectProxy(*connection, path, service) { }
+    virtual ~ModemManager1Proxy() { }
+    virtual void DeviceAdded(const DBus::Path&) { }
+    virtual void DeviceRemoved(const DBus::Path&) { }
+};
+
 class PropertiesProxy
     : public org::freedesktop::DBus::Properties_proxy,
       public DBus::ObjectProxy {
@@ -58,14 +83,13 @@ class PropertiesProxy
 
 DebugModeTool::DebugModeTool(DBus::Connection* connection)
     : connection_(connection) { }
+
 DebugModeTool::~DebugModeTool() { }
 
 void DebugModeTool::SetDebugMode(const std::string& subsystem,
                                  DBus::Error*) {
   ManagerProxy flimflam(connection_, kFlimflamPath, kFlimflamService);
   PropertiesProxy supplicant(connection_, kSupplicantPath, kSupplicantService);
-  ModemManagerProxy modemmanager(connection_, kModemManagerPath,
-                                 kModemManagerService);
   std::string flimflam_value;
   DBus::Variant supplicant_value;
   std::string modemmanager_value = "info";
@@ -83,7 +107,48 @@ void DebugModeTool::SetDebugMode(const std::string& subsystem,
   flimflam.SetDebugTags(flimflam_value);
   supplicant_value.writer().append_string(supplicant_level.c_str());
   supplicant.Set(kSupplicantIface, "DebugLevel", supplicant_value);
-  modemmanager.SetLogging(modemmanager_value);
+  SetAllModemManagersLogging(modemmanager_value);
+}
+
+void DebugModeTool::GetAllModemManagers(std::vector<std::string>* managers) {
+  managers->clear();
+
+  DBus::CallMessage msg;
+  DBus::MessageIter iter;
+  msg.destination(kDBusInterface);
+  msg.interface(kDBusInterface);
+  msg.member(kDBusListNames);
+  msg.path(kDBusPath);
+  DBus::Message ret = connection_->send_blocking(msg, -1);
+  iter = ret.reader();
+  iter = iter.recurse();
+  while (!iter.at_end()) {
+    std::string name(iter.get_string());
+    if (name.find(kModemManager) != std::string::npos)
+      managers->push_back(name);
+    ++iter;
+  }
+}
+
+void DebugModeTool::SetAllModemManagersLogging(const std::string& level) {
+  std::vector<std::string> managers;
+  GetAllModemManagers(&managers);
+  for (size_t i = 0; i < managers.size(); ++i) {
+    const std::string& manager = managers[i];
+    if (manager == kShillModemManagerService) {
+      ModemManagerProxy modemmanager(connection_, kShillModemManagerPath,
+                                     kShillModemManagerService);
+      modemmanager.SetLogging(level == "err" ? "error" : level);
+    } else if (manager == kModemManagerService) {
+      ModemManagerProxy modemmanager(connection_, kModemManagerPath,
+                                     kModemManagerService);
+      modemmanager.SetLogging(level);
+    } else if (manager == kModemManager1Service) {
+      ModemManager1Proxy modemmanager(connection_, kModemManager1Path,
+                                      kModemManager1Service);
+      modemmanager.SetLogging(level);
+    }
+  }
 }
 
 };  // namespace debugd
--- debugd/src/debug_mode_tool.h
+++ debugd/src/debug_mode_tool.h
@@ -6,6 +6,7 @@
 #define DEBUG_MODE_TOOL_H
 
 #include <string>
+#include <vector>
 
 #include <base/basictypes.h>
 #include <dbus-c++/dbus.h>
@@ -18,7 +19,11 @@ class DebugModeTool {
   virtual ~DebugModeTool();
 
   virtual void SetDebugMode(const std::string& subsystem, DBus::Error* error);
+
  private:
+  void GetAllModemManagers(std::vector<std::string>* managers);
+  void SetAllModemManagersLogging(const std::string& level);
+
   DBus::Connection* connection_;
   DISALLOW_COPY_AND_ASSIGN(DebugModeTool);
 };
