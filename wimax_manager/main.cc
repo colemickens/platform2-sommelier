f8220d2bb94bfa1397f9591cda7a0f7300e7d8b8
Ben Chan <benchan@chromium.org>
2012-10-18 21:45:15 -0700

    Use base::CommandLine instead gflags for parsing command line.
    BUG=chromium-os:35491
    TEST=Tested the following:
    1. Build and run unit tests.
    2. Verify wimax-manager is launched properly by upstart.
    3. Verify different command line switches:
         wimax-manager --help
         wimax-manager --foreground
         wimax-manager --foreground --log-level=0
    
    Change-Id: I4893996fc3dcd1637b643f268658410f261e5b98
    Reviewed-on: https://gerrit.chromium.org/gerrit/36040
    Commit-Ready: Ben Chan <benchan@chromium.org>
    Reviewed-by: Ben Chan <benchan@chromium.org>
    Tested-by: Ben Chan <benchan@chromium.org>

--- wimax_manager/main.cc
+++ wimax_manager/main.cc
@@ -10,30 +10,60 @@
 #include <base/basictypes.h>
 #include <base/command_line.h>
 #include <base/logging.h>
+#include <base/string_number_conversions.h>
 #include <chromeos/syslog_logging.h>
-#include <gflags/gflags.h>
 
 #include "wimax_manager/manager.h"
 #include "wimax_manager/manager_dbus_adaptor.h"
 
-DEFINE_bool(foreground, false,
-            "Don't daemon()ize; run in foreground.");
-DEFINE_int32(minloglevel, logging::LOG_WARNING,
-             "Messages logged at a lower level than "
-             "this don't actually get logged anywhere");
+using std::string;
 
 namespace {
 
-const char kUsageMessage[] = "Chromium OS WiMAX Manager";
+namespace switches {
+
+// Command line switch to run WiMAX manager in foreground.
+const char kForeground[] = "foreground";
+// Command line switch to show the help message and exit.
+const char kHelp[] = "help";
+// Command line switch to set the logging level:
+//   0 = LOG(INFO), 1 = LOG(WARNING), 2 = LOG(ERROR)
+const char kLogLevel[] = "log-level";
+// Help message to show when the --help command line switch is specified.
+const char kHelpMessage[] =
+    "Chromium OS WiMAX Manager\n"
+    "\n"
+    "Available Switches:\n"
+    "  --foreground\n"
+    "    Do not daemonize; run in foreground.\n"
+    "  --log-level=N\n"
+    "    Logging level:\n"
+    "      0: LOG(INFO), 1: LOG(WARNING), 2: LOG(ERROR)\n"
+    "      -1: VLOG(1), -2: VLOG(2), etc\n"
+    "  --help\n"
+    "    Show this help.\n"
+    "\n";
+
+}  // namespace switches
+
+int GetLogLevel(const string &log_level_value) {
+  int log_level = 0;
+  if (!base::StringToInt(log_level_value, &log_level)) {
+    LOG(WARNING) << "Invalid log level '" << log_level_value << "'";
+  } else if (log_level >= logging::LOG_NUM_SEVERITIES) {
+    log_level = logging::LOG_NUM_SEVERITIES;
+  }
+  return log_level;
+}
 
 // Always logs to syslog and stderr when running in the foreground.
-void SetupLogging() {
+void SetupLogging(bool foreground, int log_level) {
   int log_flags = chromeos::kLogToSyslog;
-  if (FLAGS_foreground)
+  if (foreground)
     log_flags |= chromeos::kLogToStderr;
 
   chromeos::InitLog(log_flags);
-  logging::SetMinLogLevel(FLAGS_minloglevel);
+  logging::SetMinLogLevel(log_level);
 }
 
 // This callback will be inovked when this process receives SIGINT or SIGTERM.
@@ -51,15 +81,22 @@ int main(int argc, char** argv) {
   base::AtExitManager exit_manager;
   g_type_init();
   g_thread_init(NULL);
-  google::SetUsageMessage(kUsageMessage);
-  google::ParseCommandLineFlags(&argc, &argv, true);
-  CommandLine::Init(argc, argv);
-  SetupLogging();
 
-  if (!FLAGS_foreground) {
-    LOG(INFO) << "Daemonizing WiMAX manager";
-    PLOG_IF(FATAL, ::daemon(0, 0) == 1) << "Failed to create daemon";
+  CommandLine::Init(argc, argv);
+  CommandLine* cl = CommandLine::ForCurrentProcess();
+
+  if (cl->HasSwitch(switches::kHelp)) {
+    LOG(INFO) << switches::kHelpMessage;
+    return 0;
   }
+
+  bool foreground = cl->HasSwitch(switches::kForeground);
+  if (!foreground)
+    PLOG_IF(FATAL, ::daemon(0, 0) == 1) << "Failed to create daemon";
+
+  int log_level = cl->HasSwitch(switches::kLogLevel) ?
+      GetLogLevel(cl->GetSwitchValueASCII(switches::kLogLevel)) : 0;
+  SetupLogging(foreground, log_level);
 
   GMainLoop *loop = g_main_loop_new(g_main_context_default(), FALSE);
   CHECK(loop) << "Failed to create GLib main loop";
