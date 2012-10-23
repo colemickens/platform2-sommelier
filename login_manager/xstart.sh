74ce9be6e11cff76c3f8db0b5d82e25e8547b6c8
yoshiki iguchi <yoshiki@chromium.org>
2012-10-23 13:08:01 +0900

    Use -maxvt flag to disable VT switching.
    BUG=chromium:153961, chromium-os:35606
    TEST=manual
    
    Change-Id: Ica7e32e2206c2b3e9e4410dd493d8ff2e719068f
    Reviewed-on: https://gerrit.chromium.org/gerrit/36315
    Reviewed-by: Daniel Erat <derat@chromium.org>
    Tested-by: Yoshiki IGUCHI <yoshiki@chromium.org>
    Commit-Ready: Yoshiki IGUCHI <yoshiki@chromium.org>

--- login_manager/xstart.sh
+++ login_manager/xstart.sh
@@ -5,6 +5,17 @@
 # found in the LICENSE file.
 
 trap '' USR1 TTOU TTIN
+
+# Disables all the Ctrl-Alt-Fn shortcuts for switching between virtual terminals
+# if we're in verified boot mode. Otherwise, disables only Fn (n>=3) keys.
+MAXVT=0
+if crossystem "cros_debug?1" ||
+    grep -q '^chronos:[^*]' /etc/shadow; then
+  # developer end-user
+  MAXVT=2
+fi
+
 # The '-noreset' works around an apparent bug in the X server that
 # makes us fail to boot sometimes; see http://crosbug.com/7578.
-exec nice -n -20 /usr/bin/X -noreset -nolisten tcp vt01 -auth $1 2> /dev/null
+exec nice -n -20 /usr/bin/X \
+    -noreset -maxvt $MAXVT -nolisten tcp vt01 -auth $1 2> /dev/null
