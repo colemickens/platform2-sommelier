d911b500ca683376db73af8f9679451a25642e2b
Daniel Erat <derat@chromium.org>
2012-10-31 07:59:10 -0700

    power: Add --set_brightness_percent to backlight-tool.
    This adds a flag to the backlight-tool utility to set the
    backlight brightness to a (linearly-calculated) percent of
    the device's maximum brightness in the range [0.0, 100.0].
    
    BUG=chromium-os:31795
    TEST=run "backlight-tool --set_brightness_percent 50.0" and
         see brightness change
    
    Change-Id: I3a0611d8ffebe96fa4fdca6677ca61564656cbed
    Reviewed-on: https://gerrit.chromium.org/gerrit/37019
    Tested-by: Daniel Erat <derat@chromium.org>
    Reviewed-by: Simon Que <sque@chromium.org>
    Commit-Ready: Daniel Erat <derat@chromium.org>

--- power_manager/tools/backlight_tool.cc
+++ power_manager/tools/backlight_tool.cc
@@ -5,6 +5,7 @@
 #include <gflags/gflags.h>
 #include <inttypes.h>
 
+#include <cmath>
 #include <cstdio>
 
 #include "base/logging.h"
@@ -19,13 +20,18 @@
 DEFINE_bool(get_brightness, false, "Get current brightness level.");
 DEFINE_bool(get_max_brightness, false, "Get max brightness level.");
 DEFINE_int64(set_brightness, -1, "Set brightness level.");
+DEFINE_double(set_brightness_percent, -1.0,
+              "Set brightness as a percent in [0.0, 100.0].");
 
 // A simple tool to get and set the brightness level of the display backlight.
 int main(int argc, char* argv[]) {
   google::ParseCommandLineFlags(&argc, &argv, true);
   CHECK_EQ(argc, 1) << "Unexpected arguments. Try --help";
-  CHECK(!FLAGS_get_brightness || !FLAGS_get_max_brightness) <<
-      "-get_brightness and -get_max_brightness are mutually exclusive";
+  CHECK(!FLAGS_get_brightness || !FLAGS_get_max_brightness)
+      << "-get_brightness and -get_max_brightness are mutually exclusive";
+  CHECK(FLAGS_set_brightness < 0 || FLAGS_set_brightness_percent < 0)
+      << "-set_brightness and -set_brightness_percent are mutually exclusive";
+
 #ifdef IS_DESKTOP
   power_manager::ExternalBacklight backlight;
   CHECK(backlight.Init());
@@ -40,11 +46,20 @@ int main(int argc, char* argv[]) {
     printf("%" PRIi64 "\n", level);
   }
   if (FLAGS_get_max_brightness) {
-    int64 max = 0;
-    CHECK(backlight.GetMaxBrightnessLevel(&max));
-    printf("%" PRIi64 "\n", max);
+    int64 max_level = 0;
+    CHECK(backlight.GetMaxBrightnessLevel(&max_level));
+    printf("%" PRIi64 "\n", max_level);
   }
   if (FLAGS_set_brightness >= 0)
     CHECK(backlight.SetBrightnessLevel(FLAGS_set_brightness));
+  if (FLAGS_set_brightness_percent >= 0.0) {
+    FLAGS_set_brightness_percent =
+        std::min(FLAGS_set_brightness_percent, 100.0);
+    int64 max_level = 0;
+    CHECK(backlight.GetMaxBrightnessLevel(&max_level));
+    int64 new_level = static_cast<int64>(
+        roundl(FLAGS_set_brightness_percent * max_level / 100.0));
+    CHECK(backlight.SetBrightnessLevel(new_level));
+  }
   return 0;
 }
