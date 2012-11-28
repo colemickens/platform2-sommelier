7110f70f0b0eed7785d1293dcae6e05b80ed0c52
Gaurav Shah <gauravsh@chromium.org>
2012-11-27 17:43:28 -0800

    Revert "session_manager: Reset stdin/stdout/stderr to /dev/null before exec-ing child"
    This reverts commit 631908b971f979aad7bfc60b13d074409967fa8b
    
    Some non-Chrome internal components (Pepper flash, GPU driver, etc) rely on stderr being redirected to the /var/log/ui* for their logging (since they can't use the Chrome's logging infrastructure).
    
    BUG=chromium-os:36688
    
    Change-Id: I280d9c450580fc1e0965af334481041816e3d92e
    Reviewed-on: https://gerrit.chromium.org/gerrit/38788
    Reviewed-by: Gaurav Shah <gauravsh@chromium.org>
    Tested-by: Gaurav Shah <gauravsh@chromium.org>

--- login_manager/child_job.h
+++ login_manager/child_job.h
@@ -142,10 +142,6 @@ class ChildJob : public ChildJobInterface {
   // call fails.
   int SetIDs();
 
-  // Close any logging file descriptors and reset I/O file descriptors
-  // to /dev/null.
-  void ResetFds();
-
   // Arguments to pass to exec.
   std::vector<std::string> arguments_;
 
