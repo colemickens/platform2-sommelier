18c6209c3632974650b70c5716780f0b9a264a8b
Daniel Erat <derat@chromium.org>
2013-01-14 16:17:03 -0800

    power: Add lock_vt and unlock_vt to powerd_setuid_helper.
    This adds two more tiny commands (the last ones, I hope!) to
    powerd_setuid_helper to make VT_UNLOCKSWITCH and
    VT_LOCKSWITCH ioctls to /dev/tty0.  powerd was previously
    doing this itself by virtue of the CAP_SYS_TTY_CONFIG
    capability, but asking minijail to set capabilities on
    powerd results in powerd_setuid_helper() being unable to
    call setuid(0).
    
    BUG=chromium-os:36804
    TEST=manual: commands succeed when executed by powerd
    
    Change-Id: I321ed740744ced6bc29a7a0f6e3bdaf8dee4135c
    Reviewed-on: https://gerrit.chromium.org/gerrit/41244
    Tested-by: Daniel Erat <derat@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Daniel Erat <derat@chromium.org>

--- power_manager/powerd/powerd_setuid_helper.cc
+++ power_manager/powerd/powerd_setuid_helper.cc
@@ -5,6 +5,9 @@
 // This is a small setuid-root program that runs a few commands on behalf of
 // the powerd process.
 
+#include <fcntl.h>
+#include <linux/vt.h>
+#include <sys/ioctl.h>
 #include <sys/types.h>
 #include <unistd.h>
 
@@ -18,7 +21,8 @@
 #include "base/string_number_conversions.h"
 
 DEFINE_string(action, "", "Action to perform.  Must be one of "
-              "\"clean_shutdown\", \"reboot\", \"shutdown\", and \"suspend\".");
+              "\"clean_shutdown\", \"lock_vt\", \"reboot\", \"shutdown\", "
+              "\"suspend\", and \"unlock_vt\".");
 DEFINE_string(shutdown_reason, "", "Optional shutdown reason starting with a "
               "lowercase letter and consisting only of lowercase letters and "
               "dashes.");
@@ -29,6 +33,10 @@ DEFINE_int32(suspend_wakeup_count, -1, "Pass --wakeup_count <INT> to "
 
 // Maximum number of arguments supported for internally-defined commands.
 const size_t kMaxArgs = 64;
+
+// Path to device on which VT_UNLOCKSWITCH and VT_LOCKSWITCH ioctls can be made
+// to enable or disable VT switching.
+const char kConsolePath[] = "/dev/tty0";
 
 // Paths to various binaries that are executed.
 const char kInitctlPath[] = "/sbin/initctl";
@@ -60,11 +68,26 @@ void RunCommand(const char* path, const char* arg, ...) {
   PCHECK(execv(path, argv) != -1) << "execv() failed";
 }
 
+// Locks or unlocks VT switching.  In a perfect world this would live in powerd,
+// but these ioctls require the CAP_SYS_TTY_CONFIG capability and setting that
+// breaks the setuid() call in RunCommand().  In a slightly less perfect world
+// this would live in the powerd_suspend script, but there doesn't seem to be
+// any way for a script to lock or unlock switching directly.
+void SetVTSwitchingAllowed(bool allowed) {
+  int fd = open(kConsolePath, O_WRONLY);
+  PCHECK(fd >= 0) << "open(" << kConsolePath << ") failed";
+  PCHECK(ioctl(fd, allowed ? VT_UNLOCKSWITCH : VT_LOCKSWITCH) == 0)
+      << "ioctl() failed";
+  close(fd);
+}
+
 int main(int argc, char* argv[]) {
   google::ParseCommandLineFlags(&argc, &argv, true);
 
   if (FLAGS_action == "clean_shutdown") {
     RunCommand(kInitctlPath, "emit", "power-manager-clean-shutdown", NULL);
+  } else if (FLAGS_action == "lock_vt") {
+    SetVTSwitchingAllowed(false);
   } else if (FLAGS_action == "reboot") {
     RunCommand(kShutdownPath, "-r", "now", NULL);
   } else if (FLAGS_action == "shutdown") {
@@ -86,8 +109,12 @@ int main(int argc, char* argv[]) {
         base::IntToString(FLAGS_suspend_wakeup_count);
     RunCommand(kPowerdSuspendPath, cancel_flag.c_str(), wakeup_flag.c_str(),
                NULL);
+  } else if (FLAGS_action == "unlock_vt") {
+    SetVTSwitchingAllowed(true);
   } else {
     LOG(ERROR) << "Unknown action \"" << FLAGS_action << "\"";
     return 1;
   }
+
+  return 0;
 }
