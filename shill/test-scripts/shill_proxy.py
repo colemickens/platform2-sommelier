daa34581af348af7fa22990052024496eeac410d
Christopher Wiley <wiley@chromium.org>
2013-03-01 09:27:30 -0800

    shill-test: Add ShillProxy and update connect-wifi
    Add a less crufty, updated version of flimflam.py with functionality to
    connect a WiFi network: shill_proxy.py.  Rewrite a script: connect-wifi
    to use this new proxy to connect to a WiFi service.
    
    With these scripts and a logged out Chromebook with a test image, I can
    now repetitively run (from /usr/local/lib/flimflam/test/):
    
      ./create-profile test && ./push-profile test && \
      ./connect-wifi GoogleGuest none && \
      ./pop-profile && ./rm-profile test
    
    This gives me the following output:
    
      New profile created at /profile/test
      Pushed profile /profile/test
      Attempting to connect service GoogleGuest
      Attempting to conect to GoogleGuest
      Discovering...
      Discovered service: GoogleGuest
      Connecting...
      Called connect on service
      Associating...
      Associated with service: GoogleGuest
      Configuring...
      Configured service: GoogleGuest
      Connected to WiFi service.
      Operation succeeded.
      Discovery time: 0.000166.
      Association time: 3.668617.
      Configuration time: 1.018661.
    
    BUG=chromium:224449
    TEST=As documented above.
    
    Change-Id: Id62d3673ee617a0d395efb31da7b548c46cc531e
    Reviewed-on: https://gerrit.chromium.org/gerrit/46706
    Commit-Queue: Christopher Wiley <wiley@chromium.org>
    Reviewed-by: Christopher Wiley <wiley@chromium.org>
    Tested-by: Christopher Wiley <wiley@chromium.org>
    Reviewed-by: Paul Stewart <pstew@chromium.org>

---
+++ shill/test-scripts/shill_proxy.py
@@ -0,0 +1,235 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import dbus
+import logging
+import time
+
+def dbus2primitive(value):
+    if isinstance(value, dbus.Boolean):
+        return bool(value)
+    elif isinstance(value, int):
+        return int(value)
+    elif isinstance(value, float):
+        return float(value)
+    elif isinstance(value, str):
+        return str(value)
+    elif isinstance(value, unicode):
+        return unicode(value)
+    elif isinstance(value, list):
+        return [dbus2primitive(x) for x in value]
+    elif isinstance(value, tuple):
+        return tuple([dbus2primitive(x) for x in value])
+    elif isinstance(value, dict):
+        return dict([(dbus2primitive(k), dbus2primitive(v))
+                     for k,v in value.items()])
+    else:
+        logging.error('Failed to convert dbus object of class: %r',
+                      value.__class__.__name__)
+        return value
+
+
+class ShillProxy(object):
+    UNKNOWN_METHOD = 'org.freedesktop.DBus.Error.UnknownMethod'
+
+    DBUS_INTERFACE = 'org.chromium.flimflam'
+    DBUS_TYPE_MANAGER = 'org.chromium.flimflam.Manager'
+    DBUS_TYPE_SERVICE = 'org.chromium.flimflam.Service'
+
+
+    SERVICE_PROPERTY_TYPE = 'Type'
+    SERVICE_PROPERTY_NAME = 'Name'
+    SERVICE_PROPERTY_MODE = 'Mode'
+    SERVICE_PROPERTY_SSID = 'SSID'
+    SERVICE_PROPERTY_STATE = 'State'
+    SERVICE_PROPERTY_SECURITY = 'Security'
+    SERVICE_PROPERTY_PASSPHRASE = 'Passphrase'
+    SERVICE_PROPERTY_SAVE_CREDENTIALS = 'SaveCredentials'
+    SERVICE_PROPERTY_GUID = 'GUID'
+
+
+    POLLING_INTERVAL_SECONDS = 0.2
+
+
+    def wait_for_property_in(self, dbus_object, property_name,
+                             expected_values, timeout_seconds):
+        """
+        Block until the property |property_name| in |dbus_object| is in
+        |expected_values|, or |timeout_seconds|.
+
+        @param dbus_object DBus proxy object as returned by
+            self.get_dbus_object.
+        @param property_name string property key in dbus_object.
+        @param expected_values iterable set of values to return successfully
+            upon seeing.
+        @param timeout_seconds float number of seconds to return if we haven't
+            seen the appropriate property value in time.
+        @return tuple(successful, final_value, duration)
+            where successful is True iff we saw one of |expected_values| for
+            |property_name|, final_value is the member of |expected_values| we
+            saw, and duration is how long we waited to see that value.
+
+        """
+        # TODO(wiley) This should be changed to be event based (not polling).
+        value = None
+        start_time = time.time()
+        successful = False
+        while time.time() < start_time + timeout_seconds:
+            properties = dbus_object.GetProperties(utf8_strings=True)
+            value = properties.get(property_name, None)
+            if value in expected_values:
+                successful = True
+                break
+            time.sleep(self.POLLING_INTERVAL_SECONDS)
+        duration = time.time() - start_time
+        return (successful, dbus2primitive(value), duration)
+
+
+    def __init__(self, bus=None):
+        if bus is None:
+            bus = dbus.SystemBus()
+        self._bus = bus
+        self._manager = self.get_dbus_object(self.DBUS_TYPE_MANAGER, '/')
+
+
+    @property
+    def manager(self):
+        """ @return DBus proxy object representing the shill Manager. """
+        return self._manager
+
+
+    def get_dbus_object(self, type_str, path):
+        """
+        Return the DBus object of type |type_str| at |path| in shill.
+
+        @param type_str string (e.g. self.DBUS_TYPE_SERVICE).
+        @param path path to object in shill (e.g. '/service/12').
+        @return DBus proxy object.
+
+        """
+        return dbus.Interface(
+                self._bus.get_object(self.DBUS_INTERFACE, path),
+                type_str)
+
+
+    def get_service(self, params):
+        """
+        Get the shill service that matches |params|.
+
+        @param params dict of strings understood by shill to describe
+            a service.
+        @return DBus object interface representing a service.
+
+        """
+        path = self.manager.GetService(params)
+        return self.get_dbus_object(self.DBUS_TYPE_SERVICE, path)
+
+
+    def connect_to_wifi_network(self,
+                                ssid,
+                                security,
+                                psk,
+                                save_credentials,
+                                discovery_timeout_seconds=15,
+                                association_timeout_seconds=15,
+                                configuration_timeout_seconds=15):
+        """
+        Connect to a WiFi network with the given association parameters.
+
+        @param ssid string name of network to connect to.
+        @param security string type of security used in network (e.g. psk)
+        @param psk string password or pre shared key for appropriate security
+                types.
+        @param save_credentials bool True if we should save EAP credentials.
+        @param discovery_timeout_seconds float timeout for service discovery.
+        @param association_timeout_seconds float timeout for service
+            association.
+        @param configuration_timeout_seconds float timeout for DHCP
+            negotiations.
+        @return (successful, discovery_time, association_time,
+                 configuration_time, reason)
+            where successful is True iff the operation succeeded, *_time is
+            the time spent waiting for each transition, and reason is a string
+            which may contain a meaningful description of failures.
+
+        """
+        # TODO(wiley) This doesn't work for hidden ssids
+        logging.info('Attempting to conect to %s', ssid)
+        service_proxy = None
+        start_time = time.time()
+        discovery_time = -1.0
+        association_time = -1.0
+        configuration_time = -1.0
+        logging.info('Discovering...')
+        while time.time() - start_time < discovery_timeout_seconds:
+            discovery_time = time.time() - start_time
+            discovery_params = {self.SERVICE_PROPERTY_TYPE: 'wifi',
+                                self.SERVICE_PROPERTY_NAME: ssid,
+                                self.SERVICE_PROPERTY_SECURITY: security}
+            try:
+                service_path = self.manager.FindMatchingService(
+                        discovery_params)
+            except dbus.exceptions.DBusException, e:
+                # There really is not a better way to check the error type.
+                if e.get_dbus_message() != 'Matching service was not found':
+                    logging.error('Caught an error while discovering: %s',
+                                  e.get_dbus_message())
+                # This is spammy, but shill handles that for us.
+                self.manager.RequestScan('wifi')
+                time.sleep(self.POLLING_INTERVAL_SECONDS)
+                continue
+            logging.info('Discovered service: %s', ssid)
+            break
+        else:
+            return (False, discovery_time, association_time,
+                    configuration_time, 'FAIL(Discovery timed out)')
+
+        # At this point, we know |service| is in the service list.  Attempt
+        # to connect it, and watch the states roll by.
+        logging.info('Connecting...')
+        try:
+            service_object = self.get_dbus_object(self.DBUS_TYPE_SERVICE,
+                                                  service_path)
+            # Don't give a passphrase unless we actually have one.
+            if psk:
+                service_object.SetProperty(self.SERVICE_PROPERTY_PASSPHRASE,
+                                           psk)
+            service_object.Connect()
+            logging.info('Called connect on service')
+        except dbus.exceptions.DBusException, e:
+            logging.error('Caught an error while trying to connect: %s',
+                          e.get_dbus_message())
+            return (False, discovery_time, association_time,
+                    configuration_time, 'FAIL(Failed to call connect)')
+
+        logging.info('Associating...')
+        result = self.wait_for_property_in(
+                service_object,
+                self.SERVICE_PROPERTY_STATE,
+                ('configuration', 'ready', 'portal', 'online'),
+                association_timeout_seconds)
+        (successful, _, association_time) = result
+        if not successful:
+            return (False, discovery_time, association_time,
+                    configuration_time, 'FAIL(Association timed out)')
+
+        logging.info('Associated with service: %s', ssid)
+
+        logging.info('Configuring...')
+        result = self.wait_for_property_in(
+                service_object,
+                self.SERVICE_PROPERTY_STATE,
+                ('ready', 'portal', 'online'),
+                configuration_timeout_seconds)
+        (successful, _, configuration_time) = result
+        if not successful:
+            return (False, discovery_time, association_time,
+                    configuration_time, 'FAIL(Configuration timed out)')
+
+        logging.info('Configured service: %s', ssid)
+
+        # Great success!
+        logging.info('Connected to WiFi service.')
+        return (True, discovery_time, association_time, configuration_time,
+                'SUCCESS(Connection successful)')
