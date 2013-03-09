cd82fa912e170680dba8488ac6ccc8a1c7eed88b
Simon Que <sque@chromium.org>
2013-03-08 18:04:50 -0800

    Move perf sample utils into perf parser
    This way, they can be separated from the other utils funcs.
    
    BUG=none
    TEST=unit tests pass
    
    Change-Id: I74bd1cb1ff70fd7662508fcc88fcdb93ac217992
    Signed-off-by: Simon Que <sque@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/45001

--- chromiumos-wide-profiling/utils.h
+++ chromiumos-wide-profiling/utils.h
@@ -30,18 +30,4 @@ bool CreateNamedTempFile(string* name);
 // compared.
 bool ComparePerfReports(const string& a, const string& b);
 
-// Copies only the event-specific data from one event_t to another.  The sample
-// info is not copied.
-void CopyPerfEventSpecificInfo(const event_t& source, event_t* destination);
-
-// Returns info about the raw perf sample that recorded a perf event.
-bool ReadPerfSampleInfo(const event_t& event,
-                        uint64 sample_type,
-                        struct perf_sample* sample);
-
-// Requires that event->header has already been filled with the sample data.
-bool WritePerfSampleInfo(const perf_sample& sample,
-                         uint64 sample_type,
-                         event_t* event);
-
 #endif  // UTILS_H_
