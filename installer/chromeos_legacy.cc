0f588dabad6a58052b5258b7db3f6818f37cb3bd
Paul Drews <paul.drews@intel.com>
2013-03-25 08:37:35 -0700

    Edit efi grub configuration in EFI post-install
    This adds code to edit the EFI grub configuration during the
    post-install step when the BIOS type is EFI.  Partitions on
    the hard drive are created with new partition UUIDs that are
    different from the USB-key partition UUIDS.  The EFI grub
    configuration needs to get edited in a corresponding way to
    refer to those hard-drive partition UUIDs.
    
    BUG=none
    TEST=EFI-boot from USB ; chromeos-install ; remove USB ; EFI-boot
    
    Change-Id: Ic8f86414abd7987c37c619af24a619c56f5da78d
    Reviewed-on: https://gerrit.chromium.org/gerrit/46285
    Tested-by: paul drews <paul.drews@intel.com>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: paul drews <paul.drews@intel.com>

--- installer/chromeos_legacy.cc
+++ installer/chromeos_legacy.cc
@@ -110,11 +110,85 @@ bool RunLegacyUBootPostInstall(const InstallConfig& install_config) {
 
 bool RunEfiPostInstall(const InstallConfig& install_config) {
   printf("Running EfiPostInstall\n");
-  // TODO(dgarret) Support EFI by fixing GRUB configuration files.
-  printf("EFI BIOS is temporarily not supported.\n"
-         "You probably need to manually edit the GRUB configuration files "
-         "after installation.\n");
-  // 'true' is required here to prevent blocking factory installation for most
-  // partner devices, until EFI is officially supported.
+  printf("Editing grubpart%s PARTUUID to %s in %s using template in %s\n",
+      install_config.slot.c_str(),
+      install_config.root.uuid().c_str(),
+      install_config.boot.mount().c_str(),
+      install_config.root.mount().c_str());
+  string grub_from = StringPrintf("%s/boot/efi/boot/grub.cfg",
+                                  install_config.root.mount().c_str());
+  string grub_to = StringPrintf("%s/efi/boot/grub.cfg",
+                                install_config.boot.mount().c_str());
+
+  // Find the line we want from the template.
+  string grub_from_str;
+  if (!ReadFileToString(grub_from, &grub_from_str)) {
+    printf("Unable to read grub template file %s\n",
+           grub_from.c_str());
+    return false;
+  }
+  // Unfortunately std::regex is "experimental" at this time, so we have to
+  // approximate the behavior of a regular-expression such as
+  // "grubpartA.*linuxpartA".
+  string s1 = StringPrintf("grubpart%s",
+                           install_config.slot.c_str());
+  string s2 = StringPrintf("linuxpart%s",
+                           install_config.slot.c_str());
+  string kernel_line;
+  std::vector<string> file_lines;
+  SplitString(grub_from_str, '\n', &file_lines);
+  std::vector<string>::iterator line;
+  for (line = file_lines.begin(); line < file_lines.end(); line++) {
+    if ((line->find(s1) != string::npos) &&
+        (line->find(s2) != string::npos)) {
+      kernel_line = *line;
+      break;
+    }
+  }
+  if (kernel_line.empty()) {
+    printf("error - bad grub.cfg template\n");
+    return false;
+  }
+
+  // Substitute-in the correct UUID.
+  string new_val = StringPrintf("PARTUUID=%s",
+                                install_config.root.uuid().c_str());
+  if (!SetKernelArg("root", new_val, &kernel_line)) {
+    printf("error - setting new key value for root\n");
+    return false;
+  }
+
+  // Overwrite the corresponding line in the actual boot menu.
+  string new_boot_str;
+  string grub_to_str;
+  if (!ReadFileToString(grub_to, &grub_to_str)) {
+    printf("Unable to read boot menu file %s\n",
+           grub_to.c_str());
+    return false;
+  }
+  // These search strings approximate the behavior of a regular-expression such
+  // as "grubpartA.*PARTUUID".
+  s1 = StringPrintf("grubpart%s",
+                           install_config.slot.c_str());
+  s2 = "PARTUUID";
+  SplitString(grub_to_str, '\n', &file_lines);
+  for (line = file_lines.begin(); line < file_lines.end(); line++) {
+    if ((line->find(s1) != string::npos) &&
+        (line->find(s2) != string::npos)) {
+      new_boot_str += kernel_line;
+      printf("Replaced:\n    %s\n  with:\n    %s\n",
+             line->c_str(),
+             kernel_line.c_str());
+    } else {
+      new_boot_str += *line;
+    }
+    new_boot_str += "\n";
+  }
+  if (!WriteStringToFile(new_boot_str, grub_to)) {
+    printf("Unable to write boot menu file %s\n",
+            grub_to.c_str());
+    return false;
+  }
+  // Other EFI post-install actions, if any, go here.
   return true;
 }
