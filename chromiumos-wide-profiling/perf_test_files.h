befaec431cd36e60789bc4ff1c1b2bb0733c0893
Simon Que <sque@chromium.org>
2013-04-17 12:10:13 -0700

    Add branch events to unit test perf data
    Also add descriptions to perf_test_files.h of what the test files
    contain / how they were generated.
    
    BUG=none
    TEST=unit tests pass
    CQ-DEPEND=I32ffe31e7518ef917e4a09ada56b6c2ea8c2aa09
    
    Change-Id: Iba30446f8d8ac1229fb71606b4569b1d37d9fa16
    Signed-off-by: Simon Que <sque@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/48391
    Reviewed-by: asharif <asharif@chromium.org>

--- chromiumos-wide-profiling/perf_test_files.h
+++ chromiumos-wide-profiling/perf_test_files.h
@@ -7,14 +7,34 @@
 
 namespace perf_test_files {
 
+// The following perf data contains the following event types, as passed to
+// perf record via the -e option:
+// - cycles
+// - instructions
+// - cache-references
+// - cache-misses
+// - branches
+// - branch-misses
+
 const char* kPerfDataFiles[] = {
+  // Obtained with "perf record -- echo > /dev/null"
   "perf.data.singleprocess",
+
+  // Obtained with "perf record -a -- sleep $N", for N in {0, 1, 5}.
   "perf.data.systemwide.0",
   "perf.data.systemwide.1",
   "perf.data.systemwide.5",
+
+  // Obtained with "perf record -a -- sleep $N", for N in {0, 1, 5}.
+  // While in the background, this loop is running:
+  //   while true; do ls > /dev/null; done
   "perf.data.busy.0",
   "perf.data.busy.1",
   "perf.data.busy.5",
+
+  // Obtained with "perf record -a -- sleep 2"
+  // While in the background, this loop is running:
+  //   while true; do restart powerd; sleep .2; done
   "perf.data.forkexit",
 };
 
