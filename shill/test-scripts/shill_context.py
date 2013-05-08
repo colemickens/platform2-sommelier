41fa1e716c26effd3e9668307ba268eca02cfad2
Ben Chan <benchan@chromium.org>
2013-05-08 14:41:20 -0700

    shill-test: Add shill_context.ServiceAutoConnectContext helper.
    Add a context manager 'shill_context.ServiceAutoConnectContext' for
    temporarily overriding the 'AutoConnect' property of a service object.
    
    BUG=chromium:238855
    TEST=Tested the following:
    1. Run pylint on shill_context.py.
    2. Test the following code in a Python shell on daisy with a cellular service:
    
        from shill_context import ServiceAutoConnectContext
        from shill_proxy import ShillProxy
    
        shill = ShillProxy()
        get_service = lambda: shill.find_object('Service', {'Type': 'cellular'})
    
        with ServiceAutoConnectContext(get_service, autoconnect=False) as c:
            print('initial autoconnect: %s' % c.initial_autoconnect)
            print('autoconnect: %s' % c.autoconnect)
    
        with ServiceAutoConnectContext(get_service, autoconnect=True) as c:
            print('initial autoconnect: %s' % c.initial_autoconnect)
            print('autoconnect: %s' % c.autoconnect)
    
    Change-Id: I3e7d2cfb2d6ce81e5363e3e91e3886a44a9e8190
    Reviewed-on: https://gerrit.chromium.org/gerrit/50497
    Reviewed-by: Thieu Le <thieule@chromium.org>
    Reviewed-by: Christopher Wiley <wiley@chromium.org>
    Commit-Queue: Ben Chan <benchan@chromium.org>
    Tested-by: Ben Chan <benchan@chromium.org>

---
+++ shill/test-scripts/shill_context.py
@@ -0,0 +1,87 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A collection of context managers for working with shill objects."""
+
+import logging
+import shill_proxy
+
+
+class ContextError(Exception):
+    """An error raised by a context managers dealing with shill objects."""
+    pass
+
+
+class ServiceAutoConnectContext(object):
+    """A context manager for overriding a service's 'AutoConnect' property.
+
+    As the service object of the same service may change during the lifetime
+    of the context, this context manager does not take a service object at
+    construction. Instead, it takes a |get_service| function at construction,
+    which it invokes to obtain a service object when entering and exiting the
+    context. It is assumed that |get_service| always returns a service object
+    that refers to the same service.
+
+    Usage:
+        def get_service():
+            # Some function that returns a service object.
+
+        with ServiceAutoConnectContext(get_service, False):
+            # Within this context, the 'AutoConnect' property of the service
+            # returned by |get_service| is temporarily set to False if it's
+            # initially set to True. The property is restored to its initial
+            # value after the context ends.
+
+    """
+    def __init__(self, get_service, autoconnect):
+        self._get_service = get_service
+        self._autoconnect = autoconnect
+        self._initial_autoconnect = None
+
+
+    def __enter__(self):
+        service = self._get_service()
+        if service is None:
+            raise ContextError('Could not obtain a service object.')
+
+        service_properties = service.GetProperties()
+        self._initial_autoconnect = shill_proxy.dbus2primitive(
+            service_properties[
+                shill_proxy.ShillProxy.SERVICE_PROPERTY_AUTOCONNECT])
+        if self._initial_autoconnect != self._autoconnect:
+            logging.info('ServiceAutoConnectContext: change autoconnect to %s',
+                         self._autoconnect)
+            service.SetProperty(
+                shill_proxy.ShillProxy.SERVICE_PROPERTY_AUTOCONNECT,
+                self._autoconnect)
+        else:
+            logging.info('ServiceAutoConnectContext: autoconnect remains %s',
+                         self._initial_autoconnect)
+        return self
+
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self._initial_autoconnect != self._autoconnect:
+            service = self._get_service()
+            if service is None:
+                raise ContextError('Could not obtain a service object.')
+
+            logging.info('ServiceAutoConnectContext: restore autoconnect to %s',
+                         self._initial_autoconnect)
+            service.SetProperty(
+                shill_proxy.ShillProxy.SERVICE_PROPERTY_AUTOCONNECT,
+                self._initial_autoconnect)
+        return False
+
+
+    @property
+    def autoconnect(self):
+        """AutoConnect property value within this context."""
+        return self._autoconnect
+
+
+    @property
+    def initial_autoconnect(self):
+        """Initial AutoConnect property value when entering this context."""
+        return self._initial_autoconnect
