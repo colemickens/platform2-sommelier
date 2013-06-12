53ead41ded2b7943a22f0a5c7768444d830f67fb
Ben Chan <benchan@chromium.org>
2013-06-11 21:14:08 -0700

    Add a UdevDevice and a MockUdevDevice class.
    This CL adds a UdevDevice class, which wraps a udev_device C struct from
    libudev into a C++ object. It also adds a MockUdevDevice class for unit
    testing.
    
    BUG=chromium:248824
    TEST=Build and run unit tests.
    
    Change-Id: I8828473be84680808f6faaa7f9635f83e10a8d23
    Reviewed-on: https://gerrit.chromium.org/gerrit/58339
    Reviewed-by: Darin Petkov <petkov@chromium.org>
    Tested-by: Ben Chan <benchan@chromium.org>
    Commit-Queue: Ben Chan <benchan@chromium.org>

---
+++ mist/udev_device.cc
@@ -0,0 +1,125 @@
+// Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mist/udev_device.h"
+
+#include <libudev.h>
+
+#include <base/logging.h>
+
+namespace mist {
+
+UdevDevice::UdevDevice() : device_(NULL) {}
+
+UdevDevice::UdevDevice(udev_device* device) : device_(device) {
+  CHECK(device_);
+
+  udev_device_ref(device_);
+}
+
+UdevDevice::~UdevDevice() {
+  if (device_) {
+    udev_device_unref(device_);
+    device_ = NULL;
+  }
+}
+
+UdevDevice* UdevDevice::GetParent() const {
+  // udev_device_get_parent does not increase the reference count of the
+  // returned udev_device struct.
+  udev_device* parent_device = udev_device_get_parent(device_);
+  return parent_device ? new UdevDevice(parent_device) : NULL;
+}
+
+UdevDevice* UdevDevice::GetParentWithSubsystemDeviceType(
+    const char* subsystem,
+    const char* device_type) const {
+  // udev_device_get_parent_with_subsystem_devtype does not increase the
+  // reference count of the returned udev_device struct.
+  udev_device* parent_device = udev_device_get_parent_with_subsystem_devtype(
+      device_, subsystem, device_type);
+  return parent_device ? new UdevDevice(parent_device) : NULL;
+}
+
+bool UdevDevice::IsInitialized() const {
+  return udev_device_get_is_initialized(device_);
+}
+
+uint64 UdevDevice::GetMicrosecondsSinceInitialized() const {
+  return udev_device_get_usec_since_initialized(device_);
+}
+
+uint64 UdevDevice::GetSequenceNumber() const {
+  return udev_device_get_seqnum(device_);
+}
+
+const char* UdevDevice::GetDevicePath() const {
+  return udev_device_get_devpath(device_);
+}
+
+const char* UdevDevice::GetDeviceNode() const {
+  return udev_device_get_devnode(device_);
+}
+
+dev_t UdevDevice::GetDeviceNumber() const {
+  return udev_device_get_devnum(device_);
+}
+
+const char* UdevDevice::GetDeviceType() const {
+  return udev_device_get_devtype(device_);
+}
+
+const char* UdevDevice::GetDriver() const {
+  return udev_device_get_driver(device_);
+}
+
+const char* UdevDevice::GetSubsystem() const {
+  return udev_device_get_subsystem(device_);
+}
+
+const char* UdevDevice::GetSysPath() const {
+  return udev_device_get_syspath(device_);
+}
+
+const char* UdevDevice::GetSysName() const {
+  return udev_device_get_sysname(device_);
+}
+
+const char* UdevDevice::GetSysNumber() const {
+  return udev_device_get_sysnum(device_);
+}
+
+const char* UdevDevice::GetAction() const {
+  return udev_device_get_action(device_);
+}
+
+UdevListEntry* UdevDevice::GetDeviceLinksListEntry() const {
+  udev_list_entry* list_entry = udev_device_get_devlinks_list_entry(device_);
+  return list_entry ? new UdevListEntry(list_entry) : NULL;
+}
+
+UdevListEntry* UdevDevice::GetPropertiesListEntry() const {
+  udev_list_entry* list_entry = udev_device_get_properties_list_entry(device_);
+  return list_entry ? new UdevListEntry(list_entry) : NULL;
+}
+
+const char* UdevDevice::GetPropertyValue(const char* key) const {
+  return udev_device_get_property_value(device_, key);
+}
+
+UdevListEntry* UdevDevice::GetTagsListEntry() const {
+  udev_list_entry* list_entry = udev_device_get_tags_list_entry(device_);
+  return list_entry ? new UdevListEntry(list_entry) : NULL;
+}
+
+UdevListEntry* UdevDevice::GetSysAttributeListEntry() const {
+  udev_list_entry* list_entry = udev_device_get_sysattr_list_entry(device_);
+  return list_entry ? new UdevListEntry(list_entry) : NULL;
+}
+
+const char* UdevDevice::GetSysAttributeValue(const char* attribute) const {
+  return udev_device_get_sysattr_value(device_, attribute);
+}
+
+}  // namespace mist
---
+++ mist/udev_device.h
@@ -0,0 +1,118 @@
+// Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MIST_UDEV_DEVICE_H_
+#define MIST_UDEV_DEVICE_H_
+
+#include <sys/types.h>
+
+#include <base/basictypes.h>
+
+#include "mist/udev_list_entry.h"
+
+struct udev_device;
+
+namespace mist {
+
+// A udev device, which wraps a udev_device C struct from libudev and related
+// library functions into a C++ object.
+class UdevDevice {
+ public:
+  // Constructs a UdevDevice object by taking a raw pointer to a udev_device
+  // struct as |device|. The ownership of |device| is not transferred, but its
+  // reference count is increased by one during the lifetime of this object.
+  explicit UdevDevice(udev_device* device);
+
+  // Destructs this UdevDevice object and decreases the reference count of the
+  // underlying udev_device struct by one.
+  virtual ~UdevDevice();
+
+  // Wraps udev_device_get_parent(). The returned UdevDevice object is not
+  // managed and should be deleted by the caller after use.
+  virtual UdevDevice* GetParent() const;
+
+  // Wraps udev_device_get_parent_with_subsystem_devtype(). The returned
+  // UdevDevice object is not managed and should be deleted by the caller after
+  // use.
+  virtual UdevDevice* GetParentWithSubsystemDeviceType(
+      const char* subsystem,
+      const char* device_type) const;
+
+  // Wraps udev_device_get_is_initialized().
+  virtual bool IsInitialized() const;
+
+  // Wraps udev_device_get_usec_since_initialized().
+  virtual uint64 GetMicrosecondsSinceInitialized() const;
+
+  // Wraps udev_device_get_seqnum().
+  virtual uint64 GetSequenceNumber() const;
+
+  // Wraps udev_device_get_devpath().
+  virtual const char* GetDevicePath() const;
+
+  // Wraps udev_device_get_devnode().
+  virtual const char* GetDeviceNode() const;
+
+  // Wraps udev_device_get_devnum().
+  virtual dev_t GetDeviceNumber() const;
+
+  // Wraps udev_device_get_devtype().
+  virtual const char* GetDeviceType() const;
+
+  // Wraps udev_device_get_driver().
+  virtual const char* GetDriver() const;
+
+  // Wraps udev_device_get_subsystem().
+  virtual const char* GetSubsystem() const;
+
+  // Wraps udev_device_get_syspath().
+  virtual const char* GetSysPath() const;
+
+  // Wraps udev_device_get_sysname().
+  virtual const char* GetSysName() const;
+
+  // Wraps udev_device_get_sysnum().
+  virtual const char* GetSysNumber() const;
+
+  // Wraps udev_device_get_action().
+  virtual const char* GetAction() const;
+
+  // Wraps udev_device_get_devlinks_list_entry(). The returned UdevListEntry
+  // object is not managed and should be deleted by the caller after use.
+  virtual UdevListEntry* GetDeviceLinksListEntry() const;
+
+  // Wraps udev_device_get_properties_list_entry(). The returned UdevListEntry
+  // object is not managed and should be deleted by the caller after use.
+  virtual UdevListEntry* GetPropertiesListEntry() const;
+
+  // Wraps udev_device_get_property_value().
+  virtual const char* GetPropertyValue(const char* key) const;
+
+  // Wraps udev_device_get_tags_list_entry(). The returned UdevListEntry
+  // object is not managed and should be deleted by the caller after use.
+  virtual UdevListEntry* GetTagsListEntry() const;
+
+  // Wraps udev_device_get_sysattr_list_entry(). The returned UdevListEntry
+  // object is not managed and should be deleted by the caller after use.
+  virtual UdevListEntry* GetSysAttributeListEntry() const;
+
+  // Wraps udev_device_get_sysattr_value().
+  virtual const char* GetSysAttributeValue(const char* attribute) const;
+
+ private:
+  // Allows MockUdevDevice to invoke the private default constructor below.
+  friend class MockUdevDevice;
+
+  // Constructs a UdevDevice object without referencing a udev_device struct,
+  // which is only allowed to be called by MockUdevDevice.
+  UdevDevice();
+
+  udev_device* device_;
+
+  DISALLOW_COPY_AND_ASSIGN(UdevDevice);
+};
+
+}  // namespace mist
+
+#endif  // MIST_UDEV_DEVICE_H_
