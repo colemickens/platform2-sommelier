3699a36384fb16847cd4ecf7aa9032b5ed94e108
WeiNan-Peter, Wen <wnwen@chromium.org>
2013-08-06 16:40:46 -0400

    Change touch-calibration flag to match firmware change.
    Increasing side bezels' calibration from 0 to 45 pixels to match change
    in firmware. Events from side bezels are filtered by chrome according to
    this flag, thus the two changes should be atomic.
    
    BUG=chromium:263591
    TEST=Tested on dev pixel. Works well with this calibration.
    CQ-DEPEND=CL:*42404
    
    Change-Id: Ie6fc1720a7d78295f741e329d6834d4882637a43
    Reviewed-on: https://gerrit.chromium.org/gerrit/64816
    Reviewed-by: Ryan Harrison <rharrison@chromium.org>
    Reviewed-by: Yufeng Shen <miletus@chromium.org>
    Tested-by: WeiNan-Peter, Wen <wnwen@chromium.org>
    Commit-Queue: WeiNan-Peter, Wen <wnwen@chromium.org>

--- login_manager/session_manager_setup.sh
+++ login_manager/session_manager_setup.sh
@@ -328,7 +328,7 @@ fi
 
 TOUCHUI_FLAGS=
 if use_flag_is_set touchui && is_board link; then
-  TOUCHUI_FLAGS="--touch-calibration=0,0,0,50"
+  TOUCHUI_FLAGS="--touch-calibration=45,45,0,50"
 fi
 
 # Device Manager Server used to fetch the enterprise policy, if applicable.
