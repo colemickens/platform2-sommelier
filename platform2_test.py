8d338f382a74fc26d290d4dae98c88951594db81
Liam McLoughlin <lmcloughlin@chromium.org>
2013-10-26 21:25:38 +0100

    Add ability to filter tests run by package and test name
    Currently this runs test binaries even if we aren't going
    to run tests from them which wastes some time. It will
    prevent us running test binaries at all for packages we
    didn't specify a filter for.
    
    Filters are specified in the P2_TEST_FILTER env variable
    as follows:
    
    P2_TEST_FILTER = "<package>::<gtest_filter> <package>::<gtest_filter> ..."
    
    eg:
    
    P2_TEST_FILTER="chaps::TestSlotManager.*" will only run tests
    in chaps that begin with "TestSlotManager".
    
    BUG=chromium:220003
    TEST=Emerging with P2_TEST_FILTER runs only specified tests
    
    Change-Id: I9f8ad9f5a94976f169a4b45c923855ada1cb7745
    Reviewed-on: https://chromium-review.googlesource.com/174732
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Liam McLoughlin <lmcloughlin@chromium.org>
    Reviewed-by: Liam McLoughlin <lmcloughlin@chromium.org>
    Commit-Queue: Liam McLoughlin <lmcloughlin@chromium.org>

--- platform2_test.py
+++ platform2_test.py
@@ -16,14 +16,17 @@ from platform2 import Platform2
 class Platform2Test(object):
   _BIND_MOUNT_PATHS = ('dev', 'proc', 'sys')
 
-  def __init__(self, bin, board, host, use_flags, framework, run_as_root,
-               gtest_filter):
+  def __init__(self, bin, board, host, use_flags, package, framework,
+               run_as_root, gtest_filter, user_gtest_filter):
     self.bin = bin
     self.board = board
     self.host = host
+    self.package = package
     self.use_flags = use_flags
     self.run_as_root = run_as_root
-    self.gtest_filter = gtest_filter
+    (self.gtest_filter, self.user_gtest_filter) = \
+        self.generateGtestFilter(gtest_filter, user_gtest_filter)
+
 
     self.framework = framework
     if self.framework == 'auto':
@@ -34,7 +37,43 @@ class Platform2Test(object):
     self.qemu_path = os.path.join(p2.get_buildroot(), 'qemu-arm')
     self.lib_dir = os.path.join(p2.get_products_path(), 'lib')
 
-  def remove_sysroot_prefix(self, path):
+  @classmethod
+  def generateGtestSubfilter(cls, gtest_filter):
+    """Split a gtest_filter down into positive and negative filters.
+
+    Args:
+      gtest_filter: A filter string as normally passed to --gtest_filter.
+    Returns:
+      A tuple of format (positive_filters, negative_filters).
+    """
+
+    filters = gtest_filter.split('-', 1)
+    positive_filters = [x for x in filters[0].split(':') if x]
+    if len(filters) > 1:
+      negative_filters = [x for x in filters[1].split(':') if x]
+    else:
+      negative_filters = []
+
+    return (positive_filters, negative_filters)
+
+  @classmethod
+  def generateGtestFilter(cls, filters, user_filters):
+    """Merge internal gtest filters and user-supplied gtest filters.
+
+    Returns:
+      A string that can be passed to --gtest_filter.
+    """
+
+    gtest_filter = cls.generateGtestSubfilter(filters)
+    user_gtest_filter = {}
+
+    pkg_filters = dict([x.split('::') for x in user_filters.split()])
+    for pkg, pkg_filter in pkg_filters.items():
+      user_gtest_filter[pkg] = cls.generateGtestSubfilter(pkg_filter)
+
+    return (gtest_filter, user_gtest_filter)
+
+  def removeSysrootPrefix(self, path):
     """Returns the given path with any sysroot prefix removed."""
 
     if path.startswith(self.sysroot):
@@ -75,12 +114,22 @@ class Platform2Test(object):
   def run(self):
     """Runs the test in a proper environment (e.g. qemu)."""
 
+    if not (self.user_gtest_filter and self.package in self.user_gtest_filter):
+      return
+    else:
+      positive_filters = self.gtest_filter[0] + \
+                         self.user_gtest_filter[self.package][0]
+      negative_filters = self.gtest_filter[1] + \
+                         self.user_gtest_filter[self.package][1]
+      filters = (':'.join(positive_filters), ':'.join(negative_filters))
+      gtest_filter = '%s-%s' % filters
+
     cmd = []
     env = {}
 
     if self.framework == 'qemu':
-      self.lib_dir = self.remove_sysroot_prefix(self.lib_dir)
-      self.bin = self.remove_sysroot_prefix(self.bin)
+      self.lib_dir = self.removeSysrootPrefix(self.lib_dir)
+      self.bin = self.removeSysrootPrefix(self.bin)
 
     # TODO(lmcloughlin): This code is fundamentally "broken" in the non-QEMU
     # case: it uses the SDK ldso, not the board ldso.
@@ -89,7 +138,7 @@ class Platform2Test(object):
     ld_paths += glob.glob(self.sysroot + '/usr/lib*/')
 
     if self.framework == 'qemu':
-      ld_paths = [self.remove_sysroot_prefix(path) for path in ld_paths]
+      ld_paths = [self.removeSysrootPrefix(path) for path in ld_paths]
 
     env['LD_LIBRARY_PATH'] = ':'.join(ld_paths)
 
@@ -101,13 +150,13 @@ class Platform2Test(object):
     if self.framework == 'qemu':
       cmd.append('chroot')
       cmd.append(self.sysroot)
-      cmd.append(self.remove_sysroot_prefix(self.qemu_path))
+      cmd.append(self.removeSysrootPrefix(self.qemu_path))
       cmd.append('-drop-ld-preload')
 
     cmd.append(self.bin)
 
     if len(self.gtest_filter) > 0:
-      cmd.append('--gtest_filter=' + self.gtest_filter)
+      cmd.append('--gtest_filter=' + gtest_filter)
 
     try:
       subprocess.check_call(cmd, env=env)
@@ -137,11 +186,16 @@ def main(argv):
                       help='args to pass to gtest/test binary')
   parser.add_argument('--host', action='store_true',
                       help='specify that we\'re testing for the host')
+  parser.add_argument('--package', required=True,
+                      help='name of the package we\'re running tests for')
   parser.add_argument('--run_as_root', action='store_true',
                       help='should the test be run as root')
   parser.add_argument('--use_flags', default=set(),
                       action=_ParseStringSetAction,
                       help='USE flags to enable')
+  parser.add_argument('--user_gtest_filter',
+                      default='',
+                      help=argparse.SUPPRESS)
 
   options = parser.parse_args(argv)
 
@@ -153,8 +207,9 @@ def main(argv):
     raise AssertionError('You must provide only one of --board or --host')
 
   p2test = Platform2Test(options.bin, options.board, options.host,
-                         options.use_flags, options.framework,
-                         options.run_as_root, options.gtest_filter)
+                         options.use_flags, options.package, options.framework,
+                         options.run_as_root, options.gtest_filter,
+                         options.user_gtest_filter)
   getattr(p2test, options.action)()
 
 
