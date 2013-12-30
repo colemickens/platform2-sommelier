0c27d01d2af98b65ba07736b4716bfd98bd5ed69
Vic (Chun-Ju) Yang <victoryang@chromium.org>
2013-12-30 14:36:34 +0800

    Remove use of /root/.factory_test
    We are deprecating the tag file /root/.factory_test. Instead of checking
    for this tag, let's check for the following two conditions to determine
    if we are running factory tests:
      - 'crossystem debug_build' is 1
      - /usr/local/factory exists
    
    BUG=chrome-os-partner:24798
    TEST=Build a factory test image without .factory_test tag. Boot and
    check mount-encrypted is using factory mode.
    CQ-DEPEND=CL:181430
    
    Change-Id: Ib2045cb03e3979f3d726b4ca051fd315a8039ab9
    Signed-off-by: Vic (Chun-Ju) Yang <victoryang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/181216
    Reviewed-by: Richard Barnette <jrbarnette@chromium.org>

---
+++ init/factory_utils.sh
@@ -0,0 +1,16 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+is_factory_mode() {
+  # The path to factory enabled tag. If this path exists in a debug build,
+  # we assume factory test mode.
+  FACTORY_TAG_PATH="/mnt/stateful_partition/dev_image/factory/enabled"
+
+  # Factory mode can be either:
+  # 1. We are running factory tests if this is a debug build and factory
+  #    enabled tag exists.
+  # 2. We are running a factory installer.
+  (crossystem "debug_build?1" && [ -f "$FACTORY_TAG_PATH" ]) ||
+      [ -f /root/.factory_installer ]
+}
