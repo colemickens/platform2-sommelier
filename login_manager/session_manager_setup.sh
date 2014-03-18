2958eefffef78ece0a093019652c7934cdcbefdd
Jorge Lucangeli Obes <jorgelo@chromium.org>
2014-03-18 11:49:44 -0700

    Make GPU sandbox failures fatal.
    BUG=chromium:327824
    TEST=Build, boot locally; lumpy, link, leon, daisy trybots.
    CQ-DEPEND=CL:190792
    
    Change-Id: Ia80e77079777a900994bd34680c6268d42422578
    Reviewed-on: https://chromium-review.googlesource.com/190514
    Tested-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- login_manager/session_manager_setup.sh
+++ login_manager/session_manager_setup.sh
@@ -382,7 +382,7 @@ elif [ -f ${HANG_DETECTION_FLAG_FILE} ]; then
   HANG_DETECTION_FLAG="--enable-hang-detection=5"  # And do it FASTER!
 fi
 
-GPU_FLAGS="--gpu-sandbox-failures-fatal=no"
+GPU_FLAGS="--gpu-sandbox-failures-fatal=yes"
 if use_flag_is_set gpu_sandbox_allow_sysv_shm; then
   GPU_FLAGS="$GPU_FLAGS --gpu-sandbox-allow-sysv-shm"
 fi
