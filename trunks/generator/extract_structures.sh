9262f245cd4f7313c138ed8aa996b980c089aa0b
Darren Krahn <dkrahn@chromium.org>
2014-05-14 17:19:16 -0700

    Added extraction of attribute types and preprocessor defines.
    BUG=chromium:371999
    TEST=-manual inspection of output
         -works with a parser
    
    Change-Id: I8a19c39d25c2778fa6385a73fe6eb2e6debd8fa8
    Reviewed-on: https://chromium-review.googlesource.com/199929
    Reviewed-by: Luigi Semenzato <semenzato@chromium.org>
    Commit-Queue: Darren Krahn <dkrahn@chromium.org>
    Tested-by: Darren Krahn <dkrahn@chromium.org>

--- trunks/generator/extract_structures.sh
+++ trunks/generator/extract_structures.sh
@@ -7,9 +7,10 @@
 cat $1 |
   # Mark tables and section boundaries.
   sed 's/^[0-9][0-9]*\([.][0-9][0-9]*\)\+.*$/_SECTION_BOUNDARY/' |
-  sed 's/^Table [0-9]* . Definition of \(.*\) Types[^.]*$/_TYPES \1/' |
+  sed 's/^Table [0-9]* . Definition of .*Types[^.]*$/_TYPES/' |
   # Keep only table sections.
-  awk '/^_TYPES .*$/,/^_SECTION_BOUNDARY$/ { print $0; }' |
+  awk '/^_TYPES$/,/^_SECTION_BOUNDARY$/ { print $0; }' |
+  sed 's/^_TYPES$//' |
   sed 's/^_SECTION_BOUNDARY$//' |
   # Remove headers and footers.
   sed 's/^.*Trusted Platform Module Library.*$//' |
@@ -57,6 +58,32 @@ cat $1 |
     /^Table [0-9]* . Definition of \{.*\} \([A-Z_]*\) TPMI_.* Type[^.]*$/ {
       print "_OLD_TYPE " $7;
       print "_NEW_TYPE " $8; }
+    /^Table [0-9]* . Definition of \([A-Z_]*\) \{.*\} TPMI_.* Type[^.]*$/ {
+      print "_OLD_TYPE " $6;
+      print "_NEW_TYPE " $8; }
+    END { print "_END"; }
+  ' |
+  # Sanity check.  The format should now follow this grammar:
+  # Format:Begin||Typedef*||End
+  # Begin:_BEGIN_TYPES\n
+  # End:_END\n
+  # Typedef:OldType||NewType
+  # OldType:_OLD_TYPE <type>\n
+  # NewType:_NEW_TYPE <type>\n
+  awk '
+    BEGIN { RS = ""; }
+    $0 !~ /_BEGIN_TYPES\n(_OLD_TYPE[^\n]*\n_NEW_TYPE[^\n]*\n)*_END/ {
+      print "_ERROR: Format check failed."; }
+    { print $0; }
+  '
+
+# Pull out attribute types.
+cat $1 |
+  awk '
+    BEGIN { print "_BEGIN_TYPES"; FS = "[ ()]+"; }
+    /^Table [0-9]* . Definition of \([A-Z_0-9]*\) TPM[A-Z_]* Bits[^.]*$/ {
+      print "_OLD_TYPE " $6;
+      print "_NEW_TYPE " $7; }
     END { print "_END"; }
   ' |
   # Sanity check.  The format should now follow this grammar:
@@ -78,7 +105,6 @@ cat $1 |
   # Mark tables and section boundaries.
   sed 's/^[0-9][0-9]*\([.][0-9][0-9]*\)\+.*$/_SECTION_BOUNDARY/' |
   sed 's/^Table [0-9]* . Definition of \(.*\) Constants[^.]*$/_CONSTANTS \1/' |
-  sed 's/^_CONSTANTS \(.*TPM_CAP.*\)$/\1/' |
   # Keep only table sections.
   awk '/^_CONSTANTS .*$/,/^_SECTION_BOUNDARY$/ { print $0; }' |
   sed 's/^_SECTION_BOUNDARY$//' |
@@ -282,6 +308,70 @@ cat $1 |
   awk '
     BEGIN { RS = ""; }
     $0 !~ /_BEGIN_UNIONS\n(_UNION[^\n]*\n(_TYPE[^\n]*\n_NAME[^\n]*\n)*)*_END/ {
+      print "_ERROR: Format check failed."; }
+    { print $0; }
+  '
+
+# Pull out default defines.
+cat $1 |
+  sed 's/^[A-Z]\([.][0-9][0-9]*\)\+.*$/_SECTION_BOUNDARY/' |
+  sed 's/^Table [0-9]* . Defines for \([^.]*\)$/_DEFINES \1/' |
+  sed 's/^_DEFINES Implemented Commands$//' |
+  sed 's/^_DEFINES Implemented Algorithms$//' |
+  awk '/^_DEFINES .*$/,/^_SECTION_BOUNDARY$/ { print $0; }' |
+  sed 's/^_SECTION_BOUNDARY$//' |
+  # Remove headers and footers.
+  sed 's/^.*Trusted Platform Module Library.*$//' |
+  sed 's/^.*Part 2: Structures.*$//' |
+  sed 's/^.*Family .2.0..*$//' |
+  sed 's/^.*Level 00 Revision.*$//' |
+  sed 's/^.*Published.*$//' |
+  sed 's/^.*Copyright.*$//' |
+  sed 's/^.*Page [0-9].*$//' |
+  sed 's/^.*October 31, 2013.*$//' |
+  # Remove table headers.
+  sed 's/^Name$//' | sed 's/^Value$//' | sed 's/^Description$//' |
+  # Remove most comments.
+  sed 's/^.*[g-wyz?]\+.*$//' |
+  # Remove leading spaces.
+  sed 's/^[ ][ ]*//' |
+  # Remove empty lines.
+  sed '/^$/d' |
+  # Mark begin and end.
+  awk '
+    BEGIN { print "_BEGIN_DEFINES"; }
+    { print $0; }
+    END { print "_END"; }
+  ' |
+  # Mark define names.
+  sed 's/^\([A-Z][_A-Z0-9]*\)$/_NAME \1/' |
+  sed 's/^_NAME NOTE$//' |
+  # Mark values and throw away everything else.
+  awk '
+    BEGIN { last_line_was_name = 0; }
+    /^_.*$/      { if ($0 !~ /^_NAME .*$/) { print $0; } }
+    /^_NAME .*$/ { if (last_line_was_name) {
+                     last_line_was_name = 0;
+                     if ($0 !~ /[A-Z_0-9x +()]*/) {
+                       print "_ERROR: Invalid value"; }
+                     print "_VALUE " $2; }
+                   else { last_line_was_name = 1; print $0; } }
+    /^[^_].*$/   { if (last_line_was_name) {
+                     last_line_was_name = 0;
+                     if ($0 !~ /[A-Z_0-9x +()]*/) {
+                       print "_ERROR: Invalid value"; }
+                     print "_VALUE " $0; } }
+  ' |
+  # Sanity check.  The format should now follow this grammar:
+  # Format:Begin||Define*||End
+  # Begin:_BEGIN_DEFINES\n
+  # End:_END\n
+  # Define:Name||Value
+  # Name:_NAME <name>\n
+  # Value:_VALUE <value>\n
+  awk '
+    BEGIN { RS = ""; }
+    $0 !~ /_BEGIN_DEFINES\n(_NAME[^\n]*\n_VALUE[^\n]*\n)*_END/ {
       print "_ERROR: Format check failed."; }
     { print $0; }
   '
