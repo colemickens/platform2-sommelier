9b66e2d09675aedb850922c428ff65e8f1192f7f
Paul Stewart <pstew@chromium.org>
2014-06-02 11:53:36 -0700

    shill: PropertyChangeNotifier: Initial commit
    Add a PropertyObserver, whose task is to track changes
    to the returned value of an accessor.  Further, create
    a ServicePropertyChangeNotifier, whose task is to monitor
    a collection of PropertyObservers for a Service and call
    the appropriate Emit*Property method for those properties
    which have changed.
    
    BUG=chromium:379948
    TEST=Unit tests
    Change-Id: I450b74b388f8ecb44010f277cc77149412b24950
    Reviewed-on: https://chromium-review.googlesource.com/202441
    Reviewed-by: mukesh agrawal <quiche@chromium.org>
    Commit-Queue: Paul Stewart <pstew@chromium.org>
    Tested-by: Paul Stewart <pstew@chromium.org>

---
+++ shill/property_observer.h
@@ -0,0 +1,61 @@
+// Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SHILL_PROPERTY_OBSERVER_
+#define SHILL_PROPERTY_OBSERVER_
+
+#include <base/basictypes.h>
+#include <base/callback.h>
+#include <tr1/memory>
+
+#include "shill/accessor_interface.h"
+#include "shill/error.h"
+#include "shill/property_observer_interface.h"
+
+namespace shill {
+
+// A templated object that retains a reference to a typed accessor,
+// and a saved value retrieved from the accessor.  When the update
+// method is called, it compares its saved value to the current
+// value returned by the accessor.  If the value has changed, it
+// calls the supplied callback and updates the saved value.
+template <class T>
+class PropertyObserver : public PropertyObserverInterface {
+ public:
+  typedef base::Callback<void(const T &new_value)> Callback;
+
+  PropertyObserver(std::tr1::shared_ptr<AccessorInterface<T>> accessor,
+                   Callback callback)
+      : accessor_(accessor), callback_(callback) {
+    Error unused_error;
+    saved_value_ = accessor_->Get(&unused_error);
+  }
+  virtual ~PropertyObserver() {}
+
+  // Implements PropertyObserverInterface.  Compares the saved value with
+  // what the Get() method of |accessor_| returns.  If the value has changed
+  // |callback_| is invoked and |saved_value_| is updated.
+  virtual void Update() override {
+    Error error;
+    T new_value_ = accessor_->Get(&error);
+    if (!error.IsSuccess() || saved_value_ == new_value_) {
+      return;
+    }
+    callback_.Run(new_value_);
+    saved_value_ = new_value_;
+  }
+
+ private:
+  friend class PropertyObserverTest;
+
+  std::tr1::shared_ptr<AccessorInterface<T>> accessor_;
+  Callback callback_;
+  T saved_value_;
+
+  DISALLOW_COPY_AND_ASSIGN(PropertyObserver);
+};
+
+}  // namespace shill
+
+#endif  // SHILL_PROPERTY_OBSERVER_
---
+++ shill/service_property_change_notifier.h
@@ -0,0 +1,68 @@
+// Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SHILL_PROPERTY_CHANGE_NOTIFIER_
+#define SHILL_PROPERTY_CHANGE_NOTIFIER_
+
+#include <string>
+#include <vector>
+
+#include <base/basictypes.h>
+#include <base/callback.h>
+#include <base/memory/scoped_ptr.h>
+
+#include "shill/accessor_interface.h"
+
+namespace shill {
+
+class PropertyObserverInterface;
+class ServiceAdaptorInterface;
+
+// A collection of property observers used by objects to deliver
+// property change notifications.  This object holds an un-owned
+// pointer to the ServiceAdaptor to which notifications should be
+// posted.  This pointer must be valid for the lifetime of this
+// property change notifier.
+class ServicePropertyChangeNotifier {
+ public:
+  ServicePropertyChangeNotifier(ServiceAdaptorInterface *adaptor);
+  virtual ~ServicePropertyChangeNotifier();
+
+  virtual void AddBoolPropertyObserver(const std::string &name,
+                                       BoolAccessor accessor);
+  virtual void AddUint8PropertyObserver(const std::string &name,
+                                        Uint8Accessor accessor);
+  virtual void AddUint16PropertyObserver(const std::string &name,
+                                         Uint16Accessor accessor);
+  virtual void AddUint16sPropertyObserver(const std::string &name,
+                                          Uint16sAccessor accessor);
+  virtual void AddUintPropertyObserver(const std::string &name,
+                                       Uint32Accessor accessor);
+  virtual void AddIntPropertyObserver(const std::string &name,
+                                      Int32Accessor accessor);
+  virtual void AddRpcIdentifierPropertyObserver(const std::string &name,
+                                                RpcIdentifierAccessor accessor);
+  virtual void AddStringPropertyObserver(const std::string &name,
+                                         StringAccessor accessor);
+  virtual void AddStringmapPropertyObserver(const std::string &name,
+                                            StringmapAccessor accessor);
+  virtual void UpdatePropertyObservers();
+
+ private:
+  // Redirects templated calls to a value reference to a by-copy version.
+  void BoolPropertyUpdater(const std::string &name, const bool &value);
+  void Uint8PropertyUpdater(const std::string &name, const uint8 &value);
+  void Uint16PropertyUpdater(const std::string &name, const uint16 &value);
+  void Uint32PropertyUpdater(const std::string &name, const uint32 &value);
+  void Int32PropertyUpdater(const std::string &name, const int32 &value);
+
+  ServiceAdaptorInterface *rpc_adaptor_;
+  std::vector<scoped_ptr<PropertyObserverInterface>> property_observers_;
+
+  DISALLOW_COPY_AND_ASSIGN(ServicePropertyChangeNotifier);
+};
+
+}  // namespace shill
+
+#endif  // SHILL_PROPERTY_CHANGE_NOTIFIER_
