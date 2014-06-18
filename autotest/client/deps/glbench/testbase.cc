cdd711bb6fde86f77c39dbb59971ddca772f0a71
Daniel Kurtz <djkurtz@chromium.org>
2014-06-18 12:44:26 +0800

    glbench: remove unit from .png file names
    Prepending the benchmark unit to pixel readback file name was serving
    no other purpose than to match the "testname" which also used to have
    the unit prepended.
    
    This change looks scarier than it is... it is mostly just file names
    change, plus the corresponding .html and .txt imagename indexes, some
    documentation, and a one line change to testbase.cc to stop prepending
    unit_ to the generated image names.
    
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    
    BUG=none
    TEST=test_that graphics_GLBench
    
    Change-Id: I2e0810a4bc5c4b9dcd69413f05f5fa3d4e27eb58
    Reviewed-on: https://chromium-review.googlesource.com/202888
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Commit-Queue: Daniel Kurtz <djkurtz@chromium.org>

--- autotest/client/deps/glbench/testbase.cc
+++ autotest/client/deps/glbench/testbase.cc
@@ -145,7 +145,7 @@ void RunTest(TestBase* test, const char* testname, const double coefficient,
           "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
           d[ 0],d[ 1],d[ 2],d[ 3],d[ 4],d[ 5],d[ 6],d[ 7],
           d[ 8],d[ 9],d[10],d[11],d[12],d[13],d[14],d[15]);
-        sprintf(name_png, "%s_%s.pixmd5-%s.png", test->Unit(), testname, pixmd5);
+        sprintf(name_png, "%s.pixmd5-%s.png", testname, pixmd5);
 
         if (FLAGS_save)
           SaveImage(name_png, width, height);
