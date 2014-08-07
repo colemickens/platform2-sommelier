da99179c9f3c6a1795234e5a98fbbd501c6a7bef
Chris Masone <cmasone@chromium.org>
2014-08-07 13:18:24 -0700

    login: Revert "Add logging to help diagnose race in login_OwnershipTaken"
    This reverts commit 813d151d4a204fc90d369d273d371cf96e7130b4.
    
    Since http://crbug.com/355664 is fixed, we no longer need this
    extra logging.
    
    BUG=None
    TEST=compile, unittest
    
    Change-Id: Ie5182c875d53ab12eb71173465f8f8f667eaa23f
    Reviewed-on: https://chromium-review.googlesource.com/212335
    Tested-by: Chris Masone <cmasone@chromium.org>
    Reviewed-by: Ben Chan <benchan@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>

--- login_manager/device_policy_service.cc
+++ login_manager/device_policy_service.cc
@@ -329,18 +329,12 @@ bool DevicePolicyService::StoreOwnerProperties(const std::string& current_user,
   CHECK(signing_key);
   const em::PolicyFetchResponse& policy(store()->Get());
   em::PolicyData poldata;
-  if (policy.has_policy_data()) {
+  if (policy.has_policy_data())
     poldata.ParseFromString(policy.policy_data());
-    // TODO(cmasone): remove once http://crbug.com/355664 is fixed.
-    LOG(INFO) << "Loading existing policy.";
-  }
   em::ChromeDeviceSettingsProto polval;
   if (poldata.has_policy_type() && poldata.policy_type() == kDevicePolicyType) {
-    if (poldata.has_policy_value()) {
+    if (poldata.has_policy_value())
       polval.ParseFromString(poldata.policy_value());
-      // TODO(cmasone): remove once http://crbug.com/355664 is fixed.
-      LOG(INFO) << "Loading existing settings from policy.";
-    }
   } else {
     poldata.set_policy_type(kDevicePolicyType);
   }
@@ -358,19 +352,9 @@ bool DevicePolicyService::StoreOwnerProperties(const std::string& current_user,
       break;
     }
   }
-
   if (poldata.has_username() && poldata.username() == current_user &&
       on_list &&
       key()->Equals(policy.new_public_key())) {
-    // TODO(cmasone): remove once http://crbug.com/355664 is fixed.
-    LOG(INFO) << "Leaving settings unchanged; user is owner and on whitelist.";
-    if (!polval.has_allow_new_users()) {
-      LOG(INFO) << "No allow_new_users setting!";
-    } else {
-      LOG(INFO) << "Allow new users is "
-                << polval.allow_new_users().allow_new_users();
-    }
-
     return true;  // No changes are needed.
   }
   if (!on_list) {
@@ -381,16 +365,6 @@ bool DevicePolicyService::StoreOwnerProperties(const std::string& current_user,
       polval.mutable_allow_new_users()->set_allow_new_users(true);
   }
   poldata.set_username(current_user);
-
-  // TODO(cmasone): remove once http://crbug.com/355664 is fixed.
-  LOG(INFO) << "Settings updated. User " << (on_list ? "was" : "wasn't")
-            << " on the whitelist.";
-  if (!polval.has_allow_new_users()) {
-    LOG(INFO) << "No allow_new_users setting!";
-  } else {
-    LOG(INFO) << "Allow new users is "
-              << polval.allow_new_users().allow_new_users();
-  }
 
   // We have now updated the whitelist and owner setting in |polval|.
   // We need to put it into |poldata|, serialize that, sign it, and
