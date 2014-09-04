9acc777c5428cb41c481d0368dedad4826af31de
Garret Kelly <gdk@chromium.org>
2014-09-04 14:08:24 -0400

    feedback: Remove test relying on external service.
    This test was pointed at a sandbox instance which was not guaranteed to
    exist or be configured properly.  Remove it to prevent further
    breakages.
    
    TEST=`FEATURES=test emerge-panther feedback`
    BUG=none
    
    Change-Id: I1703a628ca8f4d33fec029b3aa64bb46937704bb
    Reviewed-on: https://chromium-review.googlesource.com/216333
    Reviewed-by: Aaron Kemp <kemp@google.com>
    Reviewed-by: Gaurav Shah <gauravsh@chromium.org>
    Commit-Queue: Garret Kelly <gdk@chromium.org>
    Tested-by: Garret Kelly <gdk@chromium.org>

--- feedback/feedback_service_unittest.cc
+++ feedback/feedback_service_unittest.cc
@@ -13,11 +13,11 @@
 #include <base/guid.h>
 #include <base/strings/stringprintf.h>
 #include <base/message_loop/message_loop.h>
-#include "bindings/extension.pb.h"
 #include "chromeos/dbus/service_constants.h"
 #include "components/feedback/feedback_common.h"
 #include "components/feedback/feedback_report.h"
 #include "components/feedback/feedback_uploader.h"
+#include "components/feedback/proto/extension.pb.h"
 #include "feedback/feedback_service.h"
 #include "feedback/feedback_uploader_curl.h"
 #include "gmock/gmock.h"
@@ -32,19 +32,6 @@ static const char kPoolName[] = "FeedbackWorkerPool";
 static const base::FilePath::CharType kFeedbackReportPath[] =
     FILE_PATH_LITERAL("Feedback Reports");
 static const int kTestProductId = 84;
-static const char kTestFeedbackUrl[] =
-    "http://sandbox.google.com/tools/feedback/chrome/__submit";
-static const char kTestEmail[] = "nobody@example.com";
-static const char kTestReportUrl[] = "http://www.example.com";
-static const char kTestReportDescription[] = "Test upload, unittests ";
-static const char kTestReportCategory[] = "Testing";
-
-// Generates a random report description.
-std::string GetTestReportDescription() {
-  std::string s = kTestReportDescription;
-  s.append(base::GenerateGUID());
-  return s;
-}
 
 }  // namespace
 
@@ -167,27 +154,6 @@ TEST_F(FeedbackServiceTest, UploadFailure) {
   EXPECT_EQ(uploader.GetFirstReport()->data(), data);
 }
 
-TEST_F(FeedbackServiceTest, FullUploadTest) {
-  FeedbackUploaderCurl uploader(temp_dir_.path(), pool_.get(),
-                                kTestFeedbackUrl);
-  userfeedback::ExtensionSubmit report;
-
-  scoped_refptr<FeedbackCommon> feedback = new FeedbackCommon();
-  feedback->set_description(GetTestReportDescription());
-  feedback->set_product_id(kTestProductId);
-  feedback->set_category_tag(kTestReportCategory);
-  feedback->set_user_email(kTestEmail);
-  feedback->set_page_url(kTestReportUrl);
-  feedback->PrepareReport(&report);
-
-  scoped_refptr<FeedbackService> svc = new FeedbackService(&uploader);
-
-  svc->SendFeedback(report,
-      base::Bind(&FeedbackServiceTest::CallbackFeedbackResult, true));
-
-  // Verify that this isn't on the queue.
-  EXPECT_TRUE(uploader.QueueEmpty());
-}
 }  // namespace feedback
 
 int main(int argc, char** argv) {
