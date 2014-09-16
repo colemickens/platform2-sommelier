874a352d52c5658512ad051344d444091ea2eb4d
Samuel Tan <samueltan@chromium.org>
2014-09-15 17:13:40 -0700

    shill: add timeout support for ResultAggregator
    Add a new ResultAggregator constructor, which takes a
    callback, event dispatcher, and timeout duration as
    parameters. If the operation does not complete (i.e.
    ResultAggregator is not destructed) within the specified
    time duration, ReusltAggregator will invoke the callback
    with the error kOperationTimeout.
    
    BUG=chromium:414524
    TEST='P2_TEST_FILTER="shill::* FEATUERS="test" USE="clang asan"
    emerge-squawks platform2' succeeds.
    
    Change-Id: I057cd7c1b4864283507952d2ef6a18255fcda32e
    Reviewed-on: https://chromium-review.googlesource.com/218328
    Reviewed-by: Samuel Tan <samueltan@chromium.org>
    Tested-by: Samuel Tan <samueltan@chromium.org>
    Commit-Queue: Samuel Tan <samueltan@chromium.org>

--- shill/result_aggregator.h
+++ shill/result_aggregator.h
@@ -5,6 +5,7 @@
 #ifndef SHILL_RESULT_AGGREGATOR_H_
 #define SHILL_RESULT_AGGREGATOR_H_
 
+#include <base/cancelable_callback.h>
 #include <base/macros.h>
 #include <base/memory/ref_counted.h>
 
@@ -13,30 +14,45 @@
 
 namespace shill {
 
+class EventDispatcher;
+
 // The ResultAggregator is used to aggregate the result of multiple
 // asynchronous operations. To use: construct a ResultAggregator, and
-// Bind its ReportResult methods to some Callbacks.
+// Bind its ReportResult methods to some Callbacks. The ResultAggregator
+// can also be constructed with an EventDispatcher pointer and timeout delay if
+// we want to wait for a limited period of time for asynchronous operations
+// to complete.
 //
 // When the Callbacks are destroyed, they will drop their references
 // to the ResultAggregator. When all references to the
-// ResultAggregator are destroyed, the ResultAggregator will invoke
-// the |callback| with which ResultAggregator was constructed. However,
-// if no Callbacks invoked ReportResult, then the original |callback|
-// will not be invoked.
+// ResultAggregator are destroyed, or if a timeout occurs, the ResultAggregator
+// will invoke the |callback| with which ResultAggregator was constructed.
+// |callback| will only be invoked exactly once by whichever of these two
+// events occurs first. However, if no callbacks invoked ReportResult, then the
+// original |callback| will not be invoked.
 //
 // |callback| will see Error type of Success if all Callbacks reported
-// Success to ResultAggregator. Otherwise, |callback| will see the first
-// of the Errors reported to ResultAggregator.
+// Success to ResultAggregator. If the timeout occurs, |callback| will see Error
+// type of OperationTimeout. Otherwise, |callback| will see the first of the
+// Errors reported to ResultAggregator.
 class ResultAggregator : public base::RefCounted<ResultAggregator> {
  public:
   explicit ResultAggregator(const ResultCallback &callback);
+  ResultAggregator(const ResultCallback &callback, EventDispatcher *dispatcher,
+                   int timeout_milliseconds);
   virtual ~ResultAggregator();
 
   void ReportResult(const Error &error);
 
  private:
+  // Callback for timeout registered with EventDispatcher.
+  void Timeout();
+
+  base::WeakPtrFactory<ResultAggregator> weak_ptr_factory_;
   const ResultCallback callback_;
+  base::CancelableClosure timeout_callback_;
   bool got_result_;
+  bool timed_out_;
   Error error_;
 
   DISALLOW_COPY_AND_ASSIGN(ResultAggregator);
--- shill/result_aggregator_unittest.cc
+++ shill/result_aggregator_unittest.cc
@@ -9,10 +9,20 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include "shill/mock_event_dispatcher.h"
+
 namespace shill {
 
+using testing::StrictMock;
+using testing::_;
 using base::Bind;
 using base::Unretained;
+
+namespace {
+
+const int kTimeoutMilliseconds = 0;
+
+}  // namespace
 
 class ResultAggregatorTest : public ::testing::Test {
  public:
@@ -25,9 +35,34 @@ class ResultAggregatorTest : public ::testing::Test {
     aggregator_ = NULL;  // Ensure ReportResult is invoked before our dtor.
   }
 
- protected:
   MOCK_METHOD1(ReportResult, void(const Error &));
+
+ protected:
   scoped_refptr<ResultAggregator> aggregator_;
+};
+
+class ResultAggregatorTestWithDispatcher : public ResultAggregatorTest {
+ public:
+  ResultAggregatorTestWithDispatcher() : ResultAggregatorTest() {}
+  virtual ~ResultAggregatorTestWithDispatcher() {}
+
+  void InitializeResultAggregatorWithTimeout() {
+    aggregator_ = new ResultAggregator(
+        Bind(&ResultAggregatorTest::ReportResult, Unretained(this)),
+        &dispatcher_, kTimeoutMilliseconds);
+  }
+
+ protected:
+  EventDispatcher dispatcher_;
+};
+
+class ResultAggregatorTestWithMockDispatcher : public ResultAggregatorTest {
+ public:
+  ResultAggregatorTestWithMockDispatcher() : ResultAggregatorTest() {}
+  virtual ~ResultAggregatorTestWithMockDispatcher() {}
+
+ protected:
+  StrictMock<MockEventDispatcher> dispatcher_;
 };
 
 class ResultGenerator {
@@ -45,15 +80,13 @@ class ResultGenerator {
   DISALLOW_COPY_AND_ASSIGN(ResultGenerator);
 };
 
-MATCHER_P(ErrorType, type, "") {
-  return arg.type() == type;
-}
+MATCHER_P(ErrorType, type, "") { return arg.type() == type; }
 
-TEST_F(ResultAggregatorTest, Unused) {
+TEST_F(ResultAggregatorTestWithMockDispatcher, Unused) {
   EXPECT_CALL(*this, ReportResult(ErrorType(Error::kSuccess))).Times(0);
 }
 
-TEST_F(ResultAggregatorTest, BothSucceed) {
+TEST_F(ResultAggregatorTestWithMockDispatcher, BothSucceed) {
   EXPECT_CALL(*this, ReportResult(ErrorType(Error::kSuccess)));
   ResultGenerator first_generator(aggregator_);
   ResultGenerator second_generator(aggregator_);
@@ -61,7 +94,7 @@ TEST_F(ResultAggregatorTest, BothSucceed) {
   second_generator.GenerateResult(Error::kSuccess);
 }
 
-TEST_F(ResultAggregatorTest, FirstFails) {
+TEST_F(ResultAggregatorTestWithMockDispatcher, FirstFails) {
   EXPECT_CALL(*this, ReportResult(ErrorType(Error::kOperationTimeout)));
   ResultGenerator first_generator(aggregator_);
   ResultGenerator second_generator(aggregator_);
@@ -69,7 +102,7 @@ TEST_F(ResultAggregatorTest, FirstFails) {
   second_generator.GenerateResult(Error::kSuccess);
 }
 
-TEST_F(ResultAggregatorTest, SecondFails) {
+TEST_F(ResultAggregatorTestWithMockDispatcher, SecondFails) {
   EXPECT_CALL(*this, ReportResult(ErrorType(Error::kOperationTimeout)));
   ResultGenerator first_generator(aggregator_);
   ResultGenerator second_generator(aggregator_);
@@ -77,12 +110,59 @@ TEST_F(ResultAggregatorTest, SecondFails) {
   second_generator.GenerateResult(Error::kOperationTimeout);
 }
 
-TEST_F(ResultAggregatorTest, BothFail) {
+TEST_F(ResultAggregatorTestWithMockDispatcher, BothFail) {
   EXPECT_CALL(*this, ReportResult(ErrorType(Error::kOperationTimeout)));
   ResultGenerator first_generator(aggregator_);
   ResultGenerator second_generator(aggregator_);
   first_generator.GenerateResult(Error::kOperationTimeout);
   second_generator.GenerateResult(Error::kPermissionDenied);
+}
+
+TEST_F(ResultAggregatorTestWithMockDispatcher,
+       TimeoutCallbackPostedOnConstruction) {
+  EXPECT_CALL(dispatcher_, PostDelayedTask(_, kTimeoutMilliseconds));
+  auto result_aggregator = make_scoped_refptr(new ResultAggregator(
+      Bind(&ResultAggregatorTest::ReportResult, Unretained(this)), &dispatcher_,
+      kTimeoutMilliseconds));
+}
+
+TEST_F(ResultAggregatorTestWithDispatcher,
+       TimeoutReceivedWithoutAnyResultsReceived) {
+  InitializeResultAggregatorWithTimeout();
+  EXPECT_CALL(*this, ReportResult(ErrorType(Error::kOperationTimeout)));
+  ResultGenerator generator(aggregator_);
+  dispatcher_.DispatchPendingEvents();  // Invoke timeout callback.
+}
+
+TEST_F(ResultAggregatorTestWithDispatcher, TimeoutAndOtherResultReceived) {
+  // Timeout should override any other error results.
+  InitializeResultAggregatorWithTimeout();
+  EXPECT_CALL(*this, ReportResult(ErrorType(Error::kOperationTimeout)));
+  ResultGenerator first_generator(aggregator_);
+  ResultGenerator second_generator(aggregator_);
+  first_generator.GenerateResult(Error::kSuccess);
+  dispatcher_.DispatchPendingEvents();  // Invoke timeout callback.
+  second_generator.GenerateResult(Error::kPermissionDenied);
+}
+
+TEST_F(ResultAggregatorTestWithDispatcher,
+       TimeoutCallbackNotInvokedIfAllActionsComplete) {
+  {
+    auto result_aggregator = make_scoped_refptr(new ResultAggregator(
+        Bind(&ResultAggregatorTest::ReportResult, Unretained(this)),
+        &dispatcher_, kTimeoutMilliseconds));
+    // The result aggregator receives the one callback it expects, and goes
+    // out of scope. At this point, it should invoke the ReportResult callback
+    // with the error type kPermissionDenied that it copied.
+    ResultGenerator generator(result_aggregator);
+    generator.GenerateResult(Error::kPermissionDenied);
+    EXPECT_CALL(*this, ReportResult(ErrorType(Error::kPermissionDenied)));
+  }
+  // The timeout callback should be canceled after the ResultAggregator went
+  // out of scope and was destructed.
+  EXPECT_CALL(*this, ReportResult(ErrorType(Error::kOperationTimeout)))
+      .Times(0);
+  dispatcher_.DispatchPendingEvents();
 }
 
 }  // namespace shill
