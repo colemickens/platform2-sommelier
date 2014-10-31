07192b58d2a643d60e1c792ddd83ea6f8c6ea189
Jorge Lucangeli Obes <jorgelo@chromium.org>
2014-10-31 15:30:35 -0700

    libchromeos: clang-format Minijail wrappers.
    There were several style guide violations in the wrappers.
    clang-format them so that we don't deviate further from the style guide.
    
    BUG=None
    TEST=Compile.
    
    Change-Id: Ibcd4fe35ac0e06b7a4c49d91a7da5e523b391e7c
    Reviewed-on: https://chromium-review.googlesource.com/226892
    Tested-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Utkarsh Sanghi <usanghi@chromium.org>
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- libchromeos/chromeos/minijail/minijail.h
+++ libchromeos/chromeos/minijail/minijail.h
@@ -22,62 +22,73 @@ class Minijail {
   virtual ~Minijail();
 
   // This is a singleton -- use Minijail::GetInstance()->Foo().
-  static Minijail *GetInstance();
+  static Minijail* GetInstance();
 
   // minijail_new
-  virtual struct minijail *New();
+  virtual struct minijail* New();
   // minijail_destroy
-  virtual void Destroy(struct minijail *jail);
+  virtual void Destroy(struct minijail* jail);
 
   // minijail_change_user/minijail_change_group
-  virtual bool DropRoot(struct minijail *jail, const char *user,
-                        const char *group);
+  virtual bool DropRoot(struct minijail* jail,
+                        const char* user,
+                        const char* group);
 
   // minijail_use_seccomp_filter/minijail_no_new_privs/
   // minijail_parse_seccomp_filters
-  virtual void UseSeccompFilter(struct minijail *jail, const char *path);
+  virtual void UseSeccompFilter(struct minijail* jail, const char* path);
 
   // minijail_use_caps
-  virtual void UseCapabilities(struct minijail *jail, uint64_t capmask);
+  virtual void UseCapabilities(struct minijail* jail, uint64_t capmask);
 
   // minijail_enter
-  virtual void Enter(struct minijail *jail);
+  virtual void Enter(struct minijail* jail);
 
   // minijail_run_pid
-  virtual bool Run(struct minijail *jail, std::vector<char *> args, pid_t *pid);
+  virtual bool Run(struct minijail* jail, std::vector<char*> args, pid_t* pid);
 
   // minijail_run_pid and waitpid
-  virtual bool RunSync(struct minijail *jail, std::vector<char *> args,
-                       int *status);
+  virtual bool RunSync(struct minijail* jail,
+                       std::vector<char*> args,
+                       int* status);
 
   // minijail_run_pid_pipe
-  virtual bool RunPipe(struct minijail *jail, std::vector<char *> args,
-                       pid_t *pid, int *stdin);
+  virtual bool RunPipe(struct minijail* jail,
+                       std::vector<char*> args,
+                       pid_t* pid,
+                       int* stdin);
 
   // minijail_run_pid_pipes
-  virtual bool RunPipes(struct minijail *jail, std::vector<char *> args,
-                        pid_t *pid, int *stdin, int *stdout, int *stderr);
+  virtual bool RunPipes(struct minijail* jail,
+                        std::vector<char*> args,
+                        pid_t* pid,
+                        int* stdin,
+                        int* stdout,
+                        int* stderr);
 
   // Run() and Destroy()
-  virtual bool RunAndDestroy(struct minijail *jail,
-                             std::vector<char *> args,
-                             pid_t *pid);
+  virtual bool RunAndDestroy(struct minijail* jail,
+                             std::vector<char*> args,
+                             pid_t* pid);
 
   // RunSync() and Destroy()
-  virtual bool RunSyncAndDestroy(struct minijail *jail,
-                                 std::vector<char *> args,
-                                 int *status);
+  virtual bool RunSyncAndDestroy(struct minijail* jail,
+                                 std::vector<char*> args,
+                                 int* status);
 
   // RunPipe() and Destroy()
-  virtual bool RunPipeAndDestroy(struct minijail *jail,
-                                 std::vector<char *> args,
-                                 pid_t *pid, int *stdin);
+  virtual bool RunPipeAndDestroy(struct minijail* jail,
+                                 std::vector<char*> args,
+                                 pid_t* pid,
+                                 int* stdin);
 
   // RunPipes() and Destroy()
-  virtual bool RunPipesAndDestroy(struct minijail *jail,
-                                  std::vector<char *> args,
-                                  pid_t *pid, int *stdin,
-                                  int *stdout, int *stderr);
+  virtual bool RunPipesAndDestroy(struct minijail* jail,
+                                  std::vector<char*> args,
+                                  pid_t* pid,
+                                  int* stdin,
+                                  int* stdout,
+                                  int* stderr);
 
  protected:
   Minijail();
