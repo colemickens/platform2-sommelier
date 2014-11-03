300498a3ddb487df76f036d6bb3cfe19b6856d63
David Pursell <dpursell@chromium.org>
2014-11-03 15:47:36 -0800

    debugd: Add functionality to ProcessWithOutput
    Adds some additional capabilities to the ProcessWithOutput class such as
    the ability to separate stdout and stderr and some helper functions to
    handle common setup and error checking logic.
    
    Used by debugd dev tools to run helper scripts.
    
    BUG=chromium:403170
    TEST=`test_that debugd_DevTools` autotest from CL 227256.
    CQ-DEPEND=CL:227431
    
    Change-Id: Ia812d50678909c64de5b93c7ba639e9a17c8bc18
    Reviewed-on: https://chromium-review.googlesource.com/227197
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Ben Chan <benchan@chromium.org>
    Trybot-Ready: David Pursell <dpursell@chromium.org>
    Commit-Queue: David Pursell <dpursell@chromium.org>
    Tested-by: David Pursell <dpursell@chromium.org>

--- debugd/src/process_with_output.cc
+++ debugd/src/process_with_output.cc
@@ -4,29 +4,60 @@
 
 #include "debugd/src/process_with_output.h"
 
-#include <base/files/file_path.h>
+#include <signal.h>
+
 #include <base/files/file_util.h>
 #include <base/strings/string_split.h>
 
 namespace debugd {
 
-ProcessWithOutput::ProcessWithOutput() : outfile_(NULL) {}
+namespace {
+
+const char kDBusErrorString[] = "org.chromium.debugd.error.RunProcess";
+const char kInitErrorString[] = "Process initialization failure.";
+const char kStartErrorString[] = "Process start failure.";
+const char kInputErrorString[] = "Process input write failure.";
+const char kPathLengthErrorString[] = "Path length is too long.";
+
+// Sets the D-Bus error if it's non-NULL.
+void SetError(const char* message, DBus::Error* error) {
+  if (error) {
+    error->set(kDBusErrorString, message);
+  }
+}
+
+}  // namespace
+
+ProcessWithOutput::ProcessWithOutput()
+    : separate_stderr_(false), use_minijail_(true) {
+}
 
 ProcessWithOutput::~ProcessWithOutput() {
-  if (outfile_)
-    fclose(outfile_);
+  outfile_.reset();
+  errfile_.reset();
 
   if (!outfile_path_.empty())
     base::DeleteFile(outfile_path_, false);  // not recursive
+  if (!errfile_path_.empty())
+    base::DeleteFile(errfile_path_, false);
 }
 
 bool ProcessWithOutput::Init() {
-  if (!SandboxedProcess::Init())
-    return false;
+  if (use_minijail_) {
+    if (!SandboxedProcess::Init())
+      return false;
+  }
 
-  outfile_ = base::CreateAndOpenTemporaryFile(&outfile_path_);
-  if (!outfile_)
+  outfile_.reset(base::CreateAndOpenTemporaryFile(&outfile_path_));
+  if (!outfile_.get()) {
     return false;
+  }
+  if (separate_stderr_) {
+    errfile_.reset(base::CreateAndOpenTemporaryFile(&errfile_path_));
+    if (!errfile_.get()) {
+      return false;
+    }
+  }
 
   // We can't just RedirectOutput to the file we just created, since
   // RedirectOutput uses O_CREAT | O_EXCL to open the target file (i.e., it'll
@@ -34,8 +65,9 @@ bool ProcessWithOutput::Init() {
   // use that filename, since we'd have to remove it before using
   // RedirectOutput, which exposes us to a /tmp race. Instead, bind outfile_'s
   // fd to the subprocess's stdout and stderr.
-  BindFd(fileno(outfile_), STDOUT_FILENO);
-  BindFd(fileno(outfile_), STDERR_FILENO);
+  BindFd(fileno(outfile_.get()), STDOUT_FILENO);
+  BindFd(fileno(separate_stderr_ ? errfile_.get() : outfile_.get()),
+         STDERR_FILENO);
   return true;
 }
 
@@ -50,6 +82,100 @@ bool ProcessWithOutput::GetOutputLines(std::vector<std::string>* output) {
 
 bool ProcessWithOutput::GetOutput(std::string* output) {
   return base::ReadFileToString(outfile_path_, output);
+}
+
+bool ProcessWithOutput::GetError(std::string* error) {
+  return base::ReadFileToString(errfile_path_, error);
+}
+
+int ProcessWithOutput::RunProcess(const std::string& command,
+                                  const ArgList& arguments,
+                                  bool requires_root,
+                                  const std::string* stdin,
+                                  std::string* stdout,
+                                  std::string* stderr,
+                                  DBus::Error* error) {
+  ProcessWithOutput process;
+  if (requires_root) {
+    process.SandboxAs("root", "root");
+  }
+  return DoRunProcess(
+      command, arguments, stdin, stdout, stderr, error, &process);
+}
+
+int ProcessWithOutput::RunHelper(const std::string& helper,
+                                 const ArgList& arguments,
+                                 bool requires_root,
+                                 const std::string* stdin,
+                                 std::string* stdout,
+                                 std::string* stderr,
+                                 DBus::Error* error) {
+  std::string helper_path;
+  if (!SandboxedProcess::GetHelperPath(helper, &helper_path)) {
+    SetError(kPathLengthErrorString, error);
+    return kRunError;
+  }
+  return RunProcess(
+      helper_path, arguments, requires_root, stdin, stdout, stderr, error);
+}
+
+int ProcessWithOutput::RunProcessFromHelper(const std::string& command,
+                                            const ArgList& arguments,
+                                            const std::string* stdin,
+                                            std::string* stdout,
+                                            std::string* stderr) {
+  ProcessWithOutput process;
+  process.set_use_minijail(false);
+  process.SetSearchPath(true);
+  return DoRunProcess(
+      command, arguments, stdin, stdout, stderr, nullptr, &process);
+}
+
+int ProcessWithOutput::DoRunProcess(const std::string& command,
+                                    const ArgList& arguments,
+                                    const std::string* stdin,
+                                    std::string* stdout,
+                                    std::string* stderr,
+                                    DBus::Error* error,
+                                    ProcessWithOutput* process) {
+  process->set_separate_stderr(true);
+  if (!process->Init()) {
+    SetError(kInitErrorString, error);
+    return kRunError;
+  }
+
+  process->AddArg(command);
+  for (const auto& argument : arguments) {
+    process->AddArg(argument);
+  }
+
+  int result = kRunError;
+  if (stdin) {
+    process->RedirectUsingPipe(STDIN_FILENO, true);
+    if (process->Start()) {
+      int stdin_fd = process->GetPipe(STDIN_FILENO);
+      // Kill the process if writing to or closing the pipe fails.
+      if (base::WriteFileDescriptor(stdin_fd, stdin->c_str(),
+                                    stdin->length()) < 0 ||
+          IGNORE_EINTR(close(stdin_fd)) < 0) {
+        process->Kill(SIGKILL, 0);
+        SetError(kInputErrorString, error);
+      }
+      result = process->Wait();
+    } else {
+      SetError(kStartErrorString, error);
+    }
+  } else {
+    result = process->Run();
+  }
+
+  if (stdout) {
+    process->GetOutput(stdout);
+  }
+  if (stderr) {
+    process->GetError(stderr);
+  }
+  return result;
 }
 
 }  // namespace debugd
