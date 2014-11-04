8bfa5cf75ff811dbbe5828abfcd0ee164281c250
David Pursell <dpursell@chromium.org>
2014-11-04 08:14:14 -0800

    debugd: Add password helper program for dev tools.
    The dev tools are a set of debugd D-Bus functions that enable various
    features required for development, such as SSH access and booting from
    USB. The goal is to make it easier for developers to transition from an
    initial base image to something that can be used with tools like `cros
    flash`.
    
    BUG=chromium:403170
    TEST=Included unittest and autotest from CL 227256.
    CQ-DEPEND=CL:227431,CL:231258,CL:231259
    
    Change-Id: I42fe0e89d7613f3388d22173aba5b34f94de69c4
    Reviewed-on: https://chromium-review.googlesource.com/227284
    Reviewed-by: David Pursell <dpursell@chromium.org>
    Trybot-Ready: David Pursell <dpursell@chromium.org>
    Commit-Queue: David Pursell <dpursell@chromium.org>
    Tested-by: David Pursell <dpursell@chromium.org>

---
+++ debugd/src/helpers/dev_features_password.cc
@@ -0,0 +1,74 @@
+// Copyright 2014 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <base/logging.h>
+#include <base/strings/string_util.h>
+#include <chromeos/flag_helper.h>
+
+#include "debugd/src/helpers/dev_features_password_utils.h"
+
+namespace {
+
+const char kUsageMessage[] =
+    "\n"
+    "Sets a password or checks whether the username already has a password.\n"
+    "When setting a password, the new password is read from stdin.\n"
+    "\n"
+    "This is needed instead of the standard passwd utility to allow scripted\n"
+    "password input, but is not meant to be a general-purpose tool as it\n"
+    "doesn't check any other fields in the file except for the password.\n"
+    "\n"
+    "By default this uses the dev mode password, but can also use system\n"
+    "passwords with the --system flag."
+    "\n";
+
+const char kDevModePath[] = "/mnt/stateful_partition/etc/devmode.passwd";
+const char kSystemPath[] = "/etc/shadow";
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  DEFINE_bool(q, false, "Query whether a password exists for the given user");
+  DEFINE_bool(system, false, "Use the system password instead of dev mode");
+  DEFINE_string(user, "chronos", "User name");
+  chromeos::FlagHelper::Init(argc, argv, kUsageMessage);
+
+  debugd::DevFeaturesPasswordUtils utils;
+
+  if (!utils.IsUsernameValid(FLAGS_user)) {
+    LOG(WARNING) << "Invalid username \"" << FLAGS_user << '"';
+    return EXIT_FAILURE;
+  }
+  base::FilePath password_file(FLAGS_system ? kSystemPath : kDevModePath);
+
+  if (FLAGS_q) {
+    if (utils.IsPasswordSet(FLAGS_user, password_file)) {
+      return EXIT_SUCCESS;
+    }
+    return EXIT_FAILURE;
+  }
+
+  // New password should be provided on stdin.
+  char* password_buffer = nullptr;
+  size_t buffer_size = 0;
+  ssize_t password_length;
+  password_length = getline(&password_buffer, &buffer_size, stdin);
+  if (password_length == -1) {
+    PLOG(WARNING) << "Failed to read password from stdin";
+    return EXIT_FAILURE;
+  }
+  std::string password(password_buffer);
+  // Remove trailing newline.
+  if (password.back() == '\n')
+    password.pop_back();
+
+  if (utils.SetPassword(FLAGS_user, password, password_file)) {
+    return EXIT_SUCCESS;
+  }
+  LOG(WARNING) << "SetPassword() failed";
+  return EXIT_FAILURE;
+}
