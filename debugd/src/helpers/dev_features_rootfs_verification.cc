e53b9676324bfb2620e39305365324123fd50eb3
David Pursell <dpursell@chromium.org>
2014-12-01 11:26:22 -0800

    debugd: Add rootfs helper program for dev tools.
    The dev tools are a set of debugd D-Bus functions that enable various
    features required for development, such as SSH access and booting from
    USB. The goal is to make it easier for developers to transition from an
    initial base image to something that can be used with tools like `cros
    flash`.
    
    BUG=chromium:403170
    TEST=Autotest from CL 227256.
    CQ-DEPEND=CL:227431
    
    Change-Id: I912036db3cee34b3feb308f082a9d120728dffae
    Reviewed-on: https://chromium-review.googlesource.com/232361
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Trybot-Ready: David Pursell <dpursell@chromium.org>
    Commit-Queue: David Pursell <dpursell@chromium.org>
    Tested-by: David Pursell <dpursell@chromium.org>

---
+++ debugd/src/helpers/dev_features_rootfs_verification.cc
@@ -0,0 +1,98 @@
+// Copyright 2014 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <array>
+#include <string>
+
+#include <base/files/file_util.h>
+#include <base/logging.h>
+#include <chromeos/flag_helper.h>
+#include <rootdev/rootdev.h>
+
+#include "debugd/src/process_with_output.h"
+
+namespace {
+
+enum { kMaxPartitionDigits = 4 };
+
+const char kUsageMessage[] =
+    "\n"
+    "Removes rootfs verification for the current partition or queries whether\n"
+    "rootfs verification has already been removed.\n"
+    "\n";
+
+// Checks if rootfs verification has been removed by testing if / is writable.
+// Must be called as root since / is never writable by the debugd user.
+bool IsRootfsVerificationRemoved() {
+  return base::PathIsWritable(base::FilePath("/"));
+}
+
+// Uses rootdev to get the partition we can safely use with make_dev_ssd.sh.
+// Returns -1 and prints to stderr on failure.
+int GetModifiablePartition() {
+  // Use the same logic as make_dev_ssd.sh: query rootdev, get the number at
+  // the end of the rootdev path, then subtract 1.
+  std::array<char, PATH_MAX> path;
+  int rootdev_result = rootdev(path.data(), path.size(), true, false);
+  if (rootdev_result != 0) {
+    LOG(WARNING) << "rootdev failed with error code " << rootdev_result;
+    return -1;
+  }
+  size_t path_length = strlen(path.data());
+  // There must be at least 1 numeric digit at the end of the path.
+  if (!IsAsciiDigit(path[path_length - 1])) {
+    LOG(WARNING) << "Couldn't determine partition from rootdev path \""
+                 << path.data() << '"';
+    return -1;
+  }
+  int partition = -1;
+  for (int i = path_length - 1; i > 0; --i) {
+    if (!IsAsciiDigit(path[i - 1])) {
+      partition = atoi(&path[i]) - 1;
+      break;
+    }
+  }
+  return partition;
+}
+
+// Removes rootfs verification.
+bool RemoveRootfsVerification() {
+  int partition = GetModifiablePartition();
+  if (partition == -1) {
+    LOG(WARNING) << "No modifiable partition";
+    return false;
+  }
+  std::array<char, kMaxPartitionDigits + 1> partition_string;
+  snprintf(partition_string.data(), partition_string.size(), "%d", partition);
+  std::string error;
+  int result = debugd::ProcessWithOutput::RunProcessFromHelper(
+      "/usr/share/vboot/bin/make_dev_ssd.sh",
+      {"--remove_rootfs_verification", "--partitions", partition_string.data()},
+      nullptr,  // stdin.
+      nullptr,  // stdout.
+      &error);  // stderr.
+  if (result != EXIT_SUCCESS) {
+    LOG(WARNING) << "\"make_dev_ssd.sh\" failed with exit code " << result
+                 << ": " << error;
+    return false;
+  }
+  return true;
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  DEFINE_bool(q, false, "Query whether verification has been removed");
+  chromeos::FlagHelper::Init(argc, argv, kUsageMessage);
+
+  if (FLAGS_q) {
+    return IsRootfsVerificationRemoved() ? EXIT_SUCCESS : EXIT_FAILURE;
+  }
+
+  return RemoveRootfsVerification() ? EXIT_SUCCESS : EXIT_FAILURE;
+}
