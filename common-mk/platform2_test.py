2222ba786dc6eef2d1fdba27da3e1a1bd79a6022
Mike Frysinger <vapier@chromium.org>
2014-12-17 01:04:13 -0500

    common-mk: platform2_test: force TERM=ansi inside the sysroot
    Users might be utilizing a TERM value that the sysroot doesn't support.
    This is because we strip out the vast majority of terminal databases on
    the device to save space.  Let's go with a basic one that still includes
    color.
    
    BUG=chromium:442474
    TEST=`./platform2_test.py --sysroot /build/link -- /usr/bin/pstree` doesn't complain
    TEST=`cbuildbot amd64-generic-full` passes
    
    Change-Id: I32a20ae4a08571166623a9b5c03a8d570d663234
    Reviewed-on: https://chromium-review.googlesource.com/236240
    Reviewed-by: Chris Masone <cmasone@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- common-mk/platform2_test.py
+++ common-mk/platform2_test.py
@@ -565,6 +565,9 @@ class Platform2Test(object):
       print('cmd: {%s} %s' % (cmd, ' '.join(map(repr, argv))))
       os.chroot(self.sysroot)
       os.chdir(cwd)
+      # The TERM the user is leveraging might not exist in the sysroot.
+      # Force a sane default that supports standard color sequences.
+      os.environ['TERM'] = 'ansi'
       # Some progs want this like bash else they get super confused.
       os.environ['PWD'] = cwd
       if not self.run_as_root:
