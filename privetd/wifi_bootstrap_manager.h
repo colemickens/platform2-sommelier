4819f5c40a52a59e815e23cc2bb3bb55b9e0cf6d
Vitaly Buka <vitalybuka@chromium.org>
2014-12-21 01:15:58 +0000

    Revert "privetd: Use only tasks_weak_factory_ to cancel pending tasks."
    This reverts commit f1c06d8ea3922e7e2d9e3230991e08dc2900462e.
    
    Contains changes that suppose to be in another CL.
    
    Change-Id: I15b6f3c48629385c097baddc4b6f4dd7ceb8a3aa
    Reviewed-on: https://chromium-review.googlesource.com/237084
    Commit-Queue: Vitaly Buka <vitalybuka@chromium.org>
    Tested-by: Vitaly Buka <vitalybuka@chromium.org>
    Reviewed-by: Vitaly Buka <vitalybuka@chromium.org>

--- privetd/wifi_bootstrap_manager.h
+++ privetd/wifi_bootstrap_manager.h
@@ -9,9 +9,9 @@
 #include <vector>
 
 #include <base/callback.h>
+#include <base/cancelable_callback.h>
 #include <base/files/file_path.h>
 #include <base/macros.h>
-#include <base/memory/weak_ptr.h>
 
 #include "privetd/daemon_state.h"
 #include "privetd/privet_types.h"
@@ -59,7 +59,6 @@ class WifiBootstrapManager : public WifiDelegate {
   // Update the current state, post tasks to notify listeners accordingly to
   // the MessageLoop.
   void UpdateState(State new_state);
-  void NotifyStateListeners(State new_state) const;
 
   // If we've been bootstrapped successfully before, and we're bootstrapping
   // again because we slipped offline for a sufficiently longtime, we want
@@ -84,10 +83,11 @@ class WifiBootstrapManager : public WifiDelegate {
   bool have_ever_been_bootstrapped_{false};
   bool currently_online_{false};
   std::string last_configured_ssid_;
-
-  // Helps to reset irrelevant tasks switching state.
+  base::CancelableClosure on_connect_success_task_;
+  base::CancelableClosure on_connect_timeout_task_;
+  base::CancelableClosure on_bootstrap_timeout_task_;
+  base::CancelableClosure on_monitoring_timeout_task_;
   base::WeakPtrFactory<WifiBootstrapManager> tasks_weak_factory_{this};
-
   base::WeakPtrFactory<WifiBootstrapManager> lifetime_weak_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(WifiBootstrapManager);
