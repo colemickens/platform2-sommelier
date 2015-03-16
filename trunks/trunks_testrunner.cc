684271039fb796d412da26a62264015cea15e054
Darren Krahn <dkrahn@chromium.org>
2015-03-16 11:19:52 -0700

    trunks: Enable verbose logging when running unit tests.
    BUG=None
    TEST=unit
    
    Change-Id: I2dddd6d19f01241d7a002d5f35911fcfa2df2450
    Reviewed-on: https://chromium-review.googlesource.com/260431
    Reviewed-by: Utkarsh Sanghi <usanghi@chromium.org>
    Commit-Queue: Darren Krahn <dkrahn@chromium.org>
    Tested-by: Darren Krahn <dkrahn@chromium.org>

--- trunks/trunks_testrunner.cc
+++ trunks/trunks_testrunner.cc
@@ -3,9 +3,16 @@
 // found in the LICENSE file.
 
 #include <base/at_exit.h>
+#include <base/command_line.h>
+#include <base/logging.h>
+#include <chromeos/syslog_logging.h>
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
+  CommandLine::Init(argc, argv);
+  chromeos::InitLog(chromeos::kLogToStderr);
+  // Enable verbose logging while running unit tests.
+  logging::SetMinLogLevel(logging::LOG_VERBOSE);
   base::AtExitManager exit_manager;
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
