615583164d3b0fa132c7d8dba89bbedb54c1b556
Reilly Grant <reillyg@chromium.org>
2015-04-14 14:35:12 -0700

    permission_broker: Poll on fd from new udev_queue_get_fd API.
    libudev now provides an API to get the correct file descriptor to poll
    for updates to the queue state. This code matches the implementation of
    the "udevadm settle" command in udev-216.
    
    BUG=chromium:455336
    TEST=Crashed Chrome and verified Security Key works immediately
    afterwards.
    
    Change-Id: I0dc607cfd7b4e4a12650bf0ac46e566652684d00
    Reviewed-on: https://chromium-review.googlesource.com/296691
    Commit-Ready: Reilly Grant <reillyg@chromium.org>
    Tested-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- permission_broker/rule_engine.cc
+++ permission_broker/rule_engine.cc
@@ -66,31 +66,25 @@ Rule::Result RuleEngine::ProcessPath(const std::string& path) {
 }
 
 void RuleEngine::WaitForEmptyUdevQueue() {
-  struct udev_queue* queue = udev_queue_new(udev_.get());
-  if (udev_queue_get_queue_is_empty(queue)) {
-    udev_queue_unref(queue);
+  ScopedUdevQueuePtr queue(udev_queue_new(udev_.get()));
+  if (udev_queue_get_queue_is_empty(queue.get()))
     return;
-  }
 
   struct pollfd udev_poll;
   memset(&udev_poll, 0, sizeof(udev_poll));
-  udev_poll.fd = inotify_init();
+  udev_poll.fd = udev_queue_get_fd(queue.get());
   udev_poll.events = POLLIN;
 
-  int watch =
-      inotify_add_watch(udev_poll.fd, udev_run_path_.c_str(), IN_MOVED_TO);
-  CHECK_NE(watch, -1) << "Could not add watch for udev run directory.";
-
-  while (!udev_queue_get_queue_is_empty(queue)) {
-    if (poll(&udev_poll, 1, poll_interval_msecs_) > 0) {
-      char buffer[sizeof(struct inotify_event)];
-      const ssize_t result = read(udev_poll.fd, buffer, sizeof(buffer));
-      if (result < 0)
-        LOG(WARNING) << "Did not read complete udev event.";
-    }
+  if (udev_poll.fd < 0) {
+    LOG(ERROR) << "Failed to get udev queue fd: "
+               << logging::SystemErrorCodeToString(-udev_poll.fd);
+    return;
   }
-  udev_queue_unref(queue);
-  close(udev_poll.fd);
+
+  while (!udev_queue_get_queue_is_empty(queue.get())) {
+    if (poll(&udev_poll, 1, poll_interval_msecs_) > 0)
+      udev_queue_flush(queue.get());
+  }
 }
 
 ScopedUdevDevicePtr RuleEngine::FindUdevDevice(const std::string& path) {
