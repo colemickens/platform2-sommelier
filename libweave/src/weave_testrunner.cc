a92ace81d27321a7f0b5696b67f464abe61fa23e
Vitaly Buka <vitalybuka@chromium.org>
2015-08-13 23:45:32 -0700

    libweave: Remove inclusion of chromeos/test_helpers.h
    The only useful part of that is initialization of logging.
    Also added call testing::InitGoogleMock.
    
    BUG=brillo:1257
    TEST=`FEATURES=test emerge-gizmo libweave`
    
    Change-Id: I097cbe38746f4b09d4df3eedbafac2a142834644
    Reviewed-on: https://chromium-review.googlesource.com/293614
    Reviewed-by: Alex Vakulenko <avakulenko@chromium.org>
    Commit-Queue: Vitaly Buka <vitalybuka@chromium.org>
    Tested-by: Vitaly Buka <vitalybuka@chromium.org>

--- libweave/src/weave_testrunner.cc
+++ libweave/src/weave_testrunner.cc
@@ -3,11 +3,20 @@
 // found in the LICENSE file.
 
 #include <base/at_exit.h>
-#include <chromeos/test_helpers.h>
+#include <base/command_line.h>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
   base::AtExitManager exit_manager;
-  SetUpTests(&argc, argv, true);
+  base::CommandLine::Init(argc, argv);
+
+  logging::LoggingSettings settings;
+  settings.logging_dest = logging::LOG_TO_SYSTEM_DEBUG_LOG;
+  logging::InitLogging(settings);
+  logging::SetLogItems(false, false, false, false);
+
+  ::testing::InitGoogleMock(&argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
