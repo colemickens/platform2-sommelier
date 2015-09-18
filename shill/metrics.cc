6d47590ea943535df0fe867a13fb64906dab2606
Samuel Tan <samueltan@google.com>
2015-09-18 14:07:33 -0700

    shill: do not use bootstat when building in Android
    Do not use bootstat in shill/metrics.cc when building for Android,
    since we do not currently plan to collect boot statistics in
    Android.
    
    BUG: 22956441
    Change-Id: I2aa995902fd30a152fd2084129712d25a6e3ec54
    TEST: shill compiles and unit tests pass when building with emerge.

--- shill/metrics.cc
+++ shill/metrics.cc
@@ -19,7 +19,9 @@
 #include <base/strings/string_util.h>
 #include <base/strings/stringprintf.h>
 #include <chromeos/dbus/service_constants.h>
+#if !defined(__ANDROID__)
 #include <metrics/bootstat.h>
+#endif  // __ANDROID__
 
 #include "shill/connection_diagnostics.h"
 #include "shill/link_monitor.h"
@@ -730,12 +732,14 @@ void Metrics::NotifyServiceStateChanged(const Service& service,
   if (new_state == Service::kStateFailure)
     SendServiceFailure(service);
 
+#if !defined(__ANDROID__)
   if (collect_bootstats_) {
     bootstat_log(base::StringPrintf("network-%s-%s",
                                     Technology::NameFromIdentifier(
                                         service.technology()).c_str(),
                                     service.GetStateString().c_str()).c_str());
   }
+#endif  // __ANDROID__
 
   if (new_state != Service::kStateConnected)
     return;
