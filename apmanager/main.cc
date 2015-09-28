8619c900877b02e906c71cb68b0f0caf3e793f95
Peter Qiu <zqiu@google.com>
2015-09-27 22:04:52 -0700

    apmanager: Do not daemonize (fork) the daemon process
    When starting the daemon using an init system, the environment setup
    is already being handled by the init system, so there is no need
    to daemonize the daemon process create such setup.
    
    By daemonizing the daemon process, it spawns off a new process to
    run the daemon and terminates the current process. This causes
    a problem with the init system, where it thinks that the daemon
    failed to start. We were able to work around it on Chrome OS's
    upstart script by adding the forking expectation in the init
    scritp ("expect fork"), but there is not such workaround on
    Android's init system.
    
    Fix it by removing the daemonize call (daemon()) and remove the
    the forking expectation on Chrome OS's init script.
    
    BUG=b:24412675
    TEST=Manual test on dragonboard
    TEST=start/stop apmanager on Chrome OS device
    
    Change-Id: I4dff678946801310982c3af366ab6182f5864a81

--- apmanager/main.cc
+++ apmanager/main.cc
@@ -117,10 +117,6 @@ int main(int argc, char* argv[]) {
     return 0;
   }
 
-  const int nochdir = 0, noclose = 0;
-  if (!cl->HasSwitch(switches::kForeground))
-    PLOG_IF(FATAL, daemon(nochdir, noclose) == -1) << "Failed to daemonize";
-
   apmanager::Daemon daemon(base::Bind(&OnStartup, argv[0], cl));
 
   daemon.Run();
