colemickens/platform2-sommelier

commit c1f501a6db0b70722e495e85cf690c772a5aad4a

settingsd: implement main() function
This implements the main() function and adds an implementation of
DBusServiceDaemon, i.e. Daemon.

BUG=chromium:522969
TEST=Compiles. Unittests pass.

Change-Id: I984cc48520b04fd27011e90bd098edd17eb86a8b
Reviewed-on: https://chromium-review.googlesource.com/294700
Commit-Ready: Christoph Schütte <cschuet@chromium.org>
Tested-by: Christoph Schütte <cschuet@chromium.org>
Reviewed-by: Mattias Nissler <mnissler@chromium.org>

settingsd/settingsd.gyp
@@ -49,7 +49,25 @@
         'version_stamp.cc',
         'version_stamp.h',
       ],
-      'includes': ['../common-mk/generate-dbus-adaptors.gypi'],
+    },
+    {
+      'target_name': 'settingsd',
+      'type': 'executable',
+      'variables': {
+        'deps': [
+          'libchromeos-<(libbase_ver)',
+        ],
+      },
+      'dependencies': [
+        'settingsd_common',
+      ],
+      'sources': [
+        'daemon.cc',
+        'daemon.h',
+        'dbus_constants.cc',
+        'dbus_constants.h',
+        'main.cc',
+      ],
     },
     {
       'target_name': 'settingsd_testrunner',

settingsd/daemon.cc
@@ -1,0 +1,50 @@
+// Copyright 2015 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "settingsd/daemon.h"
+
+#include <vector>
+
+#include "settingsd/dbus_constants.h"
+#include "settingsd/locked_settings.h"
+#include "settingsd/simple_settings_map.h"
+
+namespace settingsd {
+
+Daemon::Daemon(const ConfigPaths& config_paths)
+    : DBusServiceDaemon(kServiceName, kRootServicePath),
+      config_paths_(config_paths) {}
+
+bool Daemon::Init() {
+  // Attempt to load the trusted document from the storage.
+  std::unique_ptr<const SettingsDocument> trusted_document =
+      LoadTrustedDocument();
+  if (!trusted_document)
+    return false;
+
+  // TODO(cschuet): Setup parser_function_ and delegate_factory_function_ here.
+  // Instantiate the SettingsDocumentManager for system configuration.
+  std::unique_ptr<SimpleSettingsMap> settings_map(new SimpleSettingsMap);
+  system_settings_document_manager_.reset(new SettingsDocumentManager(
+      parser_function_, delegate_factory_function_,
+      config_paths_.system_storage.value(), std::move(settings_map),
+      std::move(trusted_document)));
+
+  // Instantiate the DBusSettingsServiceImpl for system configuration.
+  dbus_system_settings_service_.reset(new DBusSettingsServiceImpl(
+      system_settings_document_manager_.get(), object_manager_->AsWeakPtr(),
+      dbus::ObjectPath(kSystemSettingsServicePath)));
+  return true;
+}
+
+void Daemon::RegisterDBusObjectsAsync(AsyncEventSequencer* sequencer) {
+  dbus_system_settings_service_->Start(sequencer);
+}
+
+std::unique_ptr<const SettingsDocument> Daemon::LoadTrustedDocument() const {
+  // TODO(cschuet): Implement trusted document parsing.
+  return std::unique_ptr<const SettingsDocument>();
+}
+
+}  // namespace settingsd

settingsd/daemon.h
@@ -1,0 +1,69 @@
+// Copyright 2015 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SETTINGSD_DAEMON_H_
+#define SETTINGSD_DAEMON_H_
+
+#include <memory>
+#include <string>
+
+#include <base/files/file_path.h>
+#include <base/macros.h>
+#include <chromeos/daemons/dbus_daemon.h>
+
+#include "settingsd/dbus_settings_service_impl.h"
+#include "settingsd/settings_blob_parser.h"
+#include "settingsd/settings_document_manager.h"
+#include "settingsd/source_delegate.h"
+
+using chromeos::dbus_utils::AsyncEventSequencer;
+using chromeos::DBusServiceDaemon;
+
+namespace settingsd {
+
+struct ConfigPaths {
+  // Path to directory where settings blobs for system-wide configuration are
+  // stored.
+  base::FilePath system_storage;
+
+  // Path to file containing the initial trusted document.
+  base::FilePath trusted_document;
+};
+
+class Daemon : public DBusServiceDaemon {
+ public:
+  explicit Daemon(const ConfigPaths& config_paths);
+
+  // Initializes the Daemon by loading the trusted document, instantiating the
+  // SettingsDocumentManager.
+  bool Init();
+
+ protected:
+  void RegisterDBusObjectsAsync(AsyncEventSequencer* sequencer) override;
+
+ private:
+  // Loads the trusted document from disk.
+  std::unique_ptr<const SettingsDocument> LoadTrustedDocument() const;
+
+  // Contains configuration paths.
+  const ConfigPaths& config_paths_;
+
+  // Function to create SettingsBlob objects from a blob.
+  SettingsBlobParserFunction parser_function_;
+
+  // Function to create source delegates.
+  SourceDelegateFactoryFunction delegate_factory_function_;
+
+  // SettingsDocumentManager instance for system-wide configuration.
+  std::unique_ptr<SettingsDocumentManager> system_settings_document_manager_;
+
+  // DBusSettingsServiceImpl instance for system-wide configuration.
+  std::unique_ptr<DBusSettingsServiceImpl> dbus_system_settings_service_;
+
+  DISALLOW_COPY_AND_ASSIGN(Daemon);
+};
+
+}  // namespace settingsd
+
+#endif  // SETTINGSD_DAEMON_H_

settingsd/dbus_constants.cc
@@ -1,0 +1,13 @@
+// Copyright 2015 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "settingsd/dbus_constants.h"
+
+namespace settingsd {
+
+const char kServiceName[] = "org.chromium.Settingsd";
+const char kRootServicePath[] = "/org/chromium/Settingsd";
+const char kSystemSettingsServicePath[] = "/org/chromium/Settingsd/System";
+
+}  // namespace settingsd

settingsd/dbus_constants.h
@@ -1,0 +1,22 @@
+// Copyright 2015 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SETTINGSD_DBUS_CONSTANTS_H_
+#define SETTINGSD_DBUS_CONSTANTS_H_
+
+namespace settingsd {
+
+// The service name claimed by the Settingsd daemon.
+extern const char kServiceName[];
+
+// The object at this path implements the ObjectManager interface.
+extern const char kRootServicePath[];
+
+// The object at this path implements the SettingsInterface for system-wide
+// configuration.
+extern const char kSystemSettingsServicePath[];
+
+}  // namespace settingsd
+
+#endif  // SETTINGSD_DBUS_CONSTANTS_H_

settingsd/dbus_settings_service_impl.cc
@@ -97,6 +97,13 @@ void DBusSettingsServiceImpl::OnSettingsChanged(const std::set<Key>& keys) {
   dbus_adaptor_.SendOnSettingsChangedSignal(changed_keys);
 }
 
+void DBusSettingsServiceImpl::Start(
+    chromeos::dbus_utils::AsyncEventSequencer* sequencer) {
+  dbus_adaptor_.RegisterWithDBusObject(&dbus_object_);
+  dbus_object_.RegisterAsync(sequencer->GetHandler(
+      "DBusSettingsServiceImpl.RegisterAsync() failed.", true));
+}
+
 bool DBusSettingsServiceImpl::Get(chromeos::ErrorPtr* error,
                                   const std::string& in_key,
                                   std::vector<uint8_t>* out_value) {

settingsd/dbus_settings_service_impl.h
@@ -50,6 +50,8 @@ class DBusSettingsServiceImpl
   // SettingsObserver:
   void OnSettingsChanged(const std::set<Key>& keys) override;
 
+  void Start(chromeos::dbus_utils::AsyncEventSequencer* sequencer);
+
  private:
   // org::chromium::Settingsd::SettingsInterface:
   bool Get(chromeos::ErrorPtr* error,

settingsd/main.cc
@@ -1,0 +1,37 @@
+// Copyright 2015 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include <chromeos/flag_helper.h>
+
+#include "settingsd/daemon.h"
+
+namespace {
+
+const char kDefaultSystemStoragePath[] = "/var/lib/settingsd/system";
+const char kDefaultTrustedDocumentPath[] = "/etc/settingsd/system_config";
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  DEFINE_string(system_storage_path, "",
+                "Path to directory where settings blobs for system-wide "
+                "configuration are stored.");
+  DEFINE_string(trusted_document_path, "",
+                "Path to file containing the initial trusted document.");
+
+  chromeos::FlagHelper::Init(argc, argv, "Settingsd daemon");
+  if (FLAGS_system_storage_path.empty())
+    FLAGS_system_storage_path = kDefaultSystemStoragePath;
+  if (FLAGS_trusted_document_path.empty())
+    FLAGS_trusted_document_path = kDefaultTrustedDocumentPath;
+
+  settingsd::ConfigPaths config_paths;
+  config_paths.system_storage = base::FilePath{FLAGS_system_storage_path};
+  config_paths.trusted_document = base::FilePath{kDefaultTrustedDocumentPath};
+
+  settingsd::Daemon daemon{config_paths};
+  return daemon.Run();
+}

commit 96e7465fbf24a8822ffac2df81b8bc97fc96dfdc

settingsd: Fix dbus adaptor generation.
c1f501a6db0b70722e495e85cf690c772a5aad4a accidentally removed the
generate-dbus-adaptors include, this commit restores it.

BUG=None
TEST=settingsd builds.

Change-Id: I094f14ef062f6c2b6cf5b7638ec77a2fac873fdb
Reviewed-on: https://chromium-review.googlesource.com/302920
Reviewed-by: Christoph Schütte <cschuet@chromium.org>
Commit-Queue: Mattias Nissler <mnissler@chromium.org>
Tested-by: Mattias Nissler <mnissler@chromium.org>

settingsd/settingsd.gyp
@@ -49,6 +49,7 @@
         'version_stamp.cc',
         'version_stamp.h',
       ],
+      'includes': ['../common-mk/generate-dbus-adaptors.gypi'],
     },
     {
       'target_name': 'settingsd',