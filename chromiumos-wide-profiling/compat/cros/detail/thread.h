2854e50d744036411d811bac03e48c60f373ab83
David Sharp <dhsharp@chromium.org>
2016-06-13 13:57:00 -0700

    quipper: Add Notification class to compat layer threading
    TEST=none
    BUG=none
    
    Change-Id: I033fd0e187b2af155e826f0d0d872f25b1464072
    Reviewed-on: https://chromium-review.googlesource.com/354206
    Commit-Ready: David Sharp <dhsharp@chromium.org>
    Tested-by: David Sharp <dhsharp@chromium.org>
    Reviewed-by: Chong Jiang <chongjiang@chromium.org>

--- chromiumos-wide-profiling/compat/cros/detail/thread.h
+++ chromiumos-wide-profiling/compat/cros/detail/thread.h
@@ -5,6 +5,7 @@
 #ifndef CHROMIUMOS_WIDE_PROFILING_COMPAT_CROS_DETAIL_THREAD_H_
 #define CHROMIUMOS_WIDE_PROFILING_COMPAT_CROS_DETAIL_THREAD_H_
 
+#include "base/synchronization/waitable_event.h"
 #include "base/threading/simple_thread.h"
 
 namespace quipper {
@@ -22,11 +23,37 @@ class Thread : public quipper::compat::ThreadInterface,
     thread_.Join();
   }
 
+  pid_t tid() override {
+    return thread_.tid();
+  }
+
  protected:
   void Run() override = 0;
 
  private:
   base::DelegateSimpleThread thread_;
+};
+
+class Notification : public quipper::compat::NotificationInterface {
+ public:
+  Notification() : event_(true /* manual_reset */,
+                          false /* initially_signaled */) {
+  }
+
+  void Wait() override {
+    event_.Wait();
+  }
+
+  bool WaitWithTimeout(int timeout_ms) override {
+    return event_.TimedWait(base::TimeDelta::FromMilliseconds(timeout_ms));
+  }
+
+  void Notify() override {
+    event_.Signal();
+  }
+
+ private:
+  base::WaitableEvent event_;
 };
 
 }  // namespace quipper
--- chromiumos-wide-profiling/compat/thread.h
+++ chromiumos-wide-profiling/compat/thread.h
@@ -20,9 +20,32 @@ class ThreadInterface {
   virtual void Start() = 0;
   // quipper threads must be joined exactly once.
   virtual void Join() = 0;
+
+  virtual pid_t tid() = 0;
+
  protected:
   // Thread body. Override this.
   virtual void Run() = 0;
+};
+
+// Synchronization tool that blocks waiting threads until notified to proceed by
+// another.
+class NotificationInterface {
+ public:
+  // Constructor signature should match this:
+  // Notification();
+
+  virtual ~NotificationInterface() {}
+
+  // Wait for Notify() to be called.
+  virtual void Wait() = 0;
+  // Wait for up to |timeout_ms| for a notification. Returns true if the
+  // event was signaled. Returning false does not necessarily mean |timout_ms|
+  // has passed.
+  virtual bool WaitWithTimeout(int timeout_ms) = 0;
+
+  // Trigger the notification, and wake up any waiting threads.
+  virtual void Notify() = 0;
 };
 
 }  // namespace compat
