dc84594e45d3ad8d38c9fe32fc8a160f7969a706
Sabin Floares <sabin.floares@intel.com>
2016-07-15 18:05:02 +0300

    shill: Add systemd services.
    Extracts common script parts for Upstart and systemd.
    Uses the same template parameters for systemd services.
    
    BUG=chromium:583671
    CQ-DEPEND=CL:362801, CL:362800
    TEST=Checked that shill is up and running and that it passes shill
      tests.
    
    Change-Id: If3903354839824117104d9ff1cb6d0400da8d8f4
    Reviewed-on: https://chromium-review.googlesource.com/362810
    Commit-Ready: Sabin Floares <sabin.floares@intel.com>
    Tested-by: Sabin Floares <sabin.floares@intel.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

---
+++ shill/init/shill-pre-start.sh
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+bootstat shill-start
+
+# Create state directory
+mkdir -p /var/run/shill
+
+# Create storage for the shill global profile.
+mkdir -p /var/cache/shill
+
+# Set up dhcpcd's /var/{lib|run} dirs to run as user 'dhcp'.
+mkdir -m 0755 -p /var/lib/dhcpcd
+mkdir -m 0755 -p /var/run/dhcpcd
+chmod -R u+rwX,g+rX,o+rX /var/lib/dhcpcd
+chown -R dhcp:dhcp /var/lib/dhcpcd
+chown -R dhcp:dhcp /var/run/dhcpcd
+
+# Use flimflam's default profile if shill doesn't have one.
+if [ ! -f /var/cache/shill/default.profile -a \
+       -f /var/cache/flimflam/default.profile ]; then
+  mv /var/cache/flimflam/default.profile /var/cache/shill/default.profile
+  chmod a+r /var/cache/shill/default.profile
+fi
+
+# This option is no longer supported.
+rm -f /home/chronos/.disable_shill
---
+++ shill/init/shill.sh
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+DAEMONBIN="shill"
+ARGS="--log-level=${SHILL_LOG_LEVEL} --log-scopes=${SHILL_LOG_SCOPES}"
+if [ -n "${BLACKLISTED_DEVICES}" ] && [ -n "${SHILL_TEST_DEVICES}" ]; then
+  ARGS="${ARGS} --device-black-list=${BLACKLISTED_DEVICES},${SHILL_TEST_DEVICES}"
+elif [ -n "${BLACKLISTED_DEVICES}" ]; then
+  ARGS="${ARGS} --device-black-list=${BLACKLISTED_DEVICES}"
+elif [ -n "${SHILL_TEST_DEVICES}" ]; then
+  ARGS="${ARGS} --device-black-list=${SHILL_TEST_DEVICES}"
+fi
+if [ -n "${SHILL_PASSIVE_MODE}" ]; then
+  ARGS="${ARGS} --passive-mode"
+fi
+if [ -n "${SHILL_PREPEND_DNS_SERVERS}" ]; then
+  ARGS="${ARGS} --prepend-dns-servers=${SHILL_PREPEND_DNS_SERVERS}"
+fi
+if [ -n "${SHILL_ACCEPT_HOSTNAME_FROM}" ]; then
+  ARGS="${ARGS} --accept-hostname-from=${SHILL_ACCEPT_HOSTNAME_FROM}"
+fi
+if [ -n "${SHILL_MINIMUM_MTU}" ]; then
+  ARGS="${ARGS} --minimum-mtu=${SHILL_MINIMUM_MTU}"
+fi
+if [ -n "${DHCPV6_ENABLED_DEVICES}" ]; then
+  ARGS="${ARGS} --dhcpv6-enabled-devices=${DHCPV6_ENABLED_DEVICES}"
+fi
+ARGS="${ARGS} ${SHILL_TEST_ARGS}"
+
+# If OOBE has not completed (i.e. EULA not agreed to), do not run
+# portal checks
+if [ ! -f /home/chronos/.oobe_completed ]; then
+  ARGS="${ARGS} --portal-list="
+fi
+
+exec ${DAEMONBIN} ${ARGS}
