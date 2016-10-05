5b5b42be34a5d3a9eae01b9c3baf890b428374b5
Sabin Floares <sabin.floares@intel.com>
2016-10-05 10:44:33 +0300

    chaps: Add systemd service file.
    BUG=chromium:583671
    TEST=Checked login functionality.
    CQ-DEPEND=CL:394686
    
    Change-Id: I7097c6561e916df693f7b7304f28c8fa609fa90f
    Reviewed-on: https://chromium-review.googlesource.com/394647
    Commit-Ready: Sabin Floares <sabin.floares@intel.com>
    Tested-by: Sabin Floares <sabin.floares@intel.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

---
+++ chaps/init/chapsd.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+
+VERBOSE=""
+if [ -f /home/chronos/.chaps_debug_2 ]; then
+  VERBOSE="--v=2"
+  rm /home/chronos/.chaps_debug_2
+elif [ -f /home/chronos/.chaps_debug_1 ]; then
+  VERBOSE="--v=1"
+  rm /home/chronos/.chaps_debug_1
+fi
+
+exec chapsd ${VERBOSE} --auto_load_system_token
