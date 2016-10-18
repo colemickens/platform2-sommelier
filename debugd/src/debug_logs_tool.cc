39acf51490840b041f37960b5bc191fb51cdd1ae
Yusuke Sato <yusukes@google.com>
2016-10-17 21:57:00 -0700

    debugd: Add 'bugreport' output to the tar file GetDebugLogs creates
    This allows Chrome to download bugreport output via chrome://
    interface.
    
    BUG=b:31439276
    TEST=The funciton works as expected regardless of whether the pipe
     file exists.
    
    Change-Id: I257b953ec89d31cca230ae217015a821b9b4436d
    Reviewed-on: https://chromium-review.googlesource.com/399942
    Commit-Ready: Yusuke Sato <yusukes@chromium.org>
    Tested-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- debugd/src/debug_logs_tool.cc
+++ debugd/src/debug_logs_tool.cc
@@ -5,23 +5,58 @@
 #include "debugd/src/debug_logs_tool.h"
 
 #include <base/files/file_util.h>
+#include <base/files/scoped_temp_dir.h>
+#include <base/logging.h>
 #include <brillo/process.h>
-
 
 namespace debugd {
 
-const char* const kTar = "/bin/tar";
-const char* const kSystemLogs = "/var/log";
+namespace {
+
+constexpr char kCat[] = "/bin/cat";
+constexpr char kArcBugreportPipe[] = "/run/arc/bugreport/pipe";
+constexpr char kArcBugreportFile[] = "arc-bugreport.txt";
+
+constexpr char kTar[] = "/bin/tar";
+constexpr char kSystemLogs[] = "/var/log";
+
+// Executes 'bugreport' command in the container through the pipe, then writes
+// the output to |arc_bugreport_file|. When the container is not running,
+// the function returns immediately without writing anything.
+void WriteArcBugreport(const base::FilePath& arc_bugreport_file) {
+  brillo::ProcessImpl p;
+  p.AddArg(kCat);
+  p.AddArg(kArcBugreportPipe);
+  p.RedirectOutput(arc_bugreport_file.value());
+  p.Run();
+}
+
+}  // namespace
 
 void DebugLogsTool::GetDebugLogs(bool is_compressed,
                                  const DBus::FileDescriptor& fd,
                                  DBus::Error* error) {
+  base::ScopedTempDir arc_temp_dir;
+
+  // Create a temporary file and write ARC log to the file if ARC is running.
+  if (base::PathExists(base::FilePath(kArcBugreportPipe))) {
+    if (arc_temp_dir.CreateUniqueTempDir())
+      WriteArcBugreport(arc_temp_dir.path().Append(kArcBugreportFile));
+    else
+      PLOG(WARNING) << "Failed to create a temporary directory";
+  }
+
   brillo::ProcessImpl p;
   p.AddArg(kTar);
   p.AddArg("-c");
   if (is_compressed)
     p.AddArg("-z");
 
+  if (arc_temp_dir.IsValid()) {
+    p.AddArg("-C");
+    p.AddArg(arc_temp_dir.path().value());
+    p.AddArg(kArcBugreportFile);
+  }
   p.AddArg(kSystemLogs);
   p.BindFd(fd.get(), STDOUT_FILENO);
   p.Run();
