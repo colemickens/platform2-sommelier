colemickens/platform2-sommelier

commit b85bf3da4dce1a0eece3b5763adb6b2861ee3a9a

libchromeos-ui: Add Chromium --enable-features support.
Add ChromiumCommandBuilder::AddFeatureEnableOverride() and a
"enable-features=" directive to chrome_dev.conf, either of
which may be used to list features that should be passed via
Chromium's --enable-features flag.

BUG=chromium:603217
TEST=added unit tests

Change-Id: Ie4e6abd7b0217116243fc5e73fb07ff81c18a3bf
Reviewed-on: https://chromium-review.googlesource.com/364241
Commit-Ready: Dan Erat <derat@chromium.org>
Tested-by: Dan Erat <derat@chromium.org>
Reviewed-by: Jacob Dufault <jdufault@chromium.org>

libchromeos-ui/chromeos/ui/chromium_command_builder.cc
@@ -68,6 +68,19 @@ bool IsEnvironmentVariableName(const std::string& name) {
   return true;
 }
 
+// Updates |argument_index_to_update|, a arguments's position in |arguments_|,
+// in response to the argument at position |deleted_argument_index| being
+// removed. If the index-to-update is beyond the deleted index, it'll be
+// decremented; if it is itself being deleted, it'll be set to -1.
+void UpdateArgumentIndexForDeletion(int* argument_index_to_update,
+                                    int deleted_argument_index) {
+  DCHECK(argument_index_to_update);
+  if (*argument_index_to_update > deleted_argument_index)
+    (*argument_index_to_update)--;
+  else if (*argument_index_to_update == deleted_argument_index)
+    *argument_index_to_update = -1;
+}
+
 }  // namespace
 
 const char ChromiumCommandBuilder::kUser[] = "chronos";
@@ -85,7 +98,8 @@ ChromiumCommandBuilder::ChromiumCommandBuilder()
       gid_(0),
       is_chrome_os_hardware_(false),
       is_developer_end_user_(false),
-      vmodule_argument_index_(-1) {
+      vmodule_argument_index_(-1),
+      enable_features_argument_index_(-1) {
 }
 
 ChromiumCommandBuilder::~ChromiumCommandBuilder() {}
@@ -194,7 +208,7 @@ bool ChromiumCommandBuilder::SetUpChromium(const base::FilePath& xauth_path) {
     AddArg("--enable-arc");
 
   if (UseFlagIsSet("pointer_events"))
-    AddArg("--enable-features=PointerEvent");
+    AddFeatureEnableOverride("PointerEvent");
 
   AddArg("--enable-logging");
   AddArg("--log-level=1");
@@ -239,13 +253,12 @@ bool ChromiumCommandBuilder::ApplyUserConfig(const base::FilePath& path) {
       size_t num_copied = 0;
       for (size_t src_index = 0; src_index < arguments_.size(); ++src_index) {
         if (arguments_[src_index].find(pattern) == 0) {
-          // Drop the argument and shift |vmodule_argument_index_| forward if
-          // the argument precedes it (or reset the index if the --vmodule flag
-          // itself is being deleted).
-          if (vmodule_argument_index_ > static_cast<int>(src_index))
-            vmodule_argument_index_--;
-          else if (vmodule_argument_index_ == static_cast<int>(src_index))
-            vmodule_argument_index_ = -1;
+          // Drop the argument by not copying it and shift saved indexes if
+          // needed.
+          UpdateArgumentIndexForDeletion(&vmodule_argument_index_,
+                                         static_cast<int>(src_index));
+          UpdateArgumentIndexForDeletion(&enable_features_argument_index_,
+                                         static_cast<int>(src_index));
         } else {
           arguments_[num_copied] = arguments_[src_index];
           num_copied++;
@@ -257,6 +270,8 @@ bool ChromiumCommandBuilder::ApplyUserConfig(const base::FilePath& path) {
       base::SplitStringIntoKeyValuePairs(line, '=', '\n', &pairs);
       if (pairs.size() == 1U && pairs[0].first == "vmodule")
         AddVmodulePattern(pairs[0].second);
+      if (pairs.size() == 1U && pairs[0].first == "enable-features")
+        AddFeatureEnableOverride(pairs[0].second);
       else if (pairs.size() == 1U && IsEnvironmentVariableName(pairs[0].first))
         AddEnvVar(pairs[0].first, pairs[0].second);
       else
@@ -291,19 +306,34 @@ void ChromiumCommandBuilder::AddArg(const std::string& arg) {
 }
 
 void ChromiumCommandBuilder::AddVmodulePattern(const std::string& pattern) {
-  if (pattern.empty())
-    return;
+  AddListFlagEntry(&vmodule_argument_index_, "--vmodule=", ",", pattern);
+}
 
-  if (vmodule_argument_index_ < 0) {
-    AddArg("--vmodule=" + pattern);
-    vmodule_argument_index_ = arguments_.size() - 1;
-  } else {
-    arguments_[vmodule_argument_index_] += "," + pattern;
-  }
+void ChromiumCommandBuilder::AddFeatureEnableOverride(
+    const std::string& feature_name) {
+  AddListFlagEntry(&enable_features_argument_index_, "--enable-features=", ",",
+                   feature_name);
 }
 
 base::FilePath ChromiumCommandBuilder::GetPath(const std::string& path) const {
   return util::GetReparentedPath(path, base_path_for_testing_);
+}
+
+void ChromiumCommandBuilder::AddListFlagEntry(
+    int* flag_argument_index,
+    const std::string& flag_prefix,
+    const std::string& entry_separator,
+    const std::string& new_entry) {
+  DCHECK(flag_argument_index);
+  if (new_entry.empty())
+    return;
+
+  if (*flag_argument_index < 0) {
+    AddArg(flag_prefix + new_entry);
+    *flag_argument_index = arguments_.size() - 1;
+  } else {
+    arguments_[*flag_argument_index] += entry_separator + new_entry;
+  }
 }
 
 bool ChromiumCommandBuilder::SetUpX11(const base::FilePath& xauth_file) {

libchromeos-ui/chromeos/ui/chromium_command_builder_unittest.cc
@@ -187,6 +187,26 @@ TEST_F(ChromiumCommandBuilderTest, VmoduleFlag) {
             GetFirstArgWithPrefix(kVmodulePrefix));
 }
 
+TEST_F(ChromiumCommandBuilderTest, EnableFeatures) {
+  ASSERT_TRUE(Init());
+  ASSERT_TRUE(builder_.SetUpChromium(base::FilePath()));
+
+  const char kEnableFeaturesPrefix[] = "--enable-features=";
+  ASSERT_EQ("", GetFirstArgWithPrefix(kEnableFeaturesPrefix));
+  builder_.AddFeatureEnableOverride("foo");
+  ASSERT_EQ("--enable-features=foo",
+            GetFirstArgWithPrefix(kEnableFeaturesPrefix));
+  builder_.AddFeatureEnableOverride("bar");
+  ASSERT_EQ("--enable-features=foo,bar",
+            GetFirstArgWithPrefix(kEnableFeaturesPrefix));
+
+  // Add another argument and check that --enable-features still gets updated.
+  builder_.AddArg("--blah");
+  builder_.AddFeatureEnableOverride("baz");
+  ASSERT_EQ("--enable-features=foo,bar,baz",
+            GetFirstArgWithPrefix(kEnableFeaturesPrefix));
+}
+
 TEST_F(ChromiumCommandBuilderTest, UserConfig) {
   ASSERT_TRUE(Init());
   builder_.AddArg("--baz=4");
@@ -248,6 +268,42 @@ TEST_F(ChromiumCommandBuilderTest, UserConfigVmodule) {
             base::WriteFile(path, kConfig3, strlen(kConfig3)));
   ASSERT_TRUE(builder_.ApplyUserConfig(path));
   ASSERT_EQ("--vmodule=c=1,a=1,b=2", GetFirstArgWithPrefix("--vmodule="));
+}
+
+TEST_F(ChromiumCommandBuilderTest, UserConfigEnableFeatures) {
+  ASSERT_TRUE(Init());
+  builder_.AddArg("--foo");
+  builder_.AddFeatureEnableOverride("a");
+  builder_.AddArg("--bar");
+
+  // Check that we don't get confused when deleting flags surrounding the
+  // feature flag.
+  const char kConfig[] = "!--foo\n!--bar";
+  base::FilePath path(util::GetReparentedPath("/config.txt", base_path_));
+  ASSERT_EQ(strlen(kConfig), base::WriteFile(path, kConfig, strlen(kConfig)));
+  ASSERT_TRUE(builder_.ApplyUserConfig(path));
+  builder_.AddFeatureEnableOverride("b");
+  ASSERT_EQ("--enable-features=a,b",
+            GetFirstArgWithPrefix("--enable-features="));
+
+  // Delete the --enable-features flag.
+  const char kConfig2[] = "!--enable-features=";
+  ASSERT_EQ(strlen(kConfig2),
+            base::WriteFile(path, kConfig2, strlen(kConfig2)));
+  ASSERT_TRUE(builder_.ApplyUserConfig(path));
+  EXPECT_TRUE(builder_.arguments().empty());
+
+  // Now add another feature and check that the flag is re-added.
+  builder_.AddFeatureEnableOverride("c");
+  ASSERT_EQ("--enable-features=c", GetFirstArgWithPrefix("--enable-features="));
+
+  // Check that enable-features directives in config files are handled.
+  const char kConfig3[] = "enable-features=d\nenable-features=e";
+  ASSERT_EQ(strlen(kConfig3),
+            base::WriteFile(path, kConfig3, strlen(kConfig3)));
+  ASSERT_TRUE(builder_.ApplyUserConfig(path));
+  ASSERT_EQ("--enable-features=c,d,e",
+            GetFirstArgWithPrefix("--enable-features="));
 }
 
 TEST_F(ChromiumCommandBuilderTest, PepperPlugins) {

libchromeos-ui/chromeos/ui/chromium_command_builder.h
@@ -121,10 +121,23 @@ class ChromiumCommandBuilder {
   // Adds |pattern| to the --vmodule flag in |arguments_|.
   void AddVmodulePattern(const std::string& pattern);
 
+  // Adds |feature_name| to the --enable-features flag in |arguments_|.
+  void AddFeatureEnableOverride(const std::string& feature_name);
+
  private:
   // Converts absolute path |path| into a base::FilePath, rooting it under
   // |base_path_for_testing_| if it's non-empty.
   base::FilePath GetPath(const std::string& path) const;
+
+  // Adds an entry to a flag containing a list of values. For example, for a
+  // flag like "--my-list=foo,bar", |flag_prefix| would be "--my-list=",
+  // |entry_separator| would be ",", and |new_entry| would be "foo" or "bar".
+  // |flag_argument_index|'s memory holds the flag's position within
+  // |arguments_| or -1 if the flag is not yet set.
+  void AddListFlagEntry(int* flag_argument_index,
+                        const std::string& flag_prefix,
+                        const std::string& entry_separator,
+                        const std::string& new_entry);
 
   // Performs X11-specific setup and returns true on success. Called by
   // InitChromium().
@@ -164,9 +177,10 @@ class ChromiumCommandBuilder {
   // Command-line arguments that the caller should pass to the executable.
   StringVector arguments_;
 
-  // Index in |arguments_| of the --vmodule flag. -1 if the flag hasn't been
-  // set.
+  // Index in |arguments_| of the --vmodule and --enable-features flags. -1 if
+  // the flags haven't been set.
   int vmodule_argument_index_;
+  int enable_features_argument_index_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromiumCommandBuilder);
 };

login_manager/chrome_dev.conf
@@ -1,3 +1,7 @@
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
 # This file may be modified to make local changes to the environment and command
 # line that session_manager uses to run Chrome. It contains one directive per
 # line, with the following forms available:
@@ -15,6 +19,12 @@
 #   listed on separate "vmodule=" lines. A --vmodule argument containing all of
 #   the requested patterns will be automatically constructed and passed to
 #   Chrome; do not explicitly add a "--vmodule=" line.
+#
+# enable-features=foo
+#   Adds a "foo" feature to the --enable-features argument. Multiple features my
+#   be listed on separate "enable-features" lines. An --enable-features argument
+#   containing all of the requested patterns will be automatically constructed
+#   and passed to Chrome; do not explicitly add a "--enable-features=" line.
 #
 # NAME=some value
 #   Sets the environment variable "NAME" to the value "some value". As above, do

commit 33e22701cdbc23ab1f1047a98a3c747ee7a07b62

libchromeos-ui: Avoid adding extraneous "vmodule=" args.
Fix a bug I introduced to ChromiumCommandBuilder in b85bf3da
where a missing "else" after the code for handling
"vmodule=" directives in user configs resulted in the
"vmodule=" directive also being added as a standalone arg at
the end of the command line.

BUG=none
TEST=added tests

Change-Id: I6fe85d8d149ce8b960356c1621aac784463dc918
Reviewed-on: https://chromium-review.googlesource.com/405687
Commit-Ready: Dan Erat <derat@chromium.org>
Tested-by: Dan Erat <derat@chromium.org>
Reviewed-by: Jacob Dufault <jdufault@chromium.org>

libchromeos-ui/chromeos/ui/chromium_command_builder.cc
@@ -290,7 +290,7 @@ bool ChromiumCommandBuilder::ApplyUserConfig(const base::FilePath& path) {
       base::SplitStringIntoKeyValuePairs(line, '=', '\n', &pairs);
       if (pairs.size() == 1U && pairs[0].first == "vmodule")
         AddVmodulePattern(pairs[0].second);
-      if (pairs.size() == 1U && pairs[0].first == "enable-features")
+      else if (pairs.size() == 1U && pairs[0].first == "enable-features")
         AddFeatureEnableOverride(pairs[0].second);
       else if (pairs.size() == 1U && IsEnvironmentVariableName(pairs[0].first))
         AddEnvVar(pairs[0].first, pairs[0].second);

libchromeos-ui/chromeos/ui/chromium_command_builder_unittest.cc
@@ -232,6 +232,8 @@ TEST_F(ChromiumCommandBuilderTest, UserConfig) {
 }
 
 TEST_F(ChromiumCommandBuilderTest, UserConfigVmodule) {
+  const char kPrefix[] = "--vmodule=";
+
   ASSERT_TRUE(Init());
   builder_.AddArg("--foo");
   builder_.AddVmodulePattern("a=2");
@@ -244,7 +246,7 @@ TEST_F(ChromiumCommandBuilderTest, UserConfigVmodule) {
   ASSERT_EQ(strlen(kConfig), base::WriteFile(path, kConfig, strlen(kConfig)));
   ASSERT_TRUE(builder_.ApplyUserConfig(path));
   builder_.AddVmodulePattern("b=1");
-  ASSERT_EQ("--vmodule=a=2,b=1", GetFirstArgWithPrefix("--vmodule="));
+  ASSERT_EQ("--vmodule=a=2,b=1", GetFirstArgWithPrefix(kPrefix));
 
   // Delete the --vmodule flag.
   const char kConfig2[] = "!--vmodule=";
@@ -255,17 +257,22 @@ TEST_F(ChromiumCommandBuilderTest, UserConfigVmodule) {
 
   // Now add another vmodule pattern and check that the flag is re-added.
   builder_.AddVmodulePattern("c=1");
-  ASSERT_EQ("--vmodule=c=1", GetFirstArgWithPrefix("--vmodule="));
+  ASSERT_EQ("--vmodule=c=1", GetFirstArgWithPrefix(kPrefix));
 
   // Check that vmodule directives in config files are handled.
   const char kConfig3[] = "vmodule=a=1\nvmodule=b=2";
   ASSERT_EQ(strlen(kConfig3),
             base::WriteFile(path, kConfig3, strlen(kConfig3)));
   ASSERT_TRUE(builder_.ApplyUserConfig(path));
-  ASSERT_EQ("--vmodule=c=1,a=1,b=2", GetFirstArgWithPrefix("--vmodule="));
+  ASSERT_EQ("--vmodule=c=1,a=1,b=2", GetFirstArgWithPrefix(kPrefix));
+
+  // Also check that literal "vmodule=..." arguments don't get added.
+  ASSERT_EQ("", GetFirstArgWithPrefix("vmodule="));
 }
 
 TEST_F(ChromiumCommandBuilderTest, UserConfigEnableFeatures) {
+  const char kPrefix[] = "--enable-features=";
+
   ASSERT_TRUE(Init());
   builder_.AddArg("--foo");
   builder_.AddFeatureEnableOverride("a");
@@ -278,8 +285,7 @@ TEST_F(ChromiumCommandBuilderTest, UserConfigEnableFeatures) {
   ASSERT_EQ(strlen(kConfig), base::WriteFile(path, kConfig, strlen(kConfig)));
   ASSERT_TRUE(builder_.ApplyUserConfig(path));
   builder_.AddFeatureEnableOverride("b");
-  ASSERT_EQ("--enable-features=a,b",
-            GetFirstArgWithPrefix("--enable-features="));
+  ASSERT_EQ("--enable-features=a,b", GetFirstArgWithPrefix(kPrefix));
 
   // Delete the --enable-features flag.
   const char kConfig2[] = "!--enable-features=";
@@ -290,15 +296,17 @@ TEST_F(ChromiumCommandBuilderTest, UserConfigEnableFeatures) {
 
   // Now add another feature and check that the flag is re-added.
   builder_.AddFeatureEnableOverride("c");
-  ASSERT_EQ("--enable-features=c", GetFirstArgWithPrefix("--enable-features="));
+  ASSERT_EQ("--enable-features=c", GetFirstArgWithPrefix(kPrefix));
 
   // Check that enable-features directives in config files are handled.
   const char kConfig3[] = "enable-features=d\nenable-features=e";
   ASSERT_EQ(strlen(kConfig3),
             base::WriteFile(path, kConfig3, strlen(kConfig3)));
   ASSERT_TRUE(builder_.ApplyUserConfig(path));
-  ASSERT_EQ("--enable-features=c,d,e",
-            GetFirstArgWithPrefix("--enable-features="));
+  ASSERT_EQ("--enable-features=c,d,e", GetFirstArgWithPrefix(kPrefix));
+
+  // Also check that literal "enable-features=..." arguments don't get added.
+  ASSERT_EQ("", GetFirstArgWithPrefix("enable-features="));
 }
 
 TEST_F(ChromiumCommandBuilderTest, PepperPlugins) {