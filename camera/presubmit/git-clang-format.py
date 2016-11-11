dd1b7b46934f734010ae244c677eb7d64a317705
Ricky Liang <jcliang@chromium.org>
2016-11-12 01:10:28 +0800

    Add presubmit hooks
    Adds two presubmit hooks:
      1. cros lint: Lints source files to make sure they follow Chromium style.
      2. git-clang-format: Uses git-clang-format to check formatting errors.
    
    User can also run presubmit/git-clang-format.py to fix formatting errors
    automatically.
    
    BUG=b:30824236
    TEST=Make sure presubmit checks run and pass with `repo upload`.
    
    Change-Id: I8029611ff504425c5df703d29c42121c9f995593
    Reviewed-on: https://chromium-review.googlesource.com/410464
    Commit-Ready: Ricky Liang <jcliang@chromium.org>
    Tested-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Wu-cheng Li <wuchengli@chromium.org>

---
+++ camera/presubmit/git-clang-format.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A python wrapper to check style issues with git-clang-format"""
+
+from __future__ import print_function
+
+import argparse
+import os
+import re
+import subprocess
+import sys
+
+
+DIFF_FILEPATH_REGEXP = re.compile(r'^\+\+\+ b/(?P<file_path>.*)$',
+                                  flags=re.MULTILINE)
+
+# Use git-clang-format from upstream as it supports per-commit checks.
+# TODO(jcliang): Remove this once sys-devel/llvm package is upgraded.
+CLANG_FORMAT = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            'upstream-git-clang-format')
+
+
+def main(argv):
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--commit', type=str, help='The commit to check.')
+  parser.add_argument('--fix', action='store_true',
+                      help='Fix any formatting errors automatically.')
+  args = parser.parse_args(argv)
+
+  cmd = [CLANG_FORMAT, '--style', 'Chromium', '--diff', '%s^' % args.commit,
+         args.commit]
+  stdout = subprocess.check_output(cmd, universal_newlines=True)
+
+  if stdout.strip() == 'no modified files to format':
+    return 0
+
+  diff_files = DIFF_FILEPATH_REGEXP.findall(stdout)
+
+  if diff_files:
+    if args.fix:
+      proc = subprocess.Popen(['git', 'apply'], stdin=subprocess.PIPE,
+                              universal_newlines=True)
+      proc.communicate(input=stdout)
+      proc.wait()
+    else:
+      print('The following files have formatting errors:')
+      for f in diff_files:
+        print('\t%s' % f)
+      print('You can run `%s --fix %s` to fix this.' %
+            (__file__, ' '.join(argv)))
+      return 1
+
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))
