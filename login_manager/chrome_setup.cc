17058fe0c9b0367a5e5b2e188f0d0b233c5e9dae
David Reveman <reveman@chromium.org>
2017-01-14 17:26:12 -0500

    login: Enable GLImage output for Pepper3D on kevin.
    Makes it possible to use HW overlays for output.
    
    BUG=chromium:680851
    TEST=emerge-kevin chromeos-base/chromeos-login
    
    Change-Id: I7c379d3311fdf81a68e6c05e0da43da80f1cba18
    Reviewed-on: https://chromium-review.googlesource.com/428550
    Commit-Ready: David Reveman <reveman@chromium.org>
    Tested-by: David Reveman <reveman@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- login_manager/chrome_setup.cc
+++ login_manager/chrome_setup.cc
@@ -224,6 +224,7 @@ void AddUiFlags(ChromiumCommandBuilder* builder,
     builder->AddArg("--ash-enable-palette");
 
   if (builder->UseFlagIsSet("kevin")) {
+    builder->AddFeatureEnableOverride("Pepper3DImageChromium");
     // TODO(jdufault): Remove this once quick unlock launches on all boards.
     builder->AddFeatureEnableOverride("QuickUnlockPin");
 
