160f5091a0baa08670c79dc80d5fdb9c4274e83f
Junichi Uekawa <uekawa@google.com>
2017-02-24 11:31:49 +0900

    debugd: Add contents of /proc/self/mountinfo to the feedback reports.
    Mount info shouldn't expose something user-specific, but is often
    necessary for debugging when the mount gets weird.
    
    BUG=b:28658287
    TEST=view chrome://system on minnie
    
    Change-Id: I6e7fb50dc78d4410acba38f161b04ded7ab85afe
    Reviewed-on: https://chromium-review.googlesource.com/446144
    Commit-Ready: Junichi Uekawa <uekawa@chromium.org>
    Tested-by: Junichi Uekawa <uekawa@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>

--- debugd/src/log_tool.cc
+++ debugd/src/log_tool.cc
@@ -113,6 +113,7 @@ const Log common_logs[] = {
   { "meminfo", "cat /proc/meminfo" },
   { "memory_spd_info", "/bin/cat /var/log/memory_spd_info.txt" },
   { "mount-encrypted", "/bin/cat /var/log/mount-encrypted.log" },
+  { "mountinfo", "/bin/cat /proc/self/mountinfo" },
   { "net-diags.net.log", "/bin/cat /var/log/net-diags.net.log" },
   { "netlog", "/usr/share/userfeedback/scripts/getmsgs --last '2 hours'"
               " /var/log/net.log" },
