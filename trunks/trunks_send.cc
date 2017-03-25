9ea3274a1d06ae6863f2177c85068bc2dfaa705b
Vadim Bendebury <vbendeb@chromium.org>
2017-03-24 18:25:05 -0700

    trunks_send: add cr50 firmware update capability
    This patch extends trunks_send functionality with the capability to
    upload cr50 firmware update using the same protocol as one used by
    ./extra/usb_updater/usb_updater.c.
    
    This allows to update cr50 in the background over both I2C and SPI tpm
    interfaces without disrupting trunksd.
    
    The new code prints notices to stdout and errors to stderr, so that
    when ran from the command line the progress is visible to the operator.
    When run from the script the output will have to be redirected into the
    log.
    
    BUG=b:35580805
    TEST=verified updates on both gru(SPI) and pyro(I2C)
    
    Change-Id: Id3e89326e7358cf4831a35397a42cd2d680e9a97
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Signed-off-by: Andrey Pronin <apronin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/459702
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- trunks/trunks_send.cc
+++ trunks/trunks_send.cc
@@ -15,8 +15,10 @@
 //
 
 #include <stdio.h>
+#include <openssl/sha.h>
 
 #include <base/command_line.h>
+#include <base/files/file_util.h>
 #include <base/strings/string_number_conversions.h>
 #include <base/strings/string_util.h>
 #include <base/sys_byteorder.h>
@@ -29,11 +31,17 @@ namespace {
 using trunks::TrunksDBusProxy;
 
 // Commands we support
-constexpr char kRaw[] = "raw";
+constexpr char kForce[] = "force";
 constexpr char kGetLock[] = "get_lock";
+constexpr char kRaw[] = "raw";
 constexpr char kSetLock[] = "set_lock";
 constexpr char kSysInfo[] = "sysinfo";
+constexpr char kUpdate[] = "update";
 constexpr char kVerbose[] = "verbose";
+
+// Maximum image update block size expected by Cr50.
+// Equals to SIGNED_TRANSFER_SIZE in src/platform/ec/chip/g/update_fw.h
+static const uint32_t kTransferSize = 1024;
 
 static int verbose;
 
@@ -43,6 +51,7 @@ void PrintUsage() {
   printf("  trunks_send --%s\n", kSetLock);
   printf("  trunks_send --%s\n", kSysInfo);
   printf("  trunks_send --%s XX [XX ..]\n", kRaw);
+  printf("  trunks_send [--%s] --%s <bin file>\n", kForce, kUpdate);
   printf("Options:\n");
   printf("   --%s\n", kVerbose);
 }
@@ -66,9 +75,13 @@ struct TpmCmdHeader{
 // subcommand_code in struct TpmCmdHeader to further distinguish the desired
 // operation.
 #define TPM_CC_VENDOR_BIT   0x20000000
-#define VENDOR_CC_MASK      0x0000ffff
+
 // Vendor-specific command codes
 #define TPM_CC_VENDOR_CR50      0x0000
+
+// This needs to be used to be backwards compatible with older Cr50 versions.
+#define CR50_EXTENSION_COMMAND    0xbaccd00a
+#define CR50_EXTENSION_FW_UPGRADE 4
 
 // Cr50 vendor-specific subcommand codes. 16 bits available.
 enum vendor_cmd_cc {
@@ -126,12 +139,16 @@ static int HandleRaw(TrunksDBusProxy* proxy, base::CommandLine* cl) {
 // Send the TPM command, get the reply, return response code and results.
 static uint32_t VendorCommand(TrunksDBusProxy* proxy, uint16_t cc,
                               const std::string& input,
-                              std::string* output) {
+                              std::string* output,
+                              bool extendedCommandMode = false) {
   // Pack up the header and the input
   struct TpmCmdHeader header;
   header.tag = base::HostToNet16(trunks::TPM_ST_NO_SESSIONS);
   header.size = base::HostToNet32(sizeof(header) + input.size());
-  header.code = base::HostToNet32(TPM_CC_VENDOR_BIT | TPM_CC_VENDOR_CR50);
+  if (extendedCommandMode)
+    header.code = base::HostToNet32(CR50_EXTENSION_COMMAND);
+  else
+    header.code = base::HostToNet32(TPM_CC_VENDOR_BIT | TPM_CC_VENDOR_CR50);
   header.subcommand_code = base::HostToNet16(cc);
 
   std::string command(reinterpret_cast<char*>(&header), sizeof(header));
@@ -169,6 +186,325 @@ static uint32_t VendorCommand(TrunksDBusProxy* proxy, uint16_t cc,
   *output = response.substr(sizeof(header));
 
   return header.code;
+}
+
+//
+// A convenience structure which allows to group together various revision
+// fields of the header created by the signer.
+//
+// These fields are compared when deciding if versions of two images are the
+// same or when deciding which one of the available images to run.
+//
+// Originally defined in src/platform/ec/chip/g/upgrade_fw.h
+//
+struct SignedHeaderVersion {
+  uint32_t minor;
+  uint32_t major;
+  uint32_t epoch;
+};
+
+//
+// Response to the connection establishment request.
+//
+// All protocol versions starting with version 2 respond to the very first
+// packet with an 8 byte or larger response, where the first 4 bytes are a
+// version specific data, and the second 4 bytes - the protocol version
+// number.
+//
+// Originally defined in src/platform/ec/chip/g/upgrade_fw.h
+//
+struct FirstResponsePdu {
+  uint32_t return_value;
+
+  // The below fields are present in versions 2 and up.
+  uint32_t protocol_version;
+
+  // The below fields are present in versions 3 and up.
+  uint32_t  backup_ro_offset;
+  uint32_t  backup_rw_offset;
+
+  // The below fields are present in versions 4 and up.
+  // Versions of the currently active RO and RW sections.
+  SignedHeaderVersion shv[2];
+
+  // The below fields are present in versions 5 and up
+  // keyids of the currently active RO and RW sections.
+  uint32_t keyid[2];
+};
+
+struct UpdatePduHeader {
+  uint32_t pdu_digest;
+  uint32_t pdu_base_offset;
+};
+
+//
+// Cr50 image header.
+//
+// Based on SignedHeader defined in src/platform/ec/chip/g/signed_header.h
+//
+struct EssentialHeader {
+  uint32_t magic;
+  uint32_t padding0[201];
+  uint32_t image_size;
+  uint32_t padding1[12];
+  uint32_t epoch;
+  uint32_t major;
+  uint32_t minor;
+};
+
+//
+// Wraps a block of image into a Vendor Command PDU and sends it to the device.
+//
+// Wrapping includes creating a header containing the digest of the entire PDU
+// and the offset to program the PDU contents int the device's flash.
+//
+// |data| points to the entire firmware image containing RO and RW sections.
+// |data_offset| is the offset into the image and into the flash memory, and
+// |block_size| is the number of bytes to be tranferred with this block.
+//
+// Returns true on success, false on error.
+//
+static bool TransferBlock(TrunksDBusProxy* proxy,
+                          const char* data,
+                          size_t data_offset,
+                          size_t block_size) {
+  uint8_t digest[SHA_DIGEST_LENGTH];
+  UpdatePduHeader updu;
+  SHA_CTX shaCtx;
+  std::string response;
+
+  printf("sending 0x%zx bytes to offset %#zx\n", block_size, data_offset);
+
+  updu.pdu_base_offset = base::NetToHost32(data_offset);
+  SHA1_Init(&shaCtx);
+  SHA1_Update(&shaCtx, &updu.pdu_base_offset, sizeof(updu.pdu_base_offset));
+  SHA1_Update(&shaCtx, data + data_offset, block_size);
+  SHA1_Final(digest, &shaCtx);
+
+  memcpy(&updu.pdu_digest, digest, sizeof(updu.pdu_digest));
+
+  std::string request =
+      std::string(reinterpret_cast<char*>(&updu), sizeof(updu)) +
+      std::string(data + data_offset, block_size);
+
+  uint32_t rv = VendorCommand(proxy, CR50_EXTENSION_FW_UPGRADE, request,
+                              &response, true);
+  if (rv) {
+    LOG(ERROR) << "Failed to transfer image block, got 0x" << std::hex << rv;
+    return false;
+  }
+
+  if (response.size() != 1) {
+    LOG(ERROR) << "Unexpected return size " << response.size();
+    return false;
+  }
+
+  if (response.data()[0]) {
+    rv = response.data()[0];
+    LOG(ERROR) << "Error " << rv;
+    return false;
+  }
+
+  return true;
+}
+
+//
+// Sends to the TPM the first transfer PDU, it is just 8 bytes of zeros. Verify
+// the expected response (which is of FirstResponsePdu structure).
+//
+// Returns true on success, false on error.
+//
+static bool SetupConnection(TrunksDBusProxy* proxy,
+                            FirstResponsePdu* rpdu) {
+  // Connection setup is triggered by 8 bytes of zeros.
+  std::string request(8, 0);
+  std::string response;
+
+  uint32_t rv = VendorCommand(proxy, CR50_EXTENSION_FW_UPGRADE,
+                              request, &response, true);
+  if (rv) {
+    LOG(ERROR) << "Failed to set up connection, got 0x" << std::hex << rv;
+    return false;
+  }
+
+  // We got something. Check for errors.
+  if (response.size() < sizeof(FirstResponsePdu)) {
+    LOG(ERROR) << "Unexpected response size " << response.size();
+    return false;
+  }
+
+  // Let's unmarshal the response
+  memcpy(rpdu, response.data(), std::min(sizeof(*rpdu), response.size()));
+
+  rpdu->return_value = base::NetToHost32(rpdu->return_value);
+  if (rpdu->return_value) {
+    LOG(ERROR) << "Target reporting error 0x" << std::hex << rpdu->return_value;
+    return false;
+  }
+
+  rpdu->protocol_version = base::NetToHost32(rpdu->protocol_version);
+  if (rpdu->protocol_version < 5) {
+    LOG(ERROR) << "Unsupported protocol version " << rpdu->protocol_version;
+    return false;
+  }
+  printf("protocol version: %d\n", rpdu->protocol_version);
+  rpdu->backup_ro_offset = base::NetToHost32(rpdu->backup_ro_offset);
+  rpdu->backup_rw_offset = base::NetToHost32(rpdu->backup_rw_offset);
+
+  for (int i = 0; i < arraysize(FirstResponsePdu::shv); i++) {
+    rpdu->shv[i].minor = base::NetToHost32(rpdu->shv[i].minor);
+    rpdu->shv[i].major = base::NetToHost32(rpdu->shv[i].major);
+    rpdu->shv[i].epoch = base::NetToHost32(rpdu->shv[i].epoch);
+  }
+
+  printf("offsets: backup RO at %#x, backup RW at %#x\n",
+         rpdu->backup_ro_offset, rpdu->backup_rw_offset);
+  return true;
+}
+
+//
+// Compares version fields in the header of the new image to the versions
+// running on the target. Returns true if the new image is newer.
+//
+static bool ImageIsNewer(const EssentialHeader &header,
+                         const SignedHeaderVersion &shv) {
+
+  if (header.epoch != shv.epoch)
+    return header.epoch > shv.epoch;
+  if (header.major != shv.major)
+    return header.major > shv.major;
+  return header.minor > shv.minor;
+}
+
+//
+// Updates RO or RW section of the Cr50 image on the device.
+// A section is updated only if it's newer than the one currently on the
+// device, or if |force| is set to true.
+//
+// |update_image| is the entire 512K file produced by the builder,
+// |section_offset| is the offset of either inactive RO or inactive RW on
+// the device, |shv| communicates this section's version retrieved from the
+// device.
+//
+// Returns true on success, false on error. Skipping an update if the current
+// version is not older than the one in |update_image| is considered a success.
+//
+static bool TransferSection(TrunksDBusProxy* proxy,
+                            std::string &update_image,
+                            uint32_t section_offset,
+                            const SignedHeaderVersion &shv,
+                            bool force) {
+  EssentialHeader header;
+
+  // Try reading the header into the structure.
+  if ((section_offset + sizeof(EssentialHeader)) > update_image.size()) {
+    LOG(ERROR) << "Header at offset 0x" << std::hex << section_offset
+               << " does not fit into the image of "
+               << std::dec << update_image.size() << " bytes";
+    return false;
+  }
+  memcpy(&header, update_image.data() + section_offset, sizeof(header));
+
+  if (header.magic != 0xffffffff) {
+    LOG(ERROR) << "Wrong magic value 0x" << std::hex << header.magic
+               << " at offset 0x" << std::hex << section_offset;
+    return false;
+  }
+
+  if (header.image_size > (update_image.size() - section_offset)) {
+    LOG(ERROR) << "Wrong section size 0x" << std::hex << header.image_size
+               << " at offset 0x" << std::hex << section_offset;
+    return false;
+  }
+
+  printf("Offset %#x file at %d.%d.%d device at %d.%d.%d, section size %d\n",
+         section_offset,
+         header.epoch, header.major, header.minor,
+         shv.epoch, shv.major, shv.minor,
+         header.image_size);
+  if (!force && !ImageIsNewer(header, shv)) {
+    printf("Skipping update\n");
+    return true;
+  }
+
+  // Transfer section, one block at a time.
+  size_t block_size;
+  for (uint32_t transferred = 0; transferred < header.image_size;
+       transferred += block_size) {
+    block_size = std::min(header.image_size - transferred, kTransferSize);
+    if (!TransferBlock(proxy, update_image.data(), section_offset + transferred,
+                       block_size)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+//
+// Updates the Cr50 image on the device. |update_image| contains the entire
+// new Cr50 image.
+// Each of the Cr50 sections is updated only if it's newer than the one
+// currently on the device, or if |force| is set to true. Otherwise the
+// session is skipped. The information about the section offsets and current
+// versions is taken from the response to the connection request |rpdu| received
+// from the device earlier.
+//
+// Returns the number of successfully updated sections (including skipped), or
+// a negative value in case of error.
+//
+static int TransferImage(TrunksDBusProxy* proxy,
+                         std::string &update_image,
+                         const FirstResponsePdu &rpdu,
+                         bool force) {
+  int num_txed_sections = 0;
+
+  if (!TransferSection(proxy, update_image, rpdu.backup_ro_offset,
+                       rpdu.shv[0], force)) {
+    if (!force) {
+      return -1;
+    }
+  } else {
+    num_txed_sections++;
+  }
+
+  if (!TransferSection(proxy, update_image, rpdu.backup_rw_offset,
+                       rpdu.shv[1], force)) {
+    if (!force) {
+      return -1;
+    }
+  } else {
+    num_txed_sections++;
+  }
+
+  return num_txed_sections;
+}
+
+// Updathe the Cr50 image on the device.
+static int HandleUpdate(TrunksDBusProxy* proxy, base::CommandLine* cl) {
+  if (cl->GetArgs().size() != 1) {
+    LOG(ERROR) << "A single image file name must be provided.";
+    return 1;
+  }
+
+  base::FilePath filename(cl->GetArgs()[0]);
+
+  std::string update_image;
+  if (!base::ReadFileToString(filename, &update_image)) {
+    LOG(ERROR) << "Failed to read " << filename.value();
+    return 1;
+  }
+
+  FirstResponsePdu rpdu;
+  if (!SetupConnection(proxy, &rpdu)) {
+    return 1;
+  }
+
+  if (TransferImage(proxy, update_image, rpdu, cl->HasSwitch(kForce)) < 0) {
+    return 1;
+  }
+
+  return 0;
 }
 
 // Vendor command to get the console lock state
@@ -272,6 +608,9 @@ int main(int argc, char** argv) {
 
   if (cl->HasSwitch(kSysInfo))
     return VcSysInfo(&proxy, cl);
+
+  if (cl->HasSwitch(kUpdate))
+    return HandleUpdate(&proxy, cl);
 
   PrintUsage();
   return 1;
