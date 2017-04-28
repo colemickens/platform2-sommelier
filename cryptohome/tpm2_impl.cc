0eeed993adc5f6c4165264caa80328e1a11b5bdd
Yunlian Jiang <yunlian@google.com>
2017-04-28 08:55:43 -0700

    cryptohome: fix unused lambda capture.
    This fixes the error
    lambda capture 'this' is not used [-Werror,-Wunused-lambda-capture]
    
    BUG=chromium:714983
    TEST=FEATURES="test" emerge-reef cryptohome
    
    Change-Id: I64dfd2ad3527a4023873928d4bdb0e9e5d0ac234
    Reviewed-on: https://chromium-review.googlesource.com/490487
    Commit-Queue: Yunlian Jiang <yunlian@chromium.org>
    Tested-by: Yunlian Jiang <yunlian@chromium.org>
    Trybot-Ready: Yunlian Jiang <yunlian@chromium.org>
    Reviewed-by: Manoj Gupta <manojgupta@chromium.org>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>

--- cryptohome/tpm2_impl.cc
+++ cryptohome/tpm2_impl.cc
@@ -1079,7 +1079,7 @@ void Tpm2Impl::SendTpmManagerRequestAndWait(const MethodType& method,
       });
   tpm_manager_thread_.task_runner()->PostTask(
       FROM_HERE,
-      base::Bind([this, &method, &callback]() {
+      base::Bind([&method, &callback]() {
         method.Run(callback); }));
   event.Wait();
 }
