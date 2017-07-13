e6a25831af390d6421ef94f08b5510e48401a491
Ricky Liang <jcliang@chromium.org>
2017-07-13 19:41:32 +0800

    hal_adapter: respawn the camera-halv3-adapter service through upstrart
    Instead of implementing our own respawn logic let's let upstart handle
    the service respawn. Also set a respawn limit of no more than 10 times
    within 5 seconds in the upstart script, in case anything goes haywire
    causing the service to respawn indefinitely.
    
    BUG=b:36233258
    TEST=On poppy, stop ui && start ui and make sure camera still works
    TEST=On poppy, delete the camera_hal.so library and verify the service
         does not respawn indefinitely.
    
    Change-Id: I88e002976cf48e90a3c41bbe0e58fbedf3c3ef51
    Reviewed-on: https://chromium-review.googlesource.com/570140
    Commit-Ready: Ricky Liang <jcliang@chromium.org>
    Tested-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>

--- camera/hal_adapter/arc_camera3_main.cc
+++ camera/hal_adapter/arc_camera3_main.cc
@@ -41,61 +41,39 @@ int main(int argc, char* argv[]) {
   // Override the log items set by brillo::InitLog.
   SetLogItems();
 
-  while (true) {
-    pid_t pid = fork();
+  pid_t pid = fork();
 
-    // Start the CameraHalServerImpl on the child process.  The process
-    // will exit on error.  The main process will respawn the child process
-    // to resurrect CameraHalServerImpl.
-    if (!pid) {
-      // Child process: Starts ARC camera3 service provider which will host the
-      // camera HAL adapter.
-      LOGF(INFO) << "Starting ARC camera3 service provider...";
+  // Start the CameraHalServerImpl on the child process.  The process
+  // will exit on error.
+  if (!pid) {
+    // Child process: Starts ARC camera3 service provider which will host the
+    // camera HAL adapter.
 
-      // Create the daemon instance first to properly set up MessageLoop and
-      // AtExitManager.
-      brillo::Daemon daemon;
+    // Create the daemon instance first to properly set up MessageLoop and
+    // AtExitManager.
+    brillo::Daemon daemon;
 
-      arc::CameraHalServerImpl service_provider;
-      if (!service_provider.Start()) {
-        LOGF(ERROR) << "Failed to start ARC camera3 service provider";
-        return ECANCELED;
-      }
-
-      // The child process runs until an error happens which will terminate the
-      // process.
-      daemon.Run();
-      LOGF(ERROR) << "daemon stopped";
-      return 0;
-    } else if (pid > 0) {
-      // Parent process: Waits until child process exits, and respawns a new
-      // child process.
-
-      // Blocks until child process exits.
-      int wstatus;
-      wait(&wstatus);
-      LOGF(INFO) << "Child exited: status=" << WEXITSTATUS(wstatus);
-
-#if !defined(NDEBUG)
-      // For debug build we respawn the service provider only when the Mojo
-      // connection is aborted by remote (i.e. the Chrome ArcCamera3Service);
-      // in case the HAL crashes, we stop respawning the service provider.
-      // This helps us identify potential bugs in HAL more easily.
-      //
-      // For release builds we always respawn the service provider.
-      //
-      // Known issue: On debug builds, `stop ui` or browser process crash will
-      // CHECK inside libmojo due to a race condition and stop the service
-      // provider.  This is not an issue for release builds as we will always
-      // respawn the child process to resurrect the service provider.
-      if (!WIFEXITED(wstatus) || WEXITSTATUS(wstatus) != ECONNRESET) {
-        LOGF(ERROR) << "Unexpected error killed the service provider process";
-        break;
-      }
-#endif
-    } else {
-      PLOGF(ERROR) << "fork() failed";
+    arc::CameraHalServerImpl service_provider;
+    if (!service_provider.Start()) {
+      LOGF(ERROR) << "Failed to start camera HAL v3 adapter";
+      return ECANCELED;
     }
+
+    // The child process runs until an error happens which will terminate the
+    // process.
+    LOGF(INFO) << "Started camera HAL v3 adapter";
+    daemon.Run();
+    LOGF(ERROR) << "daemon stopped";
+    return 0;
+  } else if (pid > 0) {
+    // Parent process: Waits until child process exits and report exit status.
+
+    // Blocks until child process exits.
+    int wstatus;
+    wait(&wstatus);
+    LOGF(INFO) << "Child exited: status=" << WEXITSTATUS(wstatus);
+  } else {
+    PLOGF(ERROR) << "fork() failed";
+    return pid;
   }
-  return 0;
 }
