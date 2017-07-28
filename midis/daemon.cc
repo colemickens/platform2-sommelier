6789d660471dc274a5d2f3007a9cc5fe2300f31c
Prashant Malani <pmalani@google.com>
2017-07-28 15:03:40 -0700

    midis: Add skeleton D-Bus support.
    We are moving to a new IPC mechanism (Mojo bootstrapped by D-Bus).
    
    As a first step, we need to establish a D-Bus service which the client
    code (Chrome) can talk to. This patch adds that service.
    
    BUG=chromium:701005
    TEST=Run midis on veyron_minnie, call Bootstrap method successfully
    using dbus-send commandline.
    CQ-DEPEND=CL:592638
    
    Change-Id: I1a6ab50cccb70b49e67af8f72243c35e1b4c46dc
    Reviewed-on: https://chromium-review.googlesource.com/592277
    Commit-Ready: Prashant Malani <pmalani@google.com>
    Tested-by: Prashant Malani <pmalani@google.com>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Ben Chan <benchan@chromium.org>

--- midis/daemon.cc
+++ midis/daemon.cc
@@ -2,15 +2,29 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <base/bind.h>
 #include <base/memory/ptr_util.h>
+#include <dbus/bus.h>
+#include <dbus/message.h>
 
 #include "midis/daemon.h"
+
+namespace {
+
+// TODO(pmalani): Move these into system_api once names are finalized.
+constexpr char kMidisServiceName[] = "org.chromium.Midis";
+constexpr char kMidisServicePath[] = "/org/chromium/Midis";
+constexpr char kMidisInterfaceName[] = "org.chromium.Midis";
+constexpr char kBootstrapMojoConnectionMethod[] = "BootstrapMojoConnection";
+
+}  // namespace
 
 namespace midis {
 
 Daemon::Daemon()
     : device_tracker_(base::MakeUnique<DeviceTracker>()),
-      client_tracker_(base::MakeUnique<ClientTracker>()) {}
+      client_tracker_(base::MakeUnique<ClientTracker>()),
+      weak_factory_(this) {}
 
 Daemon::~Daemon() {}
 
@@ -23,7 +37,35 @@ int Daemon::OnInit() {
     return -1;
   }
 
+  InitDBus();
   return 0;
+}
+
+void Daemon::InitDBus() {
+  dbus::Bus::Options options;
+  options.bus_type = dbus::Bus::SYSTEM;
+  scoped_refptr<dbus::Bus> bus(new dbus::Bus(options));
+  CHECK(bus->Connect());
+  dbus::ExportedObject* exported_object =
+      bus->GetExportedObject(dbus::ObjectPath(kMidisServicePath));
+
+  CHECK(exported_object);
+  CHECK(exported_object->ExportMethodAndBlock(
+      kMidisInterfaceName,
+      kBootstrapMojoConnectionMethod,
+      base::Bind(&Daemon::BootstrapMojoConnection,
+                 weak_factory_.GetWeakPtr())));
+  CHECK(bus->RequestOwnershipAndBlock(kMidisServiceName,
+                                      dbus::Bus::REQUIRE_PRIMARY));
+  LOG(INFO) << "D-Bus Registration succeeded";
+}
+
+void Daemon::BootstrapMojoConnection(
+    dbus::MethodCall* method_call,
+    dbus::ExportedObject::ResponseSender response_sender) {
+  LOG(INFO) << "Successfully received call from D-Bus client";
+  // TODO(pmalani): Actually bootstrap mojo interface in ClientTracker
+  // handle.value() contains the good stuff.
 }
 
 }  // namespace midis
--- midis/daemon.h
+++ midis/daemon.h
@@ -7,10 +7,18 @@
 
 #include <memory>
 
+#include <base/memory/weak_ptr.h>
 #include <brillo/daemons/daemon.h>
+#include <dbus/exported_object.h>
 
 #include "midis/client_tracker.h"
 #include "midis/device_tracker.h"
+
+namespace dbus {
+
+class MethodCall;
+
+}  // namespace dbus
 
 namespace midis {
 
@@ -23,8 +31,23 @@ class Daemon : public brillo::Daemon {
   int OnInit() override;
 
  private:
+  // This function initializes the D-Bus service. The primary function of the
+  // D-Bus interface is to receive a FD from the Chrome process so that we can
+  // bootstrap a Mojo IPC channel. Since we should expect requests for client
+  // registration to occur as soon as the D-Bus channel is up, this
+  // initialization should be the last thing that happens in Daemon::OnInit().
+  void InitDBus();
+
+  // Handles BootstrapMojoConnection D-Bus method calls.
+  void BootstrapMojoConnection(
+      dbus::MethodCall* method_call,
+      dbus::ExportedObject::ResponseSender response_sender);
+
   std::unique_ptr<DeviceTracker> device_tracker_;
   std::unique_ptr<ClientTracker> client_tracker_;
+
+  base::WeakPtrFactory<Daemon> weak_factory_;
+
   DISALLOW_COPY_AND_ASSIGN(Daemon);
 };
 }  // namespace midis
