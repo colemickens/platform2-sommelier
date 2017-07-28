df820a2f72d674dcfa7a4701e8fc02fa7c3ab4b1
Chirantan Ekbote <chirantan@chromium.org>
2017-07-27 17:12:01 -0700

    vm_tools: maitred: Log to /dev/kmsg
    Configure maitred to send its log messages to /dev/kmsg.  There are several
    reasons to send maitred logs here instead of to the standard syslog:
    
    * When maitred runs as init, vm_syslog will not be running (since it needs to be
      started by maitred) so some early maitred logs will be lost.
    * The kernel automatically mounts /dev after mounting the rootfs.  This means
      that /dev/kmsg will always be available.
    * maitred doesn't need to depend on anything other than the kernel.
    * Developers will be able to see maitred logs just by booting a VM kernel with
      the serial console enabled.  This is useful if it's not possible to extract
      logs out of the VM in some other way.
    
    BUG=chromium:708732
    TEST=manual.  Run maitred and see that its messages show up in /dev/kmsg.
    
    Change-Id: Ia85ea78f8234a21fd018f4ef6d0088052ff9fbda
    Signed-off-by: Chirantan Ekbote <chirantan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/590947
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- vm_tools/maitred/main.cc
+++ vm_tools/maitred/main.cc
@@ -2,21 +2,100 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fcntl.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/uio.h>
 
 #include <linux/vm_sockets.h>  // Needs to come after sys/socket.h
 
+#include <string>
+
 #include <base/at_exit.h>
 #include <base/command_line.h>
+#include <base/files/scoped_file.h>
+#include <base/logging.h>
+#include <base/posix/eintr_wrapper.h>
 #include <base/strings/stringprintf.h>
 #include <grpc++/grpc++.h>
 
 #include "vm_tools/maitred/service_impl.h"
 
+using std::string;
+
 namespace {
 
 // Default port for maitred.
 constexpr unsigned int kPort = 8888;
+
+// Path to logging file.
+constexpr char kDevKmsg[] = "/dev/kmsg";
+
+// Prefix inserted before every log message.
+constexpr char kLogPrefix[] = "maitred: ";
+
+// File descriptor that points to /dev/kmsg.  Needs to be a global variable
+// because logging::LogMessageHandlerFunction is just a function pointer so we
+// can't bind any variables to it via base::Bind.
+int g_kmsg_fd = -1;
+
+bool LogToKmsg(logging::LogSeverity severity,
+               const char* file,
+               int line,
+               size_t message_start,
+               const string& message) {
+  DCHECK_NE(g_kmsg_fd, -1);
+
+  const char* priority = nullptr;
+  switch (severity) {
+    case logging::LOG_VERBOSE:
+      priority = "<7>";
+      break;
+    case logging::LOG_INFO:
+      priority = "<6>";
+      break;
+    case logging::LOG_WARNING:
+      priority = "<4>";
+      break;
+    case logging::LOG_ERROR:
+      priority = "<3>";
+      break;
+    case logging::LOG_FATAL:
+      priority = "<2>";
+      break;
+    default:
+      priority = "<5>";
+      break;
+  }
+
+  const struct iovec iovs[] = {
+      {
+          .iov_base = static_cast<void*>(const_cast<char*>(priority)),
+          .iov_len = strlen(priority),
+      },
+      {
+          .iov_base = static_cast<void*>(const_cast<char*>(kLogPrefix)),
+          .iov_len = sizeof(kLogPrefix) - 1,
+      },
+      {
+          .iov_base = static_cast<void*>(
+              const_cast<char*>(message.c_str() + message_start)),
+          .iov_len = message.length() - message_start,
+      },
+  };
+
+  ssize_t count = 0;
+  for (const struct iovec& iov : iovs) {
+    count += iov.iov_len;
+  }
+
+  ssize_t ret = HANDLE_EINTR(
+      writev(g_kmsg_fd, iovs, sizeof(iovs) / sizeof(struct iovec)));
+
+  // Even if the write wasn't successful, we can't log anything here because
+  // this _is_ the logging function.  Just return whether the write succeeded.
+  return ret == count;
+}
 
 }  // namespace
 
@@ -24,6 +103,14 @@ int main(int argc, char** argv) {
   base::AtExitManager at_exit;
   base::CommandLine::Init(argc, argv);
 
+  // Set up logging to /dev/kmsg.
+  base::ScopedFD kmsg_fd(open(kDevKmsg, O_WRONLY | O_CLOEXEC));
+  PCHECK(kmsg_fd.is_valid()) << "Failed to open " << kDevKmsg;
+
+  g_kmsg_fd = kmsg_fd.get();
+  logging::SetLogMessageHandler(LogToKmsg);
+
+  // Build the server.
   grpc::ServerBuilder builder;
   builder.AddListeningPort(
       base::StringPrintf("vsock:%u:%u", VMADDR_CID_ANY, kPort),
