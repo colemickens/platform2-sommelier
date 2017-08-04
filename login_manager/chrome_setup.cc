4016485c26034ae9726d408f0c242abb87896faa
Igor <igorcov@chromium.org>
2017-08-04 15:42:31 +0000

    Revert "login: need-arc-migration-policy-check flag"
    This reverts commit 69a3ec7f36f11a0fddc6e6b3083ebc1276358753.
    
    Reason for revert: This functionality was needed for device
    policy implemented in M-60. Starting from M-61 it is covered
    by an user policy and the flag is not needed.
    
    Original change's description:
    > login: need-arc-migration-policy-check flag
    >
    > If arc_policy_migration_check USE flag is enabled, set
    > need-arc-migration-policy-check.
    >
    > BUG=chromium:722371
    > TEST=none
    >
    > Change-Id: I8741984abc35669edbe8d443d4334d98005a7d8b
    > Reviewed-on: https://chromium-review.googlesource.com/533236
    > Commit-Ready: Igor <igorcov@chromium.org>
    > Tested-by: Igor <igorcov@chromium.org>
    > Reviewed-by: Dan Erat <derat@chromium.org>
    
    Bug: chromium:725493
    Change-Id: I1fa76bb683c6019ebff9ae47472a44d4a21182ad
    Reviewed-on: https://chromium-review.googlesource.com/602150
    Commit-Ready: Igor <igorcov@chromium.org>
    Tested-by: Igor <igorcov@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- login_manager/chrome_setup.cc
+++ login_manager/chrome_setup.cc
@@ -120,8 +120,6 @@ void AddArcFlags(ChromiumCommandBuilder* builder,
     builder->AddArg("--enable-arc-oobe-optin");
   if (builder->UseFlagIsSet("arc_always_start_with_no_play_store"))
     builder->AddArg("--arc-start-mode=always-start-with-no-play-store");
-  if (builder->UseFlagIsSet("arc_migration_policy_check"))
-    builder->AddArg("--need-arc-migration-policy-check");
 }
 
 // Blatantly copied from //components/crx_file/id_util.cc.
