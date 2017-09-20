d3aa217e86b78123385cc60eefb2d23974c90649
Chih-Yu Huang <akahuang@google.com>
2017-09-20 18:46:07 +0800

    hammerd: Update the pack_firmware script.
    This CL implements:
    - Download the touchpad firmware.
    - Download hammer EC firmware with premp and mp key with version.
      e.g. premp-v2
      Note that we should explictly assign which version of key do we use.
    
    Because we fetch the EC firmware and touchpad firmware from the same
    version and pack to a single tarball, we might face the situation that
    only touchpad firmware is upgraded. Therefore we cannot use EC
    firmware version as the tarball file name. Now the file naming is:
      <Base name>_<version>_<signed_key>.tbz2
    For example:
      hammer_9959.0.0_premp-v2.tbz2
    
    BUG=b:65230801
    TEST=Run `./pack_firmware.sh -b poppy -v 9959.0.0 -s premp-v2`
         and check the tarball is generated
    
    Change-Id: I52bbf47d4b7230e0bf6fdf8742e12ba878d6c690
    Reviewed-on: https://chromium-review.googlesource.com/674514
    Commit-Ready: Chih-Yu Huang <akahuang@chromium.org>
    Tested-by: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- hammerd/pack_firmware.sh
+++ hammerd/pack_firmware.sh
@@ -22,11 +22,19 @@ DEFINE_string board "" "The board name. e.g. poppy" b
 DEFINE_string version "" "The version of the target file. e.g. 9794.0.0" v
 DEFINE_string channel "dev" \
   "The channel of the target file. One of canary, dev, beta, or stable" c
-DEFINE_string signed_key "" \
-  "The signed key of the target file. e.g. none, mp or premp" s
+DEFINE_string signed_key "dev" \
+  "The signed key of the target file. e.g. dev, premp, premp-v2, mp, mp-v2" s
 
 FLAGS "$@" || exit 1
 eval set -- "${FLAGS_ARGV}"
+
+# Global variables that are assigned in init() function.
+# The temporary working directory.
+TMP=""
+# The base URL of the downloaded files.
+GS_URL_BASE=""
+# The detachable base code name.
+BASE_NAME=""
 
 get_base_name() {
   local board_name="${1}"
@@ -48,24 +56,29 @@ init() {
     die_notrace "Please specify the board name using -b"
   fi
 
-  base_name="$(get_base_name "${FLAGS_board}")"
-  if [[ -z "${base_name}" ]]; then
+  BASE_NAME="$(get_base_name "${FLAGS_board}")"
+  if [[ -z "${BASE_NAME}" ]]; then
     die_notrace "The board name is not supported."
   fi
-  echo "The base name: ${base_name}"
+  echo "The base name: ${BASE_NAME}"
 
   if [[ -z "${FLAGS_version}" ]]; then
-    die_notrace "Please specify a firmware version using -v"
+    die_notrace "Please specify a firmware version using -v, e.g. 9794.0.0"
   fi
 
   if [[ -z "${FLAGS_channel}" ]]; then
-    die_notrace "Please specify a channel using -c"
+    die_notrace "Please specify a channel using -c, e.g. canary"
   fi
 
-  if [[ -n "${FLAGS_signed_key}" ]] &&
-    [[ "${FLAGS_signed_key}" != "premp" ]] &&
-    [[ "${FLAGS_signed_key}" != "mp" ]]; then
-    die_notrace "Please specify a correct key name (e.g. mp, premp) using -s"
+  if [[ "${FLAGS_signed_key}" != "dev" ]] &&
+     [[ "${FLAGS_signed_key}" != premp* ]] &&
+     [[ "${FLAGS_signed_key}" != mp* ]]; then
+    die_notrace "Please specify a signed key name using -s, e.g. dev, premp, mp"
+  fi
+
+  GS_URL_BASE="gs://chromeos-releases/${FLAGS_channel}-channel/${FLAGS_board}/${FLAGS_version}"
+  if ! gsutil ls "${GS_URL_BASE}" > /dev/null; then
+    die_notrace "${GS_URL_BASE} is not a valid URL. Please check the argument."
   fi
 }
 
@@ -74,29 +87,62 @@ cleanup() {
   rm -rf "${TMP}"
 }
 
-prepare_file_url() {
-  local signed_key="${1}"
-  local gs_file="ChromeOS-firmware-*.tar.bz2"
-  if [[ -n ${signed_key} ]]; then
-    gs_file="chromeos_${FLAGS_version}_${base_name}_${FLAGS_signed_key}.bin"
+get_ec_file_name() {
+  if [[ "${FLAGS_signed_key}" == "dev" ]]; then
+    echo "ChromeOS-firmware-*.tar.bz2"
+  else
+    echo "chromeos_${FLAGS_version}_${BASE_NAME}_${FLAGS_signed_key}.bin"
   fi
-  echo "gs://chromeos-releases/${FLAGS_channel}-channel/${FLAGS_board}/\
-${FLAGS_version}/${gs_file}"
 }
 
-process_downloaded_file() {
-  EC_PATH="${base_name}/ec.bin"
-  local file_name="${1}"
-  local modified_name="${base_name}.fw"
+get_tp_tarball_name() {
+  echo "ChromeOS-accessory_rwsig-*-${FLAGS_version}-${FLAGS_board}.tar.bz2"
+}
 
-  if [[ "${file_name: -4}" == ".bin" ]]; then
-    mv "${file_name}" "${modified_name}"
+# Download a file that may have wildcard, and return the exact file name.
+download_file() {
+  local gs_url="${GS_URL_BASE}/${1}"
+  local fw_path="$(gsutil ls "${gs_url}")"
+  if [[ -z "${fw_path}" ]]; then
+    die "Please ensure your gsutil works and the firmware version is correct."
+  fi
+  gsutil cp "${fw_path}" . > /dev/null
+
+  echo "$(basename "${fw_path}")"
+}
+
+process_ec_file() {
+  local downloaded_file="$(download_file "$(get_ec_file_name)")"
+  local ec_path="${BASE_NAME}/ec.bin"
+  local old_file=
+
+  # If the firmware is signed, then the downloaded file is the binary blob,
+  # instead of a tarball.
+  if [[ "${downloaded_file}" == *.bin ]]; then
+    old_file="${downloaded_file}"
   else
-    tar jxf "${file_name}" "${EC_PATH}"
-    mv "${EC_PATH}" "${modified_name}"
+    tar xf "${downloaded_file}" "${ec_path}"
+    old_file="${ec_path}"
   fi
 
-  echo "${modified_name}"
+  # Modify file name to the firmware version.
+  local fw_version="$(strings "${old_file}" | grep "${BASE_NAME}" | head -n1)"
+  local new_file="${fw_version}.fw"
+  mv "${old_file}" "${new_file}"
+  echo "${new_file}"
+}
+
+process_tp_file() {
+  local downloaded_file="$(download_file "$(get_tp_tarball_name)")"
+
+  # Extract the symbolic link first, then extract the target file.
+  local sym_file_name="touchpad.bin"
+  tar xf "${downloaded_file}" "${BASE_NAME}/${sym_file_name}"
+  local real_file_name="$(readlink "${BASE_NAME}/${sym_file_name}")"
+  tar xf "${downloaded_file}" "${BASE_NAME}/${real_file_name}"
+  mv "${BASE_NAME}/${real_file_name}" "${real_file_name}"
+
+  echo "${real_file_name}"
 }
 
 main() {
@@ -104,30 +150,31 @@ main() {
   trap cleanup EXIT
   init
 
-  # Download firmware.
-  local gs_url="$(prepare_file_url "${FLAGS_signed_key}")"
-  local fw_path="$(gsutil ls "${gs_url}")"
-  if [[ -z "${fw_path}" ]]; then
-    die "Please ensure your gsutil works and the firmware version is correct."
-  fi
-  gsutil cp "${fw_path}" .
+  # Download and extract EC firmware and touchpad firmware.
+  local ec_file="$(process_ec_file)"
+  local tp_file="$(process_tp_file)"
+  # Pack EC and touchpad firmware and move to the current directory.
+  local output_tar="${BASE_NAME}_${FLAGS_version}_${FLAGS_signed_key}.tbz2"
+  tar jcf "${output_tar}" "${ec_file}" "${tp_file}"
+  mv "${output_tar}" "${CURRENT_DIR}"
 
-  # Extract ec firmware image from tar or just rename signed file to target
-  # name.
-  local downloaded_file_name="$(basename "${fw_path}")"
-  local image_name="$(process_downloaded_file "${downloaded_file_name}")"
+  # Print out the update instruction.
+  cat <<EOF
+${V_BOLD_GREEN}
+Successfully generated the EC and touchpad firmware tarball! ${V_VIDOFF}
 
-  # Pack firmware with the version as the file name.
-  local fw_version="$(strings "${image_name}" | grep "${base_name}" | head -n1)"
-  local output_file="${fw_version}.tbz2"
-  tar jcf "${output_file}" "${image_name}"
-  mv "${output_file}" "${CURRENT_DIR}"
+Steps to upload the EC firmware tarball:
+1. Go to CPFE and Click "Uploads - Private"
+2. Select the tarball file at "Select Component File"
+3. Select "overlay-${FLAGS_board}-private" overlay
+4. Enter "chromeos-base/chromeos-firmware-${BASE_NAME}" in \
+"Relative path to file"
+5. Update the variables of "chromeos-firmware-${BASE_NAME}" ebuild file.
 
-  echo ""
-  echo "Successfully generated the firmware tarball: ${output_file}"
-  echo "Please upload the tarball to localmirror by running:"
-  echo "  $ gsutil cp ${output_file} gs://chromeos-localmirror/distfiles/"
-  echo "And then update chromeos-firmware-${base_name} ebuild file."
+  FW_TARBALL="${output_tar}"
+  EC_FW_NAME="${ec_file}"
+  TP_FW_NAME="${tp_file}"
+EOF
 }
 
 main "$@"
