5e732c0efea43ec4e063711d2dff634ce30cb039
Chirantan Ekbote <chirantan@chromium.org>
2017-09-26 23:37:05 -0700

    vm_tools: syslog: Log to /dev/kmsg
    Currently all the logs from vm_syslog are being dropped.  Sending logs
    to /dev/log also doesn't work for early setup before the socket is
    ready.  We could buffer those logs and send them later but that's more
    complicated.  We could also set up a loopback and automatically buffer
    self-logs.  The main disadvantage is that we would lose any logs related
    to failures in sending messages to the LogCollector service, which is
    arguably the most important time when logging should work.
    
    Instead just send all the logs to /dev/kmsg.  The main justification is
    this ensures that vm_syslog logs will appear in the kernel log buffer
    and don't depend on grpc or anything other than the guest kernel.  Also,
    the code already exists and can easily be copied from main.cc in
    maitred.  If there's ever a 3rd application that logs to
    /dev/kmsg (unlikely), we should refactor the code into a single
    location.
    
    BUG=none
    TEST=start a VM and see that vm_syslog logs appear in the kernel log
         buffer
    
    Change-Id: Id793546086b6bdf93b31c57a08d5dd9a3a0fae77
    Signed-off-by: Chirantan Ekbote <chirantan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/685440
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- vm_tools/syslog/main.cc
+++ vm_tools/syslog/main.cc
@@ -2,17 +2,106 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+
 #include <memory>
+#include <string>
 
 #include <base/at_exit.h>
+#include <base/files/scoped_file.h>
 #include <base/logging.h>
+#include <base/macros.h>
 #include <base/message_loop/message_loop.h>
+#include <base/posix/eintr_wrapper.h>
 #include <base/run_loop.h>
 
 #include "vm_tools/syslog/collector.h"
 
+using std::string;
+
+namespace {
+// Path to logging file.
+constexpr char kDevKmsg[] = "/dev/kmsg";
+
+// Prefix inserted before every log message.
+constexpr char kLogPrefix[] = "vm_syslog: ";
+
+// File descriptor that points to /dev/kmsg.  Needs to be a global variable
+// because logging::LogMessageHandlerFunction is just a function pointer so we
+// can't bind any variables to it via base::Bind.
+int g_kmsg_fd = -1;
+
+bool LogToKmsg(logging::LogSeverity severity,
+               const char* file,
+               int line,
+               size_t message_start,
+               const string& message) {
+  DCHECK_NE(g_kmsg_fd, -1);
+
+  const char* priority = nullptr;
+  switch (severity) {
+    case logging::LOG_VERBOSE:
+      priority = "<7>";
+      break;
+    case logging::LOG_INFO:
+      priority = "<6>";
+      break;
+    case logging::LOG_WARNING:
+      priority = "<4>";
+      break;
+    case logging::LOG_ERROR:
+      priority = "<3>";
+      break;
+    case logging::LOG_FATAL:
+      priority = "<2>";
+      break;
+    default:
+      priority = "<5>";
+      break;
+  }
+
+  const struct iovec iovs[] = {
+      {
+          .iov_base = static_cast<void*>(const_cast<char*>(priority)),
+          .iov_len = strlen(priority),
+      },
+      {
+          .iov_base = static_cast<void*>(const_cast<char*>(kLogPrefix)),
+          .iov_len = sizeof(kLogPrefix) - 1,
+      },
+      {
+          .iov_base = static_cast<void*>(
+              const_cast<char*>(message.c_str() + message_start)),
+          .iov_len = message.length() - message_start,
+      },
+  };
+
+  ssize_t count = 0;
+  for (const struct iovec& iov : iovs) {
+    count += iov.iov_len;
+  }
+
+  ssize_t ret = HANDLE_EINTR(writev(g_kmsg_fd, iovs, arraysize(iovs)));
+
+  // Even if the write wasn't successful, we can't log anything here because
+  // this _is_ the logging function.  Just return whether the write succeeded.
+  return ret == count;
+}
+
+}  // namespace
+
 int main(int argc, char** argv) {
   base::AtExitManager at_exit;
+  logging::InitLogging(logging::LoggingSettings());
+
+  // Set up logging to /dev/kmsg.
+  base::ScopedFD kmsg_fd(open(kDevKmsg, O_WRONLY | O_CLOEXEC));
+  PCHECK(kmsg_fd.is_valid()) << "Failed to open " << kDevKmsg;
+
+  g_kmsg_fd = kmsg_fd.get();
+  logging::SetLogMessageHandler(LogToKmsg);
 
   if (argc > 1) {
     LOG(ERROR) << "Unexpected command line arguments";
