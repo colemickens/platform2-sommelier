d9ba10e324067b80792e7b403d1c65239d765612
Alec Thilenius <athilenius@chromium.org>
2017-10-03 14:56:13 -0600

    chromeos-config: Add a python device tree library and test
    Add a device tree library in Python. This is based on crbug.com/698144 with lots
    of cleanup. Also adds unit tests. This library will only be consumed by
    cros_config_host, it is not intended for cros_config (board) use.
    
    BUG=chromium:771187
    TEST=FEATURES=test sudo -E emerge chromeos-base/chromeos-config-tools
    
    Change-Id: I0e6b761d2c00e30d30ca2b2a980a2533cdaf223e
    Reviewed-on: https://chromium-review.googlesource.com/699191
    Commit-Ready: Alec Thilenius <athilenius@chromium.org>
    Tested-by: Alec Thilenius <athilenius@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>

---
+++ chromeos-config/cros_config_host_py/fdt_unittest.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# pylint: disable=module-missing-docstring,class-missing-docstring
+
+from __future__ import print_function
+
+import os
+import unittest
+
+import fdt
+import fdt_util
+
+DTS_FILE = '../libcros_config/test.dts'
+
+
+class FdtLibTest(unittest.TestCase):
+  def setUp(self):
+    path = os.path.join(os.path.dirname(__file__), DTS_FILE)
+    try:
+      (fname, temp_file) = fdt_util.EnsureCompiled(path)
+      self.test_fdt = fdt.Fdt(fname)
+    finally:
+      if temp_file is not None:
+        os.remove(temp_file.name)
+
+  def testFdtScan(self):
+    self.test_fdt.Scan()
+    self.assertIsNotNone(self.test_fdt.GetRoot())
+
+  def testGetModels(self):
+    self.test_fdt.Scan()
+    models_node = self.test_fdt.GetNode('/chromeos/models')
+    models = [m.name for m in models_node.subnodes]
+    self.assertSequenceEqual(models, ['pyro', 'caroline', 'reef', 'broken'])
+
+  def testGetStringProperty(self):
+    self.test_fdt.Scan()
+    firmware = self.test_fdt.GetNode('/chromeos/models/pyro/firmware')
+    bcs_overlay = firmware.props['bcs-overlay'].value
+    self.assertEqual(bcs_overlay, 'overlay-reef-private')
+
+
+if __name__ == '__main__':
+  unittest.main()
---
+++ chromeos-config/cros_config_host_py/fdt_util.py
@@ -0,0 +1,121 @@
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+# Taken from U-Boot v2017.07 (tools/dtoc)
+
+"""Utility functions for fdt."""
+
+from __future__ import print_function
+
+import os
+import struct
+import sys
+import tempfile
+
+from chromite.lib import cros_build_lib
+
+
+def fdt32_to_cpu(val):
+  """Convert a device tree cell to an integer
+
+  Args:
+    val: Value to convert (4-character string representing the cell value)
+
+  Returns:
+    A native-endian integer value
+  """
+  if sys.version_info > (3, 0):
+    if isinstance(val, bytes):
+      val = val.decode('utf-8')
+    val = val.encode('raw_unicode_escape')
+  return struct.unpack('>I', val)[0]
+
+
+def fdt_cells_to_cpu(val, cells):
+  """Convert one or two cells to a long integer
+
+  Args:
+    val: Value to convert (array of one or more 4-character strings)
+    cells: Cell number
+
+  Returns:
+    A native-endian long value
+  """
+  if not cells:
+    return 0
+  out = long(fdt32_to_cpu(val[0]))
+  if cells == 2:
+    out = out << 32 | fdt32_to_cpu(val[1])
+  return out
+
+def GetInt(node, propname, default=None):
+  prop = node.props.get(propname)
+  if not prop:
+    return default
+  value = fdt32_to_cpu(prop.value)
+  if type(value) == type(list):
+    raise ValueError("Node '%s' property '%s' has list value: expecting"
+                     "a single integer" % (node.name, propname))
+  return value
+
+
+def GetString(node, propname, default=None):
+  prop = node.props.get(propname)
+  if not prop:
+    return default
+  value = prop.value
+  if type(value) == type(list):
+    raise ValueError("Node '%s' property '%s' has list value: expecting"
+                     "a single string" % (node.name, propname))
+  return value
+
+
+def GetBool(node, propname, default=False):
+  if propname in node.props:
+    return True
+  return default
+
+
+def EnsureCompiled(fname):
+  """Compile an fdt .dts source file into a .dtb binary blob if needed.
+
+  Args:
+    fname: Filename (if .dts it will be compiled). It not it will be
+      left alone
+
+  Returns:
+    Filename of resulting .dtb file
+  """
+  out = None
+  _, ext = os.path.splitext(fname)
+  if ext == '.dtb':
+    return fname, None
+  elif ext == '.md':
+    out = tempfile.NamedTemporaryFile(suffix='.dts', delete=False)
+    out.write('/dts-v1/;\n/ {\n')
+    with open(fname) as fd:
+      adding = False
+      for line in fd:
+        if line == '```\n':
+          adding = not adding
+          continue
+        if adding:
+          out.write(line)
+    out.write('};\n')
+    out.close()
+    dts_input = out.name
+  else:
+    dts_input = fname
+  dtb_output = tempfile.NamedTemporaryFile(suffix='.dtb', delete=False)
+  search_paths = [os.path.join(os.getcwd(), 'include')]
+  # If we don't have a directory, put it in the tools tempdir
+  search_list = []
+  for path in search_paths:
+    search_list.extend(['-i', path])
+  args = ['-I', 'dts', '-o', dtb_output.name, '-O', 'dtb']
+  args.extend(search_list)
+  args.append(dts_input)
+  cros_build_lib.RunCommand(['dtc'] + args, quiet=True)
+  if out:
+    os.unlink(out.name)
+  return dtb_output.name, dtb_output
