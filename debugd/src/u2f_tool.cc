05eaabba718c7e4d806b6e58b955f04935a20835
Ben Chan <benchan@chromium.org>
2017-10-12 17:29:09 -0700

    debugd: fix declaration of string array constant in U2fTool
    String array constants should be declared as 'const char* const[]'
    instead of 'const char*[]'. The latter is an array of "mutable" pointers
    to constant string literal.
    
    BUG=None
    TEST=Run unit tests.
    
    Change-Id: Ic93b8716001007c769ae34605ed901036797f36b
    Reviewed-on: https://chromium-review.googlesource.com/717570
    Commit-Ready: Ben Chan <benchan@chromium.org>
    Tested-by: Ben Chan <benchan@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>

--- debugd/src/u2f_tool.cc
+++ debugd/src/u2f_tool.cc
@@ -24,7 +24,7 @@ namespace {
 constexpr char kOverrideConfigDir[] = "/var/lib/u2f/force";
 constexpr char kJobName[] = "u2fd";
 
-const char* kKnownFlags[] = {"u2f", "g2f", "verbose"};
+const char* const kKnownFlags[] = {"u2f", "g2f", "verbose"};
 
 int ControlU2fd(bool start) {
   const char* action = start ? "start" : "stop";
