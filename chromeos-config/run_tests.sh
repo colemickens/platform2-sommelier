1f1d647890b04f82252b6274d9ead4db6f174c32
Simon Glass <sjg@chromium.org>
2017-10-29 19:48:12 -0600

    chromeos-config: Drop the README test from the test script
    This test only compiles the source - it does not validate it. We handle
    this in the ebuild now by running the validator properly on the README.
    So drop this duplicate and inferior test.
    
    Also clarify that this script only runs the Python tests.
    
    BUG=chromium:761265
    TEST=FEATURES=test sudo -E emerge --nodeps  chromeos-config-tools
    
    Change-Id: I179bc890dccfcb408dca2ecdcb62e87a09197aee
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/743037
    Reviewed-by: Jason Clinton <jclinton@chromium.org>

--- chromeos-config/run_tests.sh
+++ chromeos-config/run_tests.sh
@@ -3,24 +3,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Script to run all unit tests in cros_config as well as a basic tests on the
-# config example in README.md
+# Script to run all Python unit tests in cros_config.
 
 # Run all unit tests (all files ending with test.py recursively)
 while read -d $'\0' -r pytest; do
   echo "Running tests in ${pytest}"
   "./${pytest}" || exit 1
 done < <(find -name '*test.py' -print0)
-
-# Test the README file, use awk to extract the example (between ``` markers) and
-# add a header / footer
-tmp="/tmp/test.$$"
-awk 'BEGIN {print "/dts-v1/; / {" } END {print "};"} \
-            /```/{flag = !flag; next} flag' README.md > "${tmp}"
-if dtc "${tmp}"  >/dev/null; then
-  rm "${tmp}"
-  echo "All OK"
-else
-  echo "See source file in ${tmp}"
-  exit 1
-fi
