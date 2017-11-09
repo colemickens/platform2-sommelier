9165b22ffa3bea92a1ab27578302b62476226f7c
Lann Martin <lannm@chromium.org>
2017-11-09 11:13:20 -0700

    chromeos-config: remove stub validate_config.py
    This is no longer required by the chromeos-config-host ebuild.
    
    CQ-DEPEND=CL:760759
    TEST=sudo env FEATURES=test emerge chromeos-config-host
    
    Change-Id: Ib4d877bbeb3c40c250ab3407320ce1518c262691
    Reviewed-on: https://chromium-review.googlesource.com/760876
    Commit-Ready: Lann Martin <lannm@chromium.org>
    Tested-by: Lann Martin <lannm@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- chromeos-config/validate_config.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2016 The Chromium OS Authors. All rights reserved.
-# Distributed under the terms of the GNU General Public License v2
-
-# FIXME(lannm): Temporary workaround for ebuilds while reorganizing modules.
-
-import runpy
-
-runpy.run_module('cros_config_host.validate_config')
