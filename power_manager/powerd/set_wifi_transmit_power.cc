fb5d18437804906b4df0c7babc47a5124b2af826
Daniel Erat <derat@chromium.org>
2017-11-13 09:04:58 -0800

    power: Make set_wifi_transmit_power not crash on error.
    Update powerd's set_wifi_transmit_power helper program to
    return exit code 1 when it fails to find a wifi chipset
    instead of aborting. powerd runs the program on boot, and
    the chipset sometimes isn't available at that point (e.g. on
    kevin).
    
    Also make various other cleanups to this program's code.
    
    BUG=chromium:781838
    TEST=manual: added some logging and checked via
         /var/log/powerd.out that the program still runs
         successfully after restarting powerd
    
    Change-Id: I6cecec6fdbcce69fff4d85717d73a4dbcd4250ce
    Reviewed-on: https://chromium-review.googlesource.com/766530
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>

--- power_manager/powerd/set_wifi_transmit_power.cc
+++ power_manager/powerd/set_wifi_transmit_power.cc
@@ -53,20 +53,17 @@ namespace {
 const char kMwifiexModulePath[] = "/sys/module/mwifiex";
 
 int ErrorHandler(struct sockaddr_nl* nla, struct nlmsgerr* err, void* arg) {
-  int* ret = static_cast<int*>(arg);
-  *ret = err->error;
+  *static_cast<int*>(arg) = err->error;
   return NL_STOP;
 }
 
 int FinishHandler(struct nl_msg* msg, void* arg) {
-  int* ret = static_cast<int*>(arg);
-  *ret = 0;
+  *static_cast<int*>(arg) = 0;
   return NL_SKIP;
 }
 
 int AckHandler(struct nl_msg* msg, void* arg) {
-  int* ret = static_cast<int*>(arg);
-  *ret = 0;
+  *static_cast<int*>(arg) = 0;
   return NL_STOP;
 }
 
@@ -74,165 +71,133 @@ int ValidHandler(struct nl_msg* msg, void* arg) {
   return NL_OK;
 }
 
-enum class WirelessDriver { NONE = 0, MWIFIEX, IWL };
+enum class WirelessDriver { NONE, MWIFIEX, IWL };
 
-class SetWiFiTransmitPower {
- public:
-  SetWiFiTransmitPower();
-  ~SetWiFiTransmitPower();
-
-  // Set power mode according to tablet mode state.
-  void SetPowerMode(bool tablet);
-
- private:
-  static WirelessDriver GetWirelessDriverType();
-
-  // Return the wireless device index for Chrome OS (either wlan0 or mlan0).
-  static int GetWirelessDeviceIndex();
-
-  // Fill in nl80211 message for the mwifiex driver.
-  void FillMessageMwifiex(struct nl_msg* msg, bool tablet);
-
-  // Fill in nl80211 message for the iwl driver.
-  void FillMessageIwl(struct nl_msg* msg, bool tablet);
-
-  struct nl_sock* nl_sock_;
-  struct nl_cb* cb_;
-  int err_;  // For communicating with libnl callbacks
-
-  DISALLOW_COPY_AND_ASSIGN(SetWiFiTransmitPower);
-};
-
-SetWiFiTransmitPower::SetWiFiTransmitPower()
-    : nl_sock_(nl_socket_alloc()), cb_(nl_cb_alloc(NL_CB_DEFAULT)), err_(0) {
-  CHECK(nl_sock_);
-  CHECK(cb_);
-
-  // Register libnl callbacks.
-  nl_cb_err(cb_, NL_CB_CUSTOM, ErrorHandler, &err_);
-  nl_cb_set(cb_, NL_CB_FINISH, NL_CB_CUSTOM, FinishHandler, &err_);
-  nl_cb_set(cb_, NL_CB_ACK, NL_CB_CUSTOM, AckHandler, &err_);
-  nl_cb_set(cb_, NL_CB_VALID, NL_CB_CUSTOM, ValidHandler, NULL);
+// Returns the type of wireless driver that's present on the system.
+WirelessDriver GetWirelessDriverType() {
+  // TODO(crbug.com/782924): Improve wireless driver detection.
+  return base::PathExists(base::FilePath(kMwifiexModulePath))
+             ? WirelessDriver::MWIFIEX
+             : WirelessDriver::IWL;
 }
 
-SetWiFiTransmitPower::~SetWiFiTransmitPower() {
-  nl_socket_free(nl_sock_);
-  nl_cb_put(cb_);
+// Returns the wireless device index (either wlan0 or mlan0).
+// 0 is returned if the device isn't found.
+uint32_t GetWirelessDeviceIndex() {
+  // TODO(crbug.com/782924): Don't hardcode device names.
+  uint32_t index = if_nametoindex("mlan0");
+  if (!index)
+    index = if_nametoindex("wlan0");
+
+  return index;
 }
 
-WirelessDriver SetWiFiTransmitPower::GetWirelessDriverType() {
-  if (base::PathExists(base::FilePath(kMwifiexModulePath))) {
-    return WirelessDriver::MWIFIEX;
-  }
-  return WirelessDriver::IWL;
-}
-
-int SetWiFiTransmitPower::GetWirelessDeviceIndex() {
-  // Chrome OS only has one network device
-  // The device is usually 'mlan0' for arm devices, and 'wlan0' for intel
-  // devices.
-  int index = 0;
-
-  index = if_nametoindex("mlan0");
-  if (index)
-    return index;
-
-  index = if_nametoindex("wlan0");
-  if (index)
-    return index;
-
-  LOG(FATAL) << "No wireless driver found";
-
-  return index;  // Not reached.
-}
-
-void SetWiFiTransmitPower::FillMessageMwifiex(struct nl_msg* msg, bool tablet) {
-  int err = 0;
-
-  err = nla_put_u32(msg, NL80211_ATTR_VENDOR_ID, MWIFIEX_VENDOR_ID);
-  CHECK(!err) << "Failed to put NL80211_ATTR_VENDOR_ID";
-
-  err = nla_put_u32(msg, NL80211_ATTR_VENDOR_SUBCMD,
-                    MWIFIEX_VENDOR_CMD_SET_TX_POWER_LIMIT);
-  CHECK(!err) << "Failed to put NL80211_ATTR_VENDOR_SUBCMD";
+// Fill in nl80211 message for the mwifiex driver.
+void FillMessageMwifiex(struct nl_msg* msg, bool tablet) {
+  CHECK(!nla_put_u32(msg, NL80211_ATTR_VENDOR_ID, MWIFIEX_VENDOR_ID))
+      << "Failed to put NL80211_ATTR_VENDOR_ID";
+  CHECK(!nla_put_u32(msg, NL80211_ATTR_VENDOR_SUBCMD,
+                     MWIFIEX_VENDOR_CMD_SET_TX_POWER_LIMIT))
+      << "Failed to put NL80211_ATTR_VENDOR_SUBCMD";
 
   struct nlattr* limits = nla_nest_start(msg, NL80211_ATTR_VENDOR_DATA);
   CHECK(limits) << "Failed in nla_nest_start";
 
-  err = nla_put_u8(msg, MWIFIEX_VENDOR_CMD_ATTR_TXP_LIMIT_24, tablet);
-  CHECK(!err) << "Failed to put MWIFIEX_VENDOR_CMD_ATTR_TXP_LIMIT_24";
-
-  err = nla_put_u8(msg, MWIFIEX_VENDOR_CMD_ATTR_TXP_LIMIT_52, tablet);
-  CHECK(!err) << "Failed to put MWIFIEX_VENDOR_CMD_ATTR_TXP_LIMIT_52";
-
-  err = nla_nest_end(msg, limits);
-  CHECK(!err) << "Failed in nla_nest_end";
+  CHECK(!nla_put_u8(msg, MWIFIEX_VENDOR_CMD_ATTR_TXP_LIMIT_24, tablet))
+      << "Failed to put MWIFIEX_VENDOR_CMD_ATTR_TXP_LIMIT_24";
+  CHECK(!nla_put_u8(msg, MWIFIEX_VENDOR_CMD_ATTR_TXP_LIMIT_52, tablet))
+      << "Failed to put MWIFIEX_VENDOR_CMD_ATTR_TXP_LIMIT_52";
+  CHECK(!nla_nest_end(msg, limits)) << "Failed in nla_nest_end";
 }
 
-void SetWiFiTransmitPower::FillMessageIwl(struct nl_msg* msg, bool tablet) {
-  int err = 0;
-
-  err = nla_put_u32(msg, NL80211_ATTR_VENDOR_ID, INTEL_OUI);
-  CHECK(!err) << "Failed to put NL80211_ATTR_VENDOR_ID";
-
-  err = nla_put_u32(msg, NL80211_ATTR_VENDOR_SUBCMD,
-                    IWL_MVM_VENDOR_CMD_SET_SAR_PROFILE);
-  CHECK(!err) << "Failed to put NL80211_ATTR_VENDOR_SUBCMD";
+// Fill in nl80211 message for the iwl driver.
+void FillMessageIwl(struct nl_msg* msg, bool tablet) {
+  CHECK(!nla_put_u32(msg, NL80211_ATTR_VENDOR_ID, INTEL_OUI))
+      << "Failed to put NL80211_ATTR_VENDOR_ID";
+  CHECK(!nla_put_u32(msg, NL80211_ATTR_VENDOR_SUBCMD,
+                     IWL_MVM_VENDOR_CMD_SET_SAR_PROFILE))
+      << "Failed to put NL80211_ATTR_VENDOR_SUBCMD";
 
   struct nlattr* limits = nla_nest_start(msg, NL80211_ATTR_VENDOR_DATA);
   CHECK(limits) << "Failed in nla_nest_start";
 
   int index = tablet ? IWL_TABLET_PROFILE_INDEX : IWL_CLAMSHELL_PROFILE_INDEX;
-
-  err = nla_put_u32(msg, IWL_MVM_VENDOR_ATTR_SAR_CHAIN_A_PROFILE, index);
-  CHECK(!err) << "Failed to put IWL_MVM_VENDOR_ATTR_SAR_CHAIN_A_PROFILE";
-
-  err = nla_put_u32(msg, IWL_MVM_VENDOR_ATTR_SAR_CHAIN_B_PROFILE, index);
-  CHECK(!err) << "Failed to put IWL_MVM_VENDOR_ATTR_SAR_CHAIN_B_PROFILE";
-
-  err = nla_nest_end(msg, limits);
-  CHECK(!err) << "Failed in nla_nest_end";
+  CHECK(!nla_put_u32(msg, IWL_MVM_VENDOR_ATTR_SAR_CHAIN_A_PROFILE, index))
+      << "Failed to put IWL_MVM_VENDOR_ATTR_SAR_CHAIN_A_PROFILE";
+  CHECK(!nla_put_u32(msg, IWL_MVM_VENDOR_ATTR_SAR_CHAIN_B_PROFILE, index))
+      << "Failed to put IWL_MVM_VENDOR_ATTR_SAR_CHAIN_B_PROFILE";
+  CHECK(!nla_nest_end(msg, limits)) << "Failed in nla_nest_end";
 }
 
-void SetWiFiTransmitPower::SetPowerMode(bool tablet) {
-  int err = 0;
+class PowerSetter {
+ public:
+  PowerSetter() : nl_sock_(nl_socket_alloc()), cb_(nl_cb_alloc(NL_CB_DEFAULT)) {
+    CHECK(nl_sock_);
+    CHECK(cb_);
 
-  err = genl_connect(nl_sock_);
-  CHECK(!err) << "Failed to connect to netlink";
-
-  int nl_family_id = genl_ctrl_resolve(nl_sock_, "nl80211");
-  CHECK_GE(nl_family_id, 0) << "family nl80211 not found";
-
-  struct nl_msg* msg = nlmsg_alloc();
-  CHECK(msg);
-
-  // Set header.
-  genlmsg_put(msg, NL_AUTO_PID, NL_AUTO_SEQ, nl_family_id, 0, 0,
-              NL80211_CMD_VENDOR, 0);
-
-  // Set actual message.
-  err = nla_put_u32(msg, NL80211_ATTR_IFINDEX, GetWirelessDeviceIndex());
-  CHECK(!err) << "Failed to put NL80211_ATTR_IFINDEX";
-
-  switch (GetWirelessDriverType()) {
-    case WirelessDriver::MWIFIEX:
-      FillMessageMwifiex(msg, tablet);
-      break;
-    case WirelessDriver::IWL:
-      FillMessageIwl(msg, tablet);
-      break;
-    default:
-      LOG(FATAL) << "no valid wireless driver found";
+    // Register libnl callbacks.
+    nl_cb_err(cb_, NL_CB_CUSTOM, ErrorHandler, &err_);
+    nl_cb_set(cb_, NL_CB_FINISH, NL_CB_CUSTOM, FinishHandler, &err_);
+    nl_cb_set(cb_, NL_CB_ACK, NL_CB_CUSTOM, AckHandler, &err_);
+    nl_cb_set(cb_, NL_CB_VALID, NL_CB_CUSTOM, ValidHandler, nullptr);
+  }
+  ~PowerSetter() {
+    nl_socket_free(nl_sock_);
+    nl_cb_put(cb_);
   }
 
-  err = nl_send_auto(nl_sock_, msg);
-  CHECK_GE(err, 0) << "nl_send_auto failed: " << nl_geterror(err_);
+  // Sets power mode according to tablet mode state. Returns true on success and
+  // false on failure.
+  bool SetPowerMode(bool tablet) {
+    CHECK(!genl_connect(nl_sock_)) << "Failed to connect to netlink";
 
-  while (err_ != 0)
-    nl_recvmsgs(nl_sock_, cb_);
+    int nl_family_id = genl_ctrl_resolve(nl_sock_, "nl80211");
+    CHECK_GE(nl_family_id, 0) << "family nl80211 not found";
 
-  nlmsg_free(msg);
-}
+    struct nl_msg* msg = nlmsg_alloc();
+    CHECK(msg);
+
+    // Set header.
+    genlmsg_put(msg, NL_AUTO_PID, NL_AUTO_SEQ, nl_family_id, 0, 0,
+                NL80211_CMD_VENDOR, 0);
+
+    // Set actual message.
+    const uint32_t index = GetWirelessDeviceIndex();
+    if (!index) {
+      LOG(ERROR) << "Failed to find wireless device index";
+      return false;
+    }
+    CHECK(!nla_put_u32(msg, NL80211_ATTR_IFINDEX, index))
+        << "Failed to put NL80211_ATTR_IFINDEX";
+
+    switch (GetWirelessDriverType()) {
+      case WirelessDriver::MWIFIEX:
+        FillMessageMwifiex(msg, tablet);
+        break;
+      case WirelessDriver::IWL:
+        FillMessageIwl(msg, tablet);
+        break;
+      case WirelessDriver::NONE:
+        LOG(ERROR) << "No valid wireless driver found";
+        return false;
+    }
+
+    CHECK_GE(nl_send_auto(nl_sock_, msg), 0)
+        << "nl_send_auto failed: " << nl_geterror(err_);
+    while (err_ != 0)
+      nl_recvmsgs(nl_sock_, cb_);
+
+    nlmsg_free(msg);
+    return true;
+  }
+
+ private:
+  struct nl_sock* nl_sock_;
+  struct nl_cb* cb_;
+  int err_ = 0;  // Used by |cb_| to store errors.
+
+  DISALLOW_COPY_AND_ASSIGN(PowerSetter);
+};
 
 }  // namespace
 
@@ -240,6 +205,5 @@ int main(int argc, char* argv[]) {
   DEFINE_bool(tablet, false, "Set wifi transmit power mode to tablet mode");
   brillo::FlagHelper::Init(argc, argv, "Set wifi transmit power mode");
 
-  SetWiFiTransmitPower().SetPowerMode(FLAGS_tablet);
-  return 0;
+  return PowerSetter().SetPowerMode(FLAGS_tablet) ? 0 : 1;
 }
