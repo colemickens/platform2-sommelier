932a4955058b5ec815bacaa7dc23f8caa0b49cc7
Chris Morin <cmtm@google.com>
2017-12-05 16:25:04 -0800

    login: don't support disabling of the mini-container
    We no longer need to support disabling of the mini-container since it
    now works on master-arc-dev.
    
    BUG=b:70241488
    TEST=Ensure container boots successfully.
    
    Change-Id: I59225b6a34b9bdeddf4a2a21653ebd859fa3f181
    Reviewed-on: https://chromium-review.googlesource.com/809727
    Commit-Ready: Christopher Morin <cmtm@google.com>
    Tested-by: Christopher Morin <cmtm@google.com>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- login_manager/chrome_setup.cc
+++ login_manager/chrome_setup.cc
@@ -122,8 +122,6 @@ void AddArcFlags(ChromiumCommandBuilder* builder,
     builder->AddArg("--arc-start-mode=always-start-with-no-play-store");
   if (builder->UseFlagIsSet("arc_transition_m_to_n"))
     builder->AddArg("--arc-transition-migration-required");
-  if (builder->UseFlagIsSet("arc_only_start_after_login"))
-    builder->AddArg("--arc-start-mode=only-start-after-login");
   if (builder->UseFlagIsSet("arc_force_2x_scaling"))
     builder->AddArg("--force-remote-shell-scale=2");
 }
