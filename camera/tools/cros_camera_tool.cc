6943617167eae9333620b59b2e36f28633f8783c
Tomasz Figa <tfiga@chromium.org>
2018-01-23 17:05:57 +0900

    tools: Add cros-camera-tool
    Add a simple tool that enumerates all camera modules in the system
    compliant with the V4L2 APIs used by MIPI cameras on Chrome OS devices.
    
    Example invocation:
    localhost ~ # cros-camera-tool modules list
                Name | Vendor ID
             module1 | 123
             module2 | 456
    
    BUG=b:70357756
    TEST=Run "cros-camera-tool modules list" on Poppy with MIPI cameras and see
     front and back cameras with their vendor IDs.
    
    Change-Id: Ie4b9747ae6b6cecac1906f178f2509aaedaca279
    Reviewed-on: https://chromium-review.googlesource.com/890799
    Commit-Ready: Tomasz Figa <tfiga@chromium.org>
    Tested-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>

---
+++ camera/tools/cros_camera_tool.cc
@@ -0,0 +1,183 @@
+/* Copyright 2018 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <fcntl.h>
+#include <linux/media.h>
+#include <sys/ioctl.h>
+
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include <base/at_exit.h>
+#include <base/command_line.h>
+#include <base/files/file_enumerator.h>
+#include <base/files/file_path.h>
+#include <base/files/file_util.h>
+#include <base/strings/string_util.h>
+#include <brillo/syslog_logging.h>
+
+namespace {
+
+const char kSysfsV4lClassRoot[] = "/sys/class/video4linux";
+const char kVendorIdPath[] = "device/vendor_id";
+const std::vector<std::string> kArgsPattern = {"modules", "list"};
+
+struct camera {
+  std::string name;
+  std::string vendor_id;
+
+  camera(const std::string& name_, const std::string& vendor_id_)
+      : name(name_), vendor_id(vendor_id_) {}
+};
+
+class CameraTool {
+  typedef std::vector<struct camera> CameraVector;
+
+ public:
+  void PrintCameras(void) {
+    const CameraVector& cameras = GetPlatformCameras();
+
+    if (cameras.empty()) {
+      std::cout << "No cameras detected in the system." << std::endl;
+      return;
+    }
+
+    std::cout << std::setw(16) << "Name"
+              << " | "
+              << "Vendor ID" << std::endl;
+    for (const auto& camera : cameras)
+      std::cout << std::setw(16) << camera.name << " | " << camera.vendor_id
+                << std::endl;
+  }
+
+ private:
+  void ProbeSensorSubdev(struct media_entity_desc* desc,
+                         const base::FilePath& path) {
+    const base::FilePath& vendor_id_path = path.Append(kVendorIdPath);
+    std::string vendor_id = "-1";
+
+    if (!base::ReadFileToStringWithMaxSize(vendor_id_path, &vendor_id, 64))
+      LOG(ERROR) << "Failed to read vendor ID for sensor '" << desc->name
+                 << "'";
+
+    base::TrimWhitespaceASCII(vendor_id, base::TRIM_ALL, &vendor_id);
+    platform_cameras_.emplace_back(desc->name, vendor_id);
+  }
+
+  base::FilePath FindSubdevSysfsByDevId(int major, int minor) {
+    base::FileEnumerator dev_enum(base::FilePath(kSysfsV4lClassRoot), false,
+                                  base::FileEnumerator::DIRECTORIES,
+                                  FILE_PATH_LITERAL("v4l-subdev*"));
+    for (base::FilePath name = dev_enum.Next(); !name.empty();
+         name = dev_enum.Next()) {
+      base::FilePath dev_path = name.Append("dev");
+      std::string dev_id("255:255");
+      if (!base::ReadFileToStringWithMaxSize(dev_path, &dev_id,
+                                             dev_id.size())) {
+        LOG(ERROR) << "Failed to read device ID of '" << dev_path.value()
+                   << "' from sysfs";
+        continue;
+      }
+      base::TrimWhitespaceASCII(dev_id, base::TRIM_ALL, &dev_id);
+
+      std::ostringstream stream;
+      stream << major << ":" << minor;
+      if (dev_id == stream.str())
+        return name;
+    }
+
+    return base::FilePath();
+  }
+
+  void ProbeMediaController(int media_fd) {
+    struct media_entity_desc desc;
+
+    for (desc.id = MEDIA_ENT_ID_FLAG_NEXT;
+         !ioctl(media_fd, MEDIA_IOC_ENUM_ENTITIES, &desc);
+         desc.id |= MEDIA_ENT_ID_FLAG_NEXT) {
+      if (desc.type != MEDIA_ENT_T_V4L2_SUBDEV_SENSOR)
+        continue;
+
+      const base::FilePath& path =
+          FindSubdevSysfsByDevId(desc.dev.major, desc.dev.minor);
+      if (path.empty()) {
+        LOG(ERROR) << "v4l-subdev node for sensor '" << desc.name
+                   << "' not found";
+        continue;
+      }
+
+      LOG(INFO) << "Probing sensor '" << desc.name << "' ("
+                << path.BaseName().value() << ")";
+      ProbeSensorSubdev(&desc, path);
+    }
+  }
+
+  void AddV4l2Cameras(void) {
+    base::FileEnumerator dev_enum(base::FilePath("/dev"), false,
+                                  base::FileEnumerator::FILES,
+                                  FILE_PATH_LITERAL("media*"));
+    for (base::FilePath name = dev_enum.Next(); !name.empty();
+         name = dev_enum.Next()) {
+      int fd = open(name.value().c_str(), O_RDWR);
+      if (fd < 0) {
+        LOG(ERROR) << "Failed to open '" << name.value() << "'";
+        continue;
+      }
+
+      LOG(INFO) << "Probing media device '" << name.value() << "'";
+      ProbeMediaController(fd);
+      close(fd);
+    }
+  }
+
+  const CameraVector& GetPlatformCameras() {
+    if (platform_cameras_.empty())
+      AddV4l2Cameras();
+
+    return platform_cameras_;
+  }
+
+  CameraVector platform_cameras_;
+};
+
+bool StringEqualsCaseInsensitiveASCII(const std::string& a,
+                                      const std::string& b) {
+  return base::EqualsCaseInsensitiveASCII(a, b);
+}
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  // Init CommandLine for InitLogging.
+  base::CommandLine::Init(argc, argv);
+  base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
+  base::AtExitManager at_exit_manager;
+
+  int log_flags = brillo::kLogToSyslog;
+  if (cl->HasSwitch("foreground")) {
+    log_flags |= brillo::kLogToStderr;
+  }
+  brillo::InitLog(log_flags);
+
+  // FIXME: Currently only "modules list" command is supported
+  const std::vector<std::string>& args = cl->GetArgs();
+  if (cl->GetArgs().empty() ||
+      !std::equal(kArgsPattern.begin(), kArgsPattern.end(), args.begin(),
+                  StringEqualsCaseInsensitiveASCII)) {
+    LOG(ERROR) << "Invalid command.";
+    LOG(ERROR) << "Try following supported commands:";
+    LOG(ERROR) << "  modules - operations on camera modules";
+    LOG(ERROR) << "    list - print available modules";
+    return 1;
+  }
+
+  CameraTool tool;
+  tool.PrintCameras();
+
+  return 0;
+}
--- camera/tools/module.mk
+++ camera/tools/module.mk
@@ -5,14 +5,21 @@
 include common.mk
 include pc_utils.mk
 
-tools_PC_DEPS = libcamera_timezone libchrome-$(BASE_VER) libbrillo-$(BASE_VER)
+tools_PC_DEPS = libchrome-$(BASE_VER) libbrillo-$(BASE_VER)
 tools_CPPFLAGS := $(call get_pc_cflags,$(tools_PC_DEPS))
 tools_LDLIBS := $(call get_pc_libs,$(tools_PC_DEPS))
 
-CXX_BINARY(tools/generate_camera_profile): CPPFLAGS += $(tools_CPPFLAGS)
-CXX_BINARY(tools/generate_camera_profile): LDLIBS += $(tools_LDLIBS)
+timezone_CPPFLAGS := $(call get_pc_cflags,libcamera_timezone)
+timezone_LDLIBS := $(call get_pc_libs,libcamera_timezone)
+
+CXX_BINARY(tools/generate_camera_profile): CPPFLAGS += \
+	$(timezone_CPPFLAGS) \
+	$(tools_CPPFLAGS)
+CXX_BINARY(tools/generate_camera_profile): LDLIBS += \
+	$(timezone_LDLIBS) \
+	$(tools_LDLIBS)
 CXX_BINARY(tools/generate_camera_profile): \
-	$(tools_CXX_OBJECTS) \
+	tools/generate_camera_profile.o \
 	hal/usb/camera_characteristics.o \
 	hal/usb/v4l2_camera_device.o
 
@@ -21,3 +28,14 @@ tools/generate_camera_profile: CXX_BINARY(tools/generate_camera_profile)
 clean: CLEAN(tools/generate_camera_profile)
 
 .PHONY: tools/generate_camera_profile
+
+CXX_BINARY(tools/cros-camera-tool): CPPFLAGS += $(tools_CPPFLAGS)
+CXX_BINARY(tools/cros-camera-tool): LDLIBS += $(tools_LDLIBS)
+CXX_BINARY(tools/cros-camera-tool): \
+	tools/cros_camera_tool.o
+
+tools/cros-camera-tool: CXX_BINARY(tools/cros-camera-tool)
+
+clean: CLEAN(tools/cros-camera-tool)
+
+.PHONY: tools/cros-camera-tool
