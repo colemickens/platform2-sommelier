3fde0d30abfef15d984a212a93c64e934a922f79
Ben Chan <benchan@chromium.org>
2018-02-13 17:09:28 -0800

    wimax: remove implementation for ignored D-Bus signals
    dbus-c++ has been modified to provide a default implementation for
    signal handlers in the generated proxy base class.
    
    BUG=chromium:776452
    CQ-DEPEND=CL:917528
    TEST=Run unit tests.
    
    Change-Id: Id3803a63227718cb9024e33da6f40ac0e21775e9
    Reviewed-on: https://chromium-review.googlesource.com/917533
    Commit-Ready: Ben Chan <benchan@chromium.org>
    Tested-by: Ben Chan <benchan@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- wimax_manager/power_manager_dbus_proxy.h
+++ wimax_manager/power_manager_dbus_proxy.h
@@ -27,25 +27,6 @@ class PowerManagerDBusProxy : public org::chromium::PowerManager_proxy,
   void SuspendImminent(const std::vector<uint8_t>& serialized_proto) override;
   void SuspendDone(const std::vector<uint8_t>& serialized_proto) override;
 
-  // Ignored signals:
-  void BrightnessChanged(const int32_t& brightness_percent,
-                         const bool& user_initiated) override {}
-  void KeyboardBrightnessChanged(const int32_t& brightness_percent,
-                                 const bool& user_initiated) override {}
-  void PeripheralBatteryStatus(
-      const std::vector<uint8_t>& serialized_proto) override {}
-  void PowerSupplyPoll(const std::vector<uint8_t>& serialized_proto) override {}
-  void DarkSuspendImminent(
-      const std::vector<uint8_t>& serialized_proto) override {}
-  void InputEvent(const std::vector<uint8_t>& serialized_proto) override {}
-  void IdleActionImminent(
-      const std::vector<uint8_t>& serialized_proto) override {}
-  void IdleActionDeferred() override {}
-  void ScreenIdleStateChanged(
-      const std::vector<uint8_t>& serialized_proto) override {}
-  void InactivityDelaysChanged(
-      const std::vector<uint8_t>& serialized_proto) override {}
-
  private:
   PowerManager* power_manager_;
 
