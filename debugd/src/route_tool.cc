bf05ce7c486a89d576b1e1648226e92c15a7bfb0
Kevin Cernekee <cernekee@chromium.org>
2018-03-20 13:33:12 -0700

    debugd: Display all routing tables, not just RT_TABLE_MAIN
    Chrome OS now uses routing policy rules + multiple routing tables.
    Display the whole routing configuration when the user runs `route`
    in crosh.
    
    BUG=chromium:821236
    TEST=manually run `route` command while VPN is connected
    
    Change-Id: I249625c315c976a06d1db5095315ae5df7c5d6de
    Reviewed-on: https://chromium-review.googlesource.com/972354
    Commit-Ready: Kevin Cernekee <cernekee@chromium.org>
    Tested-by: Kevin Cernekee <cernekee@chromium.org>
    Reviewed-by: Kevin Cernekee <cernekee@chromium.org>

--- debugd/src/route_tool.cc
+++ debugd/src/route_tool.cc
@@ -4,6 +4,9 @@
 
 #include "debugd/src/route_tool.h"
 
+#include <base/strings/string_number_conversions.h>
+#include <base/strings/string_util.h>
+#include <base/strings/stringprintf.h>
 #include <brillo/process.h>
 
 #include "debugd/src/process_with_output.h"
@@ -14,25 +17,73 @@ namespace {
 
 const char kIpTool[] = "/bin/ip";
 
+bool RunOneIPCommand(std::vector<std::string>* result,
+                     const std::vector<std::string>& argv) {
+  ProcessWithOutput p;
+  if (!p.Init()) {
+    result->push_back("[ ProcessWithOutput::Init() failed ]");
+    return false;
+  }
+
+  p.AddArg(kIpTool);
+  for (const auto& arg : argv) {
+    p.AddArg(arg);
+  }
+
+  if (p.Run()) {
+    result->push_back("[ ProcessWithOutput::Run() failed ]");
+    return false;
+  }
+
+  p.GetOutputLines(result);
+
+  // GetOutputLines() overwrites |result|, so the heading needs to be
+  // inserted afterward.
+  result->insert(
+      result->begin(),
+      base::StringPrintf("[ ip %s ]", base::JoinString(argv, " ").c_str()));
+  return true;
+}
+
 }  // namespace
 
 std::vector<std::string> RouteTool::GetRoutes(
     const brillo::VariantDictionary& options) {
-  std::vector<std::string> result;
-  ProcessWithOutput p;
-  if (!p.Init())
-    return result;
-  p.AddArg(kIpTool);
+  std::vector<std::string> full_result, cmd_result;
 
+  std::string ip_version = "-4";
   if (brillo::GetVariantValueOrDefault<bool>(options, "v6"))
-    p.AddArg("-6");
-  p.AddArg("r");  // route
-  p.AddArg("s");  // show
+    ip_version = "-6";
 
-  if (p.Run())
-    return result;
-  p.GetOutputLines(&result);
-  return result;
+  RunOneIPCommand(&full_result, {ip_version, "rule", "list"});
+
+  // Always print the main table first.  Ignore local and default since
+  // they'll just confuse the user.
+  RunOneIPCommand(&cmd_result, {ip_version, "route", "show", "table", "main"});
+  full_result.insert(full_result.end(), cmd_result.begin(), cmd_result.end());
+
+  // Multiple routing policy rules can reference the same table, so keep
+  // a map to make sure each table is only printed once.
+  std::map<int, bool> table_map;
+  for (auto line : full_result) {
+    base::TrimWhitespaceASCII(line, base::TRIM_ALL, &line);
+
+    const std::string prefix("lookup ");
+    size_t offset = line.rfind(prefix);
+    if (offset != std::string::npos) {
+      int table_id;
+      if (base::StringToInt(line.substr(offset + prefix.size()), &table_id) &&
+          table_map.find(table_id) == table_map.end()) {
+        table_map[table_id] = true;
+        RunOneIPCommand(&cmd_result, {ip_version, "route", "show", "table",
+                                      base::IntToString(table_id)});
+        full_result.insert(full_result.end(), cmd_result.begin(),
+                           cmd_result.end());
+      }
+    }
+  }
+
+  return full_result;
 }
 
 }  // namespace debugd
