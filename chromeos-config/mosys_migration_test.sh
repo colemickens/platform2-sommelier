0dd3aedb0c08aa69b9e755dff45b98acc364c328
C Shapiro <shapiroc@chromium.org>
2018-04-10 10:59:45 -0600

    chromeos-config: Migration tests cros_config
    Simple tests to check the integrity of the YAML before it's migrated
    over for given devices.
    
    BUG=b:66950157
    TEST=test pre/post migration and diff results
    
    Change-Id: I223a9b44750e674de9c259989ede94365008b07f
    Reviewed-on: https://chromium-review.googlesource.com/1005561
    Commit-Ready: C Shapiro <shapiroc@google.com>
    Tested-by: C Shapiro <shapiroc@google.com>
    Reviewed-by: Jason Clinton <jclinton@chromium.org>
    Reviewed-by: Nick Sanders <nsanders@chromium.org>
    Reviewed-by: Gregory Meinke <gmeinke@chromium.org>

---
+++ chromeos-config/mosys_migration_test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mosys platform vendor
+mosys platform name
+mosys platform model
+mosys platform chassis
+mosys platform sku
+mosys platform brand
+mosys platform customization
+mosys platform signature
+mosys platform version
+mosys platform family
