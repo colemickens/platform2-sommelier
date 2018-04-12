colemickens/platform2-sommelier

commit 6daa9e6c69882d0fdb093163092cdf16abc8ef69

debugd: add SetRlzPingSent method
BUG=chromium:820783
TEST=build_packages

Change-Id: Idb20cc67f92fcbcb46d49bd89e8d64341ccee5b9
Reviewed-on: https://chromium-review.googlesource.com/979269
Commit-Ready: Wenzhao (Colin) Zang <wzang@chromium.org>
Tested-by: Wenzhao (Colin) Zang <wzang@chromium.org>
Reviewed-by: Stephen Lin <stephenlin@google.com>
Reviewed-by: Hung-Te Lin <hungte@chromium.org>
Reviewed-by: Wei-Han Chen <stimim@chromium.org>
Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
Reviewed-by: Mike Frysinger <vapier@chromium.org>

debugd/src/debugd_dbus_adaptor.cc
@@ -17,6 +17,7 @@
 
 #include "debugd/src/constants.h"
 #include "debugd/src/error_utils.h"
+#include "debugd/src/process_with_output.h"
 
 namespace debugd {
 
@@ -24,6 +25,8 @@ namespace {
 
 const char kDevCoredumpDBusErrorString[] =
     "org.chromium.debugd.error.DevCoreDump";
+
+const char kShouldSendRlzPingKey[] = "should_send_rlz_ping";
 
 }  // namespace
 
@@ -421,6 +424,25 @@ void DebugdDBusAdaptor::StartVmConcierge(
 
 void DebugdDBusAdaptor::StopVmConcierge() {
   vm_concierge_tool_->StopVmConcierge();
+}
+
+bool DebugdDBusAdaptor::SetRlzPingSent(brillo::ErrorPtr* error) {
+  std::string stderr;
+  int result = ProcessWithOutput::RunProcess(
+      "vpd", {"-i", "RW_VPD", "-s", std::string(kShouldSendRlzPingKey) + "=0"},
+      true,      // requires root
+      nullptr,   // stdin
+      nullptr,   // stdout
+      &stderr, error);
+  if (result != EXIT_SUCCESS) {
+    std::string error_string =
+        "Failed to set vpd key for " + std::string(kShouldSendRlzPingKey) +
+        " with exit code: " + std::to_string(result) + " with error: " + stderr;
+    DEBUGD_ADD_ERROR(error, kDevCoredumpDBusErrorString, error_string);
+    PLOG(ERROR) << error_string;
+    return false;
+  }
+  return true;
 }
 
 }  // namespace debugd

debugd/dbus_bindings/org.chromium.debugd.xml
@@ -915,5 +915,11 @@
       </tp:docstring>
       <annotation name="org.chromium.DBus.Method.Kind" value="simple"/>
     </method>
+    <method name="SetRlzPingSent">
+      <tp:docstring>
+        Sets should_send_rlz_ping in RW_VPD to 0.
+      </tp:docstring>
+      <annotation name="org.chromium.DBus.Method.Kind" value="normal"/>
+    </method>
   </interface>
 </node>

debugd/src/debugd_dbus_adaptor.h
@@ -171,6 +171,7 @@ class DebugdDBusAdaptor : public org::chromium::debugdAdaptor,
       std::unique_ptr<brillo::dbus_utils::DBusMethodResponse<bool>> response)
       override;
   void StopVmConcierge() override;
+  bool SetRlzPingSent(brillo::ErrorPtr* error) override;
 
  private:
   brillo::dbus_utils::DBusObject dbus_object_;

commit b1b6362d66f6bb213406919d319778d4027b1fd6

debugd: Fix vpd update issue
Fixed a bug introduced by CL 979269, which did not have the correct
path for |ProcessWithOutput::RunProcess|.

Also added dump_vpd_log since |StatisticsProvider| in Chromium relies
on the cache log to look up vpd value fast.

BUG=chromium:820783
TEST=Steps in go/rlz_test

Change-Id: I48412ac1b4e422ae932e42d6d0dcd5edd41ec40a
Reviewed-on: https://chromium-review.googlesource.com/1005913
Commit-Ready: Wenzhao (Colin) Zang <wzang@chromium.org>
Tested-by: Wenzhao (Colin) Zang <wzang@chromium.org>
Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>

debugd/src/debugd_dbus_adaptor.cc
@@ -431,7 +431,8 @@ void DebugdDBusAdaptor::StopVmConcierge() {
 bool DebugdDBusAdaptor::SetRlzPingSent(brillo::ErrorPtr* error) {
   std::string stderr;
   int result = ProcessWithOutput::RunProcess(
-      "vpd", {"-i", "RW_VPD", "-s", std::string(kShouldSendRlzPingKey) + "=0"},
+      "/usr/sbin/vpd", {"-i", "RW_VPD", "-s", std::string(kShouldSendRlzPingKey)
+      + "=0"},
       true,      // requires root
       nullptr,   // stdin
       nullptr,   // stdout
@@ -447,7 +448,8 @@ bool DebugdDBusAdaptor::SetRlzPingSent(brillo::ErrorPtr* error) {
   // Remove |kRlzEmbargoEndDateKey|, which is no longer useful after
   // |kShouldSendRlzPingKey| is updated.
   result = ProcessWithOutput::RunProcess(
-      "vpd", {"-i", "RW_VPD", "-d", std::string(kRlzEmbargoEndDateKey)},
+      "/usr/sbin/vpd", {"-i", "RW_VPD", "-d",
+      std::string(kRlzEmbargoEndDateKey)},
       true,      // requires root
       nullptr,   // stdin
       nullptr,   // stdout
@@ -459,8 +461,23 @@ bool DebugdDBusAdaptor::SetRlzPingSent(brillo::ErrorPtr* error) {
     DEBUGD_ADD_ERROR(error, kDevCoredumpDBusErrorString, error_string);
     PLOG(ERROR) << error_string;
   }
+  // Regenerate the vpd cache log.
+  result = ProcessWithOutput::RunProcess(
+      "/usr/sbin/dump_vpd_log", {"--force"},
+      true,      // requires root
+      nullptr,   // stdin
+      nullptr,   // stdout
+      &stderr, error);
+  if (result != EXIT_SUCCESS) {
+    std::string error_string =
+        "Failed to dump vpd log with exit code: " + std::to_string(result) +
+        " with error: " + stderr;
+    DEBUGD_ADD_ERROR(error, kDevCoredumpDBusErrorString, error_string);
+    PLOG(ERROR) << error_string;
+  }
   // The client only cares if updating |kShouldSendRlzPingKey| is successful, so
-  // returns true regardless of the result of removing |kRlzEmbargoEndDateKey|.
+  // returns true regardless of the result of removing |kRlzEmbargoEndDateKey|
+  // or the cache log update.
   return true;
 }
 