ce837b873ab85ad19fb250c0c6708e7341197df1
Luis Hector Chavez <lhchavez@google.com>
2018-05-09 18:08:38 -0700

    debugd: Use pivot_root(2) for the service's container
    This change restricts the mounts that the debugd process is allowed to
    inherit to:
    
    * /dev (read+write, recursive)
    * /run/dbus
    * /run/cups
    * /sys (read+write, recursive)
    * /var (read+write)
    * /tmp (read+write)
    
    This prevents the debugd process tree from accidentally grabbing
    references to undesired mounts in the init namespace.
    
    BUG=b:65450844
    TEST=platform_AddPrinter.generic
    TEST=platform_DebugDaemonCupsAddPrinters
    TEST=start debugd does not complain or crashloop
    TEST=chrome://net-internals#chromeos could save logs
    TEST=No (unexpected) EROFS, EPERM, EACCES, ENOENT in strace log
    
    Change-Id: Ie21e55f35698055fd15a9a28bc8041d49d175f7c
    Reviewed-on: https://chromium-review.googlesource.com/1053426
    Commit-Ready: Luis Hector Chavez <lhchavez@chromium.org>
    Tested-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- debugd/src/main.cc
+++ debugd/src/main.cc
@@ -12,6 +12,7 @@
 #include <brillo/syslog_logging.h>
 #include <chromeos/dbus/service_constants.h>
 #include <chromeos/libminijail.h>
+#include <chromeos/scoped_minijail.h>
 
 #include "debugd/src/debugd_dbus_adaptor.h"
 
@@ -21,10 +22,48 @@ namespace {
 //
 // We don't want anyone other than our descendants to see our tmpfs.
 void enter_vfs_namespace() {
-  struct minijail* j = minijail_new();
-  minijail_namespace_vfs(j);
-  minijail_enter(j);
-  minijail_destroy(j);
+  ScopedMinijail j(minijail_new());
+
+  // Create a minimalistic mount namespace with just the bare minimum required.
+  minijail_namespace_vfs(j.get());
+  if (minijail_enter_pivot_root(j.get(), "/var/empty"))
+    LOG(FATAL) << "minijail_enter_pivot_root() failed";
+  if (minijail_bind(j.get(), "/", "/", 0))
+    LOG(FATAL) << "minijail_bind(\"/\") failed";
+  if (minijail_mount_with_data(j.get(), "none", "/proc", "proc",
+                               MS_NOSUID | MS_NOEXEC | MS_NODEV, nullptr)) {
+    LOG(FATAL) << "minijail_mount_with_data(\"/proc\") failed";
+  }
+  if (minijail_bind(j.get(), "/var", "/var", 1))
+    LOG(FATAL) << "minijail_bind(\"/var\") failed";
+
+  // Mount /run/dbus to be able to communicate with D-Bus.
+  if (minijail_mount_with_data(j.get(), "tmpfs", "/run", "tmpfs",
+                               MS_NOSUID | MS_NOEXEC | MS_NODEV, nullptr)) {
+    LOG(FATAL) << "minijail_mount_with_data(\"/run\") failed";
+  }
+  if (minijail_bind(j.get(), "/run/dbus", "/run/dbus", 0))
+    LOG(FATAL) << "minijail_bind(\"/run/dbus\") failed";
+
+  // Mount /tmp and /run/cups to be able to communicate with CUPS.
+  if (minijail_bind(j.get(), "/tmp", "/tmp", 1))
+    LOG(FATAL) << "minijail_bind(\"/tmp\") failed";
+  if (minijail_bind(j.get(), "/run/cups", "/run/cups", 0))
+    LOG(FATAL) << "minijail_bind(\"/run/cups\") failed";
+
+  // Mount /dev to be able to inspect devices.
+  if (minijail_mount_with_data(j.get(), "/dev", "/dev", "bind",
+                               MS_BIND | MS_REC, nullptr)) {
+    LOG(FATAL) << "minijail_mount_with_data(\"/dev\") failed";
+  }
+
+  // Mount /sys to access some logs.
+  if (minijail_mount_with_data(j.get(), "/sys", "/sys", "bind",
+                               MS_BIND | MS_REC, nullptr)) {
+    LOG(FATAL) << "minijail_mount_with_data(\"/sys\") failed";
+  }
+
+  minijail_enter(j.get());
 }
 
 class Daemon : public brillo::DBusServiceDaemon {
