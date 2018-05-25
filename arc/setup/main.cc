e279b1641c90aaeea2c30b279f3b7b2894bb25b4
yusukes <yusukes@google.com>
2018-05-25 13:53:50 -0700

    arc-setup: Write a log before exit
    Since we're merging all ARC logs into one file, having this should
    make it a little easier to know where each log line is from.
    
    BUG=None
    TEST=check logs
    
    Change-Id: I8d1d2942e6042bac44c4c7f7b0eaf11a68c57beb
    Reviewed-on: https://chromium-review.googlesource.com/1073892
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- arc/setup/main.cc
+++ arc/setup/main.cc
@@ -8,19 +8,30 @@
 //
 // For unit testing, see arc_setup_util_unittest.cc.
 
+#include <string>
+
 #include <base/at_exit.h>
 #include <base/command_line.h>
+#include <base/timer/elapsed_timer.h>
 #include <base/logging.h>
 
 #include "arc/setup/arc_setup.h"
 
 int main(int argc, char** argv) {
+  base::ElapsedTimer timer;
   base::AtExitManager at_exit;
 
   base::CommandLine::Init(argc, argv);
   logging::InitLogging(logging::LoggingSettings());
 
-  arc::ArcSetup setup;
-  setup.Run();
+  const std::string command_line =
+      base::CommandLine::ForCurrentProcess()->GetCommandLineString();
+  LOG(INFO) << "Starting " << command_line;
+  {
+    arc::ArcSetup setup;
+    setup.Run();
+  }
+  LOG(INFO) << command_line << " took "
+            << timer.Elapsed().InMillisecondsRoundedUp() << "ms";
   return 0;
 }
