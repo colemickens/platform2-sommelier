b86d07c54834725005dc8a3c7cc2438107239d6c
oka@chromium.org <oka@chromium.org>
2018-06-20 09:35:30 +0900

    Reland "Compile arc-adbd and chromeos-init with GN"
    This is a reland of d84d0d06e4a4aabcaff58c28a80c2aeaf3b05a81
    with a patch not to use IUSE.
    
    Original change's description:
    > Compile arc-adbd and chromeos-init with GN
    >
    > platform2.py uses GN on configure if there's the BUILD.gn file in the
    > package directory specified with --platform_subdir. It computes basically the
    > same arguments as ones for GYP and passes it to gn as --args.
    > One notable difference is it also passes toolchain names explicitely,
    > which are in GYP passed as environment variables (CC, CXX, AR_target).
    >
    > gn first looks at .gn, which points it to see common-mk/BUILDCONFIG.gn.
    > BUILDCONFIG.gn declares variables in the file and importing use.gni, and
    > sets up default compiler/linker flags defined in common-mk/BUILD.gn .
    >
    > gn then generates the ninja config reading common-mk/gn_root/BUILD.gn,
    > which lets gn to read the BUILD.gn file in the target package.
    >
    > Correspondence between gni/gn and gyp files are roughly as follows.
    > - common-mk/{BUILD.gn,BUILDCONFIG.gn,use.gni} <-> common.gyp
    > - pkg_config.gni <-> common.gyp portion related to package deps
    > - common-mk/external_dependencies/BUILD.gn <-> common-mk/external_dependencies.gypi
    > - common-mk/gn_root/BUILD.gn <-> N/A
    > - arc/adbd/BUILD.gn <-> arc/adbd/arc-adbd.gyp (same for init and cromo)
    >
    > CQ-DEPEND=CL:1112882
    > BUG=chromium:767517
    > TEST=same as 1112882
    >
    > Change-Id: Ie03c266611b69b608f0d3964c20dfbefd6f61b19
    > Reviewed-on: https://chromium-review.googlesource.com/1107024
    > Commit-Ready: Keigo Oka <oka@chromium.org>
    > Tested-by: Keigo Oka <oka@chromium.org>
    > Reviewed-by: Keigo Oka <oka@chromium.org>
    > Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    
    CQ-DEPEND=CL:1164813
    CQ-DEPEND=CL:1164868
    
    BUG=chromium:767517
    TEST=same as the original
    
    Change-Id: Id6ec8ea8de50a6093dd0488b154f2de5a766e69c
    Reviewed-on: https://chromium-review.googlesource.com/1164703
    Commit-Ready: Keigo Oka <oka@chromium.org>
    Tested-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

--- common-mk/platform2.py
+++ common-mk/platform2.py
@@ -19,18 +19,32 @@ from chromite.lib import cros_build_lib
 from chromite.lib import cros_logging as logging
 from chromite.lib import osutils
 
+import common_utils
+
+
+# List of all USE flags that can be used in BUILD.gn.
+_IUSE = ['amd64', 'arm', 'attestation', 'binder', 'buffet', 'cellular',
+         'cfm_enabled_device', 'cheets', 'chromeos_dbus', 'containers',
+         'cros_host', 'dbus', 'device_mapper', 'dhcpv6', 'direncryption',
+         'feedback', 'ftdi_tpm', 'fuzzer', 'gdmwimax', 'hammerd_api',
+         'iwlwifi_dump', 'kvm_host', 'metrics_uploader', 'mtd', 'opengles',
+         'passive_metrics', 'power_management', 'pppoe', 'profiling', 'systemd',
+         'tcmalloc', 'test', 'tpm2_simulator', 'tpm2', 'tpm', 'vpn',
+         'wake_on_wifi', 'wifi', 'wimax', 'wired_8021x']
+
 
 class Platform2(object):
   """Main builder logic for platform2"""
 
   def __init__(self, use_flags=None, board=None, host=False, libdir=None,
                incremental=True, verbose=False, enable_tests=False,
-               cache_dir=None, jobs=None):
+               cache_dir=None, jobs=None, platform_subdir=None):
     self.board = board
     self.host = host
     self.incremental = incremental
     self.jobs = jobs
     self.verbose = verbose
+    self.platform_subdir = platform_subdir
 
     if use_flags is not None:
       self.use_flags = use_flags
@@ -60,6 +74,15 @@ class Platform2(object):
     else:
       self.cache_dir = os.path.join(self.sysroot,
                                     'var/cache/portage/chromeos-base/platform2')
+
+    self.libbase_ver = os.environ.get('BASE_VER', '')
+    if not self.libbase_ver:
+      # If BASE_VER variable not set, read the content of common_mk/BASE_VER
+      # file which contains the default libchrome revision number.
+      base_ver_file = os.path.join(self.get_src_dir(), 'BASE_VER')
+      self.libbase_ver = osutils.ReadFile(base_ver_file).strip()
+    assert self.libbase_ver
+
 
   def get_src_dir(self):
     """Return the path to build tools and common GYP files"""
@@ -177,15 +200,16 @@ class Platform2(object):
     return glob.glob(os.path.join(self.get_products_path(),
                                   'gen/components_*'))
 
-  def use(self, flag):
-    """Returns a boolean describing whether or not a given USE flag is set."""
-    return flag in self.use_flags
+  def should_use_gn(self):
+    build_gn = os.path.join(self.get_platform2_root(), self.platform_subdir,
+                            'BUILD.gn')
+    return os.path.isfile(build_gn)
 
   def configure(self, args):
     """Runs the configure step of the Platform2 build.
 
-    Creates the build root if it doesn't already exists. Generates flags to
-    run GYP with, and then runs GYP.
+    Creates the build root if it doesn't already exists.  Then runs the
+    appropriate configure tool (e.g. GYP, GN, etc...).
     """
     if not os.path.isdir(self.get_buildroot()):
       os.makedirs(self.get_buildroot())
@@ -193,19 +217,50 @@ class Platform2(object):
     if not self.incremental:
       osutils.RmDir(self.get_products_path(), ignore_missing=True)
 
+    if self.should_use_gn():
+      # The args is only for gyp.
+      assert len(args) == 0 or (len(args) == 1 and '.gyp' in args[0])
+      self.configure_gn()
+    else:
+      self.configure_gyp(args)
+
+  def gen_common_args(self, should_parse_shell_string):
+    """Generates common arguments for the tools to configure as a dict.
+
+    Returned value types are str, bool or list of strs.
+    Lists are returned only when should_parse_shell_string is set to True.
+    """
+    def flags(s):
+      if should_parse_shell_string:
+        return common_utils.parse_shell_args(s)
+      return s
+
+    args = {
+        'OS': 'linux',
+        'pkg-config': self.pkgconfig,
+        'sysroot': self.sysroot,
+        'libdir': self.libdir,
+        'build_root': self.get_buildroot(),
+        'platform2_root': self.get_platform2_root(),
+        'libbase_ver': self.libbase_ver,
+        'enable_exceptions': os.environ.get('CXXEXCEPTIONS', 0) == "1",
+        'external_cflags': flags(os.environ.get('CFLAGS', '')),
+        'external_cxxflags': flags(os.environ.get('CXXFLAGS', '')),
+        'external_cppflags': flags(os.environ.get('CPPFLAGS', '')),
+        'external_ldflags': flags(os.environ.get('LDFLAGS', '')),
+    }
+    return args
+
+  def configure_gyp(self, args):
+    """Configure with GYP.
+
+    Generates flags to run GYP with, and then runs GYP.
+    """
     targets = [os.path.join(self.get_src_dir(), 'platform.gyp')]
     if args:
       targets = args
 
     common_gyp = os.path.join(self.get_src_dir(), 'common.gypi')
-    libbase_ver = os.environ.get('BASE_VER', '')
-    if not libbase_ver:
-      # If BASE_VER variable not set, read the content of common_mk/BASE_VER
-      # file which contains the default libchrome revision number.
-      base_ver_file = os.path.join(self.get_src_dir(), 'BASE_VER')
-      libbase_ver = osutils.ReadFile(base_ver_file).strip()
-
-    assert libbase_ver
 
     # The common root folder of platform2/.
     # Used as (DEPTH) variable in specific project .gyp files.
@@ -219,24 +274,71 @@ class Platform2(object):
         '--depth=%s' % src_root,
         '--toplevel-dir=%s' % self.get_platform2_root(),
         '--generator-output=%s' % self.get_buildroot(),
-        '-DOS=linux',
-        '-Dpkg-config=%s' % self.pkgconfig,
-        '-Dsysroot=%s' % self.sysroot,
-        '-Dlibdir=%s' % self.libdir,
-        '-Dbuild_root=%s' % self.get_buildroot(),
-        '-Dplatform2_root=%s' % self.get_platform2_root(),
-        '-Dlibbase_ver=%s' % libbase_ver,
-        '-Denable_exceptions=%s' % os.environ.get('CXXEXCEPTIONS', '0'),
-        '-Dexternal_cflags=%s' % os.environ.get('CFLAGS', ''),
-        '-Dexternal_cxxflags=%s' % os.environ.get('CXXFLAGS', ''),
-        '-Dexternal_cppflags=%s' % os.environ.get('CPPFLAGS', ''),
-        '-Dexternal_ldflags=%s' % os.environ.get('LDFLAGS', ''),
     ]
+    common_args = self.gen_common_args(False)
+    for k, v in common_args.iteritems():
+      if isinstance(v, bool):
+        v = int(v)
+      gyp_args.append("-D%s=%s" % (k, v))
+
     # USE flags allow some chars that gyp does not, so normalize them.
     gyp_args += ['-DUSE_%s=1' % (use_flag.replace('-', '_'),)
                  for use_flag in self.use_flags]
 
     cros_build_lib.RunCommand(gyp_args, env=self.get_build_environment(),
+                              cwd=self.get_platform2_root())
+
+  def configure_gn(self):
+    """Configure with GN.
+
+    Generates flags to run GN with, and then runs GN.
+    """
+    def to_gn_string(s):
+      return '"%s"' % s.replace('"', '\\"')
+
+    def to_gn_list(strs):
+      return '[%s]' % ','.join([to_gn_string(s) for s in strs])
+
+    gn_args_args = [
+        'platform_subdir="%s"' % self.platform_subdir,
+    ]
+    common_args = self.gen_common_args(True)
+    for k, v in common_args.iteritems():
+      if isinstance(v, bool):
+        v = str(v).lower()
+      elif isinstance(v, list):
+        v = to_gn_list(v)
+      elif isinstance(v, str):
+        v = to_gn_string(v)
+      else:
+        raise AssertionError('Unexpected value type %s' % str(type(v)))
+      gn_args_args.append('%s=%s' % (k.replace('-', '_'), v))
+
+    buildenv = self.get_build_environment()
+    gn_args_args += [
+        'cc="%s"' % buildenv.get('CC_target', buildenv.get('CC', '')),
+        'cxx="%s"' % buildenv.get('CXX_target', buildenv.get('CXX', '')),
+        'ar="%s"' % buildenv.get('AR_target', buildenv.get('AR', '')),
+    ]
+
+    # Set use flags as a scope.
+    uses = {}
+    for flag in _IUSE:
+      uses[flag] = False
+    for x in self.use_flags:
+      uses[x.replace('-', '_')] = True
+    use_args = ['%s=%s' % (x, str(uses[x]).lower()) for x in uses]
+    gn_args_args += ['use={%s}' % (' '.join(use_args))]
+
+    gn_args = ['gn', 'gen']
+    if self.verbose:
+      gn_args += ['-v']
+    gn_args += [
+        '--root=%s' % self.get_platform2_root(),
+        '--args=%s' % ' '.join(gn_args_args),
+        self.get_products_path(),
+    ]
+    cros_build_lib.RunCommand(gn_args, env=buildenv,
                               cwd=self.get_platform2_root())
 
   def compile(self, args):
@@ -249,8 +351,8 @@ class Platform2(object):
     for component in self.get_components_glob():
       os.remove(component)
 
-    if not args:
-      args = ['all']
+    if self.should_use_gn():
+      args = ['%s:all' % self.platform_subdir]
     ninja_args = ['ninja', '-C', self.get_products_path()]
     if self.jobs:
       ninja_args += ['-j', str(self.jobs)]
@@ -299,6 +401,8 @@ def GetParser():
                       help='number of jobs to run in parallel')
   parser.add_argument('--verbose', action='store_true', default=None,
                       help='enable verbose log output')
+  parser.add_argument('--platform_subdir',
+                      help='subdir in platform2 where the package is located')
   parser.add_argument('args', nargs='*')
 
   return parser
@@ -314,10 +418,10 @@ def main(argv):
   if options.verbose is None:
     # Should convert to cros_build_lib.BooleanShellValue.
     options.verbose = (os.environ.get('VERBOSE', '0') == '1')
-
   p2 = Platform2(options.use_flags, options.board, options.host,
                  options.libdir, options.incremental, options.verbose,
-                 options.enable_tests, options.cache_dir, jobs=options.jobs)
+                 options.enable_tests, options.cache_dir, jobs=options.jobs,
+                 platform_subdir=options.platform_subdir)
   getattr(p2, options.action)(options.args)
 
 
