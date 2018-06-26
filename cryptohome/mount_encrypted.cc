8e5278a2337c3b93f8c2ef363fcd36753e9c0dcd
Sarthak Kukreti <sarthakkukreti@chromium.org>
2018-06-25 18:37:13 -0700

    cryptohome: add Platform to EncryptedFs
    - Add platform to EncryptedFs: this allows us to abstract
      away a lot of file-related operations into a mockable
      interface that will (eventuall) be useful for unit-testing.
    
    - Add functions for Access(), SameVFS() and GetBlkSize().
    
    BUG=chromium:808303
    TEST=compiles
    
    Change-Id: I240934c752f110516165b69ec27f44e6b564b0e0
    Signed-off-by: Sarthak Kukreti <sarthakkukreti@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1116340
    Commit-Ready: Gwendal Grignou <gwendal@chromium.org>
    Tested-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>

--- cryptohome/mount_encrypted.cc
+++ cryptohome/mount_encrypted.cc
@@ -177,7 +177,8 @@ void RecordEnumeratedHistogram(MetricsLibrary* metrics,
 int main(int argc, char* argv[]) {
   result_code rc;
   base::FilePath rootdir = base::FilePath(getenv("MOUNT_ENCRYPTED_ROOT"));
-  cryptohome::EncryptedFs encrypted_fs(rootdir);
+  cryptohome::Platform platform;
+  cryptohome::EncryptedFs encrypted_fs(rootdir, &platform);
 
   MetricsLibrary metrics;
   metrics.Init();
