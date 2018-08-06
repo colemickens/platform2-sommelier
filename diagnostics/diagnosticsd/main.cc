8839a47298a31c81681f53292cc90fdea601621d
Maksim Ivanov <emaxx@chromium.org>
2018-08-06 17:28:33 +0200

    diagnostics: Make diagnosticsd run message loop
    Make the main() function run an (infinite) message loop,
    instead of exiting immediately.
    
    BUG=chromium:869377
    TEST=run the build "diagnosticsd" program and check that it
         runs infinitely
    
    Change-Id: I368d581854830af5edcbae3cc91737331bc8e139
    Reviewed-on: https://chromium-review.googlesource.com/1163710
    Commit-Ready: Maksim Ivanov <emaxx@chromium.org>
    Tested-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>

--- diagnostics/diagnosticsd/main.cc
+++ diagnostics/diagnosticsd/main.cc
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <cstdlib>
-
+#include <brillo/daemons/daemon.h>
 #include <brillo/flag_helper.h>
 #include <brillo/syslog_logging.h>
 
@@ -13,5 +12,5 @@ int main(int argc, char** argv) {
 
   brillo::InitLog(brillo::kLogToSyslog | brillo::kLogToStderrIfTty);
 
-  return EXIT_SUCCESS;
+  return brillo::Daemon().Run();
 }
