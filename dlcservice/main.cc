f1bb1db00004f953921939260a01d29846fcb2df
Xiaochu Liu <xiaochu@chromium.org>
2018-08-08 13:31:40 -0700

    dlcservice: print logs to syslog
    Enable printing logs to syslogs.
    
    BUG=chromium:852168
    TEST=None
    
    Change-Id: I63a5db34aea36c29654369c655bf79d81c4d8bc3
    Reviewed-on: https://chromium-review.googlesource.com/1168026
    Commit-Ready: Xiaochu Liu <xiaochu@chromium.org>
    Tested-by: Xiaochu Liu <xiaochu@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- dlcservice/main.cc
+++ dlcservice/main.cc
@@ -2,8 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <brillo/syslog_logging.h>
+
 #include "dlcservice/dlc_service.h"
 
 int main(int argc, char** argv) {
+  brillo::OpenLog("dlcservice", true);
+  brillo::InitLog(brillo::kLogToSyslog);
   return dlcservice::DlcService().Run();
 }
