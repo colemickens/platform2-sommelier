ebeb3ac6c1b97acab2684d1ada73cbe88c03f272
Micah Morton <mortonm@chromium.org>
2018-08-15 08:58:24 -0700

    shill: Add new init script for sandboxing kill switch
    We have a feature flag in Chrome (called ShillSandboxing) that
    will control whether shill is run in a minijail sandbox. Add an Upstart
    init job that queries Chrome for this policy and writes a file
    accordingly that can be accessed upon next boot. This init job will
    start on the "started system-services" Upstart event, which assures
    that Chrome is running but doesn't require the user/guest to have logged
    in.
    
    CQ-DEPEND=CL:1176106,CL:1173260,CL:1204652
    BUG=chromium:649417
    TEST=disable/enable ShillSandboxing Chrome feature flag (through
    /etc/chrome_dev.conf on DUT) and see shill running as root/non-root
    Change-Id: I45a665561b8809f8cbef42a09d6cd35884f0bfe7
    Reviewed-on: https://chromium-review.googlesource.com/1176166
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Micah Morton <mortonm@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- shill/init/shill.sh
+++ shill/init/shill.sh
@@ -61,14 +61,18 @@ if [ ! -f /home/chronos/.oobe_completed ]; then
   ARGS="${ARGS} --portal-list="
 fi
 
-ARGS="${ARGS} --jail-vpn-clients"
-# Run shill as shill user/group in a minijail:
-#   -G so shill programs can inherit supplementary groups.
-#   -n to run shill with no_new_privs.
-#   -B 20 to avoid locking SECURE_KEEP_CAPS flag.
-#   -c for runtime capabilities:
-#     CAP_WAKE_ALARM | CAP_NET_RAW | CAP_NET_ADMIN | CAP_NET_BROADCAST |
-#     CAP_NET_BIND_SERVICE | CAP_SETPCAP | CAP_SETUID | CAP_SETGID | CAP_KILL
-#   --ambient so child processes can inherit runtime capabilities:
-exec /sbin/minijail0 -u shill -g shill -G -n -B 20 -c 800003de0 --ambient \
-     -- /usr/bin/shill ${ARGS}
+if [ -e /var/lib/shill/shill_sandboxing_disabled ]; then
+  exec /usr/bin/shill ${ARGS}
+else
+  ARGS="${ARGS} --jail-vpn-clients"
+  # Run shill as shill user/group in a minijail:
+  #   -G so shill programs can inherit supplementary groups.
+  #   -n to run shill with no_new_privs.
+  #   -B 20 to avoid locking SECURE_KEEP_CAPS flag.
+  #   -c for runtime capabilities:
+  #     CAP_WAKE_ALARM | CAP_NET_RAW | CAP_NET_ADMIN | CAP_NET_BROADCAST |
+  #     CAP_NET_BIND_SERVICE | CAP_SETPCAP | CAP_SETUID | CAP_SETGID | CAP_KILL
+  #   --ambient so child processes can inherit runtime capabilities:
+  exec /sbin/minijail0 -u shill -g shill -G -n -B 20 -c 800003de0 --ambient \
+       -- /usr/bin/shill ${ARGS}
+fi
