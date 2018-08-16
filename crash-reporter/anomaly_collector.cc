f53b8c16e4cf4a921a36d898bec98cbba77b78f1
Mike Frysinger <vapier@chromium.org>
2018-08-15 20:31:52 -0400

    crash: anomaly_collector: move state dir setup to C++
    BUG=chromium:874698
    TEST=precq passes
    
    Change-Id: I9bc9cae0d340a586af7a3eead315e0c71dbda697
    Reviewed-on: https://chromium-review.googlesource.com/1214631
    Commit-Ready: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Luigi Semenzato <semenzato@chromium.org>

--- crash-reporter/anomaly_collector.cc
+++ crash-reporter/anomaly_collector.cc
@@ -4,10 +4,34 @@
 
 #include "crash-reporter/anomaly_collector.h"
 
+#include <pwd.h>
+#include <unistd.h>
+
 #include <string>
 
 #include <brillo/flag_helper.h>
 #include <brillo/syslog_logging.h>
+
+namespace {
+
+// Set up all the state dirs.
+void Initialize() {
+  if (0) {
+    // TODO(semenzato): put this back in once we decide it's safe
+    // to make /var/spool/crash rwxrwxrwx root, or use a different
+    // owner and setuid for the crash reporter as well.
+    struct passwd* user;
+
+    // Get low privilege uid, gid.
+    user = getpwnam("chronos");  // NOLINT(runtime/threadsafe_fn)
+    CHECK_NE(nullptr, user);
+
+    // Drop privileges.
+    CHECK_EQ(0, setuid(user->pw_uid));
+  }
+}
+
+}  // namespace
 
 int main(int argc, char* argv[]) {
   DEFINE_bool(filter, false, "Input is stdin and output is stdout");
@@ -16,6 +40,9 @@ int main(int argc, char* argv[]) {
   brillo::FlagHelper::Init(argc, argv, "Crash Helper: Anomaly Collector");
   brillo::OpenLog("anomaly_collector", true);
   brillo::InitLog(brillo::kLogToSyslog | brillo::kLogToStderrIfTty);
+
+  if (!FLAGS_filter && !FLAGS_test)
+    Initialize();
 
   return AnomalyLexer(FLAGS_filter, FLAGS_test);
 }
