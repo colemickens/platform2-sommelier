6a48b4abdf1ec193064d7bb0feb8e15b92224d88
Mattias Nissler <mnissler@chromium.org>
2018-08-24 14:28:40 +0200

    cryptohome: Remove Tpm dependency from LockboxCache
    Use LockboxContents directly and avoid the heavy-weight TPM
    dependency, which was stubbed out anyways. While at it, get rid of the
    LockboxCache class in favor of a simple function to simplify the code.
    
    BUG=chromium:847438
    TEST=Unit tests.
    
    Change-Id: Ie28e25b6d3713fcfb5d3ab13426dfe596479e5e9
    Reviewed-on: https://chromium-review.googlesource.com/1202423
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Mattias Nissler <mnissler@chromium.org>
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>

--- cryptohome/lockbox-cache.cc
+++ cryptohome/lockbox-cache.cc
@@ -1,84 +1,59 @@
 // Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #include "cryptohome/lockbox-cache.h"
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <memory>
 
 #include <base/files/file_path.h>
 #include <base/logging.h>
 
 #include "cryptohome/lockbox.h"
-#include "cryptohome/lockbox-cache-tpm.h"
 
-using base::FilePath;
-
+namespace cryptohome {
 namespace {
 // Permissions of cache file (modulo umask).
 const mode_t kCacheFilePermissions = 0644;
 }
 
-namespace cryptohome {
-
 bool CacheLockbox(cryptohome::Platform* platform,
                   const base::FilePath& nvram_path,
                   const base::FilePath& lockbox_path,
                   const base::FilePath& cache_path) {
-  static const uint32_t kBogusNvramIndex = 0;
-  cryptohome::LockboxCacheTpm cache_tpm(kBogusNvramIndex, nvram_path);
-  cache_tpm.Init(platform, false);
-  cryptohome::LockboxCache cache;
-  return cache.Initialize(platform, &cache_tpm) &&
-         cache.LoadAndVerify(kBogusNvramIndex, lockbox_path) &&
-         cache.Write(cache_path);
-}
-
-bool LockboxCache::Initialize(Platform* platform, Tpm* tpm) {
-  platform_ = platform;
-  tpm_ = tpm;
-  Reset();
-  return true;
-}
-
-void LockboxCache::Reset() {
-  loaded_ = false;
-  contents_.clear();
-}
-
-bool LockboxCache::LoadAndVerify(uint32_t index,
-                                 const FilePath& lockbox_path) {
-  if (loaded_) {
-    LOG(INFO) << "Load() called in succession without a Reset()";
+  brillo::SecureBlob nvram;
+  if (!platform->ReadFileToSecureBlob(nvram_path, &nvram)) {
+    LOG(INFO) << "Failed to read NVRAM contents from " << nvram_path.value();
     return false;
   }
-  if (!platform_->ReadFile(lockbox_path, &contents_)) {
-    LOG(ERROR) << "Failed to read lockbox contents from "
-               << lockbox_path.value();
+  std::unique_ptr<LockboxContents> lockbox = LockboxContents::New(nvram.size());
+  if (!lockbox) {
+    LOG(ERROR) << "Unsupported lockbox size!";
     return false;
   }
-  Lockbox lockbox(tpm_, index);
+  if (!lockbox->Decode(nvram)) {
+    LOG(ERROR) << "Lockbox failed to decode NVRAM data";
+    return false;
+  }
+
+  brillo::Blob lockbox_data;
+  if (!platform->ReadFile(lockbox_path, &lockbox_data)) {
+    LOG(INFO) << "Failed to read lockbox data from " << lockbox_path.value();
+    return false;
+  }
   LockboxError error;
-  if (!lockbox.Load(&error)) {
-    LOG(INFO) << "Lockbox failed to load NVRAM data: " << error;
-    return false;
-  }
-  if (!lockbox.Verify(contents_, &error)) {
+  if (!lockbox->Verify(lockbox_data, &error)) {
     LOG(ERROR) << "Lockbox did not verify!";
     return false;
   }
-  loaded_ = true;
-  return true;
-}
 
-bool LockboxCache::Write(const FilePath& cache_path) const {
   // Write atomically (not durably) because cache file resides on tmpfs.
-  if (!platform_->WriteFileAtomic(cache_path, contents_,
-                                  kCacheFilePermissions)) {
+  if (!platform->WriteFileAtomic(cache_path, lockbox_data,
+                                 kCacheFilePermissions)) {
     LOG(ERROR) << "Failed to write cache file";
     return false;
   }
+
   return true;
 }
 
--- cryptohome/lockbox-cache.h
+++ cryptohome/lockbox-cache.h
@@ -1,19 +1,14 @@
 // Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #ifndef CRYPTOHOME_LOCKBOX_CACHE_H_
 #define CRYPTOHOME_LOCKBOX_CACHE_H_
-
-#include <stdio.h>
-#include <unistd.h>
-
-#include <string>
 
 #include <base/files/file_path.h>
 #include <base/logging.h>
 
 #include "cryptohome/platform.h"
-#include "cryptohome/tpm.h"
 
 namespace cryptohome {
 
@@ -25,29 +20,6 @@ bool CacheLockbox(cryptohome::Platform* platform,
                   const base::FilePath& lockbox_path,
                   const base::FilePath& cache_path);
 
-// Implements a simple writer wrapper for arbitrary Lockboxes
-class LockboxCache {
- public:
-  LockboxCache() : loaded_(false), tpm_(NULL), platform_(NULL) {}
-  virtual ~LockboxCache() {}
-  // Prepares the cache for use.
-  // Does not take ownership of any pointers.
-  virtual bool Initialize(Platform* platform, Tpm* tpm);
-  // Resets the cache for another Load->Write cycle.
-  virtual void Reset();
-  // Loads the supplied |lockbox_path| and validates it against the TPM.
-  // If the data loads and verifies, returns true;
-  virtual bool LoadAndVerify(uint32_t index,
-                             const base::FilePath& lockbox_path);
-  // Iff LoadAndVerify() returned true, will write out the loaded lockbox
-  // contents to |cache_path|.
-  virtual bool Write(const base::FilePath& cache_path) const;
- private:
-  bool loaded_;
-  Tpm* tpm_;
-  Platform* platform_;
-  brillo::Blob contents_;
-};
 }  // namespace cryptohome
 
 #endif  // CRYPTOHOME_LOCKBOX_CACHE_H_
