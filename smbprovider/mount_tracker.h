2b6b536e281dd0f91ed5e9dc58801c1cd627f93a
Jimmy Gong <jimmyxgong@google.com>
2018-09-05 15:56:08 -0700

    smbprovider: Add MountInfo to MountTracker
    - MountInfo will be moved from MountManager to MountTracker.
    - MountTracker is responsible for managing data pertaining to a mount's
      credentials and status. MountInfo holds data of a mount's credentials.
    
    BUG=chromium:878498
    TEST=emerges
    
    Change-Id: I0bb308a3a72d30c3ad8096f599e58d0705f6419f
    Reviewed-on: https://chromium-review.googlesource.com/1208891
    Commit-Ready: jimmy gong <jimmyxgong@chromium.org>
    Tested-by: jimmy gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>

--- smbprovider/mount_tracker.h
+++ smbprovider/mount_tracker.h
@@ -5,7 +5,21 @@
 #ifndef SMBPROVIDER_MOUNT_TRACKER_H_
 #define SMBPROVIDER_MOUNT_TRACKER_H_
 
+#include <memory>
+#include <string>
+#include <utility>
+
+#include <base/callback.h>
 #include <base/macros.h>
+
+#include "smbprovider/constants.h"
+#include "smbprovider/metadata_cache.h"
+#include "smbprovider/samba_interface.h"
+#include "smbprovider/smb_credential.h"
+
+namespace base {
+class TickClock;
+};
 
 namespace smbprovider {
 
@@ -13,6 +27,34 @@ class MountTracker {
  public:
   MountTracker();
   ~MountTracker();
+
+ private:
+  // Maintains the state of a single mount. Contains the mount root path and
+  // the metadata cache.
+  struct MountInfo {
+    MountInfo() = default;
+    MountInfo(MountInfo&& other) = default;
+    MountInfo(const std::string& mount_root,
+              base::TickClock* tick_clock,
+              SmbCredential credential,
+              std::unique_ptr<SambaInterface> samba_interface)
+        : mount_root(mount_root),
+          credential(std::move(credential)),
+          samba_interface(std::move(samba_interface)) {
+      cache = std::make_unique<MetadataCache>(
+          tick_clock, base::TimeDelta::FromMicroseconds(
+                          kMetadataCacheLifetimeMicroseconds));
+    }
+
+    MountInfo& operator=(MountInfo&& other) = default;
+
+    std::string mount_root;
+    SmbCredential credential;
+    std::unique_ptr<SambaInterface> samba_interface;
+    std::unique_ptr<MetadataCache> cache;
+
+    DISALLOW_COPY_AND_ASSIGN(MountInfo);
+  };
 
   DISALLOW_COPY_AND_ASSIGN(MountTracker);
 };
