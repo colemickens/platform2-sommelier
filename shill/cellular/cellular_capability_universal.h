89a721bc626d812a9fc63a75e93d5670b6bd7c3d
Alex Khouderchah <akhouderchah@chromium.org>
2018-10-04 10:12:53 -0700

    shill: cellular: add IPv6 whitelist
    In the past, IPv6 has been unconditionally disabled on cellular due to
    issues with suspend/resume in which a device receives a new IPv6
    address on resume but has applications which continue using the old IPv6
    address.
    
    This change does not address that issue, but simply whitelists the
    L850-GL modem for use with IPv6. The change is unmodified from
    @ejcaruso's original CL:740577.
    
    BUG=chromium:726815
    TEST=Deploying `USE=dhcpv6 emerge-nocturne shill` leads to the DUT
         getting an IPv6 device on the wwan interface. Putting the DUT to
         sleep and resuming indeed leads to a cycle of connecting and
         disconnecting to the LTE network for a few minutes before
         connecting.
    
    Change-Id: I7d71a42dc53b0f695692e4eab02b36fa34046b80
    Reviewed-on: https://chromium-review.googlesource.com/1319981
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Alex Khouderchah <akhouderchah@chromium.org>
    Reviewed-by: Alex Khouderchah <akhouderchah@chromium.org>

--- shill/cellular/cellular_capability_universal.h
+++ shill/cellular/cellular_capability_universal.h
@@ -43,6 +43,7 @@ class CellularCapabilityUniversal : public CellularCapability {
   using SignalQuality = std::tuple<uint32_t, bool>;
   using ModesData = std::tuple<uint32_t, uint32_t>;
   using SupportedModes = std::vector<ModesData>;
+  using SupportedIpFamilies = uint32_t;
   using PcoList = std::vector<std::tuple<uint32_t, bool, std::vector<uint8_t>>>;
 
   // Constants used in connect method call.  Make available to test matchers.
@@ -55,6 +56,7 @@ class CellularCapabilityUniversal : public CellularCapability {
   static const char kConnectAllowedAuth[];
   static const char kConnectNumber[];
   static const char kConnectAllowRoaming[];
+  static const char kConnectIpType[];
 
   CellularCapabilityUniversal(Cellular* cellular, ModemInfo* modem_info);
   ~CellularCapabilityUniversal() override;
@@ -303,6 +305,7 @@ class CellularCapabilityUniversal : public CellularCapability {
   void OnAccessTechnologiesChanged(uint32_t access_technologies);
   void OnSupportedModesChanged(const std::vector<ModemModes>& supported_modes);
   void OnCurrentModesChanged(const ModemModes& current_modes);
+  void OnSupportedIpFamiliesChanged(SupportedIpFamilies supported_ip_families);
   void OnBearersChanged(const RpcIdentifiers& bearers);
   void OnLockRetriesChanged(const LockRetryData& lock_retries);
   void OnLockTypeChanged(MMModemLock unlock_required);
@@ -402,6 +405,7 @@ class CellularCapabilityUniversal : public CellularCapability {
   std::unique_ptr<CellularBearer> active_bearer_;
   RpcIdentifiers bearer_paths_;
   bool reset_done_;
+  SupportedIpFamilies supported_ip_families_;
 
   // If the modem is not in a state to be enabled when StartModem is called,
   // enabling is deferred using this callback.
