42f6c32c9cee633d46dd47076a82d2f857b6844d
Yunlian Jiang <yunlian@google.com>
2018-10-16 12:37:22 -0700

    installer: include sys/sysmacros.h for major/minor
    These functions have always been defined in sys/sysmacros.h under
    Linux C libraries.  For some, including sys/types.h implicitly
    includes that as well, but glibc 2.25 deprecated that.  Include
    the header explicitly for the funcs to make glibc 2.27 happy.
    
    BUG=chromium:895954
    TEST=sudo emerge chromeos-installer with glibc 2.27.
    
    Change-Id: I2b6d2ba3fd33c6ca375558732153721dd9f60dbb
    Reviewed-on: https://chromium-review.googlesource.com/1284111
    Commit-Ready: Yunlian Jiang <yunlian@chromium.org>
    Tested-by: Yunlian Jiang <yunlian@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- installer/cgpt_manager.cc
+++ installer/cgpt_manager.cc
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/sysmacros.h>
 #include <sys/types.h>
 #include <unistd.h>
 
--- installer/chromeos_verity.cc
+++ installer/chromeos_verity.cc
@@ -14,6 +14,7 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
+#include <sys/sysmacros.h>
 #include <sys/types.h>
 #include <unistd.h>
 
