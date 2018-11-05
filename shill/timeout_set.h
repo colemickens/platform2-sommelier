108bc08b5bf05295a846f510f0470968766aa27d
Alex Khouderchah <akhouderchah@chromium.org>
2018-11-05 08:50:36 -0800

    shill: Add data structure to contain elements with finite lifetimes
    There are various scenarios in which we want to maintain a set of
    elements that have finite lifetimes, such as expiring routing table
    entries or dns mappings. The TimeoutSet class serves this purpose,
    taking care of removing elements from the set when they expire, and
    optionally informing the client when elements expire.
    
    BUG=chromium:726815
    TEST=- Manual testing with integer and IPAddress element types
         - All unit tests are passing
    
    Change-Id: Ia38cada01343c364acf00d5161f8906aa01ce20a
    Reviewed-on: https://chromium-review.googlesource.com/1319983
    Commit-Ready: Eric Caruso <ejcaruso@chromium.org>
    Tested-by: Alex Khouderchah <akhouderchah@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

---
+++ shill/timeout_set.h
@@ -0,0 +1,152 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SHILL_TIMEOUT_SET_H_
+#define SHILL_TIMEOUT_SET_H_
+
+#include <algorithm>
+#include <map>
+#include <set>
+#include <utility>
+#include <vector>
+
+#include "base/cancelable_callback.h"
+#include "base/time/time.h"
+
+#include "shill/event_dispatcher.h"
+
+namespace shill {
+
+template <typename T>
+class TimeoutSetTest;
+
+// Class representing a set of elements, in which each element has its own
+// lifetime. When the lifetime of an element has expired the element will be
+// removed from the set. Users may also provide a callback in order to be
+// informed if any element in the set has timed out.
+//
+// This class uses base::TimeTicks to represent times, resulting in a notion of
+// time that never decreases, but may not increase in cases such as the computer
+// being suspended. Thus elements cannot be expected to be removed exactly when
+// their lifetime is expired, but they are guaranteed not to be removed prior to
+// the expiration of their lifetime.
+//
+// Note that this class currently uses no synchronization methods. Therefore
+// insertions should be called on the |dispatcher_| thread so that there are no
+// race conditions involving elements being inserted while timeouts occur.
+template <typename T>
+class TimeoutSet {
+ public:
+  explicit TimeoutSet(EventDispatcher* dispatcher)
+      : dispatcher_(dispatcher) {}
+
+  virtual ~TimeoutSet() {
+    Clear();
+  }
+
+  // Set the callback used to inform clients that some elements have timed out.
+  void SetInformCallback(base::Callback<void(std::vector<T>)> inform_callback) {
+    inform_callback_ = std::move(inform_callback);
+  }
+
+  // Insert an element into the list with the specified lifetime. If the element
+  // already exists, its lifetime will be updated.
+  //
+  // This method currently runs in time linear to the number of elements in the
+  // set, as duplicates are checked for prior to insertion.
+  void Insert(T element, base::TimeDelta lifetime) {
+    // Check for existing element.
+    for (auto iter = elements_.begin(); iter != elements_.end(); ++iter) {
+      if (iter->element == element) {
+        elements_.erase(iter);
+        std::make_heap(elements_.begin(), elements_.end());
+        break;
+      }
+    }
+    // Perform element insertion.
+    base::TimeTicks now = TimeNow();
+    base::TimeTicks deathtime = now + lifetime;
+    elements_.push_back({std::move(element), deathtime});
+    std::push_heap(elements_.begin(), elements_.end());
+
+    int64_t shortest_lifetime = (elements_[0].deathtime - now).InMilliseconds();
+    timeout_callback_.Reset(
+      base::Bind(&TimeoutSet::OnTimeout, base::Unretained(this)));
+    dispatcher_->PostDelayedTask(FROM_HERE,
+                                 timeout_callback_.callback(),
+                                 std::max(shortest_lifetime,
+                                          static_cast<int64_t>(0)));
+  }
+
+  // Remove all elements and cancel any pending timeout.
+  void Clear() {
+    elements_.clear();
+    timeout_callback_.Cancel();
+  }
+
+  inline bool IsEmpty() const { return elements_.empty(); }
+
+  // Call |apply_func| on each element that hasn't timed out.
+  void Apply(base::Callback<void(const T& element)> apply_func) {
+    for (const auto& elem : elements_) {
+      apply_func.Run(elem.element);
+    }
+  }
+
+ private:
+  struct TimeElement {
+    T element;
+    base::TimeTicks deathtime;
+
+    bool operator<(const TimeElement& rhs) const {
+      // Since std::make_heap makes a max heap, define ordering such that the
+      // greatest elements expire first.
+      return deathtime >= rhs.deathtime;
+    }
+  };
+
+  virtual base::TimeTicks TimeNow() const {
+    return base::TimeTicks::Now();
+  }
+
+  void OnTimeout() {
+    std::vector<T> removed_elements;
+    // Invalidate all elements that have timed out.
+    base::TimeTicks now = TimeNow();
+    while (elements_.size() && elements_[0].deathtime <= now) {
+      removed_elements.push_back(std::move(elements_[0].element));
+      std::pop_heap(elements_.begin(), elements_.end());
+      elements_.pop_back();
+    }
+    // Post task for earliest subsequent timeout.
+    if (elements_.size()) {
+      int64_t shortest_lifetime =
+        (elements_[0].deathtime - now).InMilliseconds();
+      timeout_callback_.Reset(
+        base::Bind(&TimeoutSet::OnTimeout, base::Unretained(this)));
+      dispatcher_->PostDelayedTask(FROM_HERE,
+                                   timeout_callback_.callback(),
+                                   std::max(shortest_lifetime,
+                                            static_cast<int64_t>(0)));
+    }
+
+    if (!inform_callback_.is_null()) {
+      inform_callback_.Run(std::move(removed_elements));
+    }
+  }
+
+  friend class TimeoutSetTest<T>;
+
+  std::vector<TimeElement> elements_;
+
+  // Executes when an element times out. Calls OnTimeout.
+  base::CancelableClosure timeout_callback_;
+  // Called at the end of OnTimeout to inform user of timeout.
+  base::Callback<void(std::vector<T>)> inform_callback_;
+  EventDispatcher* dispatcher_;
+};
+
+}  // namespace shill
+
+#endif  // SHILL_TIMEOUT_SET_H_
---
+++ shill/timeout_set_test.cc
@@ -0,0 +1,173 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "shill/timeout_set.h"
+
+#include <algorithm>
+#include <utility>
+#include <vector>
+
+#include <base/bind.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "shill/logging.h"
+#include "shill/mock_event_dispatcher.h"
+#include "shill/net/ip_address.h"
+
+using testing::_;
+using testing::AtLeast;
+using testing::Test;
+
+namespace shill {
+
+// Expect that the provided elements are the exact elements that timeout.
+// This macro will generally be called after calling IncrementTime, and must be
+// called within a TYPED_TEST.
+#define EXPECT_TIMEOUT(...)                                         \
+  do {                                                              \
+    std::vector<TypeParam> expected_elements{__VA_ARGS__};          \
+    std::sort(expected_elements.begin(), expected_elements.end());  \
+    this->SimulateTimeout();                                        \
+    std::sort(this->timeout_elements_.begin(),                      \
+              this->timeout_elements_.end());                       \
+    EXPECT_EQ(expected_elements, this->timeout_elements_);          \
+  } while (0)
+
+template <typename T>
+struct TestData {
+  TestData() {
+    data.push_back(1);
+    data.push_back(2);
+    data.push_back(3);
+  }
+
+  std::vector<T> data;
+};
+
+template <>
+struct TestData<IPAddress> {
+  TestData() {
+    data.push_back(IPAddress("121.44.30.54"));
+    data.push_back(IPAddress("192.144.30.54"));
+    data.push_back(IPAddress("0.0.0.0"));
+  }
+
+  std::vector<IPAddress> data;
+};
+
+template <typename T>
+class TimeoutSetTest : public Test {
+ public:
+  TimeoutSetTest()
+      : current_time_(0),
+        elements_(&current_time_, &dispatcher_) {
+    elements_.SetInformCallback(base::Bind(&TimeoutSetTest::OnTimeout,
+                                           base::Unretained(this)));
+  }
+
+ protected:
+  class TestTimeoutSet : public TimeoutSet<T> {
+   public:
+    TestTimeoutSet(const int64_t* current_time, EventDispatcher* dispatcher)
+        : TimeoutSet<T>(dispatcher),
+          current_time_(current_time) {}
+
+   private:
+    base::TimeTicks TimeNow() const override {
+      return base::TimeTicks::FromInternalValue(*current_time_);
+    }
+
+    const int64_t* current_time_;
+  };
+
+  void IncrementTime(int64_t amount_ms) { current_time_ += amount_ms*1000; }
+  // Acts as though a timeout event occurred. EXPECT_TIMEOUT should generally be
+  // used instead of this.
+  void SimulateTimeout() { elements_.OnTimeout(); }
+  void OnTimeout(std::vector<T> timeout_elements) {
+    timeout_elements_ = std::move(timeout_elements);
+  }
+
+  int64_t current_time_;
+  MockEventDispatcher dispatcher_;
+  TestData<T> data_;
+  TestTimeoutSet elements_;
+  std::vector<T> timeout_elements_;
+};
+
+typedef ::testing::Types<char, int, float, IPAddress> TestTypes;
+TYPED_TEST_CASE(TimeoutSetTest, TestTypes);
+
+TYPED_TEST(TimeoutSetTest, EmptyInsertion) {
+  EXPECT_TRUE(this->elements_.IsEmpty());
+
+  EXPECT_CALL(this->dispatcher_, PostDelayedTask(_, _, _));
+  this->elements_.Insert(this->data_.data[0],
+                         base::TimeDelta::FromMilliseconds(10));
+  EXPECT_FALSE(this->elements_.IsEmpty());
+}
+
+TYPED_TEST(TimeoutSetTest, SingleTimeout) {
+  EXPECT_CALL(this->dispatcher_, PostDelayedTask(_, _, _))
+      .Times(AtLeast(1));
+  this->elements_.Insert(this->data_.data[0],
+                         base::TimeDelta::FromMilliseconds(10));
+
+  this->IncrementTime(10);
+  EXPECT_TIMEOUT(this->data_.data[0]);
+
+  EXPECT_TRUE(this->elements_.IsEmpty());
+}
+
+TYPED_TEST(TimeoutSetTest, MultipleSequentialTimeouts) {
+  EXPECT_CALL(this->dispatcher_, PostDelayedTask(_, _, _))
+      .Times(AtLeast(1));
+  this->elements_.Insert(this->data_.data[0],
+                         base::TimeDelta::FromMilliseconds(10));
+  this->elements_.Insert(this->data_.data[1],
+                         base::TimeDelta::FromMilliseconds(20));
+
+  this->IncrementTime(10);
+  EXPECT_TIMEOUT(this->data_.data[0]);
+
+  this->IncrementTime(10);
+  EXPECT_TIMEOUT(this->data_.data[1]);
+
+  EXPECT_TRUE(this->elements_.IsEmpty());
+}
+
+// Single timeout has multiple elements expiring.
+TYPED_TEST(TimeoutSetTest, MultiTimeout) {
+  EXPECT_CALL(this->dispatcher_, PostDelayedTask(_, _, _))
+      .Times(AtLeast(1));
+  this->elements_.Insert(this->data_.data[0],
+                         base::TimeDelta::FromMilliseconds(10));
+  this->elements_.Insert(this->data_.data[1],
+                         base::TimeDelta::FromMilliseconds(10));
+
+  this->IncrementTime(10);
+  EXPECT_TIMEOUT(this->data_.data[0], this->data_.data[1]);
+
+  EXPECT_TRUE(this->elements_.IsEmpty());
+}
+
+TYPED_TEST(TimeoutSetTest, InsertResetTimeout) {
+  EXPECT_CALL(this->dispatcher_, PostDelayedTask(_, _, _))
+      .Times(AtLeast(1));
+  this->elements_.Insert(this->data_.data[0],
+                         base::TimeDelta::FromMilliseconds(20));
+  this->elements_.Insert(this->data_.data[1],
+                         base::TimeDelta::FromMilliseconds(10));
+
+  this->IncrementTime(10);
+  EXPECT_TIMEOUT(this->data_.data[1]);
+
+  this->IncrementTime(10);
+  EXPECT_TIMEOUT(this->data_.data[0]);
+
+  EXPECT_TRUE(this->elements_.IsEmpty());
+}
+
+}  // namespace shill
