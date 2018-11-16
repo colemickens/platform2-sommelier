763e67284e2b5dcd206030111bb94c413624c9f7
Wei-Han Chen <stimim@google.com>
2018-11-16 12:23:19 +0800

    regions: add US ANSI keyboard for Japan
    BUG=b:118821791
    TEST=./regions_unittest.py
    
    Change-Id: I07bdb4f1acfa39381af27564dc35832f4eb2e3d0
    Reviewed-on: https://chromium-review.googlesource.com/1338785
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Wei-Han Chen <stimim@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Vineeth Starly <vineeths@google.com>
    Reviewed-by: Philip Chen <philipchen@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>

--- regions/regions.py
+++ regions/regions.py
@@ -463,6 +463,9 @@ REGIONS_LIST = [
         'bg', ['xkb:bg::bul', 'xkb:bg:phonetic:bul'], 'Europe/Sofia', [
             'bg', 'tr', 'en-US'], KML.ANSI,
         'Bulgaria'),
+    Region(
+        'jp.us', 'xkb:us::eng', 'Asia/Tokyo', 'ja', KML.ANSI,
+        'Japan with US keyboard'),
     ]
 
 """A list of :py:class:`regions.Region` objects for
@@ -1266,7 +1269,7 @@ def main(args=sys.argv[1:], out=None):
         out.write(line[column_no].ljust(max_lengths[column_no] + 2))
       out.write('\n')
   else:
-    exit('Sorry, unknown format specified: %s' % args.format)
+    sys.exit('Sorry, unknown format specified: %s' % args.format)
 
 
 if __name__ == '__main__':
