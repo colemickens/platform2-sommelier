4482351ad2d4c66d7666ae665806bd11c7106b81
Brian Norris <briannorris@chromium.org>
2018-11-20 17:30:57 -0800

    shill: fix verizon...fuzzer header ordering
    BUG=none
    TEST=see if someone else agrees I read the style guide correctly
    
    Change-Id: I6d854dd5c3f7378dcacee1096a397827371ffc4c
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1344831
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Ben Chan <benchan@chromium.org>

--- shill/cellular/verizon_subscription_state_fuzzer.cc
+++ shill/cellular/verizon_subscription_state_fuzzer.cc
@@ -2,12 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "shill/cellular/verizon_subscription_state.h"
-
+#include <stdint.h>
 #include <memory>
 #include <vector>
 
-#include <stdint.h>
+#include "shill/cellular/verizon_subscription_state.h"
 
 namespace shill {
 namespace {
