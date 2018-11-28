545550c71f7f901c33391e64eecded2f9f4bca89
Sonny Sasaka <sonnysasaka@chromium.org>
2018-11-27 16:05:01 -0800

    bluetooth: Add upstart script to prepare kernel splitter
    This adds an upstart task that runs early to prepare the LE kernel
    splitter configuration before any Bluetooth daemon (BlueZ or NewBlue)
    starts. Future patches will make those daemons depend on the completion
    of this task.
    
    BUG=chromium:909045
    TEST=Tested on device with kernel splitter built into the kernel. Start
    the device and check that:
      cat /sys/devices/virtual/misc/hci_le/le_splitter_enabled
    is set.
    
    Change-Id: I31715b8e28f12c2e2b611c2feaf710f736448dec
    Reviewed-on: https://chromium-review.googlesource.com/1352383
    Commit-Ready: Sonny Sasaka <sonnysasaka@chromium.org>
    Tested-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Reviewed-by: Dmitry Grinberg <dmitrygr@google.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

---
+++ bluetooth/init/scripts/bluetooth-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+sysfs_config_file=/sys/devices/virtual/misc/hci_le/le_splitter_enabled
+newblue_config_file=/var/lib/bluetooth/newblue
+
+# Make sure the bluetooth module is loaded, otherwise the sysfs splitter config
+# file won't exist.
+modprobe bluetooth
+
+if [ ! -e "${sysfs_config_file}" ]; then
+  # The config file doesn't exist, this means that this device doesn't support
+  # LE splitter, so just ignore and finish the task.
+  exit 0
+fi
+
+if [ -e "${newblue_config_file}" ]; then
+  cat "${newblue_config_file}" >"${sysfs_config_file}"
+else
+  # Default for LE splitter is disabled.
+  echo 0 >"${sysfs_config_file}"
+fi
+
+# Simulate that the USB Bluetooth adapter is re-plugged to trigger BlueZ to
+# re-initialize it.
+# For devices with UART Bluetooth adapter we don't need to trigger the
+# re-initialization here since we can defer the brcm_patchram_plus daemon to run
+# later after this script is done.
+rmmod btusb
+modprobe btusb
