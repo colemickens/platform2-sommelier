fdd4e481c798c0e49c84b901533f835b6be24dba
Oleh Lamzin <lamzin@google.com>
2018-11-29 16:00:32 +0100

    diagnostics: disable flaky test in grpc adapter
    Disable AsyncGrpcClientServerTest.ExcessivelyBigRpcResponse flaky test
    in grpc_async_adapter.
    
    BUG=chromium:910079
    TEST=existing unit tests
    
    Change-Id: Ic128c1253e174a5df571608c0fcbd4c0473e1a67
    Reviewed-on: https://chromium-review.googlesource.com/1355220
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>

--- diagnostics/grpc_async_adapter/async_grpc_client_server_test.cc
+++ diagnostics/grpc_async_adapter/async_grpc_client_server_test.cc
@@ -432,7 +432,9 @@ TEST_F(AsyncGrpcClientServerTest, ExcessivelyBigRpcRequest) {
 
 // Test than an excessively big response gets rejected and results in the
 // request being resolved with an error.
-TEST_F(AsyncGrpcClientServerTest, ExcessivelyBigRpcResponse) {
+//
+// TODO(crbug.com/910079): Disabled due to flakiness.
+TEST_F(AsyncGrpcClientServerTest, DISABLED_ExcessivelyBigRpcResponse) {
   const int kDataSize = 5 * 1024 * 1024;
   const std::string kData(kDataSize, '\1');
 
