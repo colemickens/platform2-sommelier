8115dc54b71e9213db2bc48e470c55172f9e2bce
Alex Khouderchah <akhouderchah@chromium.org>
2018-11-30 10:01:42 -0800

    hermes: smdp: Modify functionality to work with google-lpa library
    CQ-DEPEND=CL:1368986,CL:1490871
    BUG=chromium:910199
    TEST=None.
    
    Change-Id: Ib001e6ab96574ebe2bbfc91b2dc2e42b24e91339
    Reviewed-on: https://chromium-review.googlesource.com/1368985
    Commit-Ready: Alex Khouderchah <akhouderchah@chromium.org>
    Tested-by: Alex Khouderchah <akhouderchah@chromium.org>
    Reviewed-by: Alex Khouderchah <akhouderchah@chromium.org>

--- hermes/smdp.cc
+++ hermes/smdp.cc
@@ -9,264 +9,87 @@
 
 #include <base/base64.h>
 #include <base/bind.h>
+#include <base/files/file_path.h>
 #include <base/json/json_reader.h>
 #include <base/json/json_writer.h>
 #include <base/values.h>
 
 namespace hermes {
 
-Smdp::Smdp(const std::string& server_hostname)
-    : server_hostname_(server_hostname),
-      server_transport_(brillo::http::Transport::CreateDefault()),
-      weak_factory_(this) {}
+SmdpFactory::SmdpFactory(Logger* logger, Executor* executor)
+    : logger_(logger),
+      executor_(executor) {}
 
-void Smdp::InitiateAuthentication(
-    const std::vector<uint8_t>& info1,
-    const std::vector<uint8_t>& challenge,
-    const InitiateAuthenticationCallback& data_callback,
-    const ErrorCallback& error_callback) {
-  std::string encoded_info1(info1.begin(), info1.end());
-  base::Base64Encode(encoded_info1, &encoded_info1);
-  std::string encoded_challenge(challenge.begin(), challenge.end());
-  base::Base64Encode(encoded_challenge, &encoded_challenge);
-
-  base::DictionaryValue http_params;
-  http_params.SetString("euiccInfo1", encoded_info1);
-  http_params.SetString("euiccChallenge", encoded_challenge);
-  http_params.SetString("smdpAddress", server_hostname_);
-  std::string http_body;
-  base::JSONWriter::Write(http_params, &http_body);
-
-  SendJsonRequest(
-      "https://" + server_hostname_ +
-          "/gsma/rsp2/es9plus/initiateAuthentication",
-      http_body,
-      base::Bind(&Smdp::OnInitiateAuthenticationResponse,
-                 weak_factory_.GetWeakPtr(), data_callback, error_callback),
-      error_callback);
+std::unique_ptr<lpa::smdp::SmdpClient> SmdpFactory::NewSmdpClient(
+      std::string tls_certs_dir, std::string smdp_addr,
+      const lpa::proto::EuiccSpecVersion& card_verison) {
+  return std::make_unique<Smdp>(std::move(smdp_addr), std::move(tls_certs_dir),
+                                logger_, executor_);
 }
 
-void Smdp::OnHttpResponse(
-    const base::Callback<void(DictionaryPtr)>& data_callback,
-    const ErrorCallback& error_callback,
-    brillo::http::RequestID request_id,
-    std::unique_ptr<brillo::http::Response> response) {
+Smdp::Smdp(std::string server_addr, std::string certs_dir, Logger* logger,
+           Executor* executor)
+    : server_transport_(brillo::http::Transport::CreateDefault()),
+      logger_(logger),
+      executor_(executor),
+      weak_factory_(this) {
+  server_transport_->UseCustomCertificate(
+    brillo::http::Transport::Certificate::kHermesProd);
+  smdp_addr_ = std::move(server_addr);
+  // Ensure |smdp_addr_| does not begin with a scheme (e.g. "https://"), as this
+  // variable will be used for the smdpAddress field in SM-DP+ communications.
+  size_t found = smdp_addr_.find("://");
+  if (found != std::string::npos) {
+    smdp_addr_.erase(0, found + 3);
+  }
+}
+
+lpa::util::EuiccLog* Smdp::logger() {
+  return logger_;
+}
+
+lpa::util::Executor* Smdp::executor() {
+  return executor_;
+}
+
+void Smdp::SendHttps(const std::string& path, const std::string& request,
+                     LpaCallback cb) {
+  brillo::ErrorPtr error = nullptr;
+  std::string url = "https://" + smdp_addr_ + path;
+  VLOG(1) << __func__ << ": sending data to " << url << ": " << request;
+  brillo::http::Request http_request(url,
+                                     brillo::http::request_type::kPost,
+                                     server_transport_);
+  http_request.SetContentType("application/json");
+  http_request.SetUserAgent("gsma-rsp-lpad");
+  http_request.AddHeader("X-Admin-Protocol", "gsma/rsp/v2.0.0");
+  http_request.AddRequestBody(&request[0], request.size(), &error);
+  CHECK(!error);
+  http_request.GetResponse(
+      base::Bind(&Smdp::OnHttpsResponse, weak_factory_.GetWeakPtr(), cb),
+      base::Bind(&Smdp::OnHttpsError, weak_factory_.GetWeakPtr(), cb));
+}
+
+void Smdp::OnHttpsResponse(LpaCallback cb, brillo::http::RequestID request_id,
+                           std::unique_ptr<brillo::http::Response> response) {
+  std::string raw_data;
   if (!response) {
-    error_callback.Run(std::vector<uint8_t>());
+    cb(0, raw_data, lpa::smdp::SmdpClient::kErrorResponse);
     return;
   }
 
-  std::string raw_data = response->ExtractDataAsString();
-
+  raw_data = response->ExtractDataAsString();
   VLOG(1) << __func__ << ": Response raw_data : " << raw_data;
 
-  std::unique_ptr<base::Value> result = base::JSONReader::Read(raw_data);
-  if (result) {
-    data_callback.Run(base::DictionaryValue::From(std::move(result)));
-  } else {
-    error_callback.Run(std::vector<uint8_t>());
-    return;
-  }
+  cb(response->GetStatusCode(), raw_data, lpa::smdp::SmdpClient::kNoError);
 }
 
-void Smdp::OnInitiateAuthenticationResponse(
-    const InitiateAuthenticationCallback& data_callback,
-    const ErrorCallback& error_callback,
-    std::unique_ptr<base::DictionaryValue> json_dict) {
-  std::string encoded_buffer;
-  std::string server_signed1;
-  if (!json_dict->GetString("serverSigned1", &encoded_buffer)) {
-    error_callback.Run(std::vector<uint8_t>());
-    return;
-  }
-  base::Base64Decode(encoded_buffer, &server_signed1);
-
-  std::string server_signature1;
-  if (!json_dict->GetString("serverSignature1", &encoded_buffer)) {
-    error_callback.Run(std::vector<uint8_t>());
-    return;
-  }
-  base::Base64Decode(encoded_buffer, &server_signature1);
-
-  std::string euicc_ci_pk_id_to_be_used;
-  if (!json_dict->GetString("euiccCiPKIdToBeUsed", &encoded_buffer)) {
-    error_callback.Run(std::vector<uint8_t>());
-    return;
-  }
-  base::Base64Decode(encoded_buffer, &euicc_ci_pk_id_to_be_used);
-
-  std::string server_certificate;
-  if (!json_dict->GetString("serverCertificate", &encoded_buffer)) {
-    error_callback.Run(std::vector<uint8_t>());
-    return;
-  }
-  base::Base64Decode(encoded_buffer, &server_certificate);
-
-  std::string transaction_id;
-  if (!json_dict->GetString("transactionId", &transaction_id)) {
-    error_callback.Run(std::vector<uint8_t>());
-    return;
-  }
-
-  data_callback.Run(
-      transaction_id,
-      std::vector<uint8_t>(server_signed1.begin(), server_signed1.end()),
-      std::vector<uint8_t>(server_signature1.begin(), server_signature1.end()),
-      std::vector<uint8_t>(euicc_ci_pk_id_to_be_used.begin(),
-                           euicc_ci_pk_id_to_be_used.end()),
-      std::vector<uint8_t>(server_certificate.begin(),
-                           server_certificate.end()));
-}
-
-void Smdp::OnHttpError(const ErrorCallback& error_callback,
-                           brillo::http::RequestID request_id,
-                           const brillo::Error* error) {
-  error_callback.Run(std::vector<uint8_t>());
-}
-
-void Smdp::SendJsonRequest(
-    const std::string& url,
-    const std::string& json_data,
-    const base::Callback<void(DictionaryPtr)>& data_callback,
-    const ErrorCallback& error_callback) {
-  VLOG(1) << __func__ << ": sending data : " << json_data;
-  brillo::ErrorPtr error = nullptr;
-  brillo::http::Request request(url, brillo::http::request_type::kPost,
-                                server_transport_);
-  request.SetContentType("application/json");
-  request.SetUserAgent("gsma-rsp-lpad");
-  request.AddHeader("X-Admin-Protocol", "gsma/rsp/v2.0.0");
-  request.AddRequestBody(json_data.data(), json_data.size(), &error);
-  CHECK(!error);
-  request.GetResponse(
-      base::Bind(&Smdp::OnHttpResponse, weak_factory_.GetWeakPtr(),
-                 data_callback, error_callback),
-      base::Bind(&Smdp::OnHttpError, weak_factory_.GetWeakPtr(),
-                 error_callback));
-}
-
-void Smdp::AuthenticateClient(
-    const std::string& transaction_id,
-    const std::vector<uint8_t>& data,
-    const AuthenticateClientCallback& data_callback,
-    const ErrorCallback& error_callback) {
-  std::string encoded_data(data.begin(), data.end());
-  base::Base64Encode(encoded_data, &encoded_data);
-
-  base::DictionaryValue http_params;
-  http_params.SetString("transactionId", transaction_id);
-  http_params.SetString("authenticateServerResponse", encoded_data);
-  std::string http_body;
-  base::JSONWriter::Write(http_params, &http_body);
-
-  SendJsonRequest(
-      "https://" + server_hostname_ + "/gsma/rsp2/es9plus/authenticateClient",
-      http_body,
-      base::Bind(&Smdp::OnAuthenticateClientResponse,
-                 weak_factory_.GetWeakPtr(), data_callback, error_callback),
-      error_callback);
-}
-
-void Smdp::OnAuthenticateClientResponse(
-    const AuthenticateClientCallback& data_callback,
-    const ErrorCallback& error_callback,
-    DictionaryPtr json_dict) {
-  std::string transaction_id;
-  if (!json_dict->GetString("transactionId", &transaction_id)) {
-    LOG(ERROR) << __func__ << ": transactionId not received";
-    error_callback.Run(std::vector<uint8_t>());
-    return;
-  }
-
-  std::string encoded_buffer;
-  std::string profile_metadata;
-  if (!json_dict->GetString("profileMetadata", &encoded_buffer)) {
-    LOG(ERROR) << __func__ << ": profileMetadata not received";
-  }
-  base::Base64Decode(encoded_buffer, &profile_metadata);
-
-  std::string smdp_signed2;
-  if (!json_dict->GetString("smdpSigned2", &encoded_buffer)) {
-    LOG(ERROR) << __func__ << ": smdpSigned2 not received";
-    error_callback.Run(std::vector<uint8_t>());
-    return;
-  }
-  base::Base64Decode(encoded_buffer, &smdp_signed2);
-
-  std::string smdp_signature2;
-  if (!json_dict->GetString("smdpSignature2", &encoded_buffer)) {
-    LOG(ERROR) << __func__ << ": smdpSignature2 not received";
-    error_callback.Run(std::vector<uint8_t>());
-    return;
-  }
-  base::Base64Decode(encoded_buffer, &smdp_signature2);
-
-  std::string server_certificate;
-  if (!json_dict->GetString("smdpCertificate", &encoded_buffer)) {
-    LOG(ERROR) << __func__ << ": smdpCertificate not received";
-    error_callback.Run(std::vector<uint8_t>());
-    return;
-  }
-  base::Base64Decode(encoded_buffer, &server_certificate);
-
-  VLOG(1) << __func__ << ": Client authenticated successfully";
-
-  data_callback.Run(
-      transaction_id,
-      std::vector<uint8_t>(profile_metadata.begin(), profile_metadata.end()),
-      std::vector<uint8_t>(smdp_signed2.begin(), smdp_signed2.end()),
-      std::vector<uint8_t>(smdp_signature2.begin(), smdp_signature2.end()),
-      std::vector<uint8_t>(server_certificate.begin(),
-                           server_certificate.end()));
-}
-
-void Smdp::GetBoundProfilePackage(
-    const std::string& transaction_id,
-    const std::vector<uint8_t>& data,
-    const GetBoundProfilePackageCallback& data_callback,
-    const ErrorCallback& error_callback) {
-  std::string encoded_data(data.begin(), data.end());
-  base::Base64Encode(encoded_data, &encoded_data);
-  // base::Base64Encode(transaction_id, &transaction_id);
-
-  base::DictionaryValue http_params;
-  http_params.SetString("transactionId", transaction_id);
-  http_params.SetString("prepareDownloadResponse", encoded_data);
-  std::string http_body;
-  base::JSONWriter::Write(http_params, &http_body);
-
-  SendJsonRequest(
-      "https://" + server_hostname_ +
-          "/gsma/rsp2/es9plus/getBoundProfilePackage",
-      http_body,
-      base::Bind(&Smdp::OnGetBoundProfilePackageResponse,
-                 weak_factory_.GetWeakPtr(), data_callback, error_callback),
-      error_callback);
-}
-
-void Smdp::OnGetBoundProfilePackageResponse(
-    const GetBoundProfilePackageCallback& data_callback,
-    const ErrorCallback& error_callback,
-    DictionaryPtr json_dict) {
-  std::string transaction_id;
-  if (!json_dict->GetString("transactionId", &transaction_id)) {
-    LOG(ERROR) << __func__ << ": transactionId not received";
-    error_callback.Run(std::vector<uint8_t>());
-    return;
-  }
-
-  std::string encoded_buffer;
-  std::string bound_profile_package;
-  if (!json_dict->GetString("boundProfilePackage", &encoded_buffer)) {
-    LOG(ERROR) << __func__ << ": boundProfilePackage not received";
-    error_callback.Run(std::vector<uint8_t>());
-    return;
-  }
-  base::Base64Decode(encoded_buffer, &bound_profile_package);
-
-  data_callback.Run(transaction_id,
-                    std::vector<uint8_t>(bound_profile_package.begin(),
-                                         bound_profile_package.end()));
+void Smdp::OnHttpsError(LpaCallback cb, brillo::http::RequestID request_id,
+                        const brillo::Error* error) {
+  LOG(WARNING) << "HTTPS request failed (brillo error code " << error->GetCode()
+               << "): " << error->GetMessage();
+  std::string empty;
+  cb(0, empty, lpa::smdp::SmdpClient::kSendHttpsError);
 }
 
 }  // namespace hermes
--- hermes/smdp.h
+++ hermes/smdp.h
@@ -5,104 +5,67 @@
 #ifndef HERMES_SMDP_H_
 #define HERMES_SMDP_H_
 
-#include <cstdint>
 #include <memory>
 #include <string>
-#include <vector>
 
-#include <base/callback.h>
-#include <base/values.h>
 #include <brillo/http/http_request.h>
 #include <brillo/http/http_transport.h>
+#include <google-lpa/lpa/smdx/smdp_client.h>
+#include <google-lpa/lpa/smdx/smdp_client_factory.h>
 
-#include "hermes/smdp_interface.h"
+#include "hermes/executor.h"
+#include "hermes/logger.h"
 
 namespace hermes {
+
+class SmdpFactory : public lpa::smdp::SmdpClientFactory {
+ public:
+  SmdpFactory(Logger* logger, Executor* executor);
+
+  std::unique_ptr<lpa::smdp::SmdpClient> NewSmdpClient(
+      std::string tls_certs_dir, std::string smdp_addr,
+      const lpa::proto::EuiccSpecVersion& card_verison) override;
+
+ private:
+  // Objects owned by the Daemon instance.
+  Logger* logger_;
+  Executor* executor_;
+};
 
 // Class to facilitate communication between the LPD and SM-DP+
 // server. Responsible for opening, maintaining, and closing an
 // HTTPS connection with the SM-DP+ server.
-class Smdp : public SmdpInterface{
+class Smdp : public lpa::smdp::SmdpClient {
  public:
-  using InitiateAuthenticationCallback =
-    base::Callback<void(const std::string& transaction_id,
-                        const std::vector<uint8_t>& server_signed1,
-                        const std::vector<uint8_t>& server_signature1,
-                        const std::vector<uint8_t>& euicc_ci_pk_id_to_be_used,
-                        const std::vector<uint8_t>& server_certificate)>;
-  using AuthenticateClientCallback =
-    base::Callback<void(const std::string& transaction_id,
-                        const std::vector<uint8_t>& profile_metadata,
-                        const std::vector<uint8_t>& smdp_signed2,
-                        const std::vector<uint8_t>& smdp_signature2,
-                        const std::vector<uint8_t>& public_key)>;
-  using GetBoundProfilePackageCallback =
-    base::Callback<void(const std::string& transaction_id,
-                        const std::vector<uint8_t>& bound_profile_package)>;
-  using ErrorCallback =
-      base::Callback<void(const std::vector<uint8_t>& error_data)>;
-  using DictionaryPtr = std::unique_ptr<base::DictionaryValue>;
+  using LpaCallback =
+      std::function<void(int code, std::string& http_resp, int err)>;
 
-  explicit Smdp(const std::string& server_hostname);
-  ~Smdp() = default;
+  Smdp(std::string server_addr, std::string certs_dir, Logger* logger,
+       Executor* executor);
 
-  // First, establishes a connection to the SM-DP+ server over which
-  // the ES8+ secure channel will be tunneled, then sends server the eSIM
-  // challenge and info1 to begin the Authentication procedure. |callback| is
-  // called upon server's response, or |error_callback| is called on server
-  // error.
-  //
-  // Parameters
-  //    challenge - eSIM challenge as returned by Esim.GetEuiccChallenge
-  //    info1 - eSIM info1 as returned by Esim.GetEuiccInfo
-  void InitiateAuthentication(
-      const std::vector<uint8_t>& info1,
-      const std::vector<uint8_t>& challenge,
-      const InitiateAuthenticationCallback& data_callback,
-      const ErrorCallback& error_callback);
+  // lpa::smdp::SmdpClient override.
+  lpa::util::EuiccLog* logger() override;
 
-  void AuthenticateClient(const std::string& transaction_id,
-                          const std::vector<uint8_t>& data,
-                          const AuthenticateClientCallback& data_callback,
-                          const ErrorCallback& error_callback);
-
-  void GetBoundProfilePackage(
-      const std::string& transaction_id,
-      const std::vector<uint8_t>& data,
-      const GetBoundProfilePackageCallback& data_callback,
-      const ErrorCallback& error_callback);
+ protected:
+  // lpa::smdp::SmdpClient overrides.
+  lpa::util::Executor* executor() override;
+  void SendHttps(const std::string& path, const std::string& request,
+                 LpaCallback cb) override;
 
  private:
-  void OnHttpResponse(const base::Callback<void(DictionaryPtr)>& data_callback,
-                      const ErrorCallback& error_callback,
-                      brillo::http::RequestID request_id,
-                      std::unique_ptr<brillo::http::Response> response);
-  void OnHttpError(const ErrorCallback& error_callback,
-                   brillo::http::RequestID request_id,
-                   const brillo::Error* error);
-  void OnInitiateAuthenticationResponse(
-      const InitiateAuthenticationCallback& data_callback,
-      const ErrorCallback& error_callback,
-      DictionaryPtr json_dict);
-  void OnAuthenticateClientResponse(
-      const AuthenticateClientCallback& data_callback,
-      const ErrorCallback& error_callback,
-      DictionaryPtr json_dict);
-  void OnGetBoundProfilePackageResponse(
-      const GetBoundProfilePackageCallback& data_callback,
-      const ErrorCallback& error_callback,
-      DictionaryPtr json_dict);
+  void OnHttpsResponse(LpaCallback cb, brillo::http::RequestID request_id,
+                       std::unique_ptr<brillo::http::Response> response);
 
-  void SendJsonRequest(const std::string& url,
-                       const std::string& json_data,
-                       const base::Callback<void(DictionaryPtr)>& data_callback,
-                       const ErrorCallback& error_callback);
+  void OnHttpsError(LpaCallback cb, brillo::http::RequestID request_id,
+                    const brillo::Error* error);
 
-  const std::string server_hostname_;
   std::shared_ptr<brillo::http::Transport> server_transport_;
-  base::WeakPtrFactory<Smdp> weak_factory_;
 
-  DISALLOW_COPY_AND_ASSIGN(Smdp);
+  // Objects owned by the Daemon instance.
+  Logger* logger_;
+  Executor* executor_;
+
+  base::WeakPtrFactory<Smdp> weak_factory_;
 };
 
 }  // namespace hermes
