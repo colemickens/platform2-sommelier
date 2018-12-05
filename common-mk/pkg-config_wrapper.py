e5a7d50822dcacf39bdf7632b6a1840302bddb40
Hidehiko Abe <hidehiko@chromium.org>
2018-12-06 02:00:50 +0900

    Use lib_switch, lib_dir_switch.
    In GN files, libs and lib_dirs variables should not have "-l" or "-L"
    prefixes respectively, which should be appended by toolchain configuration.
    
    cf)
    https://gn.googlesource.com/gn/+/master/docs/reference.md#libs
    https://gn.googlesource.com/gn/+/master/docs/reference.md#lib_dirs
    
    BUG=chromium:807158
    TEST=Build.
    
    Change-Id: I64eba6b54d851fad1d047fb02c6799445f6c0828
    Reviewed-on: https://chromium-review.googlesource.com/1363650
    Commit-Ready: Hidehiko Abe <hidehiko@chromium.org>
    Tested-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

---
+++ common-mk/pkg-config_wrapper.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Wrapper of pkg-config command line to format output for gn.
+
+Parses the pkg-config output and format it into json,
+so that it can be used in GN files easily.
+
+Usage:
+  pkg-config_wrapper.py pkg-config pkg1 pkg2 ...
+
+Specifically, this script does not expect any additional flags.
+"""
+
+from __future__ import print_function
+
+import json
+import shlex
+import sys
+import subprocess
+
+
+def main(argv):
+  cflags = shlex.split(subprocess.check_output(argv + ['--cflags']))
+  libs = []
+  lib_dirs = []
+  ldflags = []
+  for ldflag in shlex.split(subprocess.check_output(argv + ['--libs'])):
+    if ldflag.startswith('-l'):
+      # Strip -l.
+      libs.append(ldflag[2:])
+    elif ldflag.startswith('-L'):
+      # Strip -L.
+      lib_dirs.append(ldflag[2:])
+    else:
+      ldflags.append(ldflag)
+
+  # Set sort_keys=True for stabilization.
+  result = {
+      'cflags': cflags,
+      'libs': libs,
+      'lib_dirs': lib_dirs,
+      'ldflags': ldflags,
+  }
+  json.dump(result, sys.stdout, sort_keys=True)
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))
