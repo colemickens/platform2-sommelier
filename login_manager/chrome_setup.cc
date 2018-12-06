d367879eb5fed24259edb56f9a3e50ac3b350917
Victor Hsieh <victorhsieh@chromium.org>
2018-12-06 10:49:52 -0800

    login: remove arc_always_start_with_no_play_store flag
    TEST=None (the only known customer, novato, is no longer using it)
    BUG=b:69960761
    
    Change-Id: I942be0cdf4ef57af5ac836bdf39373d5f42844fd
    Reviewed-on: https://chromium-review.googlesource.com/1366377
    Commit-Ready: Victor Hsieh <victorhsieh@chromium.org>
    Tested-by: Victor Hsieh <victorhsieh@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Nicolas Norvez <norvez@chromium.org>

--- login_manager/chrome_setup.cc
+++ login_manager/chrome_setup.cc
@@ -126,8 +126,6 @@ void AddArcFlags(ChromiumCommandBuilder* builder,
     builder->AddArg("--enable-arc-oobe-optin");
   if (builder->UseFlagIsSet("arc_oobe_optin_no_skip"))
     builder->AddArg("--enable-arc-oobe-optin-no-skip");
-  if (builder->UseFlagIsSet("arc_always_start_with_no_play_store"))
-    builder->AddArg("--arc-start-mode=always-start-with-no-play-store");
   if (builder->UseFlagIsSet("arc_transition_m_to_n"))
     builder->AddArg("--arc-transition-migration-required");
   if (builder->UseFlagIsSet("arc_force_2x_scaling"))
