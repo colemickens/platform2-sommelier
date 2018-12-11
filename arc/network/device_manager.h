53fd3e203dbd246ab6275e7fa6e3f595884ca3e1
Garrick Evans <garrick@google.com>
2018-12-11 15:18:42 +0900

    arc: networkd - introduce device manager class and package unit tests.
    This patch was originally intended to add the first unit tests (for Device) to the arc-networkd package but adding more tests (in particular
    to the Manager class) drove the creation of the DeviceManager class to make it easier to test this behavior without the complications of
    the Manager also being a dbus daemon.
    
    BUG=b:77293260
    TEST=Ran new unit test with 'cros_run_unittests --board=$BOARD --package arc-networkd'
    
    Change-Id: Ic65f6c6afeeb07651dac9286d1f79a3d679ddc2c
    Reviewed-on: https://chromium-review.googlesource.com/c/1373189
    Tested-by: Garrick Evans <garrick@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Ben Chan <benchan@chromium.org>
    Trybot-Ready: Garrick Evans <garrick@chromium.org>
    Commit-Queue: Garrick Evans <garrick@chromium.org>

---
+++ arc/network/device_manager.h
@@ -0,0 +1,52 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ARC_NETWORK_DEVICE_MANAGER_H_
+#define ARC_NETWORK_DEVICE_MANAGER_H_
+
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+
+#include "arc/network/device.h"
+#include "arc/network/ipc.pb.h"
+
+namespace arc_networkd {
+
+// Convenience class for managing the collection of host devices and their
+// presentation in the container.
+class DeviceManager {
+ public:
+  // The legacy Android device is initialized in the constructor.
+  explicit DeviceManager(const Device::MessageSink& msg_sink);
+  ~DeviceManager();
+
+  // Provides the list of devices the manager should track.
+  // Returns the number of devices that will be tracked.
+  size_t Reset(const std::set<std::string>& devices);
+
+  // Adds a new device by name. Returns whether the device was successfully
+  // added.
+  bool Add(const std::string& name);
+
+  // Enable/disable device traffic. This is only applicable for the legacy
+  // Android device. Returns false is the device is unknown.
+  bool Enable(const std::string& name, const std::string& ifname);
+  bool Disable(const std::string& name);
+
+ private:
+  // Enables the devices to send messages to the helper process.
+  const Device::MessageSink msg_sink_;
+
+  // Connected devices keyed by the interface name.
+  // The legacy device is mapped to the Android interface name.
+  std::map<std::string, std::unique_ptr<Device>> devices_;
+
+  DISALLOW_COPY_AND_ASSIGN(DeviceManager);
+};
+
+}  // namespace arc_networkd
+
+#endif  // ARC_NETWORK_DEVICE_MANAGER_H_
