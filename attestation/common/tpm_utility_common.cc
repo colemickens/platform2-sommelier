60a730a2ccca753343fb3e0d9b85ee63be39a480
Leo Lai <cylai@google.com>
2019-01-29 16:46:29 +0800

    attestation: implement |TpmUtilityCommon|
    BUG=b:123555030
    TEST=build OK
    
    Change-Id: I1cea98e3e7d541816623b640522d73a7abbcd3e6
    Reviewed-on: https://chromium-review.googlesource.com/1443653
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Leo Lai <cylai@google.com>
    Reviewed-by: Wei-Cheng Xiao <garryxiao@chromium.org>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>

---
+++ attestation/common/tpm_utility_common.cc
@@ -0,0 +1,181 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "attestation/common/tpm_utility_common.h"
+
+#include <memory>
+#include <vector>
+
+#include <base/bind.h>
+#include <base/logging.h>
+#include <crypto/scoped_openssl_types.h>
+#include <crypto/sha2.h>
+#include <openssl/rsa.h>
+
+#include "tpm_manager/client/tpm_nvram_dbus_proxy.h"
+#include "tpm_manager/client/tpm_ownership_dbus_proxy.h"
+#include "tpm_manager/common/tpm_manager_constants.h"
+
+namespace {
+
+const unsigned int kWellKnownExponent = 65537;
+
+// An authorization delegate to manage multiple authorization sessions for a
+// single command.
+
+}  // namespace
+
+namespace attestation {
+
+TpmUtilityCommon::TpmUtilityCommon(
+    tpm_manager::TpmOwnershipInterface* tpm_owner,
+    tpm_manager::TpmNvramInterface* tpm_nvram)
+    : tpm_owner_(tpm_owner), tpm_nvram_(tpm_nvram) {}
+
+TpmUtilityCommon::~TpmUtilityCommon() {}
+
+void TpmUtilityCommon::InitializationTask(base::WaitableEvent* completion) {
+  CHECK(completion);
+  CHECK(tpm_manager_thread_.task_runner()->RunsTasksOnCurrentThread());
+
+  default_tpm_owner_ = std::make_unique<tpm_manager::TpmOwnershipDBusProxy>();
+  default_tpm_nvram_ = std::make_unique<tpm_manager::TpmNvramDBusProxy>();
+  if (default_tpm_owner_->Initialize()) {
+    tpm_owner_ = default_tpm_owner_.get();
+  }
+  if (default_tpm_nvram_->Initialize()) {
+    tpm_nvram_ = default_tpm_nvram_.get();
+  }
+  completion->Signal();
+}
+
+void TpmUtilityCommon::ShutdownTask() {
+  tpm_owner_ = nullptr;
+  tpm_nvram_ = nullptr;
+  default_tpm_owner_.reset(nullptr);
+  default_tpm_nvram_.reset(nullptr);
+}
+
+bool TpmUtilityCommon::Initialize() {
+  if (!tpm_manager_thread_.StartWithOptions(base::Thread::Options(
+          base::MessageLoopForIO::TYPE_IO, 0 /* Default stack size. */))) {
+    LOG(ERROR) << "Failed to start tpm_manager thread.";
+    return false;
+  }
+  if (!tpm_owner_ || !tpm_nvram_) {
+    base::WaitableEvent event(base::WaitableEvent::ResetPolicy::MANUAL,
+                              base::WaitableEvent::InitialState::NOT_SIGNALED);
+    tpm_manager_thread_.task_runner()->PostTask(
+        FROM_HERE, base::Bind(&TpmUtilityCommon::InitializationTask,
+                              base::Unretained(this), &event));
+    event.Wait();
+  }
+  if (!tpm_owner_ || !tpm_nvram_) {
+    LOG(ERROR) << "Failed to initialize tpm_managerd clients.";
+    return false;
+  }
+
+  return true;
+}
+
+bool TpmUtilityCommon::IsTpmReady() {
+  if (!is_ready_) {
+    CacheTpmState();
+  }
+  return is_ready_;
+}
+
+template <typename ReplyProtoType, typename MethodType>
+void TpmUtilityCommon::SendTpmManagerRequestAndWait(
+    const MethodType& method, ReplyProtoType* reply_proto) {
+  base::WaitableEvent event(base::WaitableEvent::ResetPolicy::MANUAL,
+                            base::WaitableEvent::InitialState::NOT_SIGNALED);
+  auto callback = base::Bind(
+      [](ReplyProtoType* target, base::WaitableEvent* completion,
+         const ReplyProtoType& reply) {
+        *target = reply;
+        completion->Signal();
+      },
+      reply_proto, &event);
+  tpm_manager_thread_.task_runner()->PostTask(FROM_HERE,
+                                              base::Bind(method, callback));
+  event.Wait();
+}
+
+bool TpmUtilityCommon::GetEndorsementPassword(std::string* password) {
+  if (endorsement_password_.empty()) {
+    if (!CacheTpmState()) {
+      return false;
+    }
+    if (endorsement_password_.empty()) {
+      LOG(WARNING) << "TPM endorsement password is not available.";
+      return false;
+    }
+  }
+  *password = endorsement_password_;
+  return true;
+}
+
+bool TpmUtilityCommon::GetOwnerPassword(std::string* password) {
+  if (owner_password_.empty()) {
+    if (!CacheTpmState()) {
+      return false;
+    }
+    if (owner_password_.empty()) {
+      LOG(WARNING) << "TPM owner password is not available.";
+      return false;
+    }
+  }
+  *password = owner_password_;
+  return true;
+}
+
+bool TpmUtilityCommon::CacheTpmState() {
+  tpm_manager::GetTpmStatusReply tpm_status;
+  SendTpmManagerRequestAndWait(
+      base::Bind(&tpm_manager::TpmOwnershipInterface::GetTpmStatus,
+                 base::Unretained(tpm_owner_),
+                 tpm_manager::GetTpmStatusRequest()),
+      &tpm_status);
+  if (tpm_status.status() != tpm_manager::STATUS_SUCCESS) {
+    LOG(ERROR) << __func__ << ": Failed to read TPM state from tpm_managerd.";
+    return false;
+  }
+  is_ready_ = tpm_status.enabled() && tpm_status.owned();
+  endorsement_password_ = tpm_status.local_data().endorsement_password();
+  owner_password_ = tpm_status.local_data().owner_password();
+  return true;
+}
+
+bool TpmUtilityCommon::RemoveOwnerDependency() {
+  tpm_manager::RemoveOwnerDependencyReply reply;
+  tpm_manager::RemoveOwnerDependencyRequest request;
+  request.set_owner_dependency(tpm_manager::kTpmOwnerDependency_Attestation);
+  SendTpmManagerRequestAndWait(
+      base::Bind(&tpm_manager::TpmOwnershipInterface::RemoveOwnerDependency,
+                 base::Unretained(tpm_owner_), request),
+      &reply);
+  if (reply.status() != tpm_manager::STATUS_SUCCESS) {
+    LOG(WARNING) << __func__ << ": Failed to remove the dependency.";
+    return false;
+  }
+  return true;
+}
+
+crypto::ScopedRSA TpmUtilityCommon::CreateRSAFromRawModulus(
+    uint8_t* modulus_buffer, size_t modulus_size) {
+  crypto::ScopedRSA rsa(RSA_new());
+  if (!rsa.get())
+    return crypto::ScopedRSA();
+  rsa->e = BN_new();
+  if (!rsa->e)
+    return crypto::ScopedRSA();
+  BN_set_word(rsa->e, kWellKnownExponent);
+  rsa->n = BN_bin2bn(modulus_buffer, modulus_size, NULL);
+  if (!rsa->n)
+    return crypto::ScopedRSA();
+  return rsa;
+}
+
+}  // namespace attestation
---
+++ attestation/common/tpm_utility_common.h
@@ -0,0 +1,125 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ATTESTATION_COMMON_TPM_UTILITY_COMMON_H_
+#define ATTESTATION_COMMON_TPM_UTILITY_COMMON_H_
+
+#include <stdint.h>
+
+#include "attestation/common/tpm_utility.h"
+
+#include <map>
+#include <memory>
+#include <string>
+
+#include <base/macros.h>
+#include <base/threading/thread.h>
+#include <crypto/scoped_openssl_types.h>
+
+#include "tpm_manager/client/tpm_nvram_dbus_proxy.h"
+#include "tpm_manager/client/tpm_ownership_dbus_proxy.h"
+
+namespace attestation {
+
+
+// A TpmUtility implementation for version-independent functions.
+class TpmUtilityCommon : public TpmUtility {
+ public:
+  TpmUtilityCommon() = default;
+  TpmUtilityCommon(tpm_manager::TpmOwnershipInterface* tpm_owner,
+                   tpm_manager::TpmNvramInterface* tpm_nvram);
+  ~TpmUtilityCommon() override;
+
+  // TpmUtility methods.
+  bool Initialize() override;
+  bool IsTpmReady() override;
+  bool RemoveOwnerDependency() override;
+
+ protected:
+  // Tpm_manager communication thread class that cleans up after stopping.
+  class TpmManagerThread : public base::Thread {
+   public:
+    explicit TpmManagerThread(TpmUtilityCommon* tpm_utility)
+        : base::Thread("tpm_manager_thread"), tpm_utility_(tpm_utility) {
+      DCHECK(tpm_utility_);
+    }
+    ~TpmManagerThread() override {
+      Stop();
+    }
+
+   private:
+    void CleanUp() override {
+      tpm_utility_->ShutdownTask();
+    }
+
+    TpmUtilityCommon* const tpm_utility_;
+
+    DISALLOW_COPY_AND_ASSIGN(TpmManagerThread);
+  };
+
+  // Initialization operation that must be performed on the tpm_manager
+  // thread.
+  void InitializationTask(base::WaitableEvent* completion);
+
+  // Shutdown operation that must be performed on the tpm_manager thread.
+  void ShutdownTask();
+
+  // Sends a request to tpm_managerd and waits for a response. The given
+  // interface |method| will be called and a |reply_proto| will be populated.
+  //
+  // Example usage:
+  //
+  // tpm_manager::GetTpmStatusReply tpm_status;
+  // SendTpmManagerRequestAndWait(
+  //     base::Bind(&tpm_manager::TpmOwnershipInterface::GetTpmStatus,
+  //                base::Unretained(tpm_owner_),
+  //                tpm_manager::GetTpmStatusRequest()),
+  //     &tpm_status);
+  template <typename ReplyProtoType, typename MethodType>
+  void SendTpmManagerRequestAndWait(const MethodType& method,
+                                    ReplyProtoType* reply_proto);
+
+  // Gets the endorsement password from tpm_managerd. Returns false if the
+  // password is not available.
+  bool GetEndorsementPassword(std::string* password);
+
+  // Gets the owner password from tpm_managerd. Returns false if the password is
+  // not available.
+  bool GetOwnerPassword(std::string* password);
+
+  // Caches various TPM state including owner / endorsement passwords. On
+  // success, fields like is_ready_ and owner_password_ will be populated.
+  // Returns true on success.
+  bool CacheTpmState();
+
+  static crypto::ScopedRSA CreateRSAFromRawModulus(uint8_t* modulus_buffer,
+                                                   size_t modulus_size);
+
+
+  bool is_ready_{false};
+  std::string endorsement_password_;
+  std::string owner_password_;
+  // |tpm_owner_| and |tpm_nvram_| typically point to |default_tpm_owner_| and
+  // |default_tpm_nvram_| respectively, created/destroyed on the
+  // |tpm_manager_thread_|. As such, should not be accessed after that thread
+  // is stopped/destroyed.
+  tpm_manager::TpmOwnershipInterface* tpm_owner_{nullptr};
+  tpm_manager::TpmNvramInterface* tpm_nvram_{nullptr};
+  // |default_tpm_owner_| and |default_tpm_nvram_| are created and destroyed
+  // on the |tpm_manager_thread_|, and are not available after the thread is
+  // stopped/destroyed.
+  std::unique_ptr<tpm_manager::TpmOwnershipDBusProxy> default_tpm_owner_;
+  std::unique_ptr<tpm_manager::TpmNvramDBusProxy> default_tpm_nvram_;
+
+  // A message loop thread dedicated for asynchronous communication with
+  // tpm_managerd. Declared last, so that it is destroyed before the
+  // objects it uses.
+  TpmManagerThread tpm_manager_thread_{this};
+
+  DISALLOW_COPY_AND_ASSIGN(TpmUtilityCommon);
+};
+
+}  // namespace attestation
+
+#endif  // ATTESTATION_COMMON_TPM_UTILITY_COMMON_H_
