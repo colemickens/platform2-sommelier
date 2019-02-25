5680b44b55d122ee4d23b5d7cdf8a961682ebb10
Micah Morton <mortonm@chromium.org>
2019-02-25 13:27:04 -0800

    init: use SafeSetID LSM instead of chromiumos LSM when available
    This project was originally landed in the chromiumos LSM, but has now
    been upstreamed under its own standalone LSM called SafeSetID. We have
    backported this to 4.14 and 4.19, so lets start using the new LSM so we
    can rip the duplicate functionality out of the chromiumos LSM to clean
    it up a bit.
    
    BUG=chromium:845640
    TEST=security_ProcessManagementPolicy on board with/without SafeSetID
    LSM (board without needs to have chromiumos LSM setid stuff taken out)
    
    Change-Id: Ibb987b8b243cdefd7265897b71c1f6edea488d59
    Reviewed-on: https://chromium-review.googlesource.com/1489991
    Commit-Ready: Micah Morton <mortonm@chromium.org>
    Tested-by: Micah Morton <mortonm@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- init/config_process_mgmt_utils.sh
+++ init/config_process_mgmt_utils.sh
@@ -6,13 +6,25 @@
 # Functions for configuring process management policies in the chromiumos LSM.
 
 # Path to the securityfs file for configuring process management security
-# policies.
-LSM_PROCESS_MGMT_POLICIES="/sys/kernel/security/chromiumos\
+# policies in the chromiumos LSM (used for kernel version <= 4.4).
+# TODO(mortonm): Remove this and the corresponding line in
+# add_process_mgmt_policy when all devices have been updated/backported to get
+# the SafeSetID LSM functionality.
+CHROMIUMOS_PROCESS_MGMT_POLICIES="/sys/kernel/security/chromiumos\
 /process_management_policies/add_whitelist_policy"
+
+# Path to the securityfs file for configuring process management security
+# policies in the SafeSetID LSM (used for kernel version >= 4.14).
+SAFESETID_PROCESS_MGMT_POLICIES="/sys/kernel/security/safesetid\
+/add_whitelist_policy"
 
 # Add process management policy.
 add_process_mgmt_policy() {
-  printf "$1" > "${LSM_PROCESS_MGMT_POLICIES}"
+  if [ -e "${CHROMIUMOS_PROCESS_MGMT_POLICIES}" ]; then
+    printf "$1" > "${CHROMIUMOS_PROCESS_MGMT_POLICIES}"
+  else
+    printf "$1" > "${SAFESETID_PROCESS_MGMT_POLICIES}"
+  fi
 }
 
 # Project-specific process management policies. Projects may add policies by
