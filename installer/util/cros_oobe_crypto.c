fb1be5a611056952f8855801353678a89433e1e6
Amin Hassani <ahassani@chromium.org>
2019-03-01 14:19:51 -0800

    installer: cros_oobe_crypto: Move keygen calls out of assert.
    Release builders use USE=-cros-debug which removes the assert() calls
    from the source code. But we were calling some functions inside asserts
    which is wrong because they get pruned out. This brings them out and
    uses just conditional if to generate failure logs.
    
    Also we should call EC_KEY_set_asn1_flag() before generating the key,
    not after!
    
    BUG=chromium:934380
    TEST=USE=-cros-debug emerge-edgar chromeos-installer; cros_oobe_crypto
    generates correct private and public keys.
    
    Change-Id: I3c5c33723b6c26fcfc8617d7ab7a6545bb6bf292
    Reviewed-on: https://chromium-review.googlesource.com/1496425
    Commit-Ready: Amin Hassani <ahassani@chromium.org>
    Tested-by: Amin Hassani <ahassani@chromium.org>
    Reviewed-by: Nicolas Norvez <norvez@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- installer/util/cros_oobe_crypto.c
+++ installer/util/cros_oobe_crypto.c
@@ -2,23 +2,34 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <assert.h>
+#include <err.h>
 #include <stdio.h>
 
 #include <openssl/ec.h>
 #include <openssl/pem.h>
 
-// We use a P256 curve.
-static const int OOBE_AUTOCONFIG_CURVE_ID = NID_X9_62_prime256v1;
-
 static void GenerateKeyPair(void) {
-  EC_KEY* key = EC_KEY_new_by_curve_name(OOBE_AUTOCONFIG_CURVE_ID);
-  assert(key != NULL);
-  assert(EC_KEY_generate_key(key));
+  // We use a P256 curve.
+  EC_KEY* key = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1);
+  if (key == NULL) {
+    errx(EXIT_FAILURE, "Failed to create a new EC key.");
+  }
 
   EC_KEY_set_asn1_flag(key, OPENSSL_EC_NAMED_CURVE);
-  PEM_write_ECPrivateKey(stdout, key, NULL, NULL, 0, 0, NULL);
-  PEM_write_EC_PUBKEY(stdout, key);
+  if (EC_KEY_generate_key(key) == 0) {
+    errx(EXIT_FAILURE, "Failed to generate the EC key.");
+  }
+
+  if (EC_KEY_check_key(key) == 0) {
+    errx(EXIT_FAILURE, "Failed to validate the EC key.");
+  }
+
+  if (PEM_write_ECPrivateKey(stdout, key, NULL, NULL, 0, 0, NULL) == 0) {
+    errx(EXIT_FAILURE, "Failed to print the private key.");
+  }
+  if (PEM_write_EC_PUBKEY(stdout, key) == 0) {
+    errx(EXIT_FAILURE, "Failed to print the public key.");
+  }
 
   EC_KEY_free(key);
 }
@@ -43,5 +54,5 @@ int main(int argc, char** argv) {
   }
 
   GenerateKeyPair();
-  return 0;
+  return EXIT_SUCCESS;
 }
