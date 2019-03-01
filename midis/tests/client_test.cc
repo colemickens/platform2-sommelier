b27afc5af73fc4f0d517cda7dc3ecbc2f2a57117
Prashant Malani <pmalani@chromium.org>
2019-03-01 15:46:01 -0800

    midis: Correctly shutdown singleton Mojo EDK instance in unit tests
    We need to call the correct routine to perform an orderly shutdown
    and de-allocation of all resources associated with the Mojo EDK
    singleton instance. Not doing so causes several memory leaks which
    get detected by ASAN.
    
    BUG=chromium:925838
    TEST=USE=asan FEATURE=test emerge-betty midis passes without ASAN
         warnings.
    
    Change-Id: I8dfb44cd3572296ac998561726c40a0e8454ed3d
    Reviewed-on: https://chromium-review.googlesource.com/1497480
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Prashant Malani <pmalani@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- midis/tests/client_test.cc
+++ midis/tests/client_test.cc
@@ -6,6 +6,7 @@
 #include <brillo/message_loops/base_message_loop.h>
 #include <gtest/gtest.h>
 #include <mojo/edk/embedder/embedder.h>
+#include <mojo/edk/embedder/test_embedder.h>
 #include <mojo/public/cpp/bindings/binding.h>
 #include <mojo/public/cpp/bindings/interface_request.h>
 
@@ -35,15 +36,26 @@ namespace midis {
 
 class ClientTest : public ::testing::Test {
  public:
+  ClientTest() = default;
+  ~ClientTest() override = default;
+ protected:
+  void SetUp() override {
+    message_loop_.SetAsCurrent();
+    mojo::edk::Init();
+  }
+
+  void TearDown() override {
+    ASSERT_TRUE(mojo::edk::test::Shutdown());
+  }
+ private:
   brillo::BaseMessageLoop message_loop_;
+
+  DISALLOW_COPY_AND_ASSIGN(ClientTest);
 };
 
 // Check that the MidisServer implementation sends back the correct
 // number of devices.
 TEST_F(ClientTest, ListDevices) {
-  message_loop_.SetAsCurrent();
-
-  mojo::edk::Init();
   DeviceTracker tracker;
   arc::mojom::MidisServerPtr server;
 
