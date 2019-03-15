8729041cdfc58faf75510c482fe72d9e74e243cd
Tim Wawrzynczak <twawrzynczak@chromium.org>
2019-03-15 14:57:51 -0600

    timberslide: Prefix EC log lines with host timestamp
    Use the EC_CMD_GET_UPTIME_INFO (available via debugfs) to synchronize
    EC log lines with host time.
    
    BUG=b:111675966
    BRANCH=none
    TEST=verified with associated kernel patch
    
    Change-Id: Ifed7ba83657c27c55221c3187424387f99239639
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1530297
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

--- timberslide/timberslide.cc
+++ timberslide/timberslide.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <functional>
+#include <sstream>
 #include <string>
 
 #include <fcntl.h>
@@ -12,34 +14,102 @@
 
 #include <base/at_exit.h>
 #include <base/command_line.h>
-#include <brillo/daemons/daemon.h>
 #include <base/files/file_util.h>
 #include <base/files/file_path.h>
 #include <base/logging.h>
 #include <base/message_loop/message_loop.h>
+#include <base/strings/stringprintf.h>
+#include <base/strings/string_number_conversions.h>
 #include <base/strings/string_util.h>
+#include <base/time/time.h>
+#include <brillo/daemons/daemon.h>
 #include <brillo/flag_helper.h>
 
 namespace {
 
 const char kDefaultDeviceLogFile[] = "/sys/kernel/debug/cros_ec/console_log";
-
+const char kDeviceUptimeFile[] = "/sys/kernel/debug/cros_ec/uptime";
 const char kDefaultLogDirectory[] = "/var/log/";
-
 const char kCurrentLogExt[] = ".log";
 const char kPreviousLogExt[] = ".previous";
-
 const int kMaxCurrentLogSize = 10 * 1024 * 1024;
+
+// String proxy class for dealing with lines.
+class LineExtractor {
+ public:
+  // Extract one line from beginning of string stream.
+  friend std::istream& operator>>(std::istream& is, LineExtractor& l) {
+    std::getline(is, l.str_);
+    return is;
+  }
+
+  // Transparently convert to std::string.
+  operator std::string() const { return str_; }
+
+ private:
+  std::string str_;
+};
+
+//
+// Has a member function which adds the host timestamp
+// to the beginning of each line passed to it.
+//
+class StringTransformer {
+ public:
+  explicit StringTransformer(int64_t ec_uptime_ms)
+      : ec_current_uptime_ms_(ec_uptime_ms), timestamp_(base::Time::Now()) {}
+
+  // Matching lines look like: [1234.5678 EC message goes here] .
+  std::string add_host_ts(const std::string& s) {
+    const base::TimeDelta ec_sync(
+        base::TimeDelta::FromMilliseconds(ec_current_uptime_ms_));
+
+    std::string::size_type bracket = s.find('[');
+    if (bracket == std::string::npos)
+      return s;
+
+    std::string::size_type space = s.find(' ', bracket + 1);
+    if (space == std::string::npos)
+      return s;
+
+    double ec_ts;
+    const std::string potential_ts(s.substr(bracket + 1, space - bracket - 1));
+
+    if (!base::StringToDouble(potential_ts, &ec_ts)) {
+      LOG(WARNING) << "Unable to convert " << potential_ts << " to a double";
+      return s;
+    }
+
+    // Calculate delta from EC's uptime.
+    const base::TimeDelta logline_tm(base::TimeDelta::FromSecondsD(ec_ts));
+    const base::TimeDelta logline_delta(ec_sync - logline_tm);
+    const base::Time logline_host_tm = timestamp_ - logline_delta;
+    base::Time::Exploded e;
+
+    // Get datetime parts (exploded) from now and format it.
+    logline_host_tm.LocalExplode(&e);
+    return base::StringPrintf("%04d-%02d-%02d %02d:%02d:%02d %s", e.year,
+                              e.month, e.day_of_month, e.hour, e.minute,
+                              e.second, s.c_str());
+  }
+
+ private:
+  int64_t ec_current_uptime_ms_;
+  base::Time timestamp_;
+};
 
 class TimberSlide : public brillo::Daemon,
                     public base::MessageLoopForIO::Watcher {
  public:
   TimberSlide(const std::string& ec_type,
               base::File device_file,
+              base::File uptime_file,
               const base::FilePath& log_dir)
       : device_file_(std::move(device_file)),
         fd_watcher_(FROM_HERE),
-        total_size_(0) {
+        total_size_(0),
+        uptime_file_(std::move(uptime_file)),
+        uptime_file_valid_(uptime_file_.IsValid()) {
     current_log_ = log_dir.Append(ec_type + kCurrentLogExt);
     previous_log_ = log_dir.Append(ec_type + kPreviousLogExt);
   }
@@ -51,11 +121,16 @@ class TimberSlide : public brillo::Daemon,
     if (ret != EX_OK)
       return ret;
 
+    if (uptime_file_valid_)
+      LOG(INFO) << "EC uptime file is valid";
+    else
+      LOG(WARNING) << "EC uptime file is not valid; ignoring";
+
     RotateLogs(previous_log_, current_log_);
 
     CHECK(base::MessageLoopForIO::current()->WatchFileDescriptor(
-              device_file_.GetPlatformFile(), true,
-              base::MessageLoopForIO::WATCH_READ, &fd_watcher_, this));
+        device_file_.GetPlatformFile(), true,
+        base::MessageLoopForIO::WATCH_READ, &fd_watcher_, this));
     return EX_OK;
   }
 
@@ -63,11 +138,45 @@ class TimberSlide : public brillo::Daemon,
     LOG(FATAL) << "Unexpected call to write event handler";
   }
 
+  //
+  // From kernel's Documentation/filesystems/sysfs.txt: If userspace seeks back
+  // to zero or does a pread(2) with an offset of '0' the show() method will
+  // be called again, rearmed, to fill the buffer.
+  //
+  // Therefore, the 'uptime' file will be kept open and just seeked back to
+  // 0 when new uptime is needed.
+  //
+  bool GetEcUptime(int64_t* ec_uptime_ms) {
+    char uptime_buf[64] = {0};
+
+    if (!uptime_file_valid_ ||
+        uptime_file_.Seek(base::File::FROM_BEGIN, 0) != 0)
+      return false;
+
+    // Read single line from file and parse as a number.
+    int count =
+        uptime_file_.ReadAtCurrentPos(uptime_buf, sizeof(uptime_buf) - 1);
+
+    if (count <= 0)
+      return false;
+
+    uptime_buf[count] = '\0';
+    base::StringToInt64(uptime_buf, ec_uptime_ms);
+
+    // If the 'uptime' file contains zero, that means the kernel patch is
+    // available, but the EC doesn't support EC_CMD_GET_UPTIME_INFO.  In
+    // that case, this returns false so that incorrect times aren't reported
+    // in the EC log file.
+    return (*ec_uptime_ms > 0);
+  }
+
   void OnFileCanReadWithoutBlocking(int fd) override {
     char buffer[4096];
     int ret;
+    int64_t ec_current_uptime_ms = 0;
 
     CHECK_EQ(fd, device_file_.GetPlatformFile());
+    memset(buffer, 0, sizeof(buffer));
 
     ret = TEMP_FAILURE_RETRY(
         device_file_.ReadAtCurrentPosNoBestEffort(buffer, sizeof(buffer)));
@@ -75,12 +184,35 @@ class TimberSlide : public brillo::Daemon,
       return;
 
     if (ret < 0) {
-      PLOG(FATAL) << "Read error";
+      PLOG(ERROR) << "Read error";
+      Quit();
       return;
     }
 
-    if (!base::AppendToFile(current_log_, buffer, ret)) {
-      PLOG(FATAL) << "Could not append log file";
+    std::string str;
+    if (GetEcUptime(&ec_current_uptime_ms)) {
+      StringTransformer xfrm(ec_current_uptime_ms);
+      auto fn_xfrm = std::bind(&StringTransformer::add_host_ts, &xfrm,
+                               std::placeholders::_1);
+      std::istringstream iss(buffer);
+      std::ostringstream oss;
+
+      // Iterate over each line and prepend the corresponding host timestamp
+      std::transform(std::istream_iterator<LineExtractor>(iss),
+                     std::istream_iterator<LineExtractor>(),
+                     std::ostream_iterator<std::string>(oss, "\n"), fn_xfrm);
+
+      str = oss.str();
+    } else {
+      // ret is the number of characters read from the device_file_
+      str = std::string(buffer, ret);
+    }
+
+    ret = str.size();
+
+    if (!base::AppendToFile(current_log_, str.c_str(), ret)) {
+      PLOG(ERROR) << "Could not append to log file";
+      Quit();
       return;
     }
 
@@ -106,6 +238,8 @@ class TimberSlide : public brillo::Daemon,
   base::FilePath previous_log_;
   base::MessageLoopForIO::FileDescriptorWatcher fd_watcher_;
   int total_size_;
+  base::File uptime_file_;
+  bool uptime_file_valid_;
 };
 
 }  // namespace
@@ -115,10 +249,10 @@ int main(int argc, char* argv[]) {
                 "File where the recent EC logs are posted to.");
   DEFINE_string(log_directory, kDefaultLogDirectory,
                 "Directory where the output logs should be.");
-  brillo::FlagHelper::Init(argc, argv,
-      "timberslide concatenates EC logs for use in debugging.");
+  brillo::FlagHelper::Init(
+      argc, argv, "timberslide concatenates EC logs for use in debugging.");
 
-  base::FilePath path(FLAGS_device_log);
+  const base::FilePath path(FLAGS_device_log);
   base::File device_file(path, base::File::FLAG_OPEN | base::File::FLAG_READ);
   if (!device_file.IsValid()) {
     LOG(ERROR) << "Error opening " << FLAGS_device_log << ": "
@@ -126,9 +260,13 @@ int main(int argc, char* argv[]) {
     return EX_UNAVAILABLE;
   }
 
+  const base::FilePath uptime_path(kDeviceUptimeFile);
+  base::File uptime_file(uptime_path,
+                         base::File::FLAG_OPEN | base::File::FLAG_READ);
+
   std::string ec_type = path.DirName().BaseName().value();
-  TimberSlide ts(ec_type, std::move(device_file),
-                 (base::FilePath(FLAGS_log_directory)));
+  TimberSlide ts(ec_type, std::move(device_file), std::move(uptime_file),
+                 base::FilePath(FLAGS_log_directory));
 
   return ts.Run();
 }
