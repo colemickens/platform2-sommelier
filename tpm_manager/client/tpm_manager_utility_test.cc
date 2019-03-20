e6265384234b7a2cb1fbbc0d8f7cf529aa2d9bbd
Leo Lai <cylai@google.com>
2019-03-20 16:22:54 +0800

    tpm_manager: |TpmManagerUtility::TakeOwnership| unittest
    Depending on the received |status| field in the reply protobuf, the
    return value should be a corresponding mapping.
    
    BUG=b:130342401
    TEST=tested with the following comamnd and check ouptut:
    cros_workon_make tpm_manager --test|grep TpmManagerUtilityTest.TakeOwnership
    
    Change-Id: Iafce086e804398e55b8a28fc66fc2ceda92aa957
    Reviewed-on: https://chromium-review.googlesource.com/1568721
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Leo Lai <cylai@google.com>
    Reviewed-by: Louis Collard <louiscollard@chromium.org>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>

--- tpm_manager/client/tpm_manager_utility_test.cc
+++ tpm_manager/client/tpm_manager_utility_test.cc
@@ -4,14 +4,29 @@
 
 #include "tpm_manager/client/tpm_manager_utility.h"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+
+#include <tuple>
 
 #include "tpm_manager/common/mock_tpm_nvram_interface.h"
 #include "tpm_manager/common/mock_tpm_ownership_interface.h"
 
 namespace {
 
+using ::testing::_;
+using ::testing::ByRef;
+using ::testing::NiceMock;
 using ::testing::Test;
+using ::testing::WithArg;
+
+// testing::InvokeArgument<N> does not work with base::Callback, need to use
+// |ACTION_TAMPLATE| along with predefined |args| tuple.
+ACTION_TEMPLATE(InvokeCallbackArgument,
+                HAS_1_TEMPLATE_PARAMS(int, k),
+                AND_1_VALUE_PARAMS(p0)) {
+  std::get<k>(args).Run(p0);
+}
 
 }  // namespace
 
@@ -22,17 +37,32 @@ class TpmManagerUtilityTest : public Test {
   // Uses contructor and SetUp at the same time so we can take advantage of
   // both sytles of initialization.
   TpmManagerUtilityTest()
-      : tpm_manager_utility_(&mock_tpm_owner_, &mock_tpm_nvram_) {}
+      : tpm_manager_utility_(&mock_tpm_owner_, &mock_tpm_nvram_) {
+    // for the following ON_CALL setup, pass the fake replies by reference so we
+    // can change the contained data dynamically in a test item.
+    ON_CALL(mock_tpm_owner_, TakeOwnership(_, _))
+        .WillByDefault(InvokeCallbackArgument<1>(ByRef(take_ownership_reply_)));
+  }
   void SetUp() override { ASSERT_TRUE(tpm_manager_utility_.Initialize()); }
-  tpm_manager::MockTpmOwnershipInterface mock_tpm_owner_;
-  tpm_manager::MockTpmNvramInterface mock_tpm_nvram_;
+
+  NiceMock<tpm_manager::MockTpmOwnershipInterface> mock_tpm_owner_;
+  NiceMock<tpm_manager::MockTpmNvramInterface> mock_tpm_nvram_;
   TpmManagerUtility tpm_manager_utility_;
+
+  // fake replies from TpmManager
+  tpm_manager::TakeOwnershipReply take_ownership_reply_;
 };
 
-// An intial test to validate the constructor and SetUp() logic.
-// TODO(cylai): remove this item along with the first test item added.
-TEST_F(TpmManagerUtilityTest, FixtureSetUp) {
-  EXPECT_TRUE(true);
+TEST_F(TpmManagerUtilityTest, TakeOwnership) {
+  take_ownership_reply_.set_status(tpm_manager::STATUS_SUCCESS);
+  EXPECT_TRUE(tpm_manager_utility_.TakeOwnership());
+}
+
+TEST_F(TpmManagerUtilityTest, TakeOwnershipFail) {
+  take_ownership_reply_.set_status(tpm_manager::STATUS_DEVICE_ERROR);
+  EXPECT_FALSE(tpm_manager_utility_.TakeOwnership());
+  take_ownership_reply_.set_status(tpm_manager::STATUS_NOT_AVAILABLE);
+  EXPECT_FALSE(tpm_manager_utility_.TakeOwnership());
 }
 
 }  // namespace tpm_manager
