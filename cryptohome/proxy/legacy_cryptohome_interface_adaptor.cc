9f749e878178e19170efde9703ba3acf509b8a2f
John L Chen <zuan@chromium.org>
2019-05-07 19:15:29 +0800

    cryptohome: Add MountGuestEx() to cryptohome-proxy
    BUG=b:128271460
    TEST=dbus-send && FEATURES=test USE=cryptohome_new_interface emerge-eve cryptohome
    
    Change-Id: I2f308601c0ab50548b432d2921072e8258d4c911
    Reviewed-on: https://chromium-review.googlesource.com/1593037
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: John L Chen <zuan@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Leo Lai <cylai@google.com>

--- cryptohome/proxy/legacy_cryptohome_interface_adaptor.cc
+++ cryptohome/proxy/legacy_cryptohome_interface_adaptor.cc
@@ -226,10 +226,21 @@ void LegacyCryptohomeInterfaceAdaptor::MountGuestEx(
     std::unique_ptr<
         brillo::dbus_utils::DBusMethodResponse<cryptohome::BaseReply>> response,
     const cryptohome::MountGuestRequest& in_request) {
-  // Not implemented yet
-  response->ReplyWithError(FROM_HERE, brillo::errors::dbus::kDomain,
-                           DBUS_ERROR_NOT_SUPPORTED,
-                           "Method unimplemented yet");
+  auto response_shared =
+      std::make_shared<SharedDBusMethodResponse<cryptohome::BaseReply>>(
+          std::move(response));
+
+  user_data_auth::MountRequest request;
+  request.set_guest_mount(true);
+
+  userdataauth_proxy_->MountAsync(
+      request,
+      base::Bind(&LegacyCryptohomeInterfaceAdaptor::ForwardBaseReplyErrorCode<
+                     user_data_auth::MountReply>,
+                 response_shared),
+      base::Bind(&LegacyCryptohomeInterfaceAdaptor::ForwardError<
+                     cryptohome::BaseReply>,
+                 base::Unretained(this), response_shared));
 }
 
 void LegacyCryptohomeInterfaceAdaptor::RenameCryptohome(
