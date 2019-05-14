13e52216fbd524c60bbcc2cf8cc590aec957c31a
Hong-Min Chu <hmchu@google.com>
2019-05-14 13:31:14 +0800

    runtime_probe: actually report sku_nbumber in VPD
    This CL implements the actual logi to read and report sku_number
    stored in VPD.
    
    BUG=chromium:908006,b:130322365,b:132303928
    
    TEST=cros_workon-nami start system_api runtime_probe
    chromeos-bsp-nami-private
    emerge-nami system_api runtime_probe chromeos-bsp-nami-private
    cros deploy $DEVICE_IP system_api runtime_probe
    chromeos-bsp-nami-private
    
    TEST=PROTO_BYTES=$(echo -e \
    'categories:vpd_cached\n' \
    | protoc --encode runtime_probe.ProbeRequest \
    system_api/dbus/runtime_probe/runtime_probe.proto \
    | hexdump -v -e '/1 "%d,"'); \
    ssh root@$DEVICE_IP sudo -u chronos \
    dbus-send --system --print-reply \
    --type=method_call --dest=org.chromium.RuntimeProbe \
    /org/chromium/RuntimeProbe org.chromium.RuntimeProbe.ProbeCategories \
    array:byte:$PROTO_BYTES
    
    TEST=Verified result aligned with vpd -l on device.
    TEST=Verified result aligned runtime_probe --debug on DUT
    
    Change-Id: Ica77f49408cf8eeeb7985e44b762d086dbaa5e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1609631
    Tested-by: Hong-Min Chu <hmchu@chromium.org>
    Commit-Queue: Justin TerAvest <teravest@chromium.org>
    Commit-Queue: Chun-ta Lin (ping on chat if not responsive) <itspeter@chromium.org>
    Reviewed-by: Hong-Min Chu <hmchu@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>

--- runtime_probe/functions/vpd_cached.cc
+++ runtime_probe/functions/vpd_cached.cc
@@ -12,14 +12,36 @@ namespace runtime_probe {
 using base::DictionaryValue;
 using base::Value;
 
-// A dummy implementation that does not actually read vpd data
 VPDCached::DataType VPDCached::Eval() const {
+  constexpr char kSysfsVPDCached[] = "/sys/firmware/vpd/ro/";
+
+  std::vector<std::string> allowed_require_keys;
+  std::vector<std::string> allowed_optional_keys;
+
+  // sku_number is defined in public partner documentation:
+  // https://www.google.com/chromeos/partner/fe/docs/factory/vpd.html#field-sku_number
+  // sku_number is allowed to be exposed as stated in b/130322365#c28
+  allowed_optional_keys.push_back("sku_number");
+
   DataType result{};
 
-  DictionaryValue dict_with_prefix;
-  dict_with_prefix.SetString("vpd_" + vpd_name_, "");
+  const base::FilePath vpd_ro_path{kSysfsVPDCached};
+  const auto dict_value =
+      MapFilesToDict(vpd_ro_path, allowed_require_keys, allowed_optional_keys);
 
-  result.push_back(std::move(dict_with_prefix));
+  DictionaryValue dict_with_prefix;
+  std::string vpd_value;
+  if (!dict_value.GetString(vpd_name_, &vpd_value)) {
+    LOG(WARNING) << "vpd field " << vpd_name_
+                 << " does not exist or is not allowed to be probed.";
+  } else {
+    // Add vpd_ prefix to every field.
+    dict_with_prefix.SetString("vpd_" + vpd_name_, vpd_value);
+  }
+
+  if (!dict_with_prefix.empty()) {
+    result.push_back(std::move(dict_with_prefix));
+  }
 
   return result;
 }
