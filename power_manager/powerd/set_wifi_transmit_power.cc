72d01eda8fd921c17c3bfbbef5c7da73bc01d8f2
Brian Norris <briannorris@chromium.org>
2019-05-24 13:33:25 -0700

    power: set_wifi_transmit_power: refactor device, driver detection
    Fix a number of TODO issues, to prepare for adding 1 more driver.
    
     * Device detection: rather than hardcode wlan0 and mlan0, just search
       /sys/class/net/* looking for device(s) with a 'DEVTYPE=wlan' uevent
       property.
       - With this, we technically might see more than 1 WiFi device -- if,
         for example, a user plugs in a USB WiFi dongle. Support this by
         enumerating all WiFi devices, and seeing if we have supported
         methods for any of them.
     * Driver detection: rather than assume based on the presence of
       /sys/module/ paths (b/c multiple drivers can be loaded, even if
       they're not all in use), look up the driver for a given device via
       its .../device/driver symlink. This is what we do for device/driver
       validation in Autotest as well.
    
    BUG=chromium:782924
    TEST=new network.WifiSetTxPower Tast (http://crrev.com/c/1628919)
    TEST=manually run 'set_wifi_transmit_power' on ath10k devices ->
         "No valid wireless driver found for wlan0"
    
    Change-Id: Ia5515176b5fc39c6f39d4ab2733527e06eb685a1
    Reviewed-on: https://chromium-review.googlesource.com/1629093
    Commit-Ready: Brian Norris <briannorris@chromium.org>
    Tested-by: Brian Norris <briannorris@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- power_manager/powerd/set_wifi_transmit_power.cc
+++ power_manager/powerd/set_wifi_transmit_power.cc
@@ -4,6 +4,7 @@
 //
 // Helper program for setting WiFi transmission power.
 
+#include <map>
 #include <string>
 #include <vector>
 
@@ -11,10 +12,14 @@
 #include <net/if.h>
 
 #include <base/at_exit.h>
+#include <base/files/file_enumerator.h>
+#include <base/files/file_path.h>
 #include <base/files/file_util.h>
 #include <base/logging.h>
 #include <base/macros.h>
+#include <base/strings/string_split.h>
 #include <base/strings/string_util.h>
+#include <base/strings/stringprintf.h>
 #include <base/sys_info.h>
 #include <brillo/flag_helper.h>
 #include <netlink/attr.h>
@@ -57,9 +62,6 @@
 
 namespace {
 
-// Kernel module path for Marvell wireless driver.
-const char kMwifiexModulePath[] = "/sys/module/mwifiex";
-
 int ErrorHandler(struct sockaddr_nl* nla, struct nlmsgerr* err, void* arg) {
   *static_cast<int*>(arg) = err->error;
   return NL_STOP;
@@ -79,25 +81,56 @@ int ValidHandler(struct nl_msg* msg, void* arg) {
   return NL_OK;
 }
 
-enum class WirelessDriver { NONE, MWIFIEX, IWL };
+enum class WirelessDriver { NONE, MWIFIEX, IWL, ATH10K };
 
 // Returns the type of wireless driver that's present on the system.
-WirelessDriver GetWirelessDriverType() {
-  // TODO(crbug.com/782924): Improve wireless driver detection.
-  return base::PathExists(base::FilePath(kMwifiexModulePath))
-             ? WirelessDriver::MWIFIEX
-             : WirelessDriver::IWL;
+WirelessDriver GetWirelessDriverType(const std::string& device_name) {
+  const std::map<std::string, WirelessDriver> drivers = {
+      {"ath10k_pci", WirelessDriver::ATH10K},
+      {"ath10k_sdio", WirelessDriver::ATH10K},
+      {"ath10k_snoc", WirelessDriver::ATH10K},
+      {"iwlwifi", WirelessDriver::IWL},
+      {"mwifiex_pcie", WirelessDriver::MWIFIEX},
+      {"mwifiex_sdio", WirelessDriver::MWIFIEX},
+  };
+
+  // .../device/driver symlink should point at the driver's module.
+  base::FilePath link_path(base::StringPrintf("/sys/class/net/%s/device/driver",
+                                              device_name.c_str()));
+  base::FilePath driver_path;
+  CHECK(base::ReadSymbolicLink(link_path, &driver_path));
+  base::FilePath driver_name = driver_path.BaseName();
+
+  const auto driver = drivers.find(driver_name.value());
+  if (driver != drivers.end())
+    return driver->second;
+
+  return WirelessDriver::NONE;
 }
 
-// Returns the wireless device index (either wlan0 or mlan0).
-// 0 is returned if the device isn't found.
-uint32_t GetWirelessDeviceIndex() {
-  // TODO(crbug.com/782924): Don't hardcode device names.
-  uint32_t index = if_nametoindex("mlan0");
-  if (!index)
-    index = if_nametoindex("wlan0");
+// Returns a vector of wireless device name(s) found on the system. We
+// generally should only have 1 internal WiFi device, but it's possible to have
+// an external device plugged in (e.g., via USB).
+std::vector<std::string> GetWirelessDeviceNames() {
+  std::vector<std::string> names;
+  base::FileEnumerator iter(base::FilePath("/sys/class/net"), false,
+                            base::FileEnumerator::FileType::FILES |
+                                base::FileEnumerator::FileType::SHOW_SYM_LINKS,
+                            "*");
 
-  return index;
+  for (base::FilePath name = iter.Next(); !name.empty(); name = iter.Next()) {
+    std::string uevent;
+    CHECK(base::ReadFileToString(name.Append("uevent"), &uevent));
+
+    for (const auto& line : base::SplitString(
+             uevent, "\n", base::KEEP_WHITESPACE, base::SPLIT_WANT_ALL)) {
+      if (line == "DEVTYPE=wlan") {
+        names.push_back(name.BaseName().value());
+        break;
+      }
+    }
+  }
+  return names;
 }
 
 // Fill in nl80211 message for the mwifiex driver.
@@ -188,40 +221,42 @@ class PowerSetter {
     nl_cb_put(cb_);
   }
 
-  // Sets power mode according to tablet mode state. Returns true on success and
-  // false on failure.
-  bool SetPowerMode(bool tablet) {
-    CHECK(!genl_connect(nl_sock_)) << "Failed to connect to netlink";
-
-    int nl_family_id = genl_ctrl_resolve(nl_sock_, "nl80211");
-    CHECK_GE(nl_family_id, 0) << "family nl80211 not found";
+  bool SendModeSwitch(const std::string& dev_name, bool tablet) {
+    const uint32_t index = if_nametoindex(dev_name.c_str());
+    if (!index) {
+      LOG(ERROR) << "Failed to find wireless device index for " << dev_name;
+      return false;
+    }
+    WirelessDriver driver = GetWirelessDriverType(dev_name);
+    if (driver == WirelessDriver::NONE || driver == WirelessDriver::ATH10K) {
+      LOG(ERROR) << "No valid wireless driver found for " << dev_name;
+      return false;
+    }
+    LOG(INFO) << "Found wireless device " << dev_name << " (index " << index
+              << ")";
 
     struct nl_msg* msg = nlmsg_alloc();
     CHECK(msg);
 
     // Set header.
-    genlmsg_put(msg, NL_AUTO_PID, NL_AUTO_SEQ, nl_family_id, 0, 0,
+    genlmsg_put(msg, NL_AUTO_PID, NL_AUTO_SEQ, nl_family_id_, 0, 0,
                 NL80211_CMD_VENDOR, 0);
 
     // Set actual message.
-    const uint32_t index = GetWirelessDeviceIndex();
-    if (!index) {
-      LOG(ERROR) << "Failed to find wireless device index";
-      return false;
-    }
     CHECK(!nla_put_u32(msg, NL80211_ATTR_IFINDEX, index))
         << "Failed to put NL80211_ATTR_IFINDEX";
 
-    switch (GetWirelessDriverType()) {
+    switch (driver) {
       case WirelessDriver::MWIFIEX:
         FillMessageMwifiex(msg, tablet);
         break;
       case WirelessDriver::IWL:
         FillMessageIwl(msg, tablet);
         break;
+      case WirelessDriver::ATH10K:
+        // TODO(https://crbug.com/782924): implement for ath10k.
       case WirelessDriver::NONE:
-        LOG(ERROR) << "No valid wireless driver found";
-        return false;
+        NOTREACHED() << "No driver found";
     }
 
     CHECK_GE(nl_send_auto(nl_sock_, msg), 0)
@@ -233,8 +268,30 @@ class PowerSetter {
     return true;
   }
 
+  // Sets power mode according to tablet mode state. Returns true on success and
+  // false on failure.
+  bool SetPowerMode(bool tablet) {
+    CHECK(!genl_connect(nl_sock_)) << "Failed to connect to netlink";
+
+    nl_family_id_ = genl_ctrl_resolve(nl_sock_, "nl80211");
+    CHECK_GE(nl_family_id_, 0) << "family nl80211 not found";
+
+    const std::vector<std::string> device_names = GetWirelessDeviceNames();
+    if (device_names.empty()) {
+      LOG(ERROR) << "No wireless device found";
+      return false;
+    }
+
+    bool ret = true;
+    for (const auto& name : device_names)
+      if (!SendModeSwitch(name, tablet))
+        ret = false;
+    return ret;
+  }
+
  private:
   struct nl_sock* nl_sock_;
+  int nl_family_id_ = 0;
   struct nl_cb* cb_;
   int err_ = 0;  // Used by |cb_| to store errors.
 
