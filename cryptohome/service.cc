79253dcf7a6f12fec4f3db7700aa46a9e2389c8f
Maksim Ivanov <emaxx@chromium.org>
2019-05-31 20:15:02 +0000

    Revert "cryptohome: Connect to D-Bus delegate for challenge-response mount"
    This reverts commit 0f8151562148825de0829de8fcecf2d8bfe922d1.
    
    Reason for revert:
    
    This CL is causing flaky crashes of cryptohomed under VM - see https://crbug.com/945489. Apparently, there's some problem with making the Mount thread have the |message_loop_type| option equal to |base::MessageLoop::TYPE_IO|.
    
    Original change's description:
    > cryptohome: Connect to D-Bus delegate for challenge-response mount
    >
    > Instantiate the KeyChallengeServiceImpl class that is responsible for
    > sending cryptographic challenges over D-Bus to Chrome (that should
    > then forward it to the cryptographic token).
    >
    > BUG=chromium:842791
    > TEST=manual
    >
    > Change-Id: Id41c4e399a15b06b1c7da815a32e0753ea5a438f
    > Reviewed-on: https://chromium-review.googlesource.com/1520648
    > Commit-Ready: Maksim Ivanov <emaxx@chromium.org>
    > Tested-by: Maksim Ivanov <emaxx@chromium.org>
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    
    Bug: chromium:842791
    Change-Id: Ib8c8b05a95b4c059ab189fae83b88700c0280d11
    Reviewed-on: https://chromium-review.googlesource.com/1639297
    Commit-Ready: Maksim Ivanov <emaxx@chromium.org>
    Tested-by: Maksim Ivanov <emaxx@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>

--- cryptohome/service.cc
+++ cryptohome/service.cc
@@ -40,8 +40,6 @@
 #include <chaps/isolate.h>
 #include <chaps/token_manager_client.h>
 #include <chromeos/constants/cryptohome.h>
-#include <dbus/bus.h>
-#include <dbus/dbus.h>
 
 #include "cryptohome/bootlockbox/boot_attributes.h"
 #include "cryptohome/bootlockbox/boot_lockbox.h"
@@ -57,7 +55,6 @@
 #include "cryptohome/install_attributes.h"
 #include "cryptohome/interface.h"
 #include "cryptohome/key_challenge_service.h"
-#include "cryptohome/key_challenge_service_impl.h"
 #include "cryptohome/mount.h"
 #include "cryptohome/obfuscated_username.h"
 #include "cryptohome/platform.h"
@@ -694,9 +691,7 @@ bool Service::Initialize() {
         G_TYPE_UINT64);
   }
 
-  base::Thread::Options options;
-  options.message_loop_type = base::MessageLoop::TYPE_IO;
-  mount_thread_.StartWithOptions(options);
+  mount_thread_.Start();
 
   // Add task observer, message_loop is only availible after the thread start.
   // We can only add observer inside the thread.
@@ -2081,23 +2076,8 @@ void Service::DoChallengeResponseMountEx(
       BuildObfuscatedUsername(account_id, system_salt_);
   const KeyData key_data = authorization->key().data();
 
-  scoped_refptr<dbus::Bus> system_dbus_bus = system_dbus_connection_.Connect();
-  if (!system_dbus_bus) {
-    LOG(ERROR) << "Cannot do challenge-response mount without system D-Bus bus";
-    reply.set_error(CRYPTOHOME_ERROR_MOUNT_FATAL);
-    SendReply(context, reply);
-    return;
-  }
-  if (!authorization->has_key_delegate() ||
-      !authorization->key_delegate().has_dbus_service_name()) {
-    LOG(ERROR) << "Cannot do challenge-response mount without key delegate "
-                  "information";
-    reply.set_error(CRYPTOHOME_ERROR_MOUNT_FATAL);
-    SendReply(context, reply);
-    return;
-  }
-  auto key_challenge_service = std::make_unique<KeyChallengeServiceImpl>(
-      system_dbus_bus, authorization->key_delegate().dbus_service_name());
+  // TODO(crbug.com/842791): Create and pass KeyChallengeService.
+  std::unique_ptr<KeyChallengeService> key_challenge_service;
 
   std::unique_ptr<VaultKeyset> vault_keyset(homedirs_->GetVaultKeyset(
       obfuscated_username, authorization->key().data().label()));
