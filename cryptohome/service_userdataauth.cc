03e3a1e98615223f5d824e8d3774b6901ed51adc
John L Chen <zuan@chromium.org>
2019-06-03 21:25:37 +0800

    cryptohome: Add dependency for ThreadSafeDBusMethodResponse
    BUG=none
    TEST=Manually test in the coming CLs && FEATURES=test USE=cryptohome_userdataauth_interface emerge-eve cryptohome
    
    Cq-Depend: chromium:1621229
    Change-Id: I6213f86f72c3dd7e65f3d47d199c6abae96aff5b
    Reviewed-on: https://chromium-review.googlesource.com/1593033
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: John L Chen <zuan@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Meng-Huan Yu <menghuan@chromium.org>

--- cryptohome/service_userdataauth.cc
+++ cryptohome/service_userdataauth.cc
@@ -2,9 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <chromeos/libhwsec/task_dispatching_framework.h>
 #include "cryptohome/service_userdataauth.h"
 
 namespace cryptohome {
+
+using ::hwsec::ThreadSafeDBusMethodResponse;
 
 void UserDataAuthAdaptor::IsMounted(
     std::unique_ptr<brillo::dbus_utils::DBusMethodResponse<
