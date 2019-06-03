e928950270b531682d896a5e7eda538bdd5d7f61
Chirantan Ekbote <chirantan@chromium.org>
2019-06-03 19:09:45 +0900

    vm_tools: p9: Use the cros_fuzz crate for fuzzing
    Switch to using the cros_fuzz crate for fuzzing code.  This puts all the
    fuzzing boilerplate behind a macro so we only need to write the actual
    fuzzing code here.
    
    BUG=none
    TEST=`USE="asan fuzzer" emerge-amd64-generic p9` and then run the
         fuzzer with cros_fuzz
    
    Cq-Depend: chromium:1640862
    Change-Id: Iadc5aa8798fe4ea0e1963f63f648baa5cdfffd17
    Reviewed-on: https://chromium-review.googlesource.com/1641104
    Tested-by: Chirantan Ekbote <chirantan@chromium.org>
    Commit-Ready: Chirantan Ekbote <chirantan@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- vm_tools/p9/fuzz/tframe_decode.rs
+++ vm_tools/p9/fuzz/tframe_decode.rs
@@ -4,27 +4,9 @@
 
 #![no_main]
 
-extern crate p9;
-
-use std::panic;
-use std::process;
-use std::slice;
-
+use cros_fuzz::fuzz_target;
 use p9::fuzzing::tframe_decode;
 
-#[export_name = "LLVMFuzzerTestOneInput"]
-pub fn test_one_input(data: *const u8, size: usize) -> i32 {
-    // We cannot unwind past ffi boundaries.
-    panic::catch_unwind(|| {
-        // Safe because the libfuzzer runtime will guarantee that `data` is at least
-        // `size` bytes long and that it will be valid for the lifetime of this
-        // function.
-        let bytes = unsafe { slice::from_raw_parts(data, size) };
-
-        tframe_decode(bytes);
-    })
-    .err()
-    .map(|_| process::abort());
-
-    0
-}
+fuzz_target!(|bytes: &[u8]| {
+    tframe_decode(bytes);
+});
