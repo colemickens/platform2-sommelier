92821a9c523f3f3681361464cb4b39ce8e795970
Kalvin Lee <kdlee@chromium.org>
2019-06-04 09:30:35 -0700

    debugd: sandbox URI parsing code in cups_tool
    This change moves the URI parsing code out of the main debugd process
    into a sandboxed helper. This change also adjusts the unit test and
    build recipe accordingly.
    
    BUG=chromium:969652
    TEST="{FEATURES=test,} emerge-${BOARD} chromeos-base/debugd"
    
    Cq-Depend: chromium:Ib47da1645f109439614b91eefd841705d008b407
    Change-Id: Ic17104b1a1e4d49853750c13817155371e266aec
    Reviewed-on: https://chromium-review.googlesource.com/1643970
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Kalvin Lee <kdlee@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: David Valleau <valleau@chromium.org>
    Reviewed-by: Xiaoyong Zhou <xzhou@chromium.org>

--- debugd/src/cups_tool.cc
+++ debugd/src/cups_tool.cc
@@ -8,19 +8,16 @@
 #include <signal.h>
 #include <unistd.h>
 
-#include <cstddef>
 #include <string>
 #include <vector>
 
 #include <base/files/file_util.h>
 #include <base/logging.h>
-#include <base/strings/string_number_conversions.h>
-#include <base/strings/string_piece.h>
-#include <base/strings/string_util.h>
 #include <chromeos/dbus/debugd/dbus-constants.h>
 
 #include "debugd/src/constants.h"
 #include "debugd/src/process_with_output.h"
+#include "debugd/src/sandboxed_process.h"
 
 namespace debugd {
 
@@ -36,6 +33,10 @@ constexpr char kTestPPDSeccompPolicy[] =
 constexpr char kLpadminUser[] = "lpadmin";
 constexpr char kLpadminGroup[] = "lpadmin";
 constexpr char kLpGroup[] = "lp";
+
+constexpr char kUriHelperBasename[] = "cups_uri_helper";
+constexpr char kUriHelperSeccompPolicy[] =
+    "/usr/share/policy/cups-uri-helper.policy";
 
 // Returns the exit code for the executed process.
 // By default disallow root mount namespace. Passing true as optional argument
@@ -130,148 +131,6 @@ bool IppEverywhereURI(const std::string& uri) {
   return false;
 }
 
-// Evaluates true when |c| is in the reserved set specified in RFC 3986.
-bool CharIsReserved(char c) {
-  switch (c) {
-    case ':':
-    case '/':
-    case '?':
-    case '#':
-    case '[':
-    case ']':
-    case '@':
-    case '!':
-    case '$':
-    case '&':
-    case '\'':
-    case '(':
-    case ')':
-    case '*':
-    case '+':
-    case ',':
-    case ';':
-    case '=':
-      return true;
-  }
-  return false;
-}
-
-// Evaluates true when |c| is in the unreserved set specified in RFC 3986.
-bool CharIsUnreserved(char c) {
-  switch (c) {
-    case '-':
-    case '.':
-    case '_':
-    case '~':
-      return true;
-  }
-  return base::IsAsciiAlpha(c) || base::IsAsciiDigit(c);
-}
-
-// Evaluates true when |c|
-// *  is in the reserved set specified by RFC 3986,
-// *  is in the unreserved set specified by RFC 3986, or
-// *  is a literal '%' (which is in neither preceding set).
-bool CharIsAllowed(char c) {
-  return CharIsReserved(c) || CharIsUnreserved(c) || c == '%';
-}
-
-// Determines whether |uri| comprises mostly alphanumeric ASCII.
-bool UriIsGoodAscii(const std::string& uri) {
-  int expected_hex_digits = 0;
-  for (const char c : uri) {
-    if (expected_hex_digits > 0) {
-      // We are currently processing a percent-encoded segment "%XY."
-      if (!base::IsHexDigit(c)) {
-        return false;
-      }
-      expected_hex_digits--;
-    } else if (c == '%') {
-      // We are not currently processing a percent-encoded segment and
-      // we see the start of a percent-encoded segment.
-      expected_hex_digits = 2;
-    } else if (!CharIsAllowed(c)) {
-      return false;
-    }
-  }
-  return expected_hex_digits == 0;
-}
-
-// Gets the starting index of the authority from |uri_view| - that is,
-// returns the index following "://". If none is found, returns npos.
-// Caller must ensure |uri_view| is already properly percent-encoded.
-size_t UriAuthorityStartIndex(base::StringPiece uri_view) {
-  size_t scheme_ender = uri_view.find("://");
-  return scheme_ender == base::StringPiece::npos ? scheme_ender
-                                                 : scheme_ender + 3;
-}
-
-// Evaluates true when |scheme_view| (including the trailing colon and 2
-// slashes) equals a known printing URI scheme. Caller must ensure
-// |scheme_view| is already properly percent-encoded.
-bool SchemeIsForPrinting(base::StringPiece scheme_view) {
-  // Enumerate known printing URIs. Values are lifted from Chrome browser's
-  // Printer::GetProtocol().
-  const std::vector<base::StringPiece> known_schemes = {
-      "usb://",   "ipp://",    "ipps://", "http://",
-      "https://", "socket://", "lpd://",  "ippusb://"};
-  for (const base::StringPiece scheme : known_schemes) {
-    if (base::EqualsCaseInsensitiveASCII(scheme_view, scheme)) {
-      return true;
-    }
-  }
-  return false;
-}
-
-// Evaluates true when the authority portion of a printing URI appears
-// reasonable. Caller must ensure |authority_view| is already properly
-// percent-encoded and does not contain the slash that begins the path.
-bool AuthoritySeemsReasonable(base::StringPiece authority_view) {
-  if (authority_view.empty()) {
-    return false;
-  }
-
-  // My reading of RFC 3986 says to me that any non-reserved character
-  // in the host can be percent-encoded. I'm going to punt on decoding
-  // the variety of possible hosts and focus on the port number.
-  // TODO(kdlee): figure out why nobody else in platform2 uses libcurl.
-  size_t last_colon = authority_view.rfind(':');
-  if (last_colon == base::StringPiece::npos) {
-    // We don't see a port number - punt.
-    return true;
-  } else if (last_colon == authority_view.length() - 1) {
-    // We see a colon but no port number - this is unreasonable.
-    return false;
-  }
-
-  // There are several possibilities for other placements of the colon.
-  // 1. It could be inside the user info (before host and port).
-  // 2. It could be inside an IP literal (e.g. if the host is an IPv6
-  //    address and no port is attached to this authority).
-  // 3. It could be near the end of the authority with actual numeric
-  //    values following it.
-  base::StringPiece port_view = authority_view.substr(last_colon + 1);
-  if (port_view.find('@') != base::StringPiece::npos) {
-    // AFAIK, this colon is inside user info - punt.
-    return true;
-  } else if (authority_view.starts_with("[") && authority_view.ends_with("]")) {
-    // AFAIK, this colon is part of an IPv6 literal without a port number -
-    // punt.
-    return true;
-  }
-  // AFAIK, this must be intended to be a decimal port number.
-  for (const char c : port_view) {
-    if (!base::IsAsciiDigit(c)) {
-      return false;
-    }
-  }
-  size_t decimal_port;
-  if (!base::StringToSizeT(port_view, &decimal_port)) {
-    return false;
-  }
-  return (0ll <= decimal_port) && (decimal_port < 65536ll);
-}
-
 }  // namespace
 
 // Invokes lpadmin with arguments to configure a new printer using '-m
@@ -341,26 +200,21 @@ bool CupsTool::RemovePrinter(const std::string& name) {
 // This function observes a subset of RFC 3986 but is _not_ meant to serve
 // as a general-purpose URI validator (prefer Chromium's GURL).
 bool CupsTool::UriSeemsReasonable(const std::string& uri) {
-  if (!UriIsGoodAscii(uri)) {
+  ProcessWithOutput::ArgList args = {uri};
+  std::string helper_path;
+
+  if (!SandboxedProcess::GetHelperPath(kUriHelperBasename, &helper_path)) {
+    DCHECK(false) << "GetHelperPath() failed to return the CUPS URI helper!";
     return false;
   }
 
-  base::StringPiece uri_view = uri;
-  size_t authority_starter = UriAuthorityStartIndex(uri_view);
-  if (authority_starter == base::StringPiece::npos ||
-      authority_starter == uri_view.length()) {
-    return false;
+  int cups_uri_helper_exit_code =
+      RunAsUser(SandboxedProcess::kDefaultUser, SandboxedProcess::kDefaultGroup,
+                helper_path, kUriHelperSeccompPolicy, args);
+  if (cups_uri_helper_exit_code == 0) {
+    return true;
   }
-
-  base::StringPiece scheme = uri_view.substr(0, authority_starter);
-  base::StringPiece after_scheme = uri_view.substr(authority_starter);
-
-  if (!SchemeIsForPrinting(scheme)) {
-    return false;
-  }
-
-  base::StringPiece authority = after_scheme.substr(0, after_scheme.find('/'));
-  return AuthoritySeemsReasonable(authority);
+  return false;
 }
 
 }  // namespace debugd
