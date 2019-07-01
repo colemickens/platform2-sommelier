55b92fd947eb43c1aee669c445a2274c96e6c79d
Allen Webb <allenwebb@google.com>
2019-07-01 16:14:41 -0700

    libbrillo: Add basic implementation for SafeFD
    BUG=chromium:977388
    TEST=FEATURES=test emerge-${BOARD} libbrillo
    
    Change-Id: Ica836cfc2c39a672a5b709eb82e348a0a1c83294
    Reviewed-on: https://chromium-review.googlesource.com/1670287
    Tested-by: Allen Webb <allenwebb@google.com>
    Commit-Ready: Allen Webb <allenwebb@google.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- libbrillo/brillo/safe_fd.cc
+++ libbrillo/brillo/safe_fd.cc
@@ -4,6 +4,385 @@
 
 #include "brillo/safe_fd.h"
 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <base/files/file_util.h>
+#include <base/logging.h>
+#include <base/posix/eintr_wrapper.h>
+#include <brillo/syslog_logging.h>
+
 namespace brillo {
+
+namespace {
+SafeFD::SafeFDResult MakeErrorResult(SafeFD::Error error) {
+  return std::make_pair(SafeFD(), error);
+}
+
+SafeFD::SafeFDResult MakeSuccessResult(SafeFD&& fd) {
+  return std::make_pair(std::move(fd), SafeFD::Error::kNoError);
+}
+
+SafeFD::SafeFDResult OpenPathComponentInternal(int parent_fd,
+                                               const std::string& file,
+                                               int flags,
+                                               mode_t mode) {
+  if (file != "/" && file.find("/") != std::string::npos) {
+    return MakeErrorResult(SafeFD::Error::kBadArgument);
+  }
+  SafeFD fd;
+
+  // O_NONBLOCK is used to avoid hanging on edge cases (e.g. a serial port with
+  // flow control, or a FIFO without a writer).
+  if (parent_fd >= 0 || parent_fd == AT_FDCWD) {
+    fd.UnsafeReset(HANDLE_EINTR(openat(parent_fd, file.c_str(),
+                                       flags | O_NONBLOCK | O_NOFOLLOW, mode)));
+  } else if (file == "/") {
+    fd.UnsafeReset(HANDLE_EINTR(open(
+        file.c_str(), flags | O_DIRECTORY | O_NONBLOCK | O_NOFOLLOW, mode)));
+  }
+
+  if (!fd.is_valid()) {
+    // open(2) fails with ELOOP when the last component of the |path| is a
+    // symlink. It fails with ENXIO when |path| is a FIFO and |flags| is for
+    // writing because of the O_NONBLOCK flag added above.
+    switch (errno) {
+      case ELOOP:
+        // PLOG prints something along the lines of the symlink depth being too
+        // great which is is misleading so LOG is used instead.
+        LOG(ERROR) << "Symlink detected! failed to open \"" << file
+                   << "\" safely.";
+        return MakeErrorResult(SafeFD::Error::kSymlinkDetected);
+      case EISDIR:
+        PLOG(ERROR) << "Directory detected! failed to open \"" << file
+                    << "\" safely";
+        return MakeErrorResult(SafeFD::Error::kWrongType);
+      case ENOTDIR:
+        PLOG(ERROR) << "Not a directory! failed to open \"" << file
+                    << "\" safely";
+        return MakeErrorResult(SafeFD::Error::kWrongType);
+      case ENXIO:
+        PLOG(ERROR) << "FIFO detected! failed to open \"" << file
+                    << "\" safely";
+        return MakeErrorResult(SafeFD::Error::kWrongType);
+      default:
+        PLOG(ERROR) << "Failed to open \"" << file << '"';
+        return MakeErrorResult(SafeFD::Error::kIOError);
+    }
+  }
+
+  // Remove the O_NONBLOCK flag unless the original |flags| have it.
+  if ((flags & O_NONBLOCK) == 0) {
+    flags = fcntl(fd.get(), F_GETFL);
+    if (flags == -1) {
+      PLOG(ERROR) << "Failed to get fd flags for " << file;
+      return MakeErrorResult(SafeFD::Error::kIOError);
+    }
+    if (fcntl(fd.get(), F_SETFL, flags & ~O_NONBLOCK)) {
+      PLOG(ERROR) << "Failed to set fd flags for " << file;
+      return MakeErrorResult(SafeFD::Error::kIOError);
+    }
+  }
+
+  return MakeSuccessResult(std::move(fd));
+}
+
+SafeFD::SafeFDResult OpenSafelyInternal(int parent_fd,
+                                        const base::FilePath& path,
+                                        int flags,
+                                        mode_t mode) {
+  std::vector<std::string> components;
+  path.GetComponents(&components);
+
+  auto itr = components.begin();
+  if (itr == components.end()) {
+    LOG(ERROR) << "A path is required.";
+    return MakeErrorResult(SafeFD::Error::kBadArgument);
+  }
+
+  SafeFD::SafeFDResult child_fd;
+  int parent_flags = flags | O_NONBLOCK | O_RDONLY | O_DIRECTORY | O_PATH;
+  for (; itr + 1 != components.end(); ++itr) {
+    child_fd = OpenPathComponentInternal(parent_fd, *itr, parent_flags, 0);
+    // Operation failed, so directly return the error result.
+    if (!child_fd.first.is_valid()) {
+      return child_fd;
+    }
+    parent_fd = child_fd.first.get();
+  }
+
+  return OpenPathComponentInternal(parent_fd, *itr, flags, mode);
+}
+
+SafeFD::Error CheckAttributes(int fd,
+                              mode_t permissions,
+                              uid_t uid,
+                              gid_t gid) {
+  struct stat fd_attributes;
+  if (fstat(fd, &fd_attributes) != 0) {
+    PLOG(ERROR) << "fstat failed";
+    return SafeFD::Error::kIOError;
+  }
+
+  if (fd_attributes.st_uid != uid) {
+    LOG(ERROR) << "Owner uid is " << fd_attributes.st_uid << " instead of "
+               << uid;
+    return SafeFD::Error::kWrongUID;
+  }
+
+  if (fd_attributes.st_gid != gid) {
+    LOG(ERROR) << "Owner gid is " << fd_attributes.st_gid << " instead of "
+               << gid;
+    return SafeFD::Error::kWrongGID;
+  }
+
+  if ((0777 & (fd_attributes.st_mode ^ permissions)) != 0) {
+    mode_t mask = umask(0);
+    umask(mask);
+    LOG(ERROR) << "Permissions are " << std::oct
+               << (0777 & fd_attributes.st_mode) << " instead of "
+               << (0777 & permissions) << ". Umask is " << std::oct << mask
+               << std::dec;
+    return SafeFD::Error::kWrongPermissions;
+  }
+
+  return SafeFD::Error::kNoError;
+}
+
+SafeFD::Error GetFileSize(int fd, size_t* file_size) {
+  struct stat fd_attributes;
+  if (fstat(fd, &fd_attributes) != 0) {
+    return SafeFD::Error::kIOError;
+  }
+
+  *file_size = fd_attributes.st_size;
+  return SafeFD::Error::kNoError;
+}
+
+}  // namespace
+
+const char* SafeFD::RootPath = "/";
+
+SafeFD::SafeFDResult SafeFD::Root() {
+  SafeFD::SafeFDResult root =
+      OpenPathComponentInternal(-1, "/", O_DIRECTORY, 0);
+  if (strcmp(SafeFD::RootPath, "/") == 0) {
+    return root;
+  }
+
+  if (!root.first.is_valid()) {
+    LOG(ERROR) << "Failed to open root directory!";
+    return root;
+  }
+  return root.first.OpenExistingDir(base::FilePath(SafeFD::RootPath));
+}
+
+void SafeFD::SetRootPathForTesting(const char* new_root_path) {
+  SafeFD::RootPath = new_root_path;
+}
+
+int SafeFD::get() const {
+  return fd_.get();
+}
+
+bool SafeFD::is_valid() const {
+  return fd_.is_valid();
+}
+
+void SafeFD::reset() {
+  return fd_.reset();
+}
+
+void SafeFD::UnsafeReset(int fd) {
+  return fd_.reset(fd);
+}
+
+SafeFD::Error SafeFD::Write(const char* data, size_t size) {
+  if (!fd_.is_valid()) {
+    LOG(WARNING) << "Called Write() on invalid SafeFD()!";
+  }
+  errno = 0;
+  if (!base::WriteFileDescriptor(fd_.get(), data, size)) {
+    PLOG(ERROR) << "Failed to write to file";
+    return SafeFD::Error::kIOError;
+  }
+
+  if (HANDLE_EINTR(ftruncate(fd_.get(), size)) != 0) {
+    PLOG(ERROR) << "Failed to truncate file";
+    return SafeFD::Error::kIOError;
+  }
+  return SafeFD::Error::kNoError;
+}
+
+std::pair<std::vector<char>, SafeFD::Error> SafeFD::ReadContents(
+    size_t max_size) {
+  size_t file_size = 0;
+  SafeFD::Error err = GetFileSize(fd_.get(), &file_size);
+  if (err != SafeFD::Error::kNoError) {
+    return std::make_pair(std::vector<char>{}, err);
+  }
+
+  if (file_size > max_size) {
+    return std::make_pair(std::vector<char>{}, SafeFD::Error::kExceededMaximum);
+  }
+
+  std::vector<char> buffer;
+  buffer.resize(file_size);
+
+  err = Read(buffer.data(), buffer.size());
+  if (err != SafeFD::Error::kNoError) {
+    return std::make_pair(std::vector<char>{}, err);
+  }
+  return std::make_pair(std::move(buffer), err);
+}
+
+SafeFD::Error SafeFD::Read(char* data, size_t size) {
+  if (!base::ReadFromFD(fd_.get(), data, size)) {
+    PLOG(ERROR) << "Failed to read file";
+    return SafeFD::Error::kIOError;
+  }
+  return SafeFD::Error::kNoError;
+}
+
+SafeFD::SafeFDResult SafeFD::OpenExistingFile(const base::FilePath& path,
+                                              int flags) {
+  if (!fd_.is_valid()) {
+    return MakeErrorResult(SafeFD::Error::kNotInitialized);
+  }
+
+  return OpenSafelyInternal(get(), path, flags, 0 /*mode*/);
+}
+
+SafeFD::SafeFDResult SafeFD::OpenExistingDir(const base::FilePath& path,
+                                             int flags) {
+  if (!fd_.is_valid()) {
+    return MakeErrorResult(SafeFD::Error::kNotInitialized);
+  }
+
+  return OpenSafelyInternal(get(), path, O_DIRECTORY | flags /*flags*/,
+                            0 /*mode*/);
+}
+
+SafeFD::SafeFDResult SafeFD::MakeFile(const base::FilePath& path,
+                                      mode_t permissions,
+                                      uid_t uid,
+                                      gid_t gid,
+                                      int flags) {
+  if (!fd_.is_valid()) {
+    return MakeErrorResult(SafeFD::Error::kNotInitialized);
+  }
+
+  // Open (and create if necessary) the parent directory.
+  base::FilePath dir_name = path.DirName();
+  SafeFD::SafeFDResult parent_dir;
+  int parent_dir_fd = get();
+  if (!dir_name.empty()) {
+    // Apply execute permission where read permission are present for parent
+    // directories
+    int dir_permissions = permissions | ((permissions & 0444) >> 2);
+    parent_dir =
+        MakeDir(dir_name, dir_permissions, uid, gid, O_RDONLY | O_CLOEXEC);
+    if (!parent_dir.first.is_valid()) {
+      return parent_dir;
+    }
+    parent_dir_fd = parent_dir.first.get();
+  }
+
+  // If file already exists, validate permissions.
+  SafeFDResult file = OpenPathComponentInternal(
+      parent_dir_fd, path.BaseName().value(), flags, permissions /*mode*/);
+  if (file.first.is_valid()) {
+    SafeFD::Error err =
+        CheckAttributes(file.first.get(), permissions, uid, gid);
+    if (err != SafeFD::Error::kNoError) {
+      return MakeErrorResult(err);
+    }
+    return file;
+  } else if (errno != ENOENT) {
+    return file;
+  }
+
+  // The file does exist, create it and set the ownership.
+  file =
+      OpenPathComponentInternal(parent_dir_fd, path.BaseName().value(),
+                                O_CREAT | O_EXCL | flags, permissions /*mode*/);
+  if (!file.first.is_valid()) {
+    return file;
+  }
+  if (HANDLE_EINTR(fchown(file.first.get(), uid, gid)) != 0) {
+    PLOG(ERROR) << "Failed to set ownership in MakeFile() for \""
+                << path.value() << '"';
+    return MakeErrorResult(SafeFD::Error::kIOError);
+  }
+  return file;
+}
+
+SafeFD::SafeFDResult SafeFD::MakeDir(const base::FilePath& path,
+                                     mode_t permissions,
+                                     uid_t uid,
+                                     gid_t gid,
+                                     int flags) {
+  if (!fd_.is_valid()) {
+    return MakeErrorResult(SafeFD::Error::kNotInitialized);
+  }
+
+  std::vector<std::string> components;
+  path.GetComponents(&components);
+  if (components.empty()) {
+    LOG(ERROR) << "Called MakeDir() with an empty path";
+    return MakeErrorResult(SafeFD::Error::kBadArgument);
+  }
+
+  // Walk the path creating directories as necessary.
+  auto itr = components.begin();
+  SafeFD parent_fd;
+  int parent_flags = O_NONBLOCK | O_RDONLY | O_DIRECTORY | O_PATH;
+  bool made_dir = false;
+  while (itr + 1 != components.end()) {
+    SafeFDResult child = OpenPathComponentInternal(parent_fd.get(), *itr,
+                                                   parent_flags, 0 /*mode*/);
+    if (!child.first.is_valid()) {
+      return child;
+    }
+    parent_fd = std::move(child.first);
+
+    ++itr;
+
+    if (mkdirat(parent_fd.get(), itr->c_str(), permissions) != 0) {
+      if (errno != EEXIST) {
+        PLOG(ERROR) << "Failed to mkdirat() " << *itr << ": full_path=\""
+                    << path.value() << '"';
+        return MakeErrorResult(SafeFD::Error::kIOError);
+      }
+    } else {
+      made_dir = true;
+    }
+  }
+
+  // Open the resulting directory.
+  SafeFDResult dir = OpenPathComponentInternal(parent_fd.get(), *itr,
+                                               flags | O_DIRECTORY, 0 /*mode*/);
+  if (!dir.first.is_valid()) {
+    return dir;
+  }
+
+  if (made_dir) {
+    // If the directory was created, set the ownership.
+    if (HANDLE_EINTR(fchown(dir.first.get(), uid, gid)) != 0) {
+      PLOG(ERROR) << "Failed to set ownership in MakeDir() for \""
+                  << path.value() << '"';
+      return MakeErrorResult(SafeFD::Error::kIOError);
+    }
+  } else {
+    // If the directory already existed validate the permissions
+    SafeFD::Error err = CheckAttributes(dir.first.get(), permissions, uid, gid);
+    if (err != SafeFD::Error::kNoError) {
+      return MakeErrorResult(err);
+    }
+  }
+
+  return dir;
+}
 
 }  // namespace brillo
--- libbrillo/brillo/safe_fd_test.cc
+++ libbrillo/brillo/safe_fd_test.cc
@@ -4,6 +4,439 @@
 
 #include "brillo/safe_fd.h"
 
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include <base/files/file_util.h>
+#include <base/files/scoped_temp_dir.h>
+#include <base/rand_util.h>
+#include <base/strings/string_number_conversions.h>
+#include <brillo/syslog_logging.h>
+#include <gtest/gtest.h>
+
 namespace brillo {
+namespace {
+
+#define TO_STRING_HELPER(x)      \
+  case brillo::SafeFD::Error::x: \
+    return #x;
+
+std::string to_string(brillo::SafeFD::Error err) {
+  switch (err) {
+    TO_STRING_HELPER(kNoError)
+    TO_STRING_HELPER(kBadArgument)
+    TO_STRING_HELPER(kNotInitialized)
+    TO_STRING_HELPER(kIOError)
+    TO_STRING_HELPER(kSymlinkDetected)
+    TO_STRING_HELPER(kWrongType)
+    TO_STRING_HELPER(kWrongUID)
+    TO_STRING_HELPER(kWrongGID)
+    TO_STRING_HELPER(kWrongPermissions)
+    TO_STRING_HELPER(kExceededMaximum)
+    default:
+      return std::string("unknown (") + std::to_string(static_cast<int>(err)) +
+             ")";
+  }
+}
+
+#define EXPECT_STR_EQ(a, b) EXPECT_EQ(to_string(a), to_string(b))
+
+std::string GetRandomSuffix() {
+  const int kBufferSize = 6;
+  unsigned char buffer[kBufferSize];
+  base::RandBytes(buffer, arraysize(buffer));
+  return base::HexEncode(buffer, arraysize(buffer));
+}
+
+}  // namespace
+
+class SafeFDTest : public testing::Test {
+ public:
+  static constexpr char kFileName[] = "test.temp";
+  static constexpr char kSymbolicFileName[] = "sym_test.temp";
+  static constexpr char kSymbolicDirName[] = "sym_dir";
+
+  static void SetUpTestCase() {
+    brillo::InitLog(brillo::kLogToStderr);
+    umask(0);
+  }
+
+  SafeFDTest() {
+    CHECK(temp_dir_.CreateUniqueTempDir()) << strerror(errno);
+    file_path_ = temp_dir_.GetPath().Append(kFileName);
+    symlink_file_path_ = temp_dir_.GetPath().Append(kSymbolicFileName);
+    CHECK(base::CreateSymbolicLink(file_path_, symlink_file_path_))
+        << strerror(errno);
+    symlink_dir_path_ = temp_dir_.GetPath().Append(kSymbolicDirName);
+    CHECK(base::CreateSymbolicLink(temp_dir_.GetPath(), symlink_dir_path_))
+        << strerror(errno);
+
+    std::string path = temp_dir_.GetPath().value();
+    temp_dir_path_.reserve(path.size() + 1);
+    temp_dir_path_.assign(temp_dir_.GetPath().value().begin(),
+                          temp_dir_.GetPath().value().end());
+    temp_dir_path_.push_back('\0');
+
+    CHECK_EQ(chmod(temp_dir_path_.data(), SafeFD::kDefaultDirPermissions), 0);
+    SafeFD::SetRootPathForTesting(temp_dir_path_.data());
+    root_ = SafeFD::Root().first;
+    CHECK(root_.is_valid());
+  }
+
+ protected:
+  std::vector<char> temp_dir_path_;
+  base::FilePath file_path_;
+  base::FilePath symlink_file_path_;
+  base::FilePath symlink_dir_path_;
+  base::ScopedTempDir temp_dir_;
+  SafeFD root_;
+
+  // Writes |contents| to |file_path_|. Pulled into a separate function just
+  // to improve readability of tests.
+  void WriteFile(const std::string& contents) {
+    EXPECT_EQ(contents.length(),
+              base::WriteFile(file_path_, contents.c_str(), contents.length()));
+    EXPECT_EQ(
+        chmod(file_path_.value().c_str(), SafeFD::kDefaultFilePermissions), 0);
+  }
+
+  // Verifies that the file at |file_path_| exists and contains |contents|.
+  void ExpectFileContains(const std::string& contents) {
+    EXPECT_TRUE(base::PathExists(file_path_));
+    std::string new_contents;
+    EXPECT_TRUE(base::ReadFileToString(file_path_, &new_contents));
+    EXPECT_EQ(contents, new_contents);
+  }
+
+  // Verifies that the file at |file_path_| has |permissions|.
+  void ExpectFilePermissions(int permissions) {
+    int actual_permissions;
+    EXPECT_TRUE(base::GetPosixFilePermissions(file_path_, &actual_permissions));
+    EXPECT_EQ(permissions, actual_permissions);
+  }
+
+  // Creates a file with a random name in the temporary directory.
+  base::FilePath GetTempName() {
+    return temp_dir_.GetPath().Append(GetRandomSuffix());
+  }
+};
+
+constexpr char SafeFDTest::kFileName[];
+constexpr char SafeFDTest::kSymbolicFileName[];
+constexpr char SafeFDTest::kSymbolicDirName[];
+
+TEST_F(SafeFDTest, SafeFD) {
+  EXPECT_FALSE(SafeFD().is_valid());
+}
+
+TEST_F(SafeFDTest, SafeFD_Move) {
+  SafeFD moved_root = std::move(root_);
+  EXPECT_FALSE(root_.is_valid());
+  ASSERT_TRUE(moved_root.is_valid());
+
+  SafeFD moved_root2(std::move(moved_root));
+  EXPECT_FALSE(moved_root.is_valid());
+  ASSERT_TRUE(moved_root2.is_valid());
+}
+
+TEST_F(SafeFDTest, Root) {
+  SafeFD::SafeFDResult result = SafeFD::Root();
+  EXPECT_TRUE(result.first.is_valid());
+  EXPECT_STR_EQ(result.second, SafeFD::Error::kNoError);
+}
+
+TEST_F(SafeFDTest, reset) {
+  root_.reset();
+  EXPECT_FALSE(root_.is_valid());
+}
+
+TEST_F(SafeFDTest, UnsafeReset) {
+  int fd = HANDLE_EINTR(
+      open(file_path_.value().c_str(), O_CREAT | O_NONBLOCK | O_CLOEXEC, 0777));
+  ASSERT_GE(fd, 0);
+
+  {
+    SafeFD safe_fd;
+    safe_fd.UnsafeReset(fd);
+    EXPECT_EQ(safe_fd.get(), fd);
+  }
+
+  // Verify the file descriptor is closed.
+  int result = fcntl(fd, F_GETFD);
+  int error = errno;
+  EXPECT_EQ(result, -1);
+  EXPECT_EQ(error, EBADF);
+}
+
+TEST_F(SafeFDTest, Write_Success) {
+  std::string random_data = GetRandomSuffix();
+  {
+    SafeFD::SafeFDResult file = root_.OpenExistingFile(file_path_);
+    EXPECT_STR_EQ(file.second, SafeFD::Error::kIOError);
+    ASSERT_FALSE(file.first.is_valid());
+
+    file = root_.MakeFile(file_path_);
+    EXPECT_STR_EQ(file.second, SafeFD::Error::kNoError);
+    ASSERT_TRUE(file.first.is_valid());
+
+    EXPECT_STR_EQ(file.first.Write(random_data.data(), random_data.size()),
+                  SafeFD::Error::kNoError);
+  }
+
+  ExpectFileContains(random_data);
+  ExpectFilePermissions(SafeFD::kDefaultFilePermissions);
+}
+
+TEST_F(SafeFDTest, Write_Failure) {
+  std::string random_data = GetRandomSuffix();
+  EXPECT_STR_EQ(root_.Write("", 1), SafeFD::Error::kIOError);
+}
+
+TEST_F(SafeFDTest, ReadContents_Success) {
+  std::string random_data = GetRandomSuffix();
+  WriteFile(random_data);
+
+  SafeFD::SafeFDResult file = root_.OpenExistingFile(file_path_);
+  EXPECT_STR_EQ(file.second, SafeFD::Error::kNoError);
+  ASSERT_TRUE(file.first.is_valid());
+
+  auto result = file.first.ReadContents();
+  EXPECT_EQ(result.second, SafeFD::Error::kNoError);
+  ASSERT_EQ(random_data.size(), result.first.size());
+  EXPECT_EQ(memcmp(random_data.data(), result.first.data(), random_data.size()),
+            0);
+}
+
+TEST_F(SafeFDTest, ReadContents_ExceededMaximum) {
+  std::string random_data = GetRandomSuffix();
+  WriteFile(random_data);
+
+  SafeFD::SafeFDResult file = root_.OpenExistingFile(file_path_);
+  EXPECT_STR_EQ(file.second, SafeFD::Error::kNoError);
+  ASSERT_TRUE(file.first.is_valid());
+
+  ASSERT_LT(1, random_data.size());
+  auto result = file.first.ReadContents(1);
+  EXPECT_EQ(result.second, SafeFD::Error::kExceededMaximum);
+}
+
+TEST_F(SafeFDTest, ReadContents_BadFD) {
+  SafeFD invalid;
+  ASSERT_FALSE(invalid.is_valid());
+
+  auto result = invalid.ReadContents();
+  EXPECT_EQ(result.second, SafeFD::Error::kIOError);
+}
+
+TEST_F(SafeFDTest, Read_Success) {
+  std::string random_data = GetRandomSuffix();
+  WriteFile(random_data);
+
+  SafeFD::SafeFDResult file = root_.OpenExistingFile(file_path_);
+  EXPECT_STR_EQ(file.second, SafeFD::Error::kNoError);
+  ASSERT_TRUE(file.first.is_valid());
+
+  std::vector<char> buffer(random_data.size(), '\0');
+  ASSERT_EQ(file.first.Read(buffer.data(), buffer.size()),
+            SafeFD::Error::kNoError);
+  EXPECT_EQ(memcmp(random_data.data(), buffer.data(), random_data.size()), 0);
+}
+
+TEST_F(SafeFDTest, Read_BadFD) {
+  SafeFD invalid;
+  ASSERT_FALSE(invalid.is_valid());
+
+  char to_read;
+  EXPECT_EQ(invalid.Read(&to_read, 1), SafeFD::Error::kIOError);
+}
+
+TEST_F(SafeFDTest, Read_IOError) {
+  std::string random_data = GetRandomSuffix();
+  WriteFile(random_data);
+
+  SafeFD::SafeFDResult file = root_.OpenExistingFile(file_path_);
+  EXPECT_STR_EQ(file.second, SafeFD::Error::kNoError);
+  ASSERT_TRUE(file.first.is_valid());
+
+  std::vector<char> buffer(random_data.size() * 2, '\0');
+  ASSERT_EQ(file.first.Read(buffer.data(), buffer.size()),
+            SafeFD::Error::kIOError);
+}
+
+TEST_F(SafeFDTest, OpenExistingFile_Success) {
+  WriteFile("");
+  SafeFD::SafeFDResult file = root_.OpenExistingFile(file_path_);
+  EXPECT_STR_EQ(file.second, SafeFD::Error::kNoError);
+  ASSERT_TRUE(file.first.is_valid());
+}
+
+TEST_F(SafeFDTest, OpenExistingFile_NotInitialized) {
+  SafeFD::SafeFDResult file = SafeFD().OpenExistingFile(file_path_);
+  EXPECT_STR_EQ(file.second, SafeFD::Error::kNotInitialized);
+  ASSERT_FALSE(file.first.is_valid());
+}
+
+TEST_F(SafeFDTest, OpenExistingFile_IOError) {
+  SafeFD::SafeFDResult file = root_.OpenExistingFile(file_path_);
+  EXPECT_STR_EQ(file.second, SafeFD::Error::kIOError);
+  ASSERT_FALSE(file.first.is_valid());
+}
+
+TEST_F(SafeFDTest, OpenExistingFile_SymlinkDetected) {
+  WriteFile("");
+  SafeFD::SafeFDResult file = root_.OpenExistingFile(symlink_file_path_);
+  EXPECT_STR_EQ(file.second, SafeFD::Error::kSymlinkDetected);
+  ASSERT_FALSE(file.first.is_valid());
+}
+
+TEST_F(SafeFDTest, OpenExistingFile_WrongType) {
+  WriteFile("");
+  SafeFD::SafeFDResult file =
+      root_.OpenExistingFile(symlink_dir_path_.Append(kFileName));
+  EXPECT_STR_EQ(file.second, SafeFD::Error::kWrongType);
+  ASSERT_FALSE(file.first.is_valid());
+}
+
+TEST_F(SafeFDTest, OpenExistingDir_Success) {
+  SafeFD::SafeFDResult dir = root_.OpenExistingDir(temp_dir_.GetPath());
+  EXPECT_STR_EQ(dir.second, SafeFD::Error::kNoError);
+  ASSERT_TRUE(dir.first.is_valid());
+}
+
+TEST_F(SafeFDTest, OpenExistingDir_NotInitialized) {
+  SafeFD::SafeFDResult dir = SafeFD().OpenExistingDir(temp_dir_.GetPath());
+  EXPECT_STR_EQ(dir.second, SafeFD::Error::kNotInitialized);
+  ASSERT_FALSE(dir.first.is_valid());
+}
+
+TEST_F(SafeFDTest, OpenExistingDir_IOError) {
+  SafeFD::SafeFDResult dir = root_.OpenExistingDir(file_path_);
+  EXPECT_STR_EQ(dir.second, SafeFD::Error::kIOError);
+  ASSERT_FALSE(dir.first.is_valid());
+}
+
+TEST_F(SafeFDTest, OpenExistingDir_WrongType) {
+  SafeFD::SafeFDResult dir = root_.OpenExistingDir(symlink_dir_path_);
+  EXPECT_STR_EQ(dir.second, SafeFD::Error::kWrongType);
+  ASSERT_FALSE(dir.first.is_valid());
+}
+
+TEST_F(SafeFDTest, MakeFile_DoesNotExistSuccess) {
+  {
+    SafeFD::SafeFDResult file = root_.MakeFile(file_path_);
+    EXPECT_STR_EQ(file.second, SafeFD::Error::kNoError);
+    ASSERT_TRUE(file.first.is_valid());
+  }
+
+  ExpectFilePermissions(SafeFD::kDefaultFilePermissions);
+}
+
+TEST_F(SafeFDTest, MakeFile_ExistsSuccess) {
+  WriteFile("");
+  {
+    SafeFD::SafeFDResult file = root_.MakeFile(file_path_);
+    EXPECT_STR_EQ(file.second, SafeFD::Error::kNoError);
+    ASSERT_TRUE(file.first.is_valid());
+  }
+
+  ExpectFilePermissions(SafeFD::kDefaultFilePermissions);
+}
+
+TEST_F(SafeFDTest, MakeFile_IOError) {
+  ASSERT_EQ(mkfifo(file_path_.value().c_str(), 0), 0);
+  SafeFD::SafeFDResult file = root_.MakeFile(file_path_);
+  EXPECT_STR_EQ(file.second, SafeFD::Error::kIOError);
+  ASSERT_FALSE(file.first.is_valid());
+}
+
+TEST_F(SafeFDTest, MakeFile_SymlinkDetected) {
+  SafeFD::SafeFDResult file = root_.MakeFile(symlink_file_path_);
+  EXPECT_STR_EQ(file.second, SafeFD::Error::kSymlinkDetected);
+  ASSERT_FALSE(file.first.is_valid());
+}
+
+TEST_F(SafeFDTest, MakeFile_WrongGID) {
+  WriteFile("");
+  EXPECT_EQ(chown(file_path_.value().c_str(), getuid(), 0), 0)
+      << strerror(errno);
+  {
+    SafeFD::SafeFDResult file = root_.MakeFile(file_path_);
+    EXPECT_STR_EQ(file.second, SafeFD::Error::kWrongGID);
+    ASSERT_FALSE(file.first.is_valid());
+  }
+}
+
+TEST_F(SafeFDTest, MakeFile_WrongPermissions) {
+  WriteFile("");
+  EXPECT_EQ(chmod(file_path_.value().c_str(), 0777), 0) << strerror(errno);
+  {
+    SafeFD::SafeFDResult file = root_.MakeFile(file_path_);
+    EXPECT_STR_EQ(file.second, SafeFD::Error::kWrongPermissions);
+    ASSERT_FALSE(file.first.is_valid());
+  }
+  EXPECT_EQ(chmod(file_path_.value().c_str(), SafeFD::kDefaultFilePermissions),
+            0)
+      << strerror(errno);
+
+  EXPECT_EQ(chmod(temp_dir_path_.data(), 0777), 0) << strerror(errno);
+  {
+    SafeFD::SafeFDResult file = root_.MakeFile(file_path_);
+    EXPECT_STR_EQ(file.second, SafeFD::Error::kWrongPermissions);
+    ASSERT_FALSE(file.first.is_valid());
+  }
+}
+
+TEST_F(SafeFDTest, MakeDir_DoesNotExistSuccess) {
+  {
+    SafeFD::SafeFDResult dir = root_.MakeDir(file_path_);
+    EXPECT_STR_EQ(dir.second, SafeFD::Error::kNoError);
+    ASSERT_TRUE(dir.first.is_valid());
+  }
+
+  ExpectFilePermissions(SafeFD::kDefaultDirPermissions);
+}
+
+TEST_F(SafeFDTest, MakeDir_ExistsSuccess) {
+  EXPECT_TRUE(base::CreateDirectory(file_path_));
+  EXPECT_EQ(chmod(file_path_.value().c_str(), SafeFD::kDefaultDirPermissions),
+            0)
+      << strerror(errno);
+  {
+    SafeFD::SafeFDResult dir = root_.MakeDir(file_path_);
+    EXPECT_STR_EQ(dir.second, SafeFD::Error::kNoError);
+    ASSERT_TRUE(dir.first.is_valid());
+  }
+
+  ExpectFilePermissions(SafeFD::kDefaultDirPermissions);
+}
+
+TEST_F(SafeFDTest, MakeDir_WrongType) {
+  SafeFD::SafeFDResult dir = root_.MakeDir(symlink_file_path_);
+  EXPECT_STR_EQ(dir.second, SafeFD::Error::kWrongType);
+  ASSERT_FALSE(dir.first.is_valid());
+}
+
+TEST_F(SafeFDTest, MakeDir_WrongGID) {
+  EXPECT_TRUE(base::CreateDirectory(file_path_));
+  EXPECT_EQ(chmod(file_path_.value().c_str(), SafeFD::kDefaultDirPermissions),
+            0)
+      << strerror(errno);
+  EXPECT_EQ(chown(file_path_.value().c_str(), getuid(), 0), 0)
+      << strerror(errno);
+  {
+    SafeFD::SafeFDResult dir = root_.MakeDir(file_path_);
+    EXPECT_STR_EQ(dir.second, SafeFD::Error::kWrongGID);
+    ASSERT_FALSE(dir.first.is_valid());
+  }
+}
+
+TEST_F(SafeFDTest, MakeDir_WrongPermissions) {
+  EXPECT_TRUE(base::CreateDirectory(file_path_));
+  EXPECT_EQ(chmod(file_path_.value().c_str(), 0777), 0) << strerror(errno);
+
+  SafeFD::SafeFDResult dir = root_.MakeDir(file_path_);
+  EXPECT_STR_EQ(dir.second, SafeFD::Error::kWrongPermissions);
+  ASSERT_FALSE(dir.first.is_valid());
+}
 
 }  // namespace brillo
