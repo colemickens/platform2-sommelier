7a306f33c8340a2decead03a6e7d76a7f9e5f85d
Mike Frysinger <vapier@chromium.org>
2019-07-30 14:34:36 -0400

    common-mk: write_args: rewrite to be standalone for python3
    The only use of chromite here can be replaced with argparse, so cut
    that over.  This allows us to transition the script to Python 3.
    
    BUG=chromium:989152
    TEST=building with python->python3 works
    
    Change-Id: Ie5fbd10a1a5097064d7b17b8fa09246ae6483717
    Reviewed-on: https://chromium-review.googlesource.com/1726853
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Commit-Ready: Mike Frysinger <vapier@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>

--- common-mk/write_args.py
+++ common-mk/write_args.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Copyright 2018 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
 """Python script to output the given list as a space-delimited string to a file.
 
 path/to/write_args.py --output filename -- arg1 arg2 ...
@@ -16,14 +17,19 @@ https://bugs.chromium.org/p/gn/issues/detail?id=9
 
 from __future__ import print_function
 
+import argparse
 import sys
 
-from chromite.lib import commandline
 
-parser = commandline.ArgumentParser()
-parser.add_argument('--output', help='the output file name', required=True)
-parser.add_argument('args', nargs='*')
-options = parser.parse_args(sys.argv[1:])
+def main(argv):
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument('--output', required=True, help='the output file name')
+  parser.add_argument('args', nargs='*')
+  options = parser.parse_args(argv)
 
-with open(options.output, 'w') as f:
-  f.write(' '.join(options.args) + '\n')
+  with open(options.output, 'w') as f:
+    f.write(' '.join(options.args) + '\n')
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))
