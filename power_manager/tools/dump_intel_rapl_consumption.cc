bcd064f79f1a9caa86ce18e2db8a2592851b45e3
Harry Pan <harry.pan@intel.com>
2019-09-10 12:30:38 +0800

    power: refactor RAPL dumping by reading from powercap driver
    Linux powercap driver supports RAPL domains energy report as its subset;
    reading sysfs instead of MSR accessing improves program scalability and
    get rid of microarchitecture knowledge, especially some Atom processors
    have different formula in ESU (Energy Status Units), it enables support
    to more Intel SoCs from Atom to the latest generation.
    
    BUG=chromium:1003208
    TEST=do 'dump_intel_rapl_consumption --repeat --interval_ms=5000',
    compare w/ old tool, or compare w/ 'turbostat --quiet --show "PkgWatt,CorWatt,GFXWatt,RAMWatt"'
    
    Change-Id: I82eb589b700be26cfe74dbd475478ced6031c84d
    Signed-off-by: Harry Pan <harry.pan@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/1797967
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

--- power_manager/tools/dump_intel_rapl_consumption.cc
+++ power_manager/tools/dump_intel_rapl_consumption.cc
@@ -3,16 +3,18 @@
 // found in the LICENSE file.
 
 // This utility reports power consumption for certain Intel SoCs, calculated by
-// averaging the running energy consumption counter provided by the Intel RAPL
-// (Running Average Power Limit) MSRs (Model Specific Registers). RAPL provides
-// info per Power Domain: DRAM and PKG. PKG refers to the processor die, and
-// includes the PP0 (cores) and PP1 (graphics) subdomains.
+// averaging the running energy consumption counter provided by Linux powercap
+// driver subset of Intel RAPL (Running Average Power Limit) energy report.
+// RAPL provides info per Power Domain: DRAM and PKG. PKG refers to the
+// processor die, and includes the PP0 (cores) and PP1 (graphics) subdomains.
 //
 // MSRs reference can be found in "Sec. 14.9 Platform Specific Power Management
 // Support" of the "Intel 64 and IA-32 Architectures Software Developer’s
 // Manual Volume 3B: System Programming Guide, Part 2" [1].
+// Info of Linux powercap driver can be reached in kernel documentation [2].
 //
 // [1] https://www.intel.com/content/www/us/en/architecture-and-technology/64-ia-32-architectures-software-developer-vol-3b-part-2-manual.html
+// [2] https://github.com/torvalds/linux/blob/master/Documentation/power/powercap/powercap.rst
 
 #include <math.h>
 
@@ -26,48 +28,20 @@
 #include <brillo/flag_helper.h>
 #include <brillo/syslog_logging.h>
 
-#define CPU_MODEL_KABYLAKE 142
-
-// MSR values found from "Intel 64 and IA-32 Architectures Software Developer’s
-// Manual Volume 4: Model-Specifi Register", e.g.:
-//
-// https://software.intel.com/en-us/download/intel-64-and-ia-32-architectures-software-developers-manual-volume-4-model-specific-registers
-#define MSR_RAPL_POWER_UNIT 0x00000606
-
-#define MSR_PKG_ENERGY_STATUS 0x00000611
-#define MSR_DRAM_ENERGY_STATUS 0x00000619
-#define MSR_PP0_ENERGY_STATUS 0x00000639
-#define MSR_PP1_ENERGY_STATUS 0x00000641
-
-// MSR_RAPL_POWER_UNIT registers bits containing the energy units scaler.
-#define MSR_ENERGY_UNIT_OFFSET 0x08
-#define MSR_ENERGY_UNIT_MASK 0x1F
+#include "power_manager/common/util.h"
 
 namespace {
 
-// Path of the dev file containing the MSR access to the CPU #0, through which
-// we read all the power and energy consumptions.
-constexpr const char kCpu0MsrPath[] = "/dev/cpu/0/msr";
-
 struct {
-  int64_t register_offset;
+  const char* node;
   const char* name;
-} const kRegistersAndNames[] = {
-    {MSR_PKG_ENERGY_STATUS, "pkg"},
-    {MSR_PP0_ENERGY_STATUS, "pp0"},
-    {MSR_PP1_ENERGY_STATUS, "gfx"},
-    {MSR_DRAM_ENERGY_STATUS, "dram"},
+} const kPowercapDomains[] = {
+    {"/sys/class/powercap/intel-rapl:0/energy_uj", "pkg"},
+    {"/sys/class/powercap/intel-rapl:0:0/energy_uj", "pp0"},
+    {"/sys/class/powercap/intel-rapl:0:1/energy_uj", "gfx"},
+    {"/sys/class/powercap/intel-rapl:0:2/energy_uj", "dram"},
 };
-const size_t kNumRegisters = arraysize(kRegistersAndNames);
-
-// Reads a uint64_t from |file| at |offset| and returns its value or crashes.
-uint64_t ReadEnergyStatus(base::File* file, int64_t offset) {
-  uint64_t register_value = 0;
-  CHECK_EQ(sizeof(uint64_t),
-           file->Read(offset, reinterpret_cast<char*>(&register_value),
-                     sizeof(uint64_t)));
-  return register_value &= 0xFFFFFFFF;
-}
+const size_t kNumPowercapDomains = arraysize(kPowercapDomains);
 
 }  // namespace
 
@@ -82,58 +56,35 @@ int main(int argc, char** argv) {
 
   const base::CPU cpu;
   CHECK_EQ("GenuineIntel", cpu.vendor_name()) << "Only GenuineIntel supported";
-  // TODO(mcasas): Support older models; in principle MSR energy consumption is
-  // present from Sandy Bridge onwards. https://crbug.com/768518.
-  CHECK_EQ(CPU_MODEL_KABYLAKE, cpu.model()) << "Only Kaby Lake CPU supported";
 
-  base::FilePath cpu_dev_path(kCpu0MsrPath);
-  PCHECK(base::PathExists(cpu_dev_path))
-      << "Seems like MSR is unsupported, couldn't find "
-      << cpu_dev_path.MaybeAsASCII();
+  base::FilePath energy_file_path[kNumPowercapDomains];
+  for (int i = 0; i < kNumPowercapDomains; ++i) {
+    energy_file_path[i] = base::FilePath(kPowercapDomains[i].node);
+    PCHECK(base::PathExists(energy_file_path[i]))
+        << "Error opening " << energy_file_path[i].value();
+  }
 
-  base::File cpu_dev(cpu_dev_path,
-                     base::File::FLAG_OPEN | base::File::FLAG_READ);
-  PCHECK(cpu_dev.IsValid()) << "Error opening " << cpu_dev_path.MaybeAsASCII();
-
-  // MSR_RAPL_POWER_UNIT has the units for power and energy measurements.
-  uint64_t power_unit_register = 0;
-  CHECK_EQ(sizeof(uint64_t),
-           cpu_dev.Read(MSR_RAPL_POWER_UNIT,
-                        reinterpret_cast<char*>(&power_unit_register),
-                        sizeof(uint64_t)));
-
-  const double energy_units_scaler = pow(
-      0.5, static_cast<double>((power_unit_register >> MSR_ENERGY_UNIT_OFFSET) &
-                               MSR_ENERGY_UNIT_MASK));
-
-  if (FLAGS_verbose)
-    printf("Energy units = %.8fJ/unit\n", energy_units_scaler);
-
-  for (const auto& register_and_name : kRegistersAndNames)
-    printf("%10s ", register_and_name.name);
+  for (const auto& domain : kPowercapDomains)
+    printf("%10s ", domain.name);
   printf(" (Note: 'pkg' includes 'pp0' and 'gfx'. Values in Watts)\n");
 
-  uint32_t values_before[kNumRegisters] = {};
-  uint32_t values_after[kNumRegisters] = {};
+  uint64_t energy_before[kNumPowercapDomains] = {};
+  uint64_t energy_after[kNumPowercapDomains] = {};
   do {
-    for (int i = 0; i < kNumRegisters; ++i) {
-      values_before[i] =
-          ReadEnergyStatus(&cpu_dev, kRegistersAndNames[i].register_offset);
-    }
+    for (int i = 0; i < kNumPowercapDomains; ++i)
+      power_manager::util::ReadUint64File(energy_file_path[i], &energy_before[i]);
     const base::TimeTicks ticks_before = base::TimeTicks::Now();
 
     base::PlatformThread::Sleep(
         base::TimeDelta::FromMilliseconds(FLAGS_interval_ms));
 
-    for (int i = 0; i < kNumRegisters; ++i) {
-      values_after[i] =
-          ReadEnergyStatus(&cpu_dev, kRegistersAndNames[i].register_offset);
-    }
+    for (int i = 0; i < kNumPowercapDomains; ++i)
+      power_manager::util::ReadUint64File(energy_file_path[i], &energy_after[i]);
     const base::TimeDelta time_delta = base::TimeTicks::Now() - ticks_before;
 
-    for (int i = 0; i < kNumRegisters; ++i) {
-      printf("%10.6f ", (values_after[i] - values_before[i]) *
-                            energy_units_scaler / time_delta.InSecondsF());
+    for (int i = 0; i < kNumPowercapDomains; ++i) {
+      printf("%10.6f ", (energy_after[i] - energy_before[i]) /
+                            (time_delta.InSecondsF() * 1e6));
     }
     printf("\n");
   } while (FLAGS_repeat);
