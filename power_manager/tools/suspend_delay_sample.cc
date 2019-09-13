66e099d7cf127a1fcd656afd7dbcc9f9bb73af71
Ravi Chandra Sadineni <ravisadineni@chromium.org>
2019-09-13 10:40:43 -0700

    power: Extend suspend_delay_sample to delay dark suspend.
    This change extends the tool to register dark_suspend_delay. This change
    also makes this tool resilient to powerd restarts when running.
    
    BUG=b:133262012
    TEST=Deploy on hatch and test dark suspend is delayed.
    
    Change-Id: Ia3b8d2296d4431c1ce8d733db4efc94acfadecc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1804815
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Todd Broch <tbroch@chromium.org>
    Tested-by: Ravi Chandra Sadineni <ravisadineni@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>

--- power_manager/tools/suspend_delay_sample.cc
+++ power_manager/tools/suspend_delay_sample.cc
@@ -10,6 +10,7 @@
 #include <base/logging.h>
 #include <base/memory/scoped_refptr.h>
 #include <base/message_loop/message_loop.h>
+#include <base/optional.h>
 #include <base/run_loop.h>
 #include <base/time/time.h>
 #include <brillo/flag_helper.h>
@@ -21,11 +22,6 @@
 #include "power_manager/proto_bindings/suspend.pb.h"
 
 namespace {
-
-// Human-readable description of the delay's purpose.
-const char kSuspendDelayDescription[] = "suspend_delay_sample";
-
-}  // namespace
 
 // Passes |request| to powerd's |method_name| D-Bus method.
 // Copies the returned protocol buffer to |reply_out|, which may be NULL if no
@@ -53,74 +49,139 @@ bool CallMethod(dbus::ObjectProxy* powerd_proxy,
   return true;
 }
 
-// Registers a suspend delay and returns the corresponding ID.
-int RegisterSuspendDelay(dbus::ObjectProxy* powerd_proxy, int timeout_ms) {
-  power_manager::RegisterSuspendDelayRequest request;
-  request.set_timeout(
-      base::TimeDelta::FromMilliseconds(timeout_ms).ToInternalValue());
-  request.set_description(kSuspendDelayDescription);
-  power_manager::RegisterSuspendDelayReply reply;
-  CHECK(CallMethod(powerd_proxy, power_manager::kRegisterSuspendDelayMethod,
-                   request, &reply));
-  LOG(INFO) << "Registered delay " << reply.delay_id();
-  return reply.delay_id();
-}
+// Human-readable description of the delay's purpose.
+const char kSuspendDelayDescription[] = "suspend_delay_sample";
 
-// Announces that the process is ready for suspend attempt |suspend_id|.
-void SendSuspendReady(scoped_refptr<dbus::ObjectProxy> powerd_proxy,
-                      int delay_id,
-                      int suspend_id) {
-  LOG(INFO) << "Announcing readiness of delay " << delay_id
-            << " for suspend attempt " << suspend_id;
-  power_manager::SuspendReadinessInfo request;
-  request.set_delay_id(delay_id);
-  request.set_suspend_id(suspend_id);
-  CallMethod(powerd_proxy.get(), power_manager::kHandleSuspendReadinessMethod,
-             request, nullptr);
-}
+}  // namespace
 
-// Handles the start of a suspend attempt. Posts a task to run
-// SendSuspendReady() after a delay.
-void HandleSuspendImminent(scoped_refptr<dbus::ObjectProxy> powerd_proxy,
-                           int delay_id,
-                           int delay_ms,
-                           dbus::Signal* signal) {
-  power_manager::SuspendImminent info;
-  dbus::MessageReader reader(signal);
-  CHECK(reader.PopArrayOfBytesAsProto(&info));
-  int suspend_id = info.suspend_id();
+class SuspendDelayRegisterer {
+ public:
+  SuspendDelayRegisterer(int delay_ms, int timeout_ms, bool dark_suspend_delay)
+      : delay_ms_(delay_ms),
+        timeout_ms_(timeout_ms),
+        dark_suspend_delay_(dark_suspend_delay),
+        weak_ptr_factory_(this) {
+    dbus::Bus::Options options;
+    options.bus_type = dbus::Bus::SYSTEM;
+    bus_ = new dbus::Bus(options);
+    CHECK(bus_->Connect());
+    powerd_proxy_ = bus_->GetObjectProxy(
+        power_manager::kPowerManagerServiceName,
+        dbus::ObjectPath(power_manager::kPowerManagerServicePath));
+    RegisterSuspendDelay();
+    powerd_proxy_->SetNameOwnerChangedCallback(
+        base::Bind(&SuspendDelayRegisterer::NameOwnerChangedReceived,
+                   weak_ptr_factory_.GetWeakPtr()));
+  }
 
-  LOG(INFO) << "Got notification about suspend attempt " << suspend_id;
-  LOG(INFO) << "Sleeping " << delay_ms << " ms before responding";
-  base::MessageLoop::current()->task_runner()->PostDelayedTask(
-      FROM_HERE,
-      base::Bind(&SendSuspendReady, powerd_proxy, delay_id, suspend_id),
-      base::TimeDelta::FromMilliseconds(delay_ms));
-}
+ private:
+  // Announces that the process is ready for suspend attempt |suspend_id|.
+  void SendSuspendReady(int suspend_id) {
+    CHECK(delay_id_) << "Invalid suspend delay Id";
+    LOG(INFO) << "Announcing readiness of delay " << delay_id_.value()
+              << " for suspend attempt " << suspend_id;
+    power_manager::SuspendReadinessInfo request;
+    request.set_delay_id(delay_id_.value());
+    request.set_suspend_id(suspend_id);
+    CallMethod(powerd_proxy_, power_manager::kHandleSuspendReadinessMethod,
+               request, nullptr);
+  }
 
-// Handles the completion of a suspend attempt.
-void HandleSuspendDone(dbus::Signal* signal) {
-  power_manager::SuspendDone info;
-  dbus::MessageReader reader(signal);
-  CHECK(reader.PopArrayOfBytesAsProto(&info));
-  const base::TimeDelta duration =
-      base::TimeDelta::FromInternalValue(info.suspend_duration());
-  LOG(INFO) << "Suspend attempt " << info.suspend_id() << " is complete; "
-            << "system was suspended for " << duration.InMilliseconds()
-            << " ms";
-}
+  // Handles the start of a suspend attempt. Posts a task to run
+  // SendSuspendReady() after a delay.
+  void HandleSuspendImminent(dbus::Signal* signal) {
+    power_manager::SuspendImminent info;
+    dbus::MessageReader reader(signal);
+    CHECK(reader.PopArrayOfBytesAsProto(&info));
+    int suspend_id = info.suspend_id();
 
-// Handles the result of an attempt to connect to a D-Bus signal.
-void DBusSignalConnected(const std::string& interface,
-                         const std::string& signal,
-                         bool success) {
-  CHECK(success) << "Unable to connect to " << interface << "." << signal;
-}
+    LOG(INFO) << "Got notification about suspend attempt " << suspend_id;
+    LOG(INFO) << "Sleeping " << delay_ms_ << " ms before responding";
+    base::MessageLoop::current()->task_runner()->PostDelayedTask(
+        FROM_HERE,
+        base::Bind(&SuspendDelayRegisterer::SendSuspendReady,
+                   weak_ptr_factory_.GetWeakPtr(), suspend_id),
+        base::TimeDelta::FromMilliseconds(delay_ms_));
+  }
+
+  // Handles the completion of a suspend attempt.
+  void HandleSuspendDone(dbus::Signal* signal) {
+    power_manager::SuspendDone info;
+    dbus::MessageReader reader(signal);
+    CHECK(reader.PopArrayOfBytesAsProto(&info));
+    const base::TimeDelta duration =
+        base::TimeDelta::FromInternalValue(info.suspend_duration());
+    LOG(INFO) << "Suspend attempt " << info.suspend_id() << " is complete; "
+              << "system was suspended for " << duration.InMilliseconds()
+              << " ms";
+  }
+
+  // Handles the result of an attempt to connect to a D-Bus signal.
+  void DBusSignalConnected(const std::string& interface,
+                           const std::string& signal,
+                           bool success) {
+    CHECK(success) << "Unable to connect to " << interface << "." << signal;
+  }
+
+  // Registers a suspend delay and returns the corresponding ID.
+  void RegisterSuspendDelay() {
+    power_manager::RegisterSuspendDelayRequest request;
+    request.set_timeout(
+        base::TimeDelta::FromMilliseconds(timeout_ms_).ToInternalValue());
+    request.set_description(kSuspendDelayDescription);
+    std::string method_name =
+        dark_suspend_delay_ ? power_manager::kRegisterDarkSuspendDelayMethod
+                            : power_manager::kRegisterSuspendDelayMethod;
+    power_manager::RegisterSuspendDelayReply reply;
+    CHECK(CallMethod(powerd_proxy_, method_name, request, &reply));
+    LOG(INFO) << "Registered " << (dark_suspend_delay_ ? "dark " : "")
+              << "suspend delay " << reply.delay_id();
+    delay_id_ = reply.delay_id();
+
+    powerd_proxy_->ConnectToSignal(
+        power_manager::kPowerManagerInterface,
+        power_manager::kSuspendImminentSignal,
+        base::Bind(&SuspendDelayRegisterer::HandleSuspendImminent,
+                   weak_ptr_factory_.GetWeakPtr()),
+        base::Bind(&SuspendDelayRegisterer::DBusSignalConnected,
+                   weak_ptr_factory_.GetWeakPtr()));
+    powerd_proxy_->ConnectToSignal(
+        power_manager::kPowerManagerInterface,
+        power_manager::kSuspendDoneSignal,
+        base::Bind(&SuspendDelayRegisterer::HandleSuspendDone,
+                   weak_ptr_factory_.GetWeakPtr()),
+        base::Bind(&SuspendDelayRegisterer::DBusSignalConnected,
+                   weak_ptr_factory_.GetWeakPtr()));
+  }
+
+  void NameOwnerChangedReceived(const std::string& old_owner,
+                                const std::string& new_owner) {
+    //  Try to register suspend delay only if available.
+    if (!new_owner.empty()) {
+      LOG(INFO) << "Received NameOwnerChanged d-bus signal.";
+      RegisterSuspendDelay();
+    }
+  }
+
+  int delay_ms_;
+  int timeout_ms_;
+  // Id assigned by powerd to a suspend delay client.
+  base::Optional<int> delay_id_;
+  // Whether to register dark/full suspend delay.
+  bool dark_suspend_delay_ = false;
+  scoped_refptr<dbus::Bus> bus_;
+  dbus::ObjectProxy* powerd_proxy_ = nullptr;
+
+  base::WeakPtrFactory<SuspendDelayRegisterer> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(SuspendDelayRegisterer);
+};
 
 int main(int argc, char* argv[]) {
   DEFINE_int32(delay_ms, 5000,
                "Milliseconds to wait before reporting suspend readiness");
   DEFINE_int32(timeout_ms, 7000, "Suspend timeout in milliseconds");
+  DEFINE_bool(dark_suspend, false, "Register delay as a dark suspend");
 
   brillo::FlagHelper::Init(
       argc, argv,
@@ -129,25 +190,8 @@ int main(int argc, char* argv[]) {
   base::AtExitManager at_exit_manager;
   base::MessageLoopForIO message_loop;
 
-  dbus::Bus::Options options;
-  options.bus_type = dbus::Bus::SYSTEM;
-  scoped_refptr<dbus::Bus> bus(new dbus::Bus(options));
-  CHECK(bus->Connect());
-
-  dbus::ObjectProxy* powerd_proxy = bus->GetObjectProxy(
-      power_manager::kPowerManagerServiceName,
-      dbus::ObjectPath(power_manager::kPowerManagerServicePath));
-  const int delay_id = RegisterSuspendDelay(powerd_proxy, FLAGS_timeout_ms);
-  powerd_proxy->ConnectToSignal(
-      power_manager::kPowerManagerInterface,
-      power_manager::kSuspendImminentSignal,
-      base::Bind(&HandleSuspendImminent, base::WrapRefCounted(powerd_proxy),
-                 delay_id, FLAGS_delay_ms),
-      base::Bind(&DBusSignalConnected));
-  powerd_proxy->ConnectToSignal(
-      power_manager::kPowerManagerInterface, power_manager::kSuspendDoneSignal,
-      base::Bind(&HandleSuspendDone), base::Bind(&DBusSignalConnected));
-
+  SuspendDelayRegisterer suspend_delay_registerer(
+      FLAGS_delay_ms, FLAGS_timeout_ms, FLAGS_dark_suspend);
   base::RunLoop().Run();
 
   // powerd will automatically unregister this process's suspend delay when the
