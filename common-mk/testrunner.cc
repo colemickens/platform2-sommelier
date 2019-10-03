e835325abfa6acfee13582dbdf2c779708818ff9
Tom Hughes <tomhughes@chromium.org>
2019-10-03 15:18:39 -0700

    common-mk: Add TestRunner class
    Th platform2::TestRunner class makes it possible for components to
    override the default common-mk/testrunner behavior.
    
    BUG=chromium:1003564
    TEST=FEATURES="test" emerge-hatch timberslide
    TEST=FEATURES="test" emerge-hatch biod
    
    Change-Id: Ib624f162920c6e9a33c0260722371d4247c2f510
    Reviewed-on: https://chromium-review.googlesource.com/1838496
    Tested-by: Tom Hughes <tomhughes@chromium.org>
    Commit-Ready: Tom Hughes <tomhughes@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Chris McDonald <cjmcdonald@chromium.org>

--- common-mk/testrunner.cc
+++ common-mk/testrunner.cc
@@ -2,19 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <base/at_exit.h>
-#include <base/command_line.h>
-#include <base/logging.h>
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
+#include "common-mk/testrunner.h"
 
 int main(int argc, char** argv) {
-  base::CommandLine::Init(argc, argv);
-  logging::InitLogging(logging::LoggingSettings());
-
-  base::AtExitManager exit_manager;
-  testing::InitGoogleTest(&argc, argv);
-  testing::GTEST_FLAG(throw_on_failure) = true;
-  testing::InitGoogleMock(&argc, argv);
-  return RUN_ALL_TESTS();
+  auto runner = platform2::TestRunner(argc, argv);
+  return runner.Run();
 }
