c9a719445e3ae543bc1f35ace384528c6a38fae8
Jeffrey Kardatzke <jkardatzke@google.com>
2019-10-10 11:19:23 -0700

    vm_tools: Add fuzzer for garcon mime_types_parser
    BUG=chromium:1007413
    TEST=USE="asan fuzzer" emerge-tatl vm_guest_tools
    Run garcon_mime_types_fuzzer
    
    Change-Id: I88bbf60ff19213e3106216b55f65d35423a774af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1853564
    Tested-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Auto-Submit: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Chirantan Ekbote <chirantan@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>

---
+++ vm_tools/garcon/mime_types_parser_fuzzer.cc
@@ -0,0 +1,41 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+#include <vector>
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <base/files/file_path.h>
+#include <base/files/file_util.h>
+#include <base/files/scoped_temp_dir.h>
+#include <base/logging.h>
+
+#include "vm_tools/garcon/mime_types_parser.h"
+
+class Environment {
+ public:
+  Environment() {
+    // Disable logging.
+    logging::SetMinLogLevel(logging::LOG_FATAL);
+    // Setup temp dir for writing out the mime types file to parse.
+    CHECK(temp_dir_.CreateUniqueTempDir());
+  }
+
+  base::ScopedTempDir temp_dir_;
+};
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static Environment env;
+
+  // The filename has no impact on this function's execution.
+  base::FilePath file_path(env.temp_dir_.GetPath().Append("mime_types"));
+  base::WriteFile(file_path, reinterpret_cast<const char*>(data), size);
+
+  vm_tools::garcon::MimeTypeMap map;
+  vm_tools::garcon::ParseMimeTypes(file_path.value(), &map);
+
+  return 0;
+}
