906553ae6b9666bfd0a97656f4759f0930bfde49
Taoyu Li <taoyl@chromium.org>
2019-10-18 13:44:41 +0900

    arc: network: Fuzzer for NDProxy::TranslateNDFrame
    This patch adds a fuzzing target to NDProxy::TranslateNDFrame
    function, which take an input ND frame and do the L2 address
    translation.
    
    BUG=b:142768280
    TEST=$ USE="asan fuzzer" emerge-atlas arc-networkd
         $ /build/atlas/usr/libexec/fuzzers/ndproxy_fuzzer
    
    Change-Id: I791b6868c5e325f1642a3a264f44b7b4820e1e52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1866506
    Tested-by: Taoyu Li <taoyl@chromium.org>
    Reviewed-by: Garrick Evans <garrick@chromium.org>
    Reviewed-by: Manoj Gupta <manojgupta@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>

---
+++ arc/network/ndproxy_fuzzer.cc
@@ -0,0 +1,24 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "arc/network/ndproxy.h"
+
+namespace arc_networkd {
+
+namespace {
+
+constexpr const uint8_t guest_if_mac[] = "\xd2\x47\xf7\xc5\x9e\x53";
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  // Turn off logging.
+  logging::SetMinLogLevel(logging::LOG_FATAL);
+
+  uint8_t out_buffer[IP_MAXPACKET];
+  NDProxy::TranslateNDFrame(data, size, guest_if_mac, out_buffer);
+
+  return 0;
+}
+
+}  // namespace
+}  // namespace arc_networkd
