c477ca80e3f628a08b331db379b7989a40c99ad9
Tiancong Wang <tcwang@google.com>
2019-10-18 11:32:51 -0700

    Reland "obb-mounter: Add fuzzer for mount_obb."
    This is a reland of 5cbbac26226ca7a2d0444cd1c7638e5c6a8eedbe
    
    BUG=chromium:1015909
    TEST=USE="asan fuzzer" emerge-nami arc-obb-mounter; /usr/libexec/fuzzers/mount-obb_fuzzer
    
    Original change's description:
    > obb-mounter: Add fuzzer for mount_obb.
    >
    > This patch adds a fuzzer for the APIs in mount_obb.cc. In addition
    > to adding a fuzzer, this patch refactors the directory a bit to
    > split mount_obb.cc into two parts, APIs for library and a main
    > function for interpreting commandline, and put
    > the APIs in mount_obb_lib.cc into libmount_obb.
    >
    > BUG=chromium:1015909
    > TEST=USE="asan fuzzer" emerge-nami arc-obb-mounter; /usr/libexec/fuzzers/mount-obb_fuzzer
    >
    > Exempt-From-Owner-Approval: Submit for fuzzit.
    >
    > Change-Id: I3c8a9acdd3a844c6634e2619b614c8dedbe526ed
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1869709
    > Tested-by: Manoj Gupta <manojgupta@chromium.org>
    > Commit-Queue: Manoj Gupta <manojgupta@chromium.org>
    > Reviewed-by: Manoj Gupta <manojgupta@chromium.org>
    > Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    
    Bug: chromium:1015909
    Change-Id: Ifbba73979479ef54834a2ea6c9147e22c194e004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1878615
    Tested-by: Tiancong Wang <tcwang@google.com>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Tiancong Wang <tcwang@google.com>

--- arc/obb-mounter/mount_obb.cc
+++ arc/obb-mounter/mount_obb.cc
@@ -1,203 +1,12 @@
-// Copyright 2016 The Chromium OS Authors. All rights reserved.
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <fuse/fuse.h>
-#include <time.h>
+#include "arc/obb-mounter/mount_obb_fuse_main.h"
 
-#include <base/bind.h>
-#include <base/callback.h>
 #include <base/command_line.h>
-#include <base/files/file.h>
 #include <base/files/file_path.h>
-#include <base/logging.h>
-#include <base/optional.h>
-#include <base/strings/string_util.h>
-#include <base/strings/utf_string_conversions.h>
-#include <base/synchronization/lock.h>
 #include <brillo/syslog_logging.h>
-
-#include "arc/obb-mounter/volume.h"
-
-namespace {
-
-const mode_t kFileMode = S_IRUSR | S_IRGRP | S_IFREG;
-const mode_t kDirMode = S_IRUSR | S_IXUSR | S_IRGRP | S_IXGRP | S_IFDIR;
-
-fat::Volume* g_volume = nullptr;
-
-using DirectoryEntry = fat::Volume::DirectoryEntry;
-
-// Uses base::Lock to use FileReader in a thread-safe manner.
-class FileReaderThreadSafe {
- public:
-  FileReaderThreadSafe(fat::Volume* volume,
-                       int64_t start_cluster,
-                       int64_t file_size)
-      : reader_(volume, start_cluster, file_size) {}
-  ~FileReaderThreadSafe() {}
-
-  int64_t Read(char* buf, int64_t size, int64_t offset) {
-    base::AutoLock auto_lock(lock_);
-    return reader_.Read(buf, size, offset);
-  }
-
- private:
-  fat::Volume::FileReader reader_;
-  base::Lock lock_;
-
-  DISALLOW_COPY_AND_ASSIGN(FileReaderThreadSafe);
-};
-
-// Converts DirectoryEntry to stat.
-void ConvertDirectoryEntryToStat(const DirectoryEntry& entry,
-                                 struct stat* stat) {
-  if (entry.is_directory) {
-    stat->st_mode = kDirMode;
-    stat->st_nlink = 2;
-  } else {
-    stat->st_mode = kFileMode;
-    stat->st_nlink = 1;
-    stat->st_size = entry.file_size;
-  }
-  stat->st_mtime = entry.last_modification.ToBaseTime().ToTimeT();
-}
-
-// Gets a DirectoryEntry with the given path.
-bool GetDirectoryEntry(const base::StringPiece16& path, DirectoryEntry* out) {
-  if (path.empty() || path[0] != '/') {
-    return false;
-  }
-  int64_t current_directory_start_sector = g_volume->root_dir_start_sector();
-  size_t pos = 1;
-  while (true) {
-    size_t next_slash = path.find('/', pos);
-    if (next_slash == base::StringPiece::npos) {
-      next_slash = path.size();
-    }
-    base::StringPiece16 name(path.data() + pos, next_slash - pos);
-    base::Optional<DirectoryEntry> entry;
-    if (!g_volume->ReadDirectory(current_directory_start_sector,
-                                 base::BindRepeating(
-                                     [](const base::StringPiece16& name,
-                                        base::Optional<DirectoryEntry>* entry,
-                                        const base::StringPiece16& name_in,
-                                        const DirectoryEntry& entry_in) {
-                                       // TODO(hashimoto): Consider using
-                                       // base::i18n::ToLower to be
-                                       // case-insensitive for non-ASCII
-                                       // characters.
-                                       if (base::EqualsCaseInsensitiveASCII(
-                                               name, name_in)) {
-                                         *entry = entry_in;
-                                         return false;
-                                       }
-                                       return true;
-                                     },
-                                     name, &entry)) ||
-        !entry.has_value()) {
-      return false;
-    }
-    pos = next_slash + 1;
-    if (pos >= path.size()) {
-      *out = entry.value();
-      return true;
-    }
-    if (!entry->is_directory) {
-      return false;
-    }
-    current_directory_start_sector =
-        g_volume->GetClusterStartSector(entry->start_cluster);
-  }
-}
-
-int fat_getattr(const char* path, struct stat* stat) {
-  VLOG(1) << "fat_getattr: " << path;
-  if (strcmp(path, "/") == 0) {
-    stat->st_mode = kDirMode;
-    stat->st_nlink = 2;
-    return 0;
-  }
-  DirectoryEntry entry;
-  if (!GetDirectoryEntry(base::UTF8ToUTF16(path), &entry)) {
-    return -ENOENT;
-  }
-  ConvertDirectoryEntryToStat(entry, stat);
-  return 0;
-}
-
-int fat_open(const char* path, struct fuse_file_info* fi) {
-  VLOG(1) << "fat_open: " << path;
-  if ((fi->flags & O_ACCMODE) != O_RDONLY) {
-    return -EACCES;
-  }
-  DirectoryEntry entry;
-  if (!GetDirectoryEntry(base::UTF8ToUTF16(path), &entry)) {
-    return -ENOENT;
-  }
-  if (entry.is_directory) {
-    return -EISDIR;
-  }
-  fi->keep_cache = 1;
-  fi->fh = reinterpret_cast<uint64_t>(
-      new FileReaderThreadSafe(g_volume, entry.start_cluster, entry.file_size));
-  return 0;
-}
-
-int fat_read(const char* path,
-             char* buf,
-             size_t size,
-             off_t off,
-             struct fuse_file_info* fi) {
-  int64_t result =
-      reinterpret_cast<FileReaderThreadSafe*>(fi->fh)->Read(buf, size, off);
-  if (result < 0) {
-    return -EIO;
-  }
-  return result;
-}
-
-int fat_release(const char* path, struct fuse_file_info* fi) {
-  delete reinterpret_cast<FileReaderThreadSafe*>(fi->fh);
-  return 0;
-}
-
-int fat_readdir(const char* path,
-                void* buf,
-                fuse_fill_dir_t filler,
-                off_t offset,
-                struct fuse_file_info* fi) {
-  VLOG(1) << "fat_readdir: " << path;
-  filler(buf, ".", nullptr, 0);
-  filler(buf, "..", nullptr, 0);
-  int64_t start_sector = 0;
-  if (strcmp(path, "/") == 0) {
-    start_sector = g_volume->root_dir_start_sector();
-  } else {
-    DirectoryEntry entry;
-    if (!GetDirectoryEntry(base::UTF8ToUTF16(path), &entry)) {
-      return -ENOENT;
-    }
-    if (!entry.is_directory) {
-      return -ENOTDIR;
-    }
-    start_sector = g_volume->GetClusterStartSector(entry.start_cluster);
-  }
-  if (!g_volume->ReadDirectory(
-          start_sector,
-          base::BindRepeating(
-              [](fuse_fill_dir_t filler, void* buf,
-                 const base::StringPiece16& name, const DirectoryEntry& entry) {
-                filler(buf, base::UTF16ToUTF8(name).c_str(), nullptr, 0);
-                return true;
-              },
-              filler, buf))) {
-    return -EIO;
-  }
-  return 0;
-}
-
-}  // namespace
 
 int main(int argc, char** argv) {
   base::CommandLine::Init(argc, argv);
@@ -210,41 +19,12 @@ int main(int argc, char** argv) {
                << " obb_filename mount_path owner_uid owner_gid";
     return 1;
   }
-  const char* file_system_name = program.value().c_str();
-  const char* obb_filename = args[0].c_str();
-  const char* mount_path = args[1].c_str();
+  const std::string& file_system_name = program.value();
+  const std::string& obb_filename = args[0];
+  const std::string& mount_path = args[1];
   const std::string& owner_uid = args[2];
   const std::string& owner_gid = args[3];
 
-  base::File file(base::FilePath(obb_filename),
-                  base::File::FLAG_OPEN | base::File::FLAG_READ);
-  if (!file.IsValid()) {
-    LOG(ERROR) << "Failed to open: " << obb_filename;
-    return 1;
-  }
-
-  fat::Volume volume;
-  if (!volume.Initialize(std::move(file))) {
-    LOG(ERROR) << "Failed to initialize volume: " << obb_filename;
-    return 1;
-  }
-  g_volume = &volume;
-
-  const std::string mount_options =
-      std::string("allow_other,default_permissions,uid=") + owner_uid +
-      ",gid=" + owner_gid;
-  const char* fuse_argv[] = {
-      file_system_name, mount_path, "-f", "-o", mount_options.c_str(),
-  };
-  struct fuse_operations fat_ops = {};
-#define SET_FAT_OP(name) fat_ops.name = fat_##name
-  SET_FAT_OP(getattr);
-  SET_FAT_OP(open);
-  SET_FAT_OP(read);
-  SET_FAT_OP(release);
-  SET_FAT_OP(readdir);
-#undef SET_FAT_OP
-  fuse_main(arraysize(fuse_argv), const_cast<char**>(fuse_argv), &fat_ops,
-            nullptr);
-  return 0;
+  return mount_obb_fuse_main(file_system_name, obb_filename,
+                             mount_path, owner_uid, owner_gid);
 }
