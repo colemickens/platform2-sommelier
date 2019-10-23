84c792686d30194e8f0a012c05117a468b9e7dd3
Brian Geffon <bgeffon@chromium.org>
2019-10-23 12:33:23 -0700

    libchromeos-ui: increase limit on mlockable memory
    TEST=tested locally
    BUG=chromium:1017356
    
    Change-Id: I9def01babc6335becd9ec19f23b76719f86815de
    Signed-off-by: Brian Geffon <bgeffon@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1876516
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- libchromeos-ui/chromeos/ui/chromium_command_builder.cc
+++ libchromeos-ui/chromeos/ui/chromium_command_builder.cc
@@ -212,8 +212,8 @@ bool ChromiumCommandBuilder::SetUpChromium() {
 
   // Increase the limits of mlockable memory so that Chrome may mlock text
   // pages that have been copied into memory that can be backed by huge pages.
-  limit.rlim_cur = 64 * 1024 * 1024;
-  limit.rlim_max = 64 * 1024 * 1024;
+  limit.rlim_cur = 256 * 1024 * 1024;
+  limit.rlim_max = 256 * 1024 * 1024;
   if (setrlimit(RLIMIT_MEMLOCK, &limit) < 0)
     PLOG(ERROR) << "Setting memlock limit failed";
 
