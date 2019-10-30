a6317dab67c4e71cb940db76b4a523733b69396c
Qijiang Fan <fqj@chromium.org>
2019-10-30 13:00:45 +0900

    media_perception: use new method to create SharedMemoryHandle
    SharedMemoryHandle(FileDescriptor, size, guid) will be the new method to
    create a SharedMemoryHandle with a auto_close=True FileDescriptor.
    
    BUG=chromium:909719
    TEST=emerge-betty mri_package
    
    Change-Id: I529e45a2eb327e0ab4d97d981f00300ac4803b9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1888357
    Commit-Queue: Qijiang Fan <fqj@google.com>
    Tested-by: Qijiang Fan <fqj@google.com>
    Auto-Submit: Qijiang Fan <fqj@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>

--- media_perception/shared_memory_provider.cc
+++ media_perception/shared_memory_provider.cc
@@ -9,8 +9,10 @@ namespace mri {
 SharedMemoryProvider::SharedMemoryProvider(
     bool read_only, mojo::edk::ScopedPlatformHandle* scoped_handle,
     uint32_t memory_size_in_bytes) {
-  // TODO(fqj): migrate to new SharedMemoryHandle(fd, size, guid);
-  base::SharedMemoryHandle memory_handle(scoped_handle->release().handle, true);
+  base::SharedMemoryHandle memory_handle(
+      base::FileDescriptor(scoped_handle->release().handle,
+                           true /* auto_close */),
+      memory_size_in_bytes, base::UnguessableToken::Create());
   mapped_size_ = memory_size_in_bytes;
   shared_memory_.reset(new base::SharedMemory(memory_handle, read_only));
 }
