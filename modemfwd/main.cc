5285ed07f29e9b2a3f91ab2f22e8f31996b551ac
Eric Caruso <ejcaruso@chromium.org>
2019-11-06 15:25:16 -0800

    modemfwd: add debug_mode command-line flag
    This allows us to set debug mode on startup rather than
    setting it via D-Bus.
    
    BUG=b:120574149
    TEST=deploy, run from upstart with no DEBUG_MODE, DEBUG_MODE=false
      and verify debug mode is not on at startup, run with DEBUG_MODE=true
      and verify that it is, run with DEBUG_MODE=[garbage] and verify that
      modemfwd does not start
    
    Change-Id: I6b52c5cc4acf664fd78e8e86889ed03aedc59aac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1902467
    Tested-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Alex Khouderchah <akhouderchah@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Auto-Submit: Eric Caruso <ejcaruso@chromium.org>

--- modemfwd/main.cc
+++ modemfwd/main.cc
@@ -8,8 +8,10 @@
 #include <brillo/syslog_logging.h>
 
 #include "modemfwd/daemon.h"
+#include "modemfwd/logging.h"
 
 int main(int argc, char** argv) {
+  DEFINE_bool(debug_mode, false, "Turn on debug logging");
   DEFINE_string(journal_file, "/var/cache/modemfwd/journal",
                 "File to read the old journal from and write the new one to");
   DEFINE_string(helper_directory, "/opt/google/modemfwd-helpers",
@@ -17,6 +19,11 @@ int main(int argc, char** argv) {
   DEFINE_string(firmware_directory, "", "Directory to load firmware from");
   brillo::FlagHelper::Init(argc, argv, "Daemon which updates modem firmware.");
   brillo::InitLog(brillo::kLogToSyslog | brillo::kLogToStderrIfTty);
+
+  if (FLAGS_debug_mode) {
+    LOG(INFO) << "Debug mode is on";
+    modemfwd::g_extra_logging = true;
+  }
 
   if (FLAGS_journal_file.empty()) {
     LOG(ERROR) << "No journal file was supplied";
