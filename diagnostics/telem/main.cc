76dd72c23a6409ae178ee6122800df0de51526ef
Paul Moy <pmoy@chromium.org>
2019-11-12 13:21:20 -0700

    diagnostics: update telem testing tool
    Update telem to use CrosHealthdMojoAdapter instead
    of doing its own telemetry collection. This change
    makes telem capable of testing cros_healthd running
    inside of a sandbox.
    
    BUG=chromium:1023933
    TEST=emerge-grunt diagnostics
    
    Cq-Depend:chromium:1918462
    Change-Id: Idcf428d77c6e06c09255bb3ee36d59732dcc5ef8
    Signed-off-by: Paul Moy <pmoy@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1913142
    Reviewed-by: Kartik Hegde <khegde@chromium.org>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>
    Reviewed-by: Will Bradley <wbbradley@chromium.org>

--- diagnostics/telem/main.cc
+++ diagnostics/telem/main.cc
@@ -5,151 +5,95 @@
 #include <cstdlib>
 #include <iostream>
 #include <map>
-#include <memory>
 #include <sstream>
 #include <string>
+#include <utility>
 
-#include <base/files/file_path.h>
+#include <base/at_exit.h>
 #include <base/logging.h>
 #include <base/message_loop/message_loop.h>
 #include <base/optional.h>
 #include <base/strings/stringprintf.h>
-#include <base/time/time.h>
-#include <base/values.h>
 #include <brillo/flag_helper.h>
 #include <brillo/syslog_logging.h>
 
-#include "diagnostics/constants/grpc_constants.h"
-#include "diagnostics/telem/telemetry.h"
-#include "diagnostics/telem/telemetry_group_enum.h"
-#include "diagnostics/telem/telemetry_item_enum.h"
+#include "diagnostics/cros_healthd_mojo_adapter/cros_healthd_mojo_adapter.h"
+#include "mojo/cros_healthd_probe.mojom.h"
 
 namespace {
 
-void DisplayStringItem(const base::Value& string_item);
-void DisplayIntItem(const base::Value& int_item);
-void DisplayListItem(const base::Value& list_item);
-bool DisplayTelemetryItem(const base::Value& telem_item);
-bool DisplayOptionalTelemetryItem(
-    const std::string& item_name,
-    const base::Optional<base::Value>& telem_item);
-
-struct {
-  const char* switch_name;
-  diagnostics::TelemetryItemEnum telemetry_item;
-} kTelemetryItemSwitches[] = {
-    {"uptime", diagnostics::TelemetryItemEnum::kUptime},
-    {"memtotal", diagnostics::TelemetryItemEnum::kMemTotalMebibytes},
-    {"memfree", diagnostics::TelemetryItemEnum::kMemFreeMebibytes},
-    {"runnable_entities", diagnostics::TelemetryItemEnum::kNumRunnableEntities},
-    {"existing_entities", diagnostics::TelemetryItemEnum::kNumExistingEntities},
-    {"idle_time_total", diagnostics::TelemetryItemEnum::kTotalIdleTimeUserHz},
-    {"idle_time_per_cpu",
-     diagnostics::TelemetryItemEnum::kIdleTimePerCPUUserHz},
-    {"netstat", diagnostics::TelemetryItemEnum::kNetStat},
-    {"netdev", diagnostics::TelemetryItemEnum::kNetDev},
-    {"battery_cycle_count", diagnostics::TelemetryItemEnum::kBatteryCycleCount},
-    {"battery_voltage", diagnostics::TelemetryItemEnum::kBatteryVoltage},
-    {"battery_manufacturer",
-     diagnostics::TelemetryItemEnum::kBatteryManufacturer},
+constexpr std::pair<const char*,
+                    chromeos::cros_healthd::mojom::ProbeCategoryEnum>
+    kCategorySwitches[] = {
+        {"battery", chromeos::cros_healthd::mojom::ProbeCategoryEnum::kBattery},
+        {"storage", chromeos::cros_healthd::mojom::ProbeCategoryEnum::
+                        kNonRemovableBlockDevices},
+        {"cached_vpd",
+         chromeos::cros_healthd::mojom::ProbeCategoryEnum::kCachedVpdData},
 };
 
-struct {
-  const char* switch_name;
-  diagnostics::TelemetryGroupEnum telemetry_group;
-} kTelemetryGroupSwitches[] = {
-    {"memory", diagnostics::TelemetryGroupEnum::kMemory},
-};
+void DisplayBatteryInfo(
+    const chromeos::cros_healthd::mojom::BatteryInfoPtr& battery) {
+  DCHECK(!battery.is_null());
 
-// Helper function to display a base::Value object which has a string
-// representation.
-void DisplayStringItem(const base::Value& string_item) {
-  DCHECK(string_item.is_string());
+  printf(
+      "charge_full,charge_full_design,cycle_count,serial_number,"
+      "vendor(manufacturer),voltage_now,voltage_min_design,"
+      "manufacture_date_smart,temperature_smart,model_name,charge_now\n");
 
-  std::string val;
-  string_item.GetAsString(&val);
-  std::cout << val << std::endl;
+  printf("%f,%f,%ld,%s,%s,%f,%f,%ld,%ld,%s,%f\n", battery->charge_full,
+         battery->charge_full_design, battery->cycle_count,
+         battery->serial_number.c_str(), battery->vendor.c_str(),
+         battery->voltage_now, battery->voltage_min_design,
+         battery->manufacture_date_smart, battery->temperature_smart,
+         battery->model_name.c_str(), battery->charge_now);
 }
 
-// Helper function to display a base::Value object which has an integer
-// representation.
-void DisplayIntItem(const base::Value& int_item) {
-  DCHECK(int_item.is_int());
+void DisplayBlockDeviceInfo(
+    const std::vector<
+        chromeos::cros_healthd::mojom::NonRemovableBlockDeviceInfoPtr>&
+        block_devices) {
+  printf("path,size,type,manfid,name,serial\n");
 
-  int val;
-  int_item.GetAsInteger(&val);
-  std::cout << val << std::endl;
-}
-
-// Helper function to display a base::Value object which has a list
-// representation.
-void DisplayListItem(const base::Value& list_item) {
-  DCHECK(list_item.is_list());
-
-  const base::ListValue* list;
-  list_item.GetAsList(&list);
-  const base::Value* item;
-
-  // Print a newline so that the first list value starts on a new line.
-  std::cout << std::endl;
-
-  // Print each of the list values.
-  for (int i = 0; i < list->GetSize(); i++) {
-    list->Get(i, &item);
-    DisplayTelemetryItem(*item);
+  for (const auto& device : block_devices) {
+    printf("%s,%ld,%s,0x%x,%s,0x%x\n", device->path.c_str(), device->size,
+           device->type.c_str(), static_cast<int>(device->manufacturer_id),
+           device->name.c_str(), device->serial);
   }
 }
 
-// Helper function to display a base::Value object which has an arbitrary
-// representation.
-bool DisplayTelemetryItem(const base::Value& telem_item) {
-  if (telem_item.is_int()) {
-    DisplayIntItem(telem_item);
-  } else if (telem_item.is_string()) {
-    DisplayStringItem(telem_item);
-  } else if (telem_item.is_list()) {
-    DisplayListItem(telem_item);
-  } else {
-    LOG(ERROR) << "Invalid format for telemetry item.";
-    return false;
-  }
+void DisplayCachedVpdInfo(
+    const chromeos::cros_healthd::mojom::CachedVpdInfoPtr& vpd) {
+  DCHECK(!vpd.is_null());
 
-  return true;
+  printf("sku_number\n");
+
+  printf("%s\n", vpd->sku_number.c_str());
 }
 
-// Displays the telemetry item to the console. Returns
-// true iff the item was successfully displayed.
-bool DisplayOptionalTelemetryItem(
-    const std::string& item_name,
-    const base::Optional<base::Value>& telem_item) {
-  if (!telem_item) {
-    LOG(ERROR) << "No telemetry data found for " << item_name << ".";
-    return false;
-  }
+// Displays the retrieved telemetry information to the console.
+void DisplayTelemetryInfo(
+    const chromeos::cros_healthd::mojom::TelemetryInfoPtr& info) {
+  const auto& battery = info->battery_info;
+  if (!battery.is_null())
+    DisplayBatteryInfo(battery);
 
-  std::cout << item_name << ": ";
-  return DisplayTelemetryItem(telem_item.value());
+  const auto& block_devices = info->block_device_info;
+  if (block_devices)
+    DisplayBlockDeviceInfo(block_devices.value());
+
+  const auto& vpd = info->vpd_info;
+  if (!vpd.is_null())
+    DisplayCachedVpdInfo(vpd);
 }
 
-// Create a stringified list of the item names for use in help.
-std::string GetItemsAsString() {
+// Create a stringified list of the category names for use in help.
+std::string GetCategoryHelp() {
   std::stringstream ss;
-  ss << "[";
+  ss << "Category to probe: [";
   const char* sep = "";
-  for (auto telemetry_item_switch : kTelemetryItemSwitches) {
-    ss << sep << telemetry_item_switch.switch_name;
-    sep = ", ";
-  }
-  ss << "]";
-  return ss.str();
-}
-
-std::string GetGroupsAsString() {
-  std::stringstream ss;
-  ss << "[";
-  const char* sep = "";
-  for (auto telemetry_group_switch : kTelemetryGroupSwitches) {
-    ss << sep << telemetry_group_switch.switch_name;
+  for (auto pair : kCategorySwitches) {
+    ss << sep << pair.first;
     sep = ", ";
   }
   ss << "]";
@@ -160,71 +104,41 @@ std::string GetGroupsAsString() {
 
 // 'telem' command-line tool:
 //
-// Test driver for libtelem. Only supports requesting individual telemetry
-// items. This program does not exercise the caching functionality of libtelem.
+// Test driver for cros_healthd's telemetry collection. Supports requesting a
+// single category at a time.
 int main(int argc, char** argv) {
-  std::string item_help = base::StringPrintf("Telemetry item to display. %s",
-                                             GetItemsAsString().c_str());
-  std::string group_help = base::StringPrintf(
-      "Group of telemetry items to display. %s", GetGroupsAsString().c_str());
-  DEFINE_string(item, "", item_help.c_str());
-  DEFINE_string(group, "", group_help.c_str());
+  std::string category_help = GetCategoryHelp();
+  DEFINE_string(category, "", category_help.c_str());
   brillo::FlagHelper::Init(argc, argv, "telem - Device telemetry tool.");
   brillo::InitLog(brillo::kLogToSyslog | brillo::kLogToStderrIfTty);
 
-  std::map<std::string, diagnostics::TelemetryItemEnum> switch_to_item;
-  std::map<diagnostics::TelemetryItemEnum, std::string> item_to_switch;
-  for (const auto& item : kTelemetryItemSwitches) {
-    switch_to_item[item.switch_name] = item.telemetry_item;
-    item_to_switch[item.telemetry_item] = item.switch_name;
-  }
+  base::AtExitManager at_exit_manager;
 
-  std::map<std::string, diagnostics::TelemetryGroupEnum> group_map;
-  for (const auto& group : kTelemetryGroupSwitches) {
-    group_map[group.switch_name] = group.telemetry_group;
-  }
+  std::map<std::string, chromeos::cros_healthd::mojom::ProbeCategoryEnum>
+      switch_to_category(std::begin(kCategorySwitches),
+                         std::end(kCategorySwitches));
 
   logging::InitLogging(logging::LoggingSettings());
 
   base::MessageLoopForIO message_loop;
 
-  diagnostics::Telemetry telemetry;
-
-  // Make sure at least one item or group is specified.
-  if (FLAGS_item == "" && FLAGS_group == "") {
-    LOG(ERROR) << "No item or group specified.";
+  // Make sure at least one category is specified.
+  if (FLAGS_category == "") {
+    LOG(ERROR) << "No category specified.";
     return EXIT_FAILURE;
   }
-  // Validate the item flag.
-  if (FLAGS_item != "") {
-    if (!switch_to_item.count(FLAGS_item)) {
-      LOG(ERROR) << "Invalid item: " << FLAGS_item;
-      return EXIT_FAILURE;
-    }
-
-    // Retrieve and display the telemetry item.
-    const base::Optional<base::Value> telem_item = telemetry.GetItem(
-        switch_to_item.at(FLAGS_item), base::TimeDelta::FromSeconds(0));
-    if (!DisplayOptionalTelemetryItem(FLAGS_item, telem_item))
-      return EXIT_FAILURE;
+  // Validate the category flag.
+  auto iterator = switch_to_category.find(FLAGS_category);
+  if (iterator == switch_to_category.end()) {
+    LOG(ERROR) << "Invalid category: " << FLAGS_category;
+    return EXIT_FAILURE;
   }
-  // Validate the group flag.
-  if (FLAGS_group != "") {
-    if (!group_map.count(FLAGS_group)) {
-      LOG(ERROR) << "Invalid group: " << FLAGS_group;
-      return EXIT_FAILURE;
-    }
 
-    // Retrieve and display the telemetry group.
-    const std::vector<
-        std::pair<diagnostics::TelemetryItemEnum, base::Optional<base::Value>>>
-        telem_items = telemetry.GetGroup(group_map.at(FLAGS_group),
-                                         base::TimeDelta::FromSeconds(0));
-    for (auto item_pair : telem_items) {
-      if (!DisplayOptionalTelemetryItem(item_to_switch.at(item_pair.first),
-                                        item_pair.second))
-        return EXIT_FAILURE;
-    }
-  }
+  // Probe and display the category.
+  diagnostics::CrosHealthdMojoAdapter adapter;
+  const std::vector<chromeos::cros_healthd::mojom::ProbeCategoryEnum>
+      categories_to_probe = {iterator->second};
+  DisplayTelemetryInfo(adapter.GetTelemetryInfo(categories_to_probe));
+
   return EXIT_SUCCESS;
 }
