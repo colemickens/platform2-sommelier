b73d9eec6c991c94cfa3bba4bf35db255986b3ce
Paul Moy <pmoy@chromium.org>
2019-11-18 12:28:47 -0700

    diagnostics: remove testing flags from cros_healthd
    Now that telem is capable of testing cros_healthd, we
    no longer need the --probe_x command-line flags.
    They only existed to test the various probe functions,
    but they ran outside of a sandbox as root, so the
    sandbox was never tested. telem fixes this issue
    and provides the same interface that the flags used to.
    
    BUG=chromium:980580
    TEST=build and deploy diagnostics package, run
    telem --category=storage, telem --category=battery
    and telem --category=cached_vpd
    
    Change-Id: I15b1916cc8762c14551478627ba1bf65bbe4a79d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1918352
    Reviewed-by: Kartik Hegde <khegde@chromium.org>
    Commit-Queue: Paul Moy <pmoy@chromium.org>
    Tested-by: Paul Moy <pmoy@chromium.org>

--- diagnostics/cros_healthd/main.cc
+++ diagnostics/cros_healthd/main.cc
@@ -17,13 +17,6 @@
 #include "diagnostics/cros_healthd/utils/disk_utils.h"
 
 int main(int argc, char** argv) {
-  DEFINE_bool(probe_block_devices, false,
-              "Exercise the ProbeNonRemovableBlockDeviceInfo routine");
-  DEFINE_bool(probe_battery_metrics, false,
-              "Exercise the ProbeBatteryInfo routine");
-  DEFINE_bool(probe_cached_vpd, false,
-              "Exercise the ProbeCachedVPDInfo routine");
-
   brillo::FlagHelper::Init(
       argc, argv, "cros_healthd - Device telemetry and diagnostics daemon.");
 
@@ -37,49 +30,8 @@ int main(int argc, char** argv) {
     LOG(ERROR) << "cros_healthd: Failed to connect to system D-bus bus";
     return EXIT_FAILURE;
   }
-  auto proxy = std::make_unique<org::chromium::debugdProxy>(bus);
 
-  if (FLAGS_probe_block_devices) {
-    base::FilePath root_dir{"/"};
-    auto devices = diagnostics::FetchNonRemovableBlockDevicesInfo(root_dir);
-    VLOG(1) << "Found " << devices.size() << " non-removable block device(s)."
-            << std::endl;
-    printf("path,size,type,manfid,name,serial\n");
-    for (const auto& device : devices) {
-      printf("%s,%ld,%s,0x%x,%s,0x%x\n", device->path.c_str(), device->size,
-             device->type.c_str(), static_cast<int>(device->manufacturer_id),
-             device->name.c_str(), device->serial);
-    }
-  } else if (FLAGS_probe_battery_metrics) {
-    diagnostics::BatteryFetcher battery_fetcher(proxy.get());
-    auto batteries = battery_fetcher.FetchBatteryInfo();
-    printf(
-        "charge_full,charge_full_design,cycle_count,serial_number,"
-        "vendor(manufacturer),voltage_now,voltage_min_design,"
-        "manufacture_date_smart,temperature_smart,model_name,charge_now\n");
-    if (batteries.size() == 0) {
-      VLOG(1) << "No main battery information found.";
-    } else if (batteries.size() == 1) {
-      VLOG(1) << "Found information for main battery.";
-      const auto& battery = batteries[0];
-      printf("%f,%f,%ld,%s,%s,%f,%f,%ld,%ld,%s,%f\n", battery->charge_full,
-             battery->charge_full_design, battery->cycle_count,
-             battery->serial_number.c_str(), battery->vendor.c_str(),
-             battery->voltage_now, battery->voltage_min_design,
-             battery->manufacture_date_smart, battery->temperature_smart,
-             battery->model_name.c_str(), battery->charge_now);
-    }
-  } else if (FLAGS_probe_cached_vpd) {
-    auto vpd_info = diagnostics::FetchCachedVpdInfo(base::FilePath("/"));
-    std::string sku_number = vpd_info->sku_number;
-    if (sku_number == "") {
-      LOG(ERROR) << "Unable to read sku_number.";
-      return EXIT_FAILURE;
-    }
-    printf("sku_number: %s\n", sku_number.c_str());
-  } else {
-    diagnostics::CrosHealthd daemon(std::move(proxy));
-    return daemon.Run();
-  }
-  return EXIT_SUCCESS;
+  auto proxy = std::make_unique<org::chromium::debugdProxy>(bus);
+  diagnostics::CrosHealthd daemon(std::move(proxy));
+  return daemon.Run();
 }
