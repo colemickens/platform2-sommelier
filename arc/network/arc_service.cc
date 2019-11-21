colemickens/platform2-sommelier

commit ce7caa67f727f30bc650080ea6a7232c5cbf1103

arc: network: Connect NDProxy with DeviceManager
This change connects NDProxy with DeviceManager, so that proxy for
certain interface pairs would be turned on/off upon device enable/
disable. A global flag kFindIpv6Routes is added to switch between
legacy method and ndproxy for ipv6 address configuration.

Note that b:141900884 needs also be addressed before configuration
through NDProxy is full functional and can be turned on.

BUG=b:132574450
TEST=unit, manual

Change-Id: I2a1589651c220ea69f913eb49b4b2dc1736ebf26
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1833190
Tested-by: Taoyu Li <taoyl@chromium.org>
Auto-Submit: Taoyu Li <taoyl@chromium.org>
Commit-Queue: Taoyu Li <taoyl@chromium.org>
Reviewed-by: Hugo Benichi <hugobenichi@google.com>

arc/network/arc_service.cc
@@ -395,8 +395,8 @@ void ArcService::OnDefaultInterfaceChanged(const std::string& ifname) {
     LOG(ERROR) << "Expected default device missing: " << ifname;
     return;
   }
-  device->StopIPv6Routing();
-  device->StartIPv6Routing(ifname);
+  device->StopIPv6RoutingLegacy();
+  device->StartIPv6RoutingLegacy(ifname);
 }
 
 void ArcService::LinkMsgHandler(const shill::RTNLMessage& msg) {
@@ -523,15 +523,6 @@ void ArcService::SetupIPv6(Device* device) {
     return;
   }
 
-  if (!datapath_->AddIPv6Forwarding(ipv6_config.ifname,
-                                    device->config().host_ifname())) {
-    LOG(ERROR) << "Failed to setup iptables for IPv6";
-    datapath_->RemoveIPv6Neighbor(ipv6_config.ifname, addr);
-    datapath_->RemoveIPv6HostRoute(config.host_ifname(), addr,
-                                   ipv6_config.prefix_len);
-    return;
-  }
-
   ctx->SetHasIPv6(table_id);
 }
 
@@ -559,7 +550,6 @@ void ArcService::TeardownIPv6(Device* device) {
   std::string router = buf;
 
   const auto& config = device->config();
-  datapath_->RemoveIPv6Forwarding(ipv6_config.ifname, config.host_ifname());
   datapath_->RemoveIPv6Neighbor(ipv6_config.ifname, addr);
   datapath_->RemoveIPv6HostRoute(config.host_ifname(), addr,
                                  ipv6_config.prefix_len);

arc/network/device_manager.cc
@@ -23,6 +23,11 @@ constexpr const char kVpnInterfaceGuestPrefix[] = "cros_";
 constexpr std::array<const char*, 2> kEthernetInterfacePrefixes{{"eth", "usb"}};
 constexpr std::array<const char*, 2> kWifiInterfacePrefixes{{"wlan", "mlan"}};
 
+// Global compile time switch for the method configuring Ipv6 address for ARC.
+// When set to true, arc-networkd will try to generate an address and set onto
+// ARC interface (legacy method); when set to false, NDProxy is enabled.
+constexpr const bool kFindIpv6RoutesLegacy = true;
+
 bool IsArcDevice(const std::string& ifname) {
   return base::StartsWith(ifname, kArcDevicePrefix,
                           base::CompareCase::INSENSITIVE_ASCII);
@@ -85,12 +90,18 @@ bool IsMulticastInterface(const std::string& ifname) {
 
 DeviceManager::DeviceManager(std::unique_ptr<ShillClient> shill_client,
                              AddressManager* addr_mgr,
-                             bool is_arc_legacy)
+                             Datapath* datapath,
+                             bool is_arc_legacy,
+                             HelperProcess* nd_proxy)
     : shill_client_(std::move(shill_client)),
       addr_mgr_(addr_mgr),
+      datapath_(datapath),
+      nd_proxy_(nd_proxy),
       is_arc_legacy_(is_arc_legacy) {
   DCHECK(shill_client_);
   DCHECK(addr_mgr_);
+  DCHECK(datapath_);
+
   link_listener_ = std::make_unique<shill::RTNLListener>(
       shill::RTNLHandler::kRequestLink,
       Bind(&DeviceManager::LinkMsgHandler, weak_factory_.GetWeakPtr()));
@@ -156,9 +167,18 @@ bool DeviceManager::Add(const std::string& name) {
 
   LOG(INFO) << "Adding device " << *device;
 
-  device->RegisterIPv6SetupHandler(base::Bind(
-      &DeviceManager::OnIPv6AddressFound, weak_factory_.GetWeakPtr()));
-
+  if (device->options().ipv6_enabled) {
+    if (!datapath_->AddIPv6Forwarding(device->ifname(),
+                                      device->config().host_ifname())) {
+      LOG(ERROR) << "Failed to setup iptables forwarding rule for IPv6 from "
+                 << device->ifname() << " to "
+                 << device->config().host_ifname();
+    }
+    if (device->options().find_ipv6_routes_legacy) {
+      device->RegisterIPv6SetupHandler(base::Bind(
+          &DeviceManager::OnIPv6AddressFound, weak_factory_.GetWeakPtr()));
+    }
+  }
   for (auto& h : add_handlers_) {
     h.Run(device.get());
   }
@@ -173,6 +193,11 @@ bool DeviceManager::Remove(const std::string& name) {
     return false;
 
   LOG(INFO) << "Removing device " << name;
+
+  if (it->second->options().ipv6_enabled) {
+    datapath_->RemoveIPv6Forwarding(it->second->ifname(),
+                                    it->second->config().host_ifname());
+  }
 
   for (auto& h : rm_handlers_) {
     h.Run(it->second.get());
@@ -231,8 +256,18 @@ void DeviceManager::LinkMsgHandler(const shill::RTNLMessage& msg) {
   if (!link_up) {
     LOG(INFO) << ifname << " is now down";
     device->Disable();
+    if (nd_proxy_ && device->options().ipv6_enabled &&
+        !device->options().find_ipv6_routes_legacy) {
+      DeviceMessage msg;
+      msg.set_dev_ifname(device->ifname());
+      msg.set_teardown(true);
+      IpHelperMessage ipm;
+      *ipm.mutable_device_message() = msg;
+      nd_proxy_->SendMessage(ipm);
+    }
     return;
   }
+
   // The link is now up.
   LOG(INFO) << ifname << " is now up";
 
@@ -240,6 +275,16 @@ void DeviceManager::LinkMsgHandler(const shill::RTNLMessage& msg) {
     device->Enable(default_ifname_);
   else if (!device->IsAndroid())
     device->Enable(device->config().guest_ifname());
+
+  if (nd_proxy_ != nullptr && device->options().ipv6_enabled &&
+      !device->options().find_ipv6_routes_legacy) {
+    DeviceMessage msg;
+    msg.set_dev_ifname(device->ifname());
+    msg.set_br_ifname(device->config().host_ifname());
+    IpHelperMessage ipm;
+    *ipm.mutable_device_message() = msg;
+    nd_proxy_->SendMessage(ipm);
+  }
 }
 
 std::unique_ptr<Device> DeviceManager::MakeDevice(
@@ -253,7 +298,8 @@ std::unique_ptr<Device> DeviceManager::MakeDevice(
   if (name == kAndroidLegacyDevice) {
     host_ifname = "arcbr0";
     guest_ifname = "arc0";
-    opts.find_ipv6_routes = true;
+    opts.ipv6_enabled = true;
+    opts.find_ipv6_routes_legacy = kFindIpv6RoutesLegacy;
     opts.fwd_multicast = true;
   } else {
     if (name == kAndroidDevice) {
@@ -274,10 +320,11 @@ std::unique_ptr<Device> DeviceManager::MakeDevice(
     if (IsHostVpnInterface(guest_ifname)) {
       guest_ifname = kVpnInterfaceGuestPrefix + guest_ifname;
     }
-    // TODO(crbug/726815) Also enable |find_ipv6_routes| for cellular networks
+    // TODO(crbug/726815) Also enable |ipv6_enabled| for cellular networks
     // once IPv6 is enabled on cellular networks in shill.
-    opts.find_ipv6_routes =
+    opts.ipv6_enabled =
         IsEthernetInterface(guest_ifname) || IsWifiInterface(guest_ifname);
+    opts.find_ipv6_routes_legacy = kFindIpv6RoutesLegacy;
   }
 
   auto ipv4_subnet = addr_mgr_->AllocateIPv4Subnet(guest);

arc/network/device.cc
@@ -153,14 +153,15 @@ void Device::Enable(const std::string& ifname) {
     }
   }
 
-  StartIPv6Routing(ifname);
+  if (options_.ipv6_enabled && options_.find_ipv6_routes_legacy)
+    StartIPv6RoutingLegacy(ifname);
 }
 
-void Device::StartIPv6Routing(const std::string& ifname) {
+void Device::StartIPv6RoutingLegacy(const std::string& ifname) {
   if (!IsFullyUp())
     return;
 
-  if (!options_.find_ipv6_routes || router_finder_)
+  if (router_finder_)
     return;
 
   LOG(INFO) << "Starting IPV6 route finding for device " << ifname_
@@ -183,10 +184,11 @@ void Device::Disable() {
     LOG(INFO) << "Disabling mDNS forwarding for device " << ifname_;
     mdns_forwarder_.reset();
   }
-  StopIPv6Routing();
+  if (options_.ipv6_enabled && options_.find_ipv6_routes_legacy)
+    StopIPv6RoutingLegacy();
 }
 
-void Device::StopIPv6Routing() {
+void Device::StopIPv6RoutingLegacy() {
   if (neighbor_finder_ || router_finder_) {
     LOG(INFO) << "Disabling IPv6 route finding for device " << ifname_;
     neighbor_finder_.reset();
@@ -277,7 +279,9 @@ std::ostream& operator<<(std::ostream& stream, const Device& device) {
          << ", guest_mac_addr: "
          << MacAddressToString(device.config_->guest_mac_addr())
          << ", fwd_multicast: " << device.options_.fwd_multicast
-         << ", find_ipv6_routes: " << device.options_.find_ipv6_routes << '}';
+         << ", ipv6_enabled: " << device.options_.ipv6_enabled
+         << ", find_ipv6_routes: " << device.options_.find_ipv6_routes_legacy
+         << '}';
   return stream;
 }
 

arc/network/device.h
@@ -10,6 +10,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <map>
 #include <memory>
 #include <string>
 
@@ -83,7 +84,8 @@ class Device {
 
   struct Options {
     bool fwd_multicast;
-    bool find_ipv6_routes;
+    bool ipv6_enabled;
+    bool find_ipv6_routes_legacy;
   };
 
   struct IPv6Config {
@@ -123,8 +125,8 @@ class Device {
   void Enable(const std::string& ifname);
   void Disable();
 
-  void StartIPv6Routing(const std::string& ifname);
-  void StopIPv6Routing();
+  void StartIPv6RoutingLegacy(const std::string& ifname);
+  void StopIPv6RoutingLegacy();
 
   void OnGuestStart(GuestMessage::GuestType guest);
   void OnGuestStop(GuestMessage::GuestType guest);

arc/network/device_manager.h
@@ -15,7 +15,9 @@
 #include <shill/net/rtnl_listener.h>
 
 #include "arc/network/address_manager.h"
+#include "arc/network/datapath.h"
 #include "arc/network/device.h"
+#include "arc/network/helper_process.h"
 #include "arc/network/ipc.pb.h"
 #include "arc/network/shill_client.h"
 
@@ -62,10 +64,12 @@ class DeviceManagerBase {
 // Virtual methods for testing.
 class DeviceManager : public DeviceManagerBase {
  public:
-  // |addr_mgr| must not be null.
+  // |addr_mgr| and |datapath| must not be null.
   DeviceManager(std::unique_ptr<ShillClient> shill_client,
                 AddressManager* addr_mgr,
-                bool is_arc_legacy);
+                Datapath* datapath,
+                bool is_arc_legacy,
+                HelperProcess* nd_proxy = nullptr);
   virtual ~DeviceManager();
 
   // Used by guest service implementations to be notified when tracked devices
@@ -138,6 +142,9 @@ class DeviceManager : public DeviceManagerBase {
   // Connected devices keyed by the interface name.
   // The legacy device is mapped to the Android interface name.
   std::map<std::string, std::unique_ptr<Device>> devices_;
+
+  Datapath* datapath_;
+  HelperProcess* nd_proxy_;
 
   const bool is_arc_legacy_;
   std::string default_ifname_;

arc/network/device_manager_test.cc
@@ -16,7 +16,9 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include "arc/network/fake_process_runner.h"
 #include "arc/network/fake_shill_client.h"
+#include "arc/network/mock_datapath.h"
 #include "arc/network/net_util.h"
 
 namespace arc_networkd {
@@ -41,14 +43,19 @@ class DeviceManagerTest : public testing::Test {
  protected:
   DeviceManagerTest() = default;
 
-  void SetUp() override {}
+  void SetUp() override {
+    fpr_ = std::make_unique<FakeProcessRunner>();
+    fpr_->Capture(false);
+    datapath_ = std::make_unique<MockDatapath>(fpr_.get());
+  }
 
   std::unique_ptr<DeviceManager> NewManager(bool is_arc_legacy = false) {
     shill_helper_ = std::make_unique<FakeShillClientHelper>();
     auto shill_client = shill_helper_->FakeClient();
     shill_client_ = shill_client.get();
-    auto mgr = std::make_unique<DeviceManager>(std::move(shill_client),
-                                               &addr_mgr_, is_arc_legacy);
+
+    auto mgr = std::make_unique<DeviceManager>(
+        std::move(shill_client), &addr_mgr_, datapath_.get(), is_arc_legacy);
     return mgr;
   }
 
@@ -57,6 +64,9 @@ class DeviceManagerTest : public testing::Test {
  private:
   std::unique_ptr<FakeShillClientHelper> shill_helper_;
   FakeAddressManager addr_mgr_;
+
+  std::unique_ptr<MockDatapath> datapath_;
+  std::unique_ptr<FakeProcessRunner> fpr_;
 
   // This is required because of the RT netlink handler.
   base::MessageLoopForIO msg_loop_;
@@ -73,7 +83,7 @@ TEST_F(DeviceManagerTest, MakeEthernetDevices) {
   EXPECT_EQ(cfg.guest_ifname(), "eth0");
   EXPECT_EQ(cfg.host_ipv4_addr(), Ipv4Addr(100, 115, 92, 9));
   EXPECT_EQ(cfg.guest_ipv4_addr(), Ipv4Addr(100, 115, 92, 10));
-  EXPECT_TRUE(eth0->options().find_ipv6_routes);
+  EXPECT_TRUE(eth0->options().ipv6_enabled);
 
   auto usb0 = mgr->MakeDevice("usb0");
   const auto& cfgu = usb0->config();
@@ -81,7 +91,7 @@ TEST_F(DeviceManagerTest, MakeEthernetDevices) {
   EXPECT_EQ(cfgu.guest_ifname(), "usb0");
   EXPECT_EQ(cfgu.host_ipv4_addr(), Ipv4Addr(100, 115, 92, 13));
   EXPECT_EQ(cfgu.guest_ipv4_addr(), Ipv4Addr(100, 115, 92, 14));
-  EXPECT_TRUE(usb0->options().find_ipv6_routes);
+  EXPECT_TRUE(usb0->options().ipv6_enabled);
 }
 
 TEST_F(DeviceManagerTest, MakeWifiDevices) {
@@ -92,7 +102,7 @@ TEST_F(DeviceManagerTest, MakeWifiDevices) {
   EXPECT_EQ(cfg.guest_ifname(), "wlan0");
   EXPECT_EQ(cfg.host_ipv4_addr(), Ipv4Addr(100, 115, 92, 9));
   EXPECT_EQ(cfg.guest_ipv4_addr(), Ipv4Addr(100, 115, 92, 10));
-  EXPECT_TRUE(wlan0->options().find_ipv6_routes);
+  EXPECT_TRUE(wlan0->options().ipv6_enabled);
 
   auto mlan0 = mgr->MakeDevice("mlan0");
   const auto& cfgm = mlan0->config();
@@ -100,7 +110,7 @@ TEST_F(DeviceManagerTest, MakeWifiDevices) {
   EXPECT_EQ(cfgm.guest_ifname(), "mlan0");
   EXPECT_EQ(cfgm.host_ipv4_addr(), Ipv4Addr(100, 115, 92, 13));
   EXPECT_EQ(cfgm.guest_ipv4_addr(), Ipv4Addr(100, 115, 92, 14));
-  EXPECT_TRUE(mlan0->options().find_ipv6_routes);
+  EXPECT_TRUE(mlan0->options().ipv6_enabled);
 }
 
 TEST_F(DeviceManagerTest, MakeCellularDevice) {
@@ -111,7 +121,7 @@ TEST_F(DeviceManagerTest, MakeCellularDevice) {
   EXPECT_EQ(cfg.guest_ifname(), "wwan0");
   EXPECT_EQ(cfg.host_ipv4_addr(), Ipv4Addr(100, 115, 92, 9));
   EXPECT_EQ(cfg.guest_ipv4_addr(), Ipv4Addr(100, 115, 92, 10));
-  EXPECT_FALSE(wwan0->options().find_ipv6_routes);
+  EXPECT_FALSE(wwan0->options().ipv6_enabled);
 }
 
 TEST_F(DeviceManagerTest, MakeDevice_Android) {
@@ -122,7 +132,7 @@ TEST_F(DeviceManagerTest, MakeDevice_Android) {
   EXPECT_EQ(cfg.guest_ifname(), "arc0");
   EXPECT_EQ(cfg.host_ipv4_addr(), Ipv4Addr(100, 115, 92, 1));
   EXPECT_EQ(cfg.guest_ipv4_addr(), Ipv4Addr(100, 115, 92, 2));
-  EXPECT_FALSE(arc0->options().find_ipv6_routes);
+  EXPECT_FALSE(arc0->options().ipv6_enabled);
 }
 
 TEST_F(DeviceManagerTest, MakeDevice_LegacyAndroid) {
@@ -133,7 +143,7 @@ TEST_F(DeviceManagerTest, MakeDevice_LegacyAndroid) {
   EXPECT_EQ(cfg.guest_ifname(), "arc0");
   EXPECT_EQ(cfg.host_ipv4_addr(), Ipv4Addr(100, 115, 92, 1));
   EXPECT_EQ(cfg.guest_ipv4_addr(), Ipv4Addr(100, 115, 92, 2));
-  EXPECT_TRUE(arc0->options().find_ipv6_routes);
+  EXPECT_TRUE(arc0->options().ipv6_enabled);
 }
 
 TEST_F(DeviceManagerTest, MakeVpnTunDevice) {
@@ -144,7 +154,7 @@ TEST_F(DeviceManagerTest, MakeVpnTunDevice) {
   EXPECT_EQ(cfg.guest_ifname(), "cros_tun0");
   EXPECT_EQ(cfg.host_ipv4_addr(), Ipv4Addr(100, 115, 92, 9));
   EXPECT_EQ(cfg.guest_ipv4_addr(), Ipv4Addr(100, 115, 92, 10));
-  EXPECT_FALSE(tun0->options().find_ipv6_routes);
+  EXPECT_FALSE(tun0->options().ipv6_enabled);
 }
 
 TEST_F(DeviceManagerTest, MakeDevice_NoMoreSubnets) {

arc/network/device_test.cc
@@ -26,9 +26,10 @@ class DeviceTest : public testing::Test {
  protected:
   void SetUp() override { ipv6_down_ = false; }
 
-  std::unique_ptr<Device> NewDevice(
-      const std::string& name,
-      const Device::Options& options = Device::Options()) {
+  std::unique_ptr<Device> NewDevice(const std::string& name,
+                                    const Device::Options& options = {
+                                        .ipv6_enabled = true,
+                                        .find_ipv6_routes_legacy = true}) {
     auto ipv4_subnet = std::make_unique<Subnet>(Ipv4Addr(100, 100, 100, 100),
                                                 30, base::Bind(&DoNothing));
     EXPECT_TRUE(ipv4_subnet);

arc/network/main.cc
@@ -8,8 +8,8 @@
 #include <memory>
 #include <utility>
 
-#include <base/files/scoped_file.h>
 #include <base/files/file_util.h>
+#include <base/files/scoped_file.h>
 #include <base/logging.h>
 #include <base/strings/string_number_conversions.h>
 #include <brillo/flag_helper.h>
@@ -102,6 +102,7 @@ int main(int argc, char* argv[]) {
       (FLAGS_force_multinet == "on") ||
       ((FLAGS_force_multinet != "off") && ShouldEnableMultinet());
   LOG(INFO) << "Starting arc-networkd manager";
-  arc_networkd::Manager manager(std::move(adb_proxy), enable_mnet);
+  arc_networkd::Manager manager(std::move(adb_proxy), std::move(nd_proxy),
+                                enable_mnet);
   return manager.Run();
 }

commit 2980ee9f35c978be246a7dded66110333015a10a

arc: network: Fix a regression creating wrong ip6tables rule in ARC N
Refactor in chromium.googlesource.com/chromiumos/platform2/+/ce7caa6
introduced a bug that ip6tables rules was creately wrongly in ARC N.
This leads to ARC losing IPv6 connectivity.

This patch restores the original code path for ARC N, therefore fixes
the issue.

BUG=b:144501984
TEST=flashed onto samus (ARC N) / atlas (ARC P) and verified IPv6
connectivity in ARC; verified behavior upon log out / log in and
unplugging / reppluggin.

Change-Id: Ibd19172401f3e9aecb7577dca1149dc481f33378
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1919296
Tested-by: Taoyu Li <taoyl@chromium.org>
Commit-Queue: Taoyu Li <taoyl@chromium.org>
Reviewed-by: Garrick Evans <garrick@chromium.org>
Auto-Submit: Taoyu Li <taoyl@chromium.org>

arc/network/arc_service.cc
@@ -662,6 +662,15 @@ void ArcService::ContainerImpl::SetupIPv6(Device* device) {
     return;
   }
 
+  if (!datapath_->AddIPv6Forwarding(ipv6_config.ifname,
+                                    device->config().host_ifname())) {
+    LOG(ERROR) << "Failed to setup iptables for IPv6";
+    datapath_->RemoveIPv6Neighbor(ipv6_config.ifname, addr);
+    datapath_->RemoveIPv6HostRoute(config.host_ifname(), addr,
+                                   ipv6_config.prefix_len);
+    return;
+  }
+
   ctx->SetHasIPv6(table_id);
 }
 
@@ -689,6 +698,7 @@ void ArcService::ContainerImpl::TeardownIPv6(Device* device) {
   std::string router = buf;
 
   const auto& config = device->config();
+  datapath_->RemoveIPv6Forwarding(ipv6_config.ifname, config.host_ifname());
   datapath_->RemoveIPv6Neighbor(ipv6_config.ifname, addr);
   datapath_->RemoveIPv6HostRoute(config.host_ifname(), addr,
                                  ipv6_config.prefix_len);

arc/network/device_manager.cc
@@ -188,15 +188,16 @@ bool DeviceManager::Add(const std::string& name) {
   LOG(INFO) << "Adding device " << *device;
 
   if (device->options().ipv6_enabled) {
-    if (!datapath_->AddIPv6Forwarding(device->ifname(),
-                                      device->config().host_ifname())) {
-      LOG(ERROR) << "Failed to setup iptables forwarding rule for IPv6 from "
-                 << device->ifname() << " to "
-                 << device->config().host_ifname();
-    }
     if (device->options().find_ipv6_routes_legacy) {
       device->RegisterIPv6SetupHandler(base::Bind(
           &DeviceManager::OnIPv6AddressFound, weak_factory_.GetWeakPtr()));
+    } else {
+      if (!datapath_->AddIPv6Forwarding(device->ifname(),
+                                        device->config().host_ifname())) {
+        LOG(ERROR) << "Failed to setup iptables forwarding rule for IPv6 from "
+                   << device->ifname() << " to "
+                   << device->config().host_ifname();
+      }
     }
   }
   for (auto& h : add_handlers_) {
@@ -214,7 +215,8 @@ bool DeviceManager::Remove(const std::string& name) {
 
   LOG(INFO) << "Removing device " << name;
 
-  if (it->second->options().ipv6_enabled) {
+  if (it->second->options().ipv6_enabled &&
+      !it->second->options().find_ipv6_routes_legacy) {
     datapath_->RemoveIPv6Forwarding(it->second->ifname(),
                                     it->second->config().host_ifname());
   }