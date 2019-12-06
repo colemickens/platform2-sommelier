1615e5d5b09585f56e763693757bdc08bfb77055
Leo Lai <cylai@google.com>
2019-12-06 10:39:21 +0800

    attestation: change licenses terms
    BUG=chromium:887651
    BUG=b:145775570
    TEST=FEATURES=test emerge-soraka attestation
    TEST="git grep -l Android" returns none
    
    Change-Id: I5dae1b7c153554e26ea56930c32195636d5c2276
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1955191
    Commit-Queue: Leo Lai <cylai@google.com>
    Tested-by: Leo Lai <cylai@google.com>
    Reviewed-by: John L Chen <zuan@chromium.org>

--- attestation/common/proto_print.py
+++ attestation/common/proto_print.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#
-# Copyright (C) 2015 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 """A C++ code generator for printing protobufs which use the LITE_RUNTIME.
 
@@ -212,21 +200,9 @@ def GenerateFileHeaders(proto_name, package, imports, subdir,
           'package_with_subdir': package_with_subdir,
           'import': current_import} for current_import in imports])
   header = """\
-//
-// Copyright (C) %(year)s The Android Open Source Project
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+// Copyright %(year)s The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 // THIS CODE IS GENERATED.
 // Generated with command:
@@ -247,21 +223,9 @@ namespace %(package)s {
        'proto_include_dir': proto_include_dir,
        'cmd': ' '.join(sys.argv)}
   impl = """\
-//
-// Copyright (C) %(year)s The Android Open Source Project
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+// Copyright %(year)s The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 // THIS CODE IS GENERATED.
 // Generated with command:
