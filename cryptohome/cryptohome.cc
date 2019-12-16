9da81afd19c7a49a3179b1cde3b435d2814adda3
John L Chen <zuan@chromium.org>
2019-12-17 00:16:16 +0800

    cryptohome: Add --action=pkcs11_system_token_status to cryptohome commandline
    BUG=chromium:924230
    TEST=tast run $dut hwsec.ChapsPKCS11V5 && FEATURES=test emerge-$BOARD cryptohome
    
    Change-Id: I593ed592be69178b3dfce6263106c0498c5083e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1936591
    Tested-by: John L Chen <zuan@chromium.org>
    Reviewed-by: Leo Lai <cylai@google.com>
    Commit-Queue: John L Chen <zuan@chromium.org>

--- cryptohome/cryptohome.cc
+++ cryptohome/cryptohome.cc
@@ -118,6 +118,7 @@ namespace switches {
                                    "install_attributes_get",
                                    "install_attributes_finalize",
                                    "pkcs11_get_user_token_info",
+                                   "pkcs11_get_system_token_info",
                                    "pkcs11_is_user_token_ok",
                                    "pkcs11_terminate",
                                    "tpm_verify_attestation",
@@ -186,6 +187,7 @@ namespace switches {
     ACTION_INSTALL_ATTRIBUTES_GET,
     ACTION_INSTALL_ATTRIBUTES_FINALIZE,
     ACTION_PKCS11_GET_USER_TOKEN_INFO,
+    ACTION_PKCS11_GET_SYSTEM_TOKEN_INFO,
     ACTION_PKCS11_IS_USER_TOKEN_OK,
     ACTION_PKCS11_TERMINATE,
     ACTION_TPM_VERIFY_ATTESTATION,
@@ -1868,6 +1870,25 @@ int main(int argc, char **argv) {
     } else {
       printf("Account ID/Username not specified.\n");
       return 1;
+    }
+  } else if (!strcmp(switches::kActions
+                         [switches::ACTION_PKCS11_GET_SYSTEM_TOKEN_INFO],
+                     action.c_str())) {
+    brillo::glib::ScopedError error;
+    gchar* label = NULL;
+    gchar* pin = NULL;
+    int slot = 0;
+    if (!org_chromium_CryptohomeInterface_pkcs11_get_tpm_token_info(
+            proxy.gproxy(), &label, &pin, &slot,
+            &brillo::Resetter(&error).lvalue())) {
+      printf("PKCS #11 info call failed: %s.\n", error->message);
+    } else {
+      printf("System token properties:\n");
+      printf("Label = %s\n", label);
+      printf("Pin = %s\n", pin);
+      printf("Slot = %d\n", slot);
+      g_free(label);
+      g_free(pin);
     }
   } else if (!strcmp(
       switches::kActions[switches::ACTION_PKCS11_IS_USER_TOKEN_OK],
