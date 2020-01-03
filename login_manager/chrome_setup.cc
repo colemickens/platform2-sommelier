02911a53b11b7ded4ba569062abc28ec0864be65
Maksim Ivanov <emaxx@chromium.org>
2020-01-03 15:55:10 +0100

    login: Add signin_profile_extensions dir
    Add the creation of the /var/cache/signin_profile_extensions
    directory into the Chrome setup procedure.
    
    This path will be used by Chrome for storing the cache of the
    sign-in screen force-installed extensions.
    
    BUG=chromium:1012892
    TEST=verify that /var/cache/signin_profile_extensions appears on DUT
    
    Cq-Depend: chromium:1986805
    Change-Id: I5134c0ddd3f1cc9b12e9fde4b9a0b774f4046543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1980706
    Tested-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>

--- login_manager/chrome_setup.cc
+++ login_manager/chrome_setup.cc
@@ -294,6 +294,11 @@ void CreateDirectories(ChromiumCommandBuilder* builder) {
       base::FilePath("/var/cache/signin_profile_component_policy"), uid, gid,
       0700));
 
+  // Create the directory where extensions for the sign-in profile are cached.
+  // This data is read and written by chronos.
+  CHECK(EnsureDirectoryExists(
+      base::FilePath("/var/cache/signin_profile_extensions"), uid, gid, 0700));
+
   // Tell Chrome where to write logging messages before the user logs in.
   base::FilePath system_log_dir("/var/log/chrome");
   CHECK(EnsureDirectoryExists(system_log_dir, uid, gid, 0755));
