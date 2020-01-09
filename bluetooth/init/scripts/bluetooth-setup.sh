03e816b9113817f985edee8403f6d73f58b5328a
Sonny Sasaka <sonnysasaka@chromium.org>
2020-01-09 11:21:19 -0800

    bluetooth: Make bluetooth-setup.sh a noop
    bluetooth-setup was needed for NewBlue and is no longer needed. The
    patch makes it a no-op until it's ready to be removed when all
    references have been updated to stop depending on this script.
    
    BUG=b:145163508
    TEST=Builds
    
    Change-Id: I1bbc0056945c14fb584366a296f8b6fb817fe08e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1993544
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Tested-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>

--- bluetooth/init/scripts/bluetooth-setup.sh
+++ bluetooth/init/scripts/bluetooth-setup.sh
@@ -3,38 +3,5 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-sysfs_config_file=/sys/devices/virtual/misc/hci_le/le_splitter_enabled
-newblue_config_file=/var/lib/bluetooth/newblue
-
-# Set the group of sysfs config file to "bluetooth" to allow bluetoothd to read
-# the status of the splitter.
-chown :bluetooth "${sysfs_config_file}"
-
-# Make sure the bluetooth module is loaded, otherwise the sysfs splitter config
-# file won't exist.
-modprobe bluetooth
-
-if [ ! -e "${sysfs_config_file}" ]; then
-  # The config file doesn't exist, this means that this device doesn't support
-  # LE splitter, so just ignore and finish the task.
-  exit 0
-fi
-
-if [ -e "${newblue_config_file}" ]; then
-  cat "${newblue_config_file}" >"${sysfs_config_file}"
-else
-  # Default for LE splitter is disabled.
-  echo 0 >"${sysfs_config_file}"
-fi
-
-# Reinstall Marvell SDIO Bluetooth driver to trigger re-initialization of the
-# Bluetooth device.
-rmmod btmrvl_sdio
-modprobe btmrvl_sdio
-# Simulate that the USB Bluetooth adapter is re-plugged to trigger BlueZ to
-# re-initialize it.
-# For devices with UART Bluetooth adapter we don't need to trigger the
-# re-initialization here since we can defer the brcm_patchram_plus daemon to run
-# later after this script is done.
-rmmod btusb
-modprobe btusb
+# This script is no longer needed. As a transitional state, this file is left
+# intentionally blank until it is ready to be removed (b/145163508).
