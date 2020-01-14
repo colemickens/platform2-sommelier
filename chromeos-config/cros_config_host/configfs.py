f31feb74b4940b8638686ccd1ce0286bc61eadf3
Jack Rosenthal <jrosenth@chromium.org>
2020-01-14 14:06:36 -0700

    chromeos-config: Implement the ConfigFS squashfs generation
    This adds support for a new command line flag to cros_config_schema,
    --configfs-output=FILE. When this flag is passed, cros_config_schema
    will generate a SquashFS used by ChromeOS ConfigFS (see go/configfs)
    at the path specified.
    
    At /config/private/v1/identity.json, we place a "table of contents"
    for identity probing, which will not only make probing faster by
    needing to only open and parse one file for probing, but also will
    allow us to re-use our existing code in libcros_config (see
    cros_config_json.cc) for probing with minimal changes, as this JSON is
    a subset of /usr/share/chromeos-config/config.json.  Should we decide
    to change from JSON to a different format later, this identity
    table-of-contents can easily be changed.
    
    BUG=chromium:1041677
    TEST=provided unit tests
    
    Change-Id: Ic9779101662a6a76e04fc791cf69a7a997411fa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2001342
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Yuval Peress <peress@chromium.org>

---
+++ chromeos-config/cros_config_host/configfs.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Library for generating ChromeOS ConfigFS private data file."""
+
+from __future__ import print_function
+
+import json
+import os
+import subprocess
+import tempfile
+
+
+def WriteConfigFSFiles(config, base_path):
+  """Recursive function to write ConfigFS data out to files and directories.
+
+  Args:
+    config: The configuration item (dict, list, str, int, or bool).
+    base_path: The path to write out to.
+  """
+  if isinstance(config, dict):
+    iterator = config.items()
+  elif isinstance(config, list):
+    iterator = enumerate(config)
+  else:
+    iterator = None
+
+  if iterator is not None:
+    os.makedirs(base_path, mode=0o755)
+    for name, entry in iterator:
+      path = os.path.join(base_path, str(name))
+      WriteConfigFSFiles(entry, path)
+  else:
+    with open(os.open(base_path, os.O_CREAT | os.O_WRONLY, 0o644), 'w') as f:
+      f.write(str(config))
+
+
+def WriteIdentityInfo(config, output_file):
+  """Write out the data file needed to provide system identification.
+
+  This data file is used at runtime by libcros_config.  Currently, JSON is used
+  as the output format to remain compatible with the existing code in
+  libcros_config.  However, this function may be updated if libcros_config
+  adopts a new format, so do not rely on the output being JSON.
+
+  Args:
+    config: The configuration dictionary (containing "chromeos").
+    output_file: A file-like object to write to.
+  """
+  minified_configs = []
+  minified_config = {'chromeos': {'configs': minified_configs}}
+  for device_config in config['chromeos']['configs']:
+    minified_configs.append({'identity': device_config['identity']})
+  json.dump(minified_config, output_file)
+
+
+def GenerateConfigFSData(config, output_fs):
+  """Generate the ConfigFS private data.
+
+  Args:
+    config: The configuration dictionary.
+    output_fs: The file name to write the SquashFS image at.
+  """
+  with tempfile.TemporaryDirectory(prefix='configfs.') as configdir:
+    os.chmod(configdir, 0o755)
+    WriteConfigFSFiles(config, os.path.join(configdir, 'v1'))
+    with open(os.path.join(configdir, 'v1/identity.json'), 'w') as f:
+      WriteIdentityInfo(config, f)
+    subprocess.run(['mksquashfs', configdir, output_fs, '-no-xattrs',
+                    '-noappend', '-all-root'], check=True,
+                   stdout=subprocess.PIPE)
---
+++ chromeos-config/cros_config_host/configfs_unittest.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Unit tests for ConfigFS data file generator."""
+
+from __future__ import print_function
+
+import functools
+import json
+import os
+import subprocess
+import sys
+import tempfile
+
+# pylint: disable=wrong-import-position
+this_dir = os.path.dirname(__file__)
+sys.path.insert(0, this_dir)
+import configfs
+sys.path.pop(0)
+# pylint: enable=wrong-import-position
+
+from chromite.lib import cros_test_lib
+from chromite.lib import osutils
+
+
+def TestConfigs(*args):
+  """Wrapper function for tests which use configs from libcros_config/
+
+  Use like so:
+  @TestConfigs('test.json', [any other files you want...])
+  def testFoo(self, config, output_dir):
+    # do something!
+    pass
+  """
+  def _Decorator(method):
+    @functools.wraps(method)
+    def _Wrapper(self):
+      for fn in args:
+        with open(os.path.join(this_dir, '../libcros_config', fn)) as f:
+          config = json.load(f)
+
+        with tempfile.TemporaryDirectory(prefix='test.') as output_dir:
+          squashfs_img = os.path.join(output_dir, 'configfs.img')
+          configfs.GenerateConfigFSData(config, squashfs_img)
+          subprocess.run(['unsquashfs', squashfs_img], check=True,
+                         cwd=output_dir, stdout=subprocess.PIPE)
+          method(self, config, output_dir)
+    return _Wrapper
+  return _Decorator
+
+
+class ConfigFSTests(cros_test_lib.TestCase):
+  """Tests for ConfigFS."""
+
+  @TestConfigs('test.json', 'test_arm.json')
+  def testConfigV1FileStructure(self, config, output_dir):
+    def _CheckConfigRec(config, path):
+      if isinstance(config, dict):
+        iterator = config.items()
+      elif isinstance(config, list):
+        iterator = enumerate(config)
+      else:
+        self.assertTrue(os.path.isfile(path))
+        self.assertEqual(osutils.ReadFile(path), str(config))
+        return
+      self.assertTrue(os.path.isdir(path))
+      for name, entry in iterator:
+        childpath = os.path.join(path, str(name))
+        _CheckConfigRec(entry, childpath)
+    _CheckConfigRec(config, os.path.join(output_dir, 'squashfs-root/v1'))
+
+  @TestConfigs('test.json', 'test_arm.json')
+  def testConfigV1Identity(self, config, output_dir):
+    identity_path = os.path.join(output_dir, 'squashfs-root/v1/identity.json')
+    self.assertTrue(os.path.isfile(identity_path))
+    with open(identity_path) as f:
+      identity_data = json.load(f)
+    for device_config, identity_config in zip(
+        config['chromeos']['configs'], identity_data['chromeos']['configs']):
+      self.assertEqual(set(identity_config.keys()), {'identity'})
+      self.assertEqual(device_config['identity'], identity_config['identity'])
+
+
+if __name__ == '__main__':
+  cros_test_lib.main(module=__name__)
